4c580d13ff968c0a07684a687f8b6ca46fbbf4b7
pappastech <pappastech@c3eb156b-1dc0-44e1-88ae-e38439141b53>
2012-08-07 23:09:09 +0000

    Incorporated changes proposed to fix '6 items in cart limit'. This limit was due to the 4kB limit imposed on cookies. The change pushes cart item storage to the database instead of in cookies. A database change was also made in this version to work with the code change.
    Version updated to 2.0_RC7.
    
    Tom
    
    git-svn-id: svn+ssh://jekkos@svn.code.sf.net/p/opensourcepos/code/@52 c3eb156b-1dc0-44e1-88ae-e38439141b53

--- database/database.sql
+++ database/database.sql
@@ -530,7 +530,7 @@ CREATE TABLE `ospos_sales_suspended_payments` (
 CREATE TABLE `ospos_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
-  `user_agent` varchar(50) NOT NULL,
+  `user_agent` varchar(120) NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
   `user_data` text,
   PRIMARY KEY (`session_id`)
