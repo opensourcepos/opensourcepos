a61e9ab274441adf5801dccade0176646de1c522
pappastech <pappastech@c3eb156b-1dc0-44e1-88ae-e38439141b53>
2012-09-06 19:48:40 +0000

    Fixed last item on the list which was to get the language selection working from the config area of the site versus having to change the language in the application/config/config.php file.
    We're now at version 2.0!
    
    Tom
    
    git-svn-id: svn+ssh://jekkos@svn.code.sf.net/p/opensourcepos/code/@55 c3eb156b-1dc0-44e1-88ae-e38439141b53

--- application/hooks/load_config.php
+++ application/hooks/load_config.php
@@ -10,7 +10,14 @@ function load_config()
 	
 	if ($CI->config->item('language'))
 	{
-		//$CI->lang->switch_to($CI->config->item('language'));
+		$CI->config->set_item( 'language', $CI->config->item( 'language' ) );
+    $loaded = $CI->lang->is_loaded;
+    $CI->lang->is_loaded = array();
+
+    foreach($loaded as $file)
+    {
+        $CI->lang->load( str_replace( '_lang.php', '', $file ) );    
+    }
 	}
 	
 	if ($CI->config->item('timezone'))
