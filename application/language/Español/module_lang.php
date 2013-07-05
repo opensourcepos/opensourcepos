d3686113f5d4bcf6d5ce20b3488cf49497720887
objecttothis <objecttothis@c3eb156b-1dc0-44e1-88ae-e38439141b53>
2013-07-05 20:11:01 +0000

    Changing the folder to the local language
    git-svn-id: svn+ssh://jekkos@svn.code.sf.net/p/opensourcepos/code/@65 c3eb156b-1dc0-44e1-88ae-e38439141b53

---
+++ application/language/Español/module_lang.php
@@ -0,0 +1,33 @@
+<?php
+$lang['module_home']='Inicio';
+
+$lang['module_customers']='Clientes';
+$lang['module_customers_desc']='Agregar, Actualizar, Borrar y Buscar clientes';
+
+$lang['module_suppliers']='Proveedores';
+$lang['module_suppliers_desc']='Agregar, Actualizar, Borrar y Buscar proveedores';
+
+$lang['module_employees']='Empleados';
+$lang['module_employees_desc']='Agregar, Actualizar, Borrar y Buscar empleados';
+
+$lang['module_sales']='Ventas';
+$lang['module_sales_desc']='Procesar ventas y reintegros';
+
+$lang['module_reports']='Reportes';
+$lang['module_reports_desc']='Ver y generar reportes';
+
+$lang['module_items']='Artículos';
+$lang['module_items_desc']='Agregar, Actualizar, Borrar y Buscar artículos';
+
+$lang['module_config']='Configuración de la Tienda';
+$lang['module_config_desc']='Cambiar la configuración de la tienda';
+
+$lang['module_receivings']='Receivings';
+$lang['module_receivings_desc']='Process Purchase orders';
+
+$lang['module_giftcards']='Tarjetas de Regalo';
+$lang['module_giftcards_desc']='Agregar, Actualizar, Borrar y Buscar Tarjetas de Regalo';
+
+$lang['module_item_kits']='Kits de Artículos';
+$lang['module_item_kits_desc']='Agregar, Actualizar, Borrar y Buscar Kits de Artículos';
+?>
