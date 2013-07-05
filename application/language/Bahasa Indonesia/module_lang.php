5506b10aa56b0cc473b5c46fe102533993ae5ee2
objecttothis <objecttothis@c3eb156b-1dc0-44e1-88ae-e38439141b53>
2013-07-05 20:09:58 +0000

    This commit is changing the folder name to the local language
    git-svn-id: svn+ssh://jekkos@svn.code.sf.net/p/opensourcepos/code/@63 c3eb156b-1dc0-44e1-88ae-e38439141b53

---
+++ application/language/Bahasa Indonesia/module_lang.php
@@ -0,0 +1,33 @@
+<?php
+$lang['module_home']='Beranda';
+
+$lang['module_customers']='Pelanggan';
+$lang['module_customers_desc']='Tambah, Update, Hapus, dan Cari pelanggan ';
+
+$lang['module_suppliers']='Pemasok';
+$lang['module_suppliers_desc']='Tambah, Update, Hapus, dan Cari pemasok';
+
+$lang['module_employees']='Karyawan';
+$lang['module_employees_desc']='Tambah, Update, Hapus, dan Cari karyawan';
+
+$lang['module_sales']='Penjualan';
+$lang['module_sales_desc']='Proses penjualan dan retur';
+
+$lang['module_reports']='Laporan';
+$lang['module_reports_desc']='Lihat dan Hasilkan laporan';
+
+$lang['module_items']='Produk/Item';
+$lang['module_items_desc']='Tambah, Update, Hapus, dan Cari produk/items';
+
+$lang['module_config']='Konfigurasi Toko';
+$lang['module_config_desc']='Ubah konfigurasi toko';
+
+$lang['module_receivings']='Receivings';
+$lang['module_receivings_desc']='Process Purchase orders';
+
+$lang['module_giftcards']='Gift Cards';
+$lang['module_giftcards_desc']='Add, Update, Delete and Search gift cards';
+
+$lang['module_item_kits']='Item Kits';
+$lang['module_item_kits_desc']='Add, Update, Delete and Search Item Kits';
+?>
