abca41d2b022bb67c5462e9b6886c6de101b8cef
objecttothis <objecttothis@c3eb156b-1dc0-44e1-88ae-e38439141b53>
2013-07-05 20:10:41 +0000

    Adding the Azerbaijani language
    git-svn-id: svn+ssh://jekkos@svn.code.sf.net/p/opensourcepos/code/@64 c3eb156b-1dc0-44e1-88ae-e38439141b53

---
+++ application/language/Azərbaycanca/login_lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['login_login']='Login';
+$lang['login_username']='Username';
+$lang['login_password']='Password';
+$lang['login_go']='Go';
+$lang['login_invalid_username_and_password']='Invalid username/password';
+$lang['login_welcome_message']='Welcome to the Open Source Point of Sale System. To continue, please login using your username and password below.';
+?>
