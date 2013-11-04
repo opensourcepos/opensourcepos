59bafd9885834fa4a669c5f536689cdb1d82e2ca
jekkos <jekkos@lenovo-t520.home>
2013-11-04 23:48:27 +0100

    Config file set correctly now

--- application/config/config.php
+++ application/config/config.php
@@ -262,9 +262,9 @@ $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
-$config['sess_table_name']		= 'ospos_sessions';
+$config['sess_table_name']		= 'sessions';
 $config['sess_match_ip']		= FALSE;
-$config['sess_match_useragent']	= TRUE;
+$config['sess_match_useragent']	= FALSE;
 $config['sess_time_to_update']	= 300;
 
 /*
