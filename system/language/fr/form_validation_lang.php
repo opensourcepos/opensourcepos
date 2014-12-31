dbec337742f7ccd68a97e7b66d6b345b93f3e0e7
Florian <flodef@free.fr>
2014-12-31 14:55:24 +0100

    Create form_validation_lang.php
    Translate form_validation_lang.php to French

---
+++ system/language/fr/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "Le champ %s est requis.";
+$lang['isset']				= "Le champ %s doit contenir une valeur.";
+$lang['valid_email']		= "Le champ %s doit contenir une adresse email valide.";
+$lang['valid_emails']		= "Les champs %s doivent tous contenir une adresse email valide.";
+$lang['valid_url']			= "Le champ %s doit contenir une URL valide.";
+$lang['valid_ip']			= "Le champ %s doit contenir une adresse IP valide.";
+$lang['min_length']			= "Le champ %s doit avoir au moins %s caractères.";
+$lang['max_length']			= "Le champ %s ne doit pas dépasser %s caractères.";
+$lang['exact_length']		= "Le champ %s doit être exactement de %s caractères.";
+$lang['alpha']				= "Le champ %s doit contenir seulement des caractères alphabétiques.";
+$lang['alpha_numeric']		= "Le champ %s doit contenir seulement des caractères alpha-numériques.";
+$lang['alpha_dash']			= "Le champ %s doit contenir seulement des caractères alpha-numériques, underscores, et slashes.";
+$lang['numeric']			= "Le champ %s doit contenir seulement des nombres.";
+$lang['is_numeric']			= "Le champ %s doit contenir seulement des caractères numériques.";
+$lang['integer']			= "Le champ %s doit contenir un entier.";
+$lang['regex_match']		= "Le champ %s n'est pas dans un format correct.";
+$lang['matches']			= "Le champ %s ne corresponds pas à %s.";
+$lang['is_unique'] 			= "Le champ %s doit contenir une valeur unique.";
+$lang['is_natural']			= "Le champ %s doit contenir seulement des nombres positifs.";
+$lang['is_natural_no_zero']	= "Le champ %s doit contenir un nombre supérieur à zéro.";
+$lang['decimal']			= "Le champ %s doit contenir un nombre décimal.";
+$lang['less_than']			= "Le champ %s doit contenir un nombre plus petit que %s.";
+$lang['greater_than']		= "Le champ %s doit contenir un nombre plus grand que %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
