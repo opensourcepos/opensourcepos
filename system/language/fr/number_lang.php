326b074a229dcf14c6aca92e0026c05547170681
Florian <flodef@free.fr>
2014-12-31 14:59:46 +0100

    Create number_lang.php
    Translate number_lang.php to French

---
+++ system/language/fr/number_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['terabyte_abbr'] = "TO";
+$lang['gigabyte_abbr'] = "GO";
+$lang['megabyte_abbr'] = "MO";
+$lang['kilobyte_abbr'] = "KO";
+$lang['bytes'] = "Octets";
+
+/* End of file number_lang.php */
+/* Location: ./system/language/fr/number_lang.php */
