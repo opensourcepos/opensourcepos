4d6a5189c1912047692c73134dd99a9a32519fc4
Florian <flodef@free.fr>
2014-12-31 15:05:43 +0100

    Create profiler_lang.php
    Translate profiler_lang.php to French.

---
+++ system/language/fr/profiler_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['profiler_database']		= 'BASE DE DONNEES';
+$lang['profiler_controller_info'] = 'CLASSE/METHODE';
+$lang['profiler_benchmarks']	= 'BANC D\'ESSAI';
+$lang['profiler_queries']		= 'REQUETES';
+$lang['profiler_get_data']		= 'RECUPERER DONNEES';
+$lang['profiler_post_data']		= 'ENVOYER DONNEES';
+$lang['profiler_uri_string']	= 'CHAINE URI';
+$lang['profiler_memory_usage']	= 'UTILISATION MEMOIRE';
+$lang['profiler_config']		= 'VARIABLES DE CONFIG';
+$lang['profiler_session_data']	= 'DONNEES DE SESSION';
+$lang['profiler_headers']		= 'ENTETES HTTP';
+$lang['profiler_no_db']			= 'Le pilote de basse de données n\'est pas chargé';
+$lang['profiler_no_queries']	= 'Aucune requête n\'a été exécutée';
+$lang['profiler_no_post']		= 'Aucune donnée POST n\'existe';
+$lang['profiler_no_get']		= 'Aucune donnée GET n\'existe';
+$lang['profiler_no_uri']		= 'Aucune donnée URI n\'existe';
+$lang['profiler_no_memory']		= 'Utilisation mémoire non disponible';
+$lang['profiler_no_profiles']	= 'Aucune donnée de profile - toutes les sections Profiler ont été désactivées.';
+$lang['profiler_section_hide']	= 'Cacher';
+$lang['profiler_section_show']	= 'Afficher';
+
+/* End of file profiler_lang.php */
+/* Location: ./system/language/fr/profiler_lang.php */
