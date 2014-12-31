1b8d40d7ed9d5783a81452cbfcfc5278decc9cbc
Florian <flodef@free.fr>
2014-12-31 15:08:33 +0100

    Create unit_test_lang.php
    Translate unit_test_lang.php to French.

---
+++ system/language/fr/unit_test_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['ut_test_name']		= 'Nom du Test';
+$lang['ut_test_datatype']	= 'Type de données du Test';
+$lang['ut_res_datatype']	= 'Type de données attendu';
+$lang['ut_result']			= 'Résultat';
+$lang['ut_undefined']		= 'Nom de Test non spécifié';
+$lang['ut_file']			= 'Nom de Fichier';
+$lang['ut_line']			= 'Numéro de Ligne';
+$lang['ut_passed']			= 'Réussi';
+$lang['ut_failed']			= 'Echoué';
+$lang['ut_boolean']			= 'Booléen';
+$lang['ut_integer']			= 'Entier';
+$lang['ut_float']			= 'Flottant';
+$lang['ut_double']			= 'Flottant'; // can be the same as float
+$lang['ut_string']			= 'Chaîne';
+$lang['ut_array']			= 'Tableau';
+$lang['ut_object']			= 'Objet';
+$lang['ut_resource']		= 'Ressource';
+$lang['ut_null']			= 'Null';
+$lang['ut_notes']			= 'Notes';
+
+
+/* End of file unit_test_lang.php */
+/* Location: ./system/language/fr/unit_test_lang.php */
