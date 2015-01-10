d7ed07f4f4ac459e930e7d59b44822f8a590f5dc
Florian <flodef@free.fr>
2015-01-10 18:39:51 +0100

    Update profiler_lang.php

--- system/language/fr/profiler_lang.php
+++ system/language/fr/profiler_lang.php
@@ -1,25 +1,26 @@
-<?php
+<?php 
 
-$lang['profiler_database']		= 'BASE DE DONNEES';
-$lang['profiler_controller_info'] = 'CLASSE/METHODE';
-$lang['profiler_benchmarks']	= 'BANC D\'ESSAI';
+$lang['profiler_database']		  = 'BASE DE DONNEES';
+$lang['profiler_controller_info'] = 'CLASS/METHODE';
+$lang['profiler_benchmarks']	= 'BENCHMARKS';
 $lang['profiler_queries']		= 'REQUETES';
-$lang['profiler_get_data']		= 'RECUPERER DONNEES';
-$lang['profiler_post_data']		= 'ENVOYER DONNEES';
-$lang['profiler_uri_string']	= 'CHAINE URI';
+$lang['profiler_get_data']		= 'DONNEES GET';
+$lang['profiler_post_data']		= 'DONNEES POST';
+$lang['profiler_uri_string']	= 'URI STRING';
 $lang['profiler_memory_usage']	= 'UTILISATION MEMOIRE';
-$lang['profiler_config']		= 'VARIABLES DE CONFIG';
-$lang['profiler_session_data']	= 'DONNEES DE SESSION';
-$lang['profiler_headers']		= 'ENTETES HTTP';
-$lang['profiler_no_db']			= 'Le pilote de basse de données n\'est pas chargé';
-$lang['profiler_no_queries']	= 'Aucune requête n\'a été exécutée';
-$lang['profiler_no_post']		= 'Aucune donnée POST n\'existe';
-$lang['profiler_no_get']		= 'Aucune donnée GET n\'existe';
-$lang['profiler_no_uri']		= 'Aucune donnée URI n\'existe';
-$lang['profiler_no_memory']		= 'Utilisation mémoire non disponible';
-$lang['profiler_no_profiles']	= 'Aucune donnée de profile - toutes les sections Profiler ont été désactivées.';
+$lang['profiler_config']		= 'VARIABLES CONFIG';
+$lang['profiler_session_data']	= 'DONNEES SESSION';
+$lang['profiler_headers']		= 'HTTP HEADERS';
+$lang['profiler_no_db']			= "Le driver de la base de donn&eacute;es n'est actuellement pas charg&eacute;";
+$lang['profiler_no_queries']	= "Aucune requ&ecirc;te n'a &eacute;t&eacute; ex&eacute;cut&eacute;e";
+$lang['profiler_no_post']		= "Aucune donn&eacute;e POST n'existe";
+$lang['profiler_no_get']		= "Aucune donn&eacute;e GET n'existe";
+$lang['profiler_no_uri']		= "Aucune donn&eacute;e URI n'existe";
+$lang['profiler_no_memory']		= 'Utilisation de la m&eacute;moire indisponible';
+$lang['profiler_no_profiles']	= 'Pas de donn&eacute;es du profil - toute section est d&eacute;sactiv&eacute;e.';
 $lang['profiler_section_hide']	= 'Cacher';
-$lang['profiler_section_show']	= 'Afficher';
+$lang['profiler_section_show']	= 'Montrer';
+
 
 /* End of file profiler_lang.php */
-/* Location: ./system/language/fr/profiler_lang.php */
+/* Location: ./system/language/french/profiler_lang.php */
