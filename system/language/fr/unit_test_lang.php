e9e6290151d875d853778770b4756cb597afe061
Florian <flodef@free.fr>
2015-01-10 18:42:41 +0100

    Update unit_test_lang.php

--- system/language/fr/unit_test_lang.php
+++ system/language/fr/unit_test_lang.php
@@ -1,19 +1,19 @@
 <?php
 
 $lang['ut_test_name']		= 'Nom du Test';
-$lang['ut_test_datatype']	= 'Type de données du Test';
-$lang['ut_res_datatype']	= 'Type de données attendu';
-$lang['ut_result']			= 'Résultat';
-$lang['ut_undefined']		= 'Nom de Test non spécifié';
+$lang['ut_test_datatype']	= 'Type de donn&eacute;es du Test';
+$lang['ut_res_datatype']	= 'Type de donn&eacute;es attendu';
+$lang['ut_result']			= 'R&eacute;sultat';
+$lang['ut_undefined']		= 'Nom de Test non sp&eacute;cifi&eacute;';
 $lang['ut_file']			= 'Nom de Fichier';
-$lang['ut_line']			= 'Numéro de Ligne';
-$lang['ut_passed']			= 'Réussi';
-$lang['ut_failed']			= 'Echoué';
-$lang['ut_boolean']			= 'Booléen';
+$lang['ut_line']			= 'Num&eacute;ro de Ligne';
+$lang['ut_passed']			= 'R&eacute;ussi';
+$lang['ut_failed']			= 'Echou&eacute;';
+$lang['ut_boolean']			= 'Bool&eacute;en';
 $lang['ut_integer']			= 'Entier';
 $lang['ut_float']			= 'Flottant';
 $lang['ut_double']			= 'Flottant'; // can be the same as float
-$lang['ut_string']			= 'Chaîne';
+$lang['ut_string']			= 'Cha&icirc;ne';
 $lang['ut_array']			= 'Tableau';
 $lang['ut_object']			= 'Objet';
 $lang['ut_resource']		= 'Ressource';
