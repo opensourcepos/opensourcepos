84a141d9c51e2e96b7e65f8e82e8c12850cb923a
jekkos-t520 <jeroen.peelaerts@gmail.com>
2015-01-16 08:31:15 +0100

    Move translations into CSV files Add generate_languages.php script (run using `php generate_languages.php`) Add generated language files

--- application/language/id/error_lang.php
+++ application/language/id/error_lang.php
@@ -1,4 +1,4 @@
-<?php
-$lang['error_no_permission_module'] = 'Anda tidak memiliki izin untuk mengakses modul ini';
-$lang['error_unknown'] = 'tidak dikenal';
-?>
+<?php 
+
+$lang["error_no_permission_module"] = "Anda tidak memiliki izin untuk mengakses modul ini";
+$lang["error_unknown"] = "tidak dikenal";
