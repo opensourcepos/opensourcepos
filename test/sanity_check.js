6a2a5278b84b765b40a6b0024b0da8359ec8a8bc
jekkos <jeroen.peelaerts@gmail.com>
2015-05-15 00:47:25 +0200

    Fix general test setup (finally) Add basic selenium test for application login Adapt default database template file Add correct db settings for travis-ci

--- test/sanity_check.js
+++ test/sanity_check.js
@@ -1,11 +1,11 @@
 var assert = require("assert"); // node.js core module
-var wd = require('wd')
+var wd = require('wd');
 
 describe('A Mocha test run by grunt-mocha-webdriver', function () {
 	it('has a browser injected into it', function () {
 		assert.ok(this.browser);
 	});
 	it('has wd injected into it for customizing', function () {
-		assert.notEqual(wd, undefined);
-	});
+        assert.notEqual(this.wd, undefined);
+    });
 });
