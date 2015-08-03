7f31e10dd780449119ed4da68de14d414f620032
FrancescoUK <francesco_lodolo@yahoo.com>
2015-08-03 22:48:32 +0100

    Update to Suppliers feature
    - added Agency name to identify agent or distributor behind a brand
    - updated view to show supplier ID useful for item imports via excel
    sheet
    - update database table
    - updated language although missing translations
    - various bit an pieces missing due to failed rebase

--- database/database.sql
+++ database/database.sql
@@ -702,6 +702,7 @@ INSERT INTO `ospos_stock_locations` ( `deleted`, `location_name` ) VALUES ('0', 
 CREATE TABLE `ospos_suppliers` (
   `person_id` int(10) NOT NULL,
   `company_name` varchar(255) NOT NULL,
+  `agency_name` varchar(255) NOT NULL,
   `account_number` varchar(255) DEFAULT NULL,
   `deleted` int(1) NOT NULL DEFAULT '0',
   UNIQUE KEY `account_number` (`account_number`),
--- test/giftcard_numbering.js
+++ test/giftcard_numbering.js
@@ -29,9 +29,13 @@ describe("giftcard numbering test", function () {
     it("issue #65: giftcard numbering should add properly", function() {
         return this.browser.get(url("/index.php/giftcards")).waitForElementByCss(".big_button").click()
             .waitForElementByName("value", 2000).type("100").elementById('giftcard_number').clear().type("10")
+<<<<<<< HEAD
+            .elementById("submit").click().waitForElementByXPath("//table/tbody/tr[td/text()='10']/td[4]", 2000).text().then(function (value) {
+=======
             .elementById("submit").click().waitForElementByXPath("//table/tbody/tr[td/text()='10']/td[4]", 1000).text().then(function (value) {
+>>>>>>> 24c5a00... Sync with latest from jekkos/master
                 assert.ok(value, "giftcard failed to be added properly!");
-            }).elementByCss(".big_button").click().waitForElementByName("value", 1000).type("100").elementById("submit").click()
+            }).elementByCss(".big_button").click().waitForElementByName("value", 2000).type("100").elementById("submit").click()
             .waitForElementByXPath("//table/tbody/tr[td/text()='11']/td[4]").text().then(function (value) {
                 assert.equal(value, "11", "giftcard number not incrementing properly!!");
             });
