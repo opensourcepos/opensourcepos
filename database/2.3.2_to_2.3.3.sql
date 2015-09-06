80f7966d1c6b28bcb8704768cfada78b7700964a
jekkos <jeroen.peelaerts@gmail.com>
2015-09-06 15:53:11 +0200

    Add default dateformat and timeformat in data scripts

--- database/2.3.2_to_2.3.3.sql
+++ database/2.3.2_to_2.3.3.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `ospos_suppliers`
    ADD COLUMN `agency_name` VARCHAR(255) NOT NULL;
+
+INSERT INTO `ospos_app_config` (`key`, `value`) VALUES
+   ('dateformat', 'm-d-Y'),
+   ('timeformat', 'H:i:s');
