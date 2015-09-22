2bc7b688ff87e14aaaaecda3107fea4fcb14dcc0
Martes Erede <martes@posteo.net>
2015-09-22 07:17:57 +0200

    Fix dateformat value in SQL-Query app_config

--- database/2.3.2_to_2.3.3.sql
+++ database/2.3.2_to_2.3.3.sql
@@ -2,5 +2,5 @@ ALTER TABLE `ospos_suppliers`
    ADD COLUMN `agency_name` VARCHAR(255) NOT NULL;
 
 INSERT INTO `ospos_app_config` (`key`, `value`) VALUES
-   ('dateformat', 'm-d-Y'),
+   ('dateformat', 'm/d/Y'),
    ('timeformat', 'H:i:s');
