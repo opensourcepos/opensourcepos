47c8d6c0bd496cb5d1dfc3d6b687208f3c7804f9
souparno <souparno.majumder@gmail.com>
2015-09-23 21:29:17 +0530

    reset database syntax contains check, so as it ccan be used to create the database for the first time

--- database/resetdatabase.sh
+++ database/resetdatabase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -s -p "Enter Password: " mypassword
-mysql -u root -p"$mypassword" -e "use ospos; drop database ospos; create database ospos;"
+mysql -u root -p"$mypassword" -e "DROP DATABASE IF EXISTS ospos; CREATE DATABASE ospos;"
 [ ! -z $1 ] && script=migrate_phppos || script=database
-mysql -u root -p"$mypassword" -e "use ospos; source $script.sql;"
+mysql -u root -p"$mypassword" -e "USE ospos; source $script.sql;"
