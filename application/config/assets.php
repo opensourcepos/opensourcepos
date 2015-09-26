f5b8567192a354aa9ad45385b55e86df8945e063
souparno <souparno.majumder@gmail.com>
2015-09-26 22:24:43 +0530

    mentioning the assets url path and including a function to load the assets from that path

---
+++ application/config/assets.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['assets_url'] = '/';
+
+/* End of file assets.php */
+/* Location: ./application/config/assets.php */
