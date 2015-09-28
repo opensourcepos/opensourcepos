e49dd4e0b90e085e8bb8f9e7ec991d68ed866c39
FrancescoUK <francesco.lodolo.uk@gmail.com>
2015-09-28 13:12:22 +0100

    #179 Make Items Kit icon visible on home page
    Remove the if statement that was in the end preventing any module having a _ to be displayed in the home page.
    This solution is consistent with navigation bar one.
    
    Also done some indentation in header and footer to improve readibility.

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -55,42 +55,41 @@ html {
 }
 </style>
 
-
 </head>
+
 <body>
-<div id="menubar">
-	<div id="menubar_container">
-		<div id="menubar_company_info">
-		<span id="company_title"><?php echo $this->config->item('company'); ?></span><br />
-		<span style='font-size:8pt;'><?php echo $this->lang->line('common_powered_by').' Open Source Point Of Sale'; ?></span>
-	</div>
-
-		<div id="menubar_navigation">
-			<?php
-			foreach($allowed_modules->result() as $module)
-			{
-			?>
-			<div class="menu_item">
-				<a href="<?php echo site_url("$module->module_id");?>">
-				<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" border="0" alt="Menubar Image"></a><br>
-				<a href="<?php echo site_url("$module->module_id");?>"><?php echo $this->lang->line("module_".$module->module_id) ?></a>
+	<div id="menubar">
+		<div id="menubar_container">
+			<div id="menubar_company_info">
+				<span id="company_title"><?php echo $this->config->item('company'); ?></span><br />
+				<span style='font-size:8pt;'><?php echo $this->lang->line('common_powered_by').' Open Source Point Of Sale'; ?></span>
 			</div>
-			<?php
-			}
-			?>
-		</div>
-		
-		<div id="menubar_footer">
-		    <?php echo $this->lang->line('common_welcome')." $user_info->first_name $user_info->last_name! | "; ?>
-		    <a href="javascript:logout(true);"><?php echo $this->lang->line("common_logout"); ?></a> 
-		</div>
-		
-		<div id="menubar_date">
-		    <?php echo date($this->config->item('dateformat').' '.$this->config->item('timeformat')) ?>
-		</div>
 
+			<div id="menubar_navigation">
+				<?php
+				foreach($allowed_modules->result() as $module)
+				{
+				?>
+					<div class="menu_item">
+						<a href="<?php echo site_url("$module->module_id");?>">
+						<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" border="0" alt="Menubar Image"></a><br>
+						<a href="<?php echo site_url("$module->module_id");?>"><?php echo $this->lang->line("module_".$module->module_id) ?></a>
+					</div>
+				<?php
+				}
+				?>
+			</div>
+			
+			<div id="menubar_footer">
+				<?php echo $this->lang->line('common_welcome')." $user_info->first_name $user_info->last_name! | "; ?>
+				<a href="javascript:logout(true);"><?php echo $this->lang->line("common_logout"); ?></a> 
+			</div>
+			
+			<div id="menubar_date">
+				<?php echo date($this->config->item('dateformat').' '.$this->config->item('timeformat')) ?>
+			</div>
+		</div>
 	</div>
-</div>
-<div id="content_area_wrapper">
-<div id="content_area">
+	<div id="content_area_wrapper">
+	<div id="content_area">
  
