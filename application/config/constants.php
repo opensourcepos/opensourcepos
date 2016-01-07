007598df6a5b66ca32b121a8fd53137b2f3ec149
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-01-07 20:53:40 +0000

    Fix merge miss in constants.php (#120)

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,8 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+ | Precision for calculations performed on decimals 
+ */
+define("PRECISION", 5);
