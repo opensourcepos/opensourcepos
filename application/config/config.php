830f714a449eb6cf3b2e2941ff8bbbb1d3155aef
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-01-18 23:01:42 +0000

    Change sessions to be stored in database and add SQL upgrade script 2.3.4 to 2.4 (#120)

--- application/config/config.php
+++ application/config/config.php
@@ -380,12 +380,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-/*$config['sess_driver'] = 'database';*/
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-/*$config['sess_save_path'] = 'sessions';*/
-$config['sess_save_path'] = APPPATH.'sessions/';
+$config['sess_save_path'] = 'sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
