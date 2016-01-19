4d9dc0111e8f80be9fe0abc75aab2efd406ba878
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-01-19 08:24:58 +0000

    Make the data blob NOT NULL in the SQL upgrade script (#120)

--- database/2.3.4_to_2.4.sql
+++ database/2.3.4_to_2.4.sql
@@ -4,7 +4,7 @@ DROP TABLE `ospos_sessions`;
 CREATE TABLE `ospos_sessions` (
   `id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(45) NOT NULL DEFAULT '0',
-  `data` blob,
+  `data` blob NOT NULL,
   `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
