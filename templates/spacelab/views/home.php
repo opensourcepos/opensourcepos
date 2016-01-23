125ffe9f738426e684dd3936c642a1c325e11b7f
cpl2015 <computerplaza.labpur@gmail.com>
2016-01-24 02:33:51 +0530

    Create home.php

---
+++ templates/spacelab/views/home.php
@@ -0,0 +1,18 @@
+<?php $this->load->view("partial/header"); ?>
+
+<h3 class="text-center"><?php echo $this->lang->line('common_welcome_message'); ?></h3>
+
+<div id="home_module_list">
+	<?php
+	foreach($allowed_modules->result() as $module)
+	{
+	?>
+		<div class="module_item" title="<?php echo $this->lang->line('module_'.$module->module_id.'_desc');?>">
+			<a href="<?php echo site_url("$module->module_id");?>"><img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" border="0" alt="Menubar Image" /></a>
+			<a href="<?php echo site_url("$module->module_id");?>"><?php echo $this->lang->line("module_".$module->module_id) ?></a>
+		</div>
+	<?php
+	}
+	?>
+</div>
+<?php $this->load->view("partial/footer"); ?>
