847c4a67c9212e5951c6e4560a556919978d971d
jekkos <jeroen.peelaerts@gmail.com>
2016-01-26 20:27:07 +0100

    Add integration test for making sale (#305)

--- test/giftcard_numbering.js
+++ test/giftcard_numbering.js
@@ -2,14 +2,13 @@ var assert = require("assert"); // node.js core module
 var ospos = require("./ospos");
 
 describe("giftcard numbering test", function () {
+    this.timeout(25000);
 
     it("should be able to login",  function (done) {
         return ospos.login(this.browser, done);
     });
 
     it("issue #65: giftcard numbering should add properly", function(done) {
-        this.timeout(25000);
-
         return this.browser.get(ospos.url("/index.php/giftcards")).waitForElementByCss(".big_button").click()
             .waitForElementByName("value", 10000).type("100").elementById('giftcard_number').clear().type("10")
             .elementById("submit").click().waitForElementByXPath("//table/tbody/tr[td/text()='10']/td[4]", 2000).text().then(function (value) {
--- test/make_sale.js
+++ test/make_sale.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
 var ospos = require('./ospos');
 
-describe("create item", function () {
+describe("create item and make sale", function () {
+    this.timeout(25000);
 
-    it("should be able to add new item", function (done) {
+    it("should be able to add item", function (done) {
         return this.browser.get(ospos.url("/index.php/items")).elementByCssSelector("a[title='New Item']", 5000).click()
             .waitForElementByName("name", 10000).type("anItem").elementById("category").type("aCategory")
             .elementById('cost_price', 2000).clear().type("10").elementById("unit_price", 2000).type("20")
@@ -11,6 +12,14 @@ describe("create item", function () {
             .elementById("1_quantity", 2000).type("1").elementById("reorder_level", 2000).type("0").elementById("submit", 2000).click()
             .waitForElementByXPath("//table/tbody/tr[td/text()='anItem']/td[3]").text().then(function (value) {
                 assert.equal(value, "anItem", "item could not be created!!");
+            }).then(done, done);
+    });
+
+    it("should be able to make sale", function(done) {
+        return this.browser.get(ospos.url("/index.php/sales")).elementById("item", 3000).type("1\r\n")
+            .waitForElementByName("quantity", 5000).clear().type("2").elementByName("discount", 1000).type("10").elementByName("edit_item").click()
+            .elementById("add_payment_button", 2000).click().elementByCssSelector("tbody#payment_contents tr td:nth-child(3)", 5000).text().then(function(value) {
+                assert.equal(value, "$43.56", "discounted price " + value + " is not correct!!");
             }).then(done, done);
     });
 
