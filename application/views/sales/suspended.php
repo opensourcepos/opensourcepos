fc74708bf5b45c1a769f24152987dfaeabd75dc1
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-02-27 22:21:04 +0000

    fix a mistake with the table entry in sales/suspended list (#116)

--- application/views/sales/suspended.php
+++ application/views/sales/suspended.php
@@ -31,18 +31,14 @@
 			</td>
 			<td><?php echo $suspended_sale['comment'];?></td>
 			<td>
-				<?php 
-				echo form_open('sales/unsuspend');
+				<?php echo form_open('sales/unsuspend');
 				echo form_hidden('suspended_sale_id', $suspended_sale['sale_id']);
 				?>
-					<input type="submit" name="submit" value="<?php echo $this->lang->line('sales_unsuspend'); ?>" id="submit" class="btn btn-primary btn-xs pull-right"></td>
-				<?php
-				echo form_close();
-				?>
+					<input type="submit" name="submit" value="<?php echo $this->lang->line('sales_unsuspend'); ?>" id="submit" class="btn btn-primary btn-xs pull-right">
+				<?php echo form_close(); ?>
+			</td>
 		</tr>
 	<?php
 	}
-	
 	?>
-	
 </table>
