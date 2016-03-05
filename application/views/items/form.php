44dc300c63cc008603e5a31ee958b5de69d56762
Aaron Gong <ais-one@users.noreply.github.com>
2016-03-05 14:50:53 +0800

    Update form.php
    removed required in class of the label

--- application/views/items/form.php
+++ application/views/items/form.php
@@ -47,7 +47,7 @@
 		</div>
 
 		<div class="form-group form-group-sm">
-			<?php echo form_label($this->lang->line('items_supplier'), 'supplier', array('class'=>'required control-label col-xs-3')); ?>
+			<?php echo form_label($this->lang->line('items_supplier'), 'supplier', array('class'=>'control-label col-xs-3')); ?>
 			<div class='col-xs-6'>
 				<?php echo form_dropdown('supplier_id', $suppliers, $selected_supplier, array('class'=>'form-control'));?>
 			</div>
