525fb7dcf918f80cdfb2816e23a699717a7257a1
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-03-08 21:28:41 +0000

    fix issue blocking searching suggestion

--- application/config/config.php
+++ application/config/config.php
@@ -451,7 +451,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_ospos_v24';
 $config['csrf_cookie_name'] = 'csrf_cookie_ospos_v24';
 $config['csrf_expire'] = 7200;
