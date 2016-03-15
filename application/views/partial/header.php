09dd7eca580d514f7142976e3e185f742dd400d8
jekkos <jeroen.peelaerts@gmail.com>
2016-03-15 08:14:16 +0100

    Make invoice header responsive, remove fixed height and make font size relative. (#116)

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -35,7 +35,7 @@
 		<!-- end js template tags -->
 	<?php else : ?>
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=1e1e459762" language="javascript"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=140db8dad4" language="javascript"></script>
 		<!-- end minjs template tags -->       
 	<?php endif; ?>
 
