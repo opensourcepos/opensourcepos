19bdcad78eaae8ea99f8743a0151275850326bd0
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-04-01 08:10:47 +0100

    fix database conversion script missing ; at the end (#429)

--- database/2.4_to_3.0.sql
+++ database/2.4_to_3.0.sql
@@ -1,21 +1,21 @@
--- alter quantity fields
+-- alter quantity fields to be all decimal
 
 ALTER TABLE `ospos_items`
  MODIFY COLUMN `reorder_level` decimal(15,3) NOT NULL DEFAULT '0',
- MODIFY COLUMN `receiving_quantity` decimal(15,3) NOT NULL DEFAULT '1'
+ MODIFY COLUMN `receiving_quantity` decimal(15,3) NOT NULL DEFAULT '1';
 
 ALTER TABLE `ospos_item_kit_items`
- MODIFY COLUMN `quantity` decimal(15,3) NOT NULL
+ MODIFY COLUMN `quantity` decimal(15,3) NOT NULL;
 
 ALTER TABLE `ospos_item_quantities`
- MODIFY COLUMN `quantity` decimal(15,3) NOT NULL DEFAULT '0'
+ MODIFY COLUMN `quantity` decimal(15,3) NOT NULL DEFAULT '0';
 
 ALTER TABLE `ospos_receivings_items`
  MODIFY COLUMN `quantity_purchased` decimal(15,3) NOT NULL DEFAULT '0',
- MODIFY COLUMN `receiving_quantity` decimal(15,3) NOT NULL DEFAULT '1'
+ MODIFY COLUMN `receiving_quantity` decimal(15,3) NOT NULL DEFAULT '1';
 
 ALTER TABLE `ospos_sales_items`
- MODIFY COLUMN `quantity_purchased` decimal(15,3) NOT NULL DEFAULT '0'
+ MODIFY COLUMN `quantity_purchased` decimal(15,3) NOT NULL DEFAULT '0';
 
 ALTER TABLE `ospos_sales_suspended_items`
- MODIFY COLUMN `quantity_purchased` decimal(15,3) NOT NULL DEFAULT '0'
+ MODIFY COLUMN `quantity_purchased` decimal(15,3) NOT NULL DEFAULT '0';
