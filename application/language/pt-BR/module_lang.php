95c4fe170b49440233d1248ae791b3c3b6f1e297
arteiro <webmaster@xyko.com.br>
2016-04-05 14:49:24 -0300

    Tradução pt-BR
    Criando tradução para Português do Brasil

---
+++ application/language/pt-BR/module_lang.php
@@ -0,0 +1,23 @@
+<?php 
+
+$lang["module_config"] = " Configurações";
+$lang["module_config_desc"] = " Alterar configuraççoes do sistema";
+$lang["module_customers"] = " Clientes";
+$lang["module_customers_desc"] = " Adicionar, atualizar,  excluir e Pesquisar clientes";
+$lang["module_employees"] = " Empregados";
+$lang["module_employees_desc"] = " Adicionar , atualizar,  excluir e Pesquisar empregados";
+$lang["module_giftcards"] = " Cupons";
+$lang["module_giftcards_desc"] = " Adicionar,  atualizar, excluir e Pesquisar cupons";
+$lang["module_home"] = " Home";
+$lang["module_item_kits"] = " Item Kits";
+$lang["module_item_kits_desc"] = " Adicionar , atualizar, excluir e Pesquisar Item Kits";
+$lang["module_items"] = " Itens";
+$lang["module_items_desc"] = " Adicionar,  atualizar,  excluir e Pesquisar itens";
+$lang["module_receivings"] = " Recebimento";
+$lang["module_receivings_desc"] = " Processar ordens";
+$lang["module_reports"] = " Relatórios";
+$lang["module_reports_desc"] = " Gerar relatórios";
+$lang["module_sales"] = " Vendas";
+$lang["module_sales_desc"] = " Processar vendas e devoluções";
+$lang["module_suppliers"] = " Fornecedores";
+$lang["module_suppliers_desc"] = " Adicionar,  atualizar,  excluir e Pesquisar fornecedores";
