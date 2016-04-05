30d12ca456ba024b79be453cc5206064ee9f6920
arteiro <webmaster@xyko.com.br>
2016-04-05 15:00:09 -0300

    Traducao pt br
    tradução para portugues do Brasil

---
+++ application/language/pt-BR/error_lang.php
@@ -0,0 +1,4 @@
+<?php 
+
+$lang["error_no_permission_module"] = "Você não tem permissão para acessar o módulo chamado";
+$lang["error_unknown"] = "Desconhecido";
---
+++ application/language/pt-BR/login_lang.php
@@ -0,0 +1,8 @@
+<?php 
+
+$lang["login_go"] = "Entrar";
+$lang["login_invalid_username_and_password"] = "Usuário ou senha inválido";
+$lang["login_login"] = "Login";
+$lang["login_password"] = "Senha";
+$lang["login_username"] = "Usuário";
+$lang["login_welcome_message"] = "bem-vindo. Para continuar, por favor acessar usando seu nome de usuário e senha abaixo.";
