fb16c4b65a11ab6180706a19ac84b58f84dbbadf
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-04-09 23:07:54 +0100

    Add Spanish System translations (#470)

--- system/language/es/form_validation_lang.php
+++ system/language/es/form_validation_lang.php
@@ -1,18 +1,41 @@
-<?php 
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @author	Iban Eguia
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang["alpha_dash"] = "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
-$lang["alpha_numeric"] = "The %s field may only contain alpha-numeric characters.";
-$lang["exact_length"] = "The %s field must be exactly %s characters in length.";
-$lang["greater_than"] = "The %s field must contain a number greater than %s.";
-$lang["is_natural"] = "The %s field must contain only positive numbers.";
-$lang["is_natural_no_zero"] = "The %s field must contain a number greater than zero.";
-$lang["is_numeric"] = "The %s field must contain only numeric characters.";
-$lang["is_unique"] = "The %s field must contain a unique value.";
-$lang["less_than"] = "The %s field must contain a number less than %s.";
-$lang["max_length"] = "The %s field can not exceed %s characters in length.";
-$lang["min_length"] = "The %s field must be at least %s characters in length.";
-$lang["regex_match"] = "The %s field is not in the correct format.";
-$lang["valid_email"] = "The %s field must contain a valid email address.";
-$lang["valid_emails"] = "The %s field must contain all valid email addresses.";
-$lang["valid_ip"] = "The %s field must contain a valid IP.";
-$lang["valid_url"] = "The %s field must contain a valid URL.";
+$lang['form_validation_required']		= 'El campo {field} es obligatorio.';
+$lang['form_validation_isset']			= 'El campo {field} debe contener un valor.';
+$lang['form_validation_valid_email']		= 'El campo {field} debe contener un email válido.';
+$lang['form_validation_valid_emails']		= 'El campo {field} debe contener todos los emails válidos.';
+$lang['form_validation_valid_url']		= 'El campo {field} debe contener una URL válida.';
+$lang['form_validation_valid_ip']		= 'El campo {field} debe contener una IP válida.';
+$lang['form_validation_min_length']		= 'El campo {field} debe ser de al menos {param} caracteres de longitud.';
+$lang['form_validation_max_length']		= 'El campo {field} no puede superar los {param} caracteres de longitud.';
+$lang['form_validation_exact_length']		= 'El campo {field} debe ser de exactamente {param} caracteres de longitud.';
+$lang['form_validation_alpha']			= 'El campo {field} solo puede contener caracteres alfabéticos.';
+$lang['form_validation_alpha_numeric']		= 'El campo {field} solo puede contener caracteres alfanuméricos.';
+$lang['form_validation_alpha_numeric_spaces']	= 'El campo {field} solo puede contener caracteres alfanuméricos y espacios.';
+$lang['form_validation_alpha_dash']		= 'El campo {field} solo puede contener caracteres alfanuméricos, guiones bajos y guiones.';
+$lang['form_validation_numeric']		= 'El campo {field} solo puede contener números.';
+$lang['form_validation_is_numeric']		= 'El campo {field} solo puede contener caracteres numéricos.';
+$lang['form_validation_integer']		= 'El campo {field} debe contener un entero.';
+$lang['form_validation_regex_match']		= 'El campo {field} no está en el formato correcto.';
+$lang['form_validation_matches']		= 'El campo {field} no coincide con el campo {param}.';
+$lang['form_validation_differs']		= 'El campo {field} debe ser distinto al campo {param}.';
+$lang['form_validation_is_unique'] 		= 'El campo {field} debe contener un valor único.';
+$lang['form_validation_is_natural']		= 'El campo {field} solo puede contener dígitos.';
+$lang['form_validation_is_natural_no_zero']	= 'El campo {field} solo puede contener dígitos y debe ser mayor que cero.';
+$lang['form_validation_decimal']		= 'El campo {field} debe contener un número decimal.';
+$lang['form_validation_less_than']		= 'El campo {field} debe contener un número menor que {param}.';
+$lang['form_validation_less_than_equal_to']	= 'El campo {field} debe contener un número menor o igual a {param}.';
+$lang['form_validation_greater_than']		= 'El campo {field} debe contener un número mayor que {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'El campo {field} debe contener un número mayor o igual a {param}.';
+$lang['form_validation_error_message_not_set']	= 'No se ha podido acceder al mensaje de error correspondiente para el campo {field}.';
+$lang['form_validation_in_list']		= 'El campo {field} debe contener uno de estos: {param}.';
--- system/language/es/upload_lang.php
+++ system/language/es/upload_lang.php
@@ -1,22 +1,28 @@
 <?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @author	Iban Eguia
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['upload_userfile_not_set'] = "Unable to find a post variable called userfile.";
-$lang['upload_file_exceeds_limit'] = "The uploaded file exceeds the maximum allowed size in your PHP configuration file.";
-$lang['upload_file_exceeds_form_limit'] = "The uploaded file exceeds the maximum size allowed by the submission form.";
-$lang['upload_file_partial'] = "The file was only partially uploaded.";
-$lang['upload_no_temp_directory'] = "The temporary folder is missing.";
-$lang['upload_unable_to_write_file'] = "The file could not be written to disk.";
-$lang['upload_stopped_by_extension'] = "The file upload was stopped by extension.";
-$lang['upload_no_file_selected'] = "You did not select a file to upload.";
-$lang['upload_invalid_filetype'] = "The filetype you are attempting to upload is not allowed.";
-$lang['upload_invalid_filesize'] = "The file you are attempting to upload is larger than the permitted size.";
-$lang['upload_invalid_dimensions'] = "The image you are attempting to upload exceeds the maximum height or width.";
-$lang['upload_destination_error'] = "A problem was encountered while attempting to move the uploaded file to the final destination.";
-$lang['upload_no_filepath'] = "The upload path does not appear to be valid.";
-$lang['upload_no_file_types'] = "You have not specified any allowed file types.";
-$lang['upload_bad_filename'] = "The file name you submitted already exists on the server.";
-$lang['upload_not_writable'] = "The upload destination folder does not appear to be writable.";
-
-
-/* End of file upload_lang.php */
-/* Location: ./system/language/english/upload_lang.php */
+$lang['upload_userfile_not_set'] = 'No ha sido posible encontrar una variable post llamada userfile.';
+$lang['upload_file_exceeds_limit'] = 'El archivo subido excede el tamaño máximo permitido por tu configuración de PHP.';
+$lang['upload_file_exceeds_form_limit'] = 'El archivo subido excede el tamaño máximo permitido por el formulario.';
+$lang['upload_file_partial'] = 'El archivo solo se subió parcialmente.';
+$lang['upload_no_temp_directory'] = 'No se encuentra el directorio temporal.';
+$lang['upload_unable_to_write_file'] = 'El archivo no pudo ser escrito en el disco.';
+$lang['upload_stopped_by_extension'] = 'La subida del archivo paró por extensión.';
+$lang['upload_no_file_selected'] = 'No seleccionaste un archivo para subir.';
+$lang['upload_invalid_filetype'] = 'El tipo de archivo que intentas subir no está permitido.';
+$lang['upload_invalid_filesize'] = 'El archivo que intentas subir es más grande que el tamaño permitido.';
+$lang['upload_invalid_dimensions'] = 'La imagen que intentas subir no encaja en las dimensiones permitidas.';
+$lang['upload_destination_error'] = 'Se encontró un problema al intentar mover el archivo subido a su destino final.';
+$lang['upload_no_filepath'] = 'La ruta de subida no parece válida.';
+$lang['upload_no_file_types'] = 'No has especificado ningún tipo de archivo permitido.';
+$lang['upload_bad_filename'] = 'El nombre de archivo que has proporcionado ya existe en el servidor.';
+$lang['upload_not_writable'] = 'La carpeta de destino de la subida no parece escribible.';
