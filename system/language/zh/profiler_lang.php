a1311dca4cf253facf19b5b4da7693a4a0f223e0
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-04-09 17:23:40 +0100

    Fixed Items page not displaying with German and Chinese (#470)

---
+++ system/language/zh/profiler_lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['profiler_database'] = '数据库';
+$lang['profiler_controller_info'] = '类 / 方法';
+$lang['profiler_benchmarks'] = '基准';
+$lang['profiler_queries'] = '查询';
+$lang['profiler_get_data'] = 'GET 数据';
+$lang['profiler_post_data'] = 'POST 数据';
+$lang['profiler_uri_string'] = 'URI 字符串';
+$lang['profiler_memory_usage'] = '内存使用';
+$lang['profiler_config'] = '设置值';
+$lang['profiler_session_data'] = 'SESSION 数据';
+$lang['profiler_headers'] = 'HTTP 头';
+$lang['profiler_no_db'] = '当前没有已经载入的数据库驱动。  ';
+$lang['profiler_no_queries'] = '运行，无查询。  ';
+$lang['profiler_no_post'] = 'POST 数据不存在。';
+$lang['profiler_no_get'] = 'GET 数据不存在。';
+$lang['profiler_no_uri'] = 'URI 数据不存在。';
+$lang['profiler_no_memory'] = '无法获得内存使用数据。';
+$lang['profiler_no_profiles'] = '没有 Profile 数据 - 全部的 Profiler 都已被关闭。';
+$lang['profiler_section_hide'] = '隐藏';
+$lang['profiler_section_show'] = '显示';
+$lang['profiler_seconds'] = '秒';
---
+++ system/language/zh/unit_test_lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['ut_test_name'] = '测试名';
+$lang['ut_test_datatype'] = '测试的数据类型';
+$lang['ut_res_datatype'] = '期望的数据类型';
+$lang['ut_result'] = '结果';
+$lang['ut_undefined'] = '未定义的测试名';
+$lang['ut_file'] = '文件名';
+$lang['ut_line'] = '行号';
+$lang['ut_passed'] = '通过';
+$lang['ut_failed'] = '失败';
+$lang['ut_boolean'] = '布尔型';
+$lang['ut_integer'] = '整型';
+$lang['ut_float'] = '浮点数';
+$lang['ut_double'] = '双精度数'; // can be the same as float
+$lang['ut_string'] = '字符串';
+$lang['ut_array'] = '数组';
+$lang['ut_object'] = '对象';
+$lang['ut_resource'] = '资源';
+$lang['ut_null'] = 'Null';
+$lang['ut_notes'] = '笔记';
