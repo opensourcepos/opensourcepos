935e9f0bc8441f17ca53d75c84c09cba171475a0
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-04-09 16:43:39 +0100

    replace plus and minus in stock_locations with glyphicons, remove old png

--- application/views/partial/stock_locations.php
+++ application/views/partial/stock_locations.php
@@ -8,19 +8,20 @@ foreach($stock_locations as $location=>$location_data)
 ?>
 	<div class="form-group form-group-sm" style="<?php echo $location_data['deleted'] ? 'display:none;' : 'display:block;' ?>">
 		<?php echo form_label($this->lang->line('config_stock_location') . ' ' . ++$i, 'stock_location_' . $i, array('class'=>'control-label col-xs-2 required')); ?>
-			<div class='col-xs-2'>
-				<?php $form_data = array(
-						'name'=>'stock_location_' . $location_id,
-						'id'=>'stock_location_' . $location_id,
-						'class'=>'stock_location valid_chars form-control input-sm required',
-						'value'=>$location_name
-					); 
-					$location_data['deleted'] && $form_data['disabled'] = 'disabled';
-					echo form_input($form_data);
-				?>
-			</div>
-		<img class="add_stock_location" src="<?php echo base_url('images/plus.png'); ?>" />
-		<img class="remove_stock_location" src="<?php echo base_url('images/minus.png'); ?>" />
+		<div class='col-xs-2'>
+			<?php $form_data = array(
+					'name'=>'stock_location_' . $location_id,
+					'id'=>'stock_location_' . $location_id,
+					'class'=>'stock_location valid_chars form-control input-sm required',
+					'value'=>$location_name
+				); 
+				$location_data['deleted'] && $form_data['disabled'] = 'disabled';
+				echo form_input($form_data);
+			?>
+		</div>
+		<span class="add_stock_location glyphicon glyphicon-plus" style="padding-top: 0.5em;"></span>
+		<span>&nbsp;&nbsp;</span>
+		<span class="remove_stock_location glyphicon glyphicon-minus" style="padding-top: 0.5em;"></span>
 	</div>
 <?php
 }
