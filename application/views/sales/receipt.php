d9b007c1af0bc4e2ca3b47270283e4cd82ee6e78
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-04-10 18:15:59 +0100

    Improve emailed receipt layout and make it look like the printed one (now added logo + company name) (#442)

--- application/views/sales/receipt.php
+++ application/views/sales/receipt.php
@@ -20,24 +20,21 @@ if (isset($error_message))
 <div id="receipt_wrapper">
 	<div id="receipt_header">
 		<?php
-		if ($this->Appconfig->get('company_logo') == '') 
+		if ($this->Appconfig->get('company_logo') != '') 
         { 
         ?>
-			<div id="company_name"><?php echo $this->config->item('company'); ?></div>
-		<?php 
-		}
-		else 
-		{ 
-		?>
 			<div id="company_name"><img id="image" src="<?php echo base_url('uploads/' . $this->Appconfig->get('company_logo')); ?>" alt="company_logo" /></div>			
 		<?php
 		}
 		?>
+
+		<div id="company_name"><?php echo $this->config->item('company'); ?></div>
 		<div id="company_address"><?php echo nl2br($this->config->item('address')); ?></div>
 		<div id="company_phone"><?php echo $this->config->item('phone'); ?></div>
 		<div id="sale_receipt"><?php echo $receipt_title; ?></div>
 		<div id="sale_time"><?php echo $transaction_time ?></div>
 	</div>
+
 	<div id="receipt_general_info">
 		<?php
 		if(isset($customer))
@@ -74,10 +71,10 @@ if (isset($error_message))
 		{
 		?>
 			<tr>
-				<td><span class='long_name'><?php echo ucfirst($item['name']); ?></span></td>
+				<td><?php echo ucfirst($item['name']); ?></td>
 				<td><?php echo to_currency($item['price']); ?></td>
 				<td><?php echo to_quantity_decimals($item['quantity']) . " " . ($show_stock_locations ? " [" . $item['stock_name'] . "]" : ""); ?></td>
-				<td><div class="total-value"><?php echo to_currency($item[($this->Appconfig->get('show_total_discount') ? 'total' : 'discounted_total')]); ?></div></td>
+				<td class="total-value"><?php echo to_currency($item[($this->Appconfig->get('show_total_discount') ? 'total' : 'discounted_total')]); ?></td>
 			</tr>
 			<tr>
 				<td colspan="2"><?php echo $item['description']; ?></td>
@@ -142,8 +139,8 @@ if (isset($error_message))
 		
 		<?php $border = (!$this->Appconfig->get('receipt_show_taxes') && !($this->Appconfig->get('show_total_discount') && $discount > 0)); ?> 
 		<tr>
-			<td colspan="3" style='<?php echo $border? 'border-top: 2px solid black;' :''; ?>text-align:right;'><?php echo $this->lang->line('sales_total'); ?></td>
-			<td style='<?php echo $border? 'border-top: 2px solid black;' :''; ?>text-align:right'><?php echo to_currency($total); ?></td>
+			<td colspan="3" style="text-align:right;<?php echo $border? 'border-top: 2px solid black;' :''; ?>"><?php echo $this->lang->line('sales_total'); ?></td>
+			<td style="text-align:right;<?php echo $border? 'border-top: 2px solid black;' :''; ?>"><?php echo to_currency($total); ?></td>
 		</tr>
 
 		<tr>
@@ -161,28 +158,30 @@ if (isset($error_message))
 		?>
 			<tr>
 				<td colspan="3" style="text-align:right;"><?php echo $splitpayment[0]; ?> </td>
-				<td><div class="total-value"><?php echo to_currency( $payment['payment_amount'] * -1 ); ?></div></td>
+				<td class="total-value"><?php echo to_currency( $payment['payment_amount'] * -1 ); ?></td>
 			</tr>
 		<?php
 		}
 		?>
 
-		<tr><td colspan="4">&nbsp;</td></tr>
+		<tr>
+			<td colspan="4">&nbsp;</td>
+		</tr>
 
 		<?php 
 		if (isset($cur_giftcard_value) && $show_giftcard_remainder)
 		{
 		?>
 		<tr>
-			<td colspan="3" style='text-align:right;'><?php echo $this->lang->line('sales_giftcard_balance'); ?></td>
-			<td style='text-align:right'><?php echo to_currency($cur_giftcard_value); ?></td>
+			<td colspan="3" style="text-align:right;"><?php echo $this->lang->line('sales_giftcard_balance'); ?></td>
+			<td class="total-value"><?php echo to_currency($cur_giftcard_value); ?></td>
 		</tr>
 		<?php 
 		}
 		?>
 		<tr>
-			<td colspan="3" style='text-align:right;'> <?php echo $this->lang->line($amount_change >= 0 ? ($only_sale_check ? 'sales_check_balance' : 'sales_change_due') : 'sales_amount_due') ; ?> </td>
-			<td style='text-align:right'><?php echo to_currency($amount_change); ?></td>
+			<td colspan="3" style="text-align:right;"> <?php echo $this->lang->line($amount_change >= 0 ? ($only_sale_check ? 'sales_check_balance' : 'sales_change_due') : 'sales_amount_due') ; ?> </td>
+			<td class="total-value"><?php echo to_currency($amount_change); ?></td>
 		</tr>
 	</table>
 
@@ -190,7 +189,7 @@ if (isset($error_message))
 		<?php echo nl2br($this->config->item('return_policy')); ?>
 	</div>
 
-	<div id='barcode'>
+	<div id="barcode">
 		<img src='data:image/png;base64,<?php echo $barcode; ?>' /><br>
 		<?php echo $sale_id; ?>
 	</div>
