488af06663d49fe689e8e9603cde3fa3285aa90d
jekkos <jeroen.peelaerts@gmail.com>
2016-05-03 22:41:44 +0200

    Fix delete in Item model (#507)

--- application/models/Item.php
+++ application/models/Item.php
@@ -248,11 +248,9 @@ class Item extends CI_Model
 	*/
 	public function delete($item_id)
 	{
-		$this->db->where('item_id', $item_id);
-
 		// set to 0 quantities
 		$this->Item_quantity->reset_quantity($item_id);
-
+		$this->db->where('item_id', $item_id);
 		return $this->db->update('items', array('deleted'=>1));
 	}
 	
