f8ef4bb570df9e2697cd6cd28567bca038057f97
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-06-02 12:04:46 +0100

    Tidied up spacelab template footer.php after notification change (#478)

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -3,11 +3,11 @@
 
 	<div id="footer">
 		<div class="jumbotron push-spaces">
-				<?php echo $this->lang->line('common_you_are_using_ospos'); ?>
-				<?php echo $this->config->item('application_version'); ?>.
-				<?php echo $this->lang->line('common_please_visit_my'); ?>
-				<a href="https://github.com/jekkos/opensourcepos" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>
-				<?php echo $this->lang->line('common_learn_about_project'); ?>
+			<?php echo $this->lang->line('common_you_are_using_ospos'); ?>
+			<?php echo $this->config->item('application_version'); ?>.
+			<?php echo $this->lang->line('common_please_visit_my'); ?>
+			<a href="https://github.com/jekkos/opensourcepos" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>
+			<?php echo $this->lang->line('common_learn_about_project'); ?>
 		</div>
 	</div>
 </body>
