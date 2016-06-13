9dc7405eaa35e64e89ed3f558895ac3c24adb76f
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-06-13 17:54:31 +0100

    How to remove index.php from URL (#645) + set a config rule to be strict with sessions matching IP

--- application/config/config.php
+++ application/config/config.php
@@ -384,7 +384,7 @@ $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ospos_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = 'sessions';
-$config['sess_match_ip'] = FALSE;
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
