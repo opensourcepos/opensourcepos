11ad1c247615414de3762cd414da0fef0d79a546
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-06-13 18:58:17 +0100

    Regen dist files

--- templates/spacelab/views/partial/header.php
+++ templates/spacelab/views/partial/header.php
@@ -73,7 +73,7 @@
 		<!-- end mincss template tags -->
 		<link rel="stylesheet" type="text/css" href="templates/spacelab/css/style.css"/>
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=cf381eb07c" language="javascript"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=6cffb1256d" language="javascript"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
