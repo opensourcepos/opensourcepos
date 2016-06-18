c8f966cbc91a005deebaa50d34386ea51e8a1de4
Eko Aprili Trisno <liko@computer4u.com>
2016-06-18 20:12:52 +0700

    Update Customer.php
    first name contain space i think when select

--- application/models/Customer.php
+++ application/models/Customer.php
@@ -241,7 +241,7 @@ class Customer extends Person
 			$this->db->or_like('email', $search);
 			$this->db->or_like('phone_number', $search);
 			$this->db->or_like('account_number', $search);
-			$this->db->or_like('CONCAT(first_name, " ", last_name)', $search);
+			$this->db->or_like('CONCAT(first_name,last_name)', $search);
 		$this->db->group_end();
 		$this->db->where('deleted', 0);
 
@@ -261,7 +261,7 @@ class Customer extends Person
 			$this->db->or_like('email', $search);
 			$this->db->or_like('phone_number', $search);
 			$this->db->or_like('account_number', $search);
-			$this->db->or_like('CONCAT(first_name, " ", last_name)', $search);
+			$this->db->or_like('CONCAT(first_name, last_name)', $search);
 		$this->db->group_end();
 		$this->db->where('deleted', 0);
 		$this->db->order_by($sort, $order);
