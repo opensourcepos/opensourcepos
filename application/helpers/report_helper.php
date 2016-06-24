d378cfdb5f387f5e548261faa9c77d78c5d0291f
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-06-24 17:55:10 +0100

    New look for the Reports listing page, code tidy up, dist regen (#695)

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -1,33 +1,11 @@
 <?php
 
-function get_random_colors($how_many)
-{
-	$colors = array();
-	
-	for($k=0;$k<$how_many;$k++)
-	{
-		$colors[] = '#'.random_color();
-	}
-	
-	return $colors;
-}
-
-function random_color()
-{
-    mt_srand((double)microtime()*1000000);
-    $c = '';
-    while(strlen($c)<6){
-        $c .= sprintf("%02X", mt_rand(0, 255));
-    }
-
-    return $c;
-}
-
-function show_report_if_allowed($report_prefix, $report_name, $person_id, $permission_id='')
+function show_report_if_allowed($report_prefix, $report_name, $person_id, $permission_id = '')
 {
 	$CI =& get_instance();
+
 	$permission_id = empty($permission_id) ? 'reports_' . $report_name : $permission_id;	
-	if ($CI->Employee->has_grant($permission_id, $person_id))
+	if($CI->Employee->has_grant($permission_id, $person_id))
 	{
 		show_report($report_prefix, $report_name, $permission_id);
 	}
@@ -36,14 +14,15 @@ function show_report_if_allowed($report_prefix, $report_name, $person_id, $permi
 function show_report($report_prefix, $report_name, $lang_key='')
 {
 	$CI =& get_instance();
+
 	$lang_key = empty($lang_key) ? $report_name : $lang_key;
 	$report_label = $CI->lang->line($lang_key);
 	$report_prefix = empty($report_prefix) ? '' : $report_prefix . '_';
 	// no summary nor detailed reports for receivings
-	if (!empty($report_label) && $report_label != $lang_key .  ' (TBD)')
+	if(!empty($report_label) && $report_label != $lang_key . ' (TBD)')
 	{
 		?>
-			<li><a href="<?php echo site_url('reports/' . $report_prefix . preg_replace('/reports_(.*)/', '$1', $report_name));?>"><?php echo $report_label; ?></a></li>
+			<a class="list-group-item" href="<?php echo site_url('reports/' . $report_prefix . preg_replace('/reports_(.*)/', '$1', $report_name));?>"><?php echo $report_label; ?></a>
 		<?php 
 	}
 }
--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -67,12 +67,12 @@
 		<link rel="stylesheet" media="print" href="css/print.css" type="text/css" />
 		<![endif]-->
 		<!-- start mincss template tags -->
-		<link rel="stylesheet" type="text/css" href="dist/bootstrap.min.css?rel=9ed20b1ee8"/>
+		<link rel="stylesheet" type="text/css" href="dist/bootstrap.min.css?rel=50ab19585f"/>
 		<link rel="stylesheet" type="text/css" href="dist/jquery-ui.css"/>
-		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=725a0444ad"/>
+		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=6565b523f9"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=057f34d35e" language="javascript"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=d18a7b4a1b" language="javascript"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
