3cff918f8133969a5f2a448cc9d6e0d067fcc929
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-07-05 20:28:32 +0100

    Inverted Sales register New Items New and Submit buttons

--- application/views/sales/register.php
+++ application/views/sales/register.php
@@ -78,7 +78,7 @@ if (isset($success))
 					<span class="ui-helper-hidden-accessible" role="status"></span>
 				</li>
 				<li class="pull-right">
-					<button id='new_item_button' class='btn btn-info btn-sm pull-right modal-dlg' data-btn-submit='<?php echo $this->lang->line('common_submit')?>' data-btn-new='<?php echo $this->lang->line('common_new') ?>' data-href='<?php echo site_url("items/view"); ?>'
+					<button id='new_item_button' class='btn btn-info btn-sm pull-right modal-dlg' data-btn-new='<?php echo $this->lang->line('common_new') ?>' data-btn-submit='<?php echo $this->lang->line('common_submit')?>' data-href='<?php echo site_url("items/view"); ?>'
 							title='<?php echo $this->lang->line($controller_name . '_new_item'); ?>'>
 						<span class="glyphicon glyphicon-tag">&nbsp</span><?php echo $this->lang->line($controller_name. '_new_item'); ?>
 					</button>
