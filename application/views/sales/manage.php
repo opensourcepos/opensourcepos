55f7bef253da5613a389170051bb6805b6da1ba6
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-07-09 10:15:13 +0100

    Added go to Sales Register button in Takings

--- application/views/sales/manage.php
+++ application/views/sales/manage.php
@@ -50,6 +50,7 @@ $(document).ready(function()
 	<button onclick="javascript:printdoc()" class='btn btn-info btn-sm pull-right'>
 		<span class="glyphicon glyphicon-print">&nbsp</span><?php echo $this->lang->line('common_print'); ?>
 	</button>
+	<?php echo anchor("sales", '<span class="glyphicon glyphicon-shopping-cart">&nbsp</span>' . $this->lang->line('sales_register'), array('class'=>'btn btn-info btn-sm pull-right', 'id'=>'show_sales_button')); ?>
 </div>
 
 <div id="toolbar">
