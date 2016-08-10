4c337aa5c60cd8b48914eba9e007ab7199ad49ff
jekkos <jeroen.peelaerts@gmail.com>
2016-08-10 15:26:18 +0200

    Try to fix travis docker setup + remove jquery validator override (#788, #616)

--- application/views/partial/header_js.php
+++ application/views/partial/header_js.php
@@ -44,8 +44,4 @@
 		}
 	});
 
-	$.validator.methods.number = function (value, element) {
-		return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
-	}
-
 </script>
