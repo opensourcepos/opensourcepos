6b63e381f9d109ca98e2a48f90599f9b274678f7
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-08-15 18:31:22 +0100

    Security fix: set OSPOS to be production code by default if CI_ENV is not set (#808)

--- index.php
+++ index.php
@@ -53,7 +53,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+//	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
