eb59079fe5cd44a523b8f6d1fa05fccfbebae8d3
jekkos <jeroen.peelaerts@gmail.com>
2016-09-01 19:20:50 +0200

    Regenerate sql scripts for hashing upgrade (#822)

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -73,7 +73,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/style.css"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=2ea05557c1"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=e149b9a4ba"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
