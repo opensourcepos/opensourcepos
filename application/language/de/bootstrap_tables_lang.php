e381b3fea5ced20d798c0f1950cf487cb20481a0
Valentin Seipt <valentin@seipt.org>
2016-09-12 13:35:46 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (8 of 8 strings)

--- application/language/de/bootstrap_tables_lang.php
+++ application/language/de/bootstrap_tables_lang.php
@@ -3,8 +3,8 @@
 $lang["tables_loading"] = "Lade, bitte warten...";
 $lang["tables_rows_per_page"] = "{0} Einträge pro Seite";
 $lang["tables_page_from_to"] = "Zeige {0} bis {1} von {2} Zeile(n)";
-$lang["tables_hide_show_pagination"] = "Hide/Show pagination";
-$lang["tables_refresh"] = "Refresh";
+$lang["tables_hide_show_pagination"] = "Seitenzahlen anzeigen/verbergen";
+$lang["tables_refresh"] = "Aktualisieren";
 $lang["tables_toggle"] = "Umschalten";
 $lang["tables_columns"] = "Spalten";
-$lang["tables_all"] = "All";
+$lang["tables_all"] = "Alle";
