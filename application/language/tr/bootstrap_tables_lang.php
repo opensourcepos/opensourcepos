7d7afd4904db3bb036bf61479b807cfe4b398d1b
Suat Donangil <donangil@gmail.com>
2016-09-17 21:51:15 +0000

    Translated using Weblate (Turkish)
    Currently translated at 98.2% (115 of 117 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (66 of 66 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (8 of 8 strings)

--- application/language/tr/bootstrap_tables_lang.php
+++ application/language/tr/bootstrap_tables_lang.php
@@ -2,7 +2,7 @@
 
 $lang["tables_loading"] = "Yükleniyor, lütfen bekleyin...";
 $lang["tables_rows_per_page"] = "Sayfa başına {0} satır";
-$lang["tables_page_from_to"] = "Showing {0} to {1} of {2} rows";
+$lang["tables_page_from_to"] = "{2} kayıttan {0} - {1} görüntüleniyor";
 $lang["tables_hide_show_pagination"] = "Sayfalandırmayı Gizle/Göster";
 $lang["tables_refresh"] = "Yenile";
 $lang["tables_toggle"] = "Değiştir";
