15e84f9b3746221fd3918ec92d61a59c4055d27c
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-10-01 15:59:07 +0100

    Fix config.php file scrambling

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,41 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); /* |-------------------------------------------------------------------------- | Code Version |-------------------------------------------------------------------------- | | This is the version of Open Source Point of Sale you're running | | */ $config['application_version'] = '3.0.0'; /* |-------------------------------------------------------------------------- | Commit sha1 |-------------------------------------------------------------------------- | | This is the commit hash for the version you are currently using | | */ $config['commit_sha1'] = '$Id$'; /* |-------------------------------------------------------------------------- | Internal to OSPOS XSS Clean |-------------------------------------------------------------------------- | | This is to indicated whether we want XSS clean to be performed or not | By default it's enabled as it's assumed the installation has Internet access and needs to be protected, | however intranet only installations may not need this so they can set FALSE to improve performance | */ $config['ospos_xss_clean'] = TRUE; /* |-------------------------------------------------------------------------- | Base Site URL
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+|-------------------------------------------------------------------------- 
+| Code Version 
+|-------------------------------------------------------------------------- 
+| 
+| This is the version of Open Source Point of Sale you're running 
+| 
+| 
+*/ 
+$config['application_version'] = '3.0.0';
+
+/* 
+|-------------------------------------------------------------------------- 
+| Commit sha1 
+|-------------------------------------------------------------------------- 
+| 
+| This is the commit hash for the version you are currently using 
+| 
+| 
+*/ 
+$config['commit_sha1'] = '$Id$';
+ 
+/* 
+|-------------------------------------------------------------------------- 
+| Internal to OSPOS XSS Clean 
+|-------------------------------------------------------------------------- 
+| 
+| This is to indicated whether we want XSS clean to be performed or not 
+| By default it's enabled as it's assumed the installation has Internet access and needs to be protected, 
+| however intranet only installations may not need this so they can set FALSE to improve performance 
+| 
+*/ 
+$config['ospos_xss_clean'] = TRUE;
+
+/* 
+|-------------------------------------------------------------------------- 
+| Base Site URL
 |--------------------------------------------------------------------------
 |
 | URL to your CodeIgniter root. Typically this will be your base URL,
