86de36d7316e9aa30745c99457d67613bda01165
jekkos <jeroen.peelaerts@gmail.com>
2016-10-02 00:19:46 +0200

    Fix include paths for debug js + css

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -2,22 +2,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<base href="<?php echo base_url();?>" />
 	<title><?php echo $this->config->item('company') . ' | ' . $this->lang->line('common_powered_by') . ' OSPOS ' . $this->config->item('application_version') ?></title>
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="<?php echo 'dist/bootswatch/' . (empty($this->config->item('theme')) ? 'flatly' : $this->config->item('theme')) . '/bootstrap.min.css' ?>"/>
 
 	<?php if ($this->input->cookie('debug') == "true" || $this->input->get("debug") == "true") : ?>
 		<!-- bower:css -->
-		<link rel="stylesheet" href="public/bower_components/jquery-ui/themes/base/jquery-ui.css" />
-		<link rel="stylesheet" href="public/bower_components/bootstrap3-dialog/dist/css/bootstrap-dialog.min.css" />
-		<link rel="stylesheet" href="public/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.css" />
-		<link rel="stylesheet" href="public/bower_components/smalot-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" />
-		<link rel="stylesheet" href="public/bower_components/bootstrap-select/dist/css/bootstrap-select.css" />
-		<link rel="stylesheet" href="public/bower_components/bootstrap-table/src/bootstrap-table.css" />
-		<link rel="stylesheet" href="public/bower_components/bootstrap-daterangepicker/daterangepicker.css" />
-		<link rel="stylesheet" href="public/bower_components/chartist/dist/chartist.min.css" />
-		<link rel="stylesheet" href="public/bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.css" />
+		<link rel="stylesheet" href="/bower_components/jquery-ui/themes/base/jquery-ui.css" />
+		<link rel="stylesheet" href="/bower_components/bootstrap3-dialog/dist/css/bootstrap-dialog.min.css" />
+		<link rel="stylesheet" href="/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.css" />
+		<link rel="stylesheet" href="/bower_components/smalot-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" />
+		<link rel="stylesheet" href="/bower_components/bootstrap-select/dist/css/bootstrap-select.css" />
+		<link rel="stylesheet" href="/bower_components/bootstrap-table/src/bootstrap-table.css" />
+		<link rel="stylesheet" href="/bower_components/bootstrap-daterangepicker/daterangepicker.css" />
+		<link rel="stylesheet" href="/bower_components/chartist/dist/chartist.min.css" />
+		<link rel="stylesheet" href="/bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.css" />
 		<!-- endbower -->
 		<!-- start css template tags -->
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.autocomplete.css"/>
@@ -31,32 +30,32 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css"/>
 		<!-- end css template tags -->
 		<!-- bower:js -->
-		<script src="public/bower_components/jquery/dist/jquery.js"></script>
-		<script src="public/bower_components/jquery-form/jquery.form.js"></script>
-		<script src="public/bower_components/jquery-validate/dist/jquery.validate.js"></script>
-		<script src="public/bower_components/jquery-ui/jquery-ui.js"></script>
-		<script src="public/bower_components/bootstrap/dist/js/bootstrap.js"></script>
-		<script src="public/bower_components/bootstrap3-dialog/dist/js/bootstrap-dialog.min.js"></script>
-		<script src="public/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.js"></script>
-		<script src="public/bower_components/smalot-bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
-		<script src="public/bower_components/bootstrap-select/dist/js/bootstrap-select.js"></script>
-		<script src="public/bower_components/bootstrap-table/src/bootstrap-table.js"></script>
-		<script src="public/bower_components/bootstrap-table/dist/extensions/export/bootstrap-table-export.js"></script>
-		<script src="public/bower_components/bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.js"></script>
-		<script src="public/bower_components/moment/moment.js"></script>
-		<script src="public/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-		<script src="public/bower_components/file-saver.js/FileSaver.js"></script>
-		<script src="public/bower_components/html2canvas/build/html2canvas.js"></script>
-		<script src="public/bower_components/jspdf/dist/jspdf.min.js"></script>
-		<script src="public/bower_components/jspdf-autotable/dist/jspdf.plugin.autotable.js"></script>
-		<script src="public/bower_components/tableExport.jquery.plugin/tableExport.min.js"></script>
-		<script src="public/bower_components/chartist/dist/chartist.min.js"></script>
-		<script src="public/bower_components/chartist-plugin-axistitle/dist/chartist-plugin-axistitle.min.js"></script>
-		<script src="public/bower_components/chartist-plugin-pointlabels/dist/chartist-plugin-pointlabels.min.js"></script>
-		<script src="public/bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.min.js"></script>
-		<script src="public/bower_components/remarkable-bootstrap-notify/bootstrap-notify.js"></script>
-		<script src="public/bower_components/js-cookie/src/js.cookie.js"></script>
-		<script src="public/bower_components/blockUI/jquery.blockUI.js"></script>
+		<script src="/bower_components/jquery/dist/jquery.js"></script>
+		<script src="/bower_components/jquery-form/jquery.form.js"></script>
+		<script src="/bower_components/jquery-validate/dist/jquery.validate.js"></script>
+		<script src="/bower_components/jquery-ui/jquery-ui.js"></script>
+		<script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+		<script src="/bower_components/bootstrap3-dialog/dist/js/bootstrap-dialog.min.js"></script>
+		<script src="/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.js"></script>
+		<script src="/bower_components/smalot-bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+		<script src="/bower_components/bootstrap-select/dist/js/bootstrap-select.js"></script>
+		<script src="/bower_components/bootstrap-table/src/bootstrap-table.js"></script>
+		<script src="/bower_components/bootstrap-table/dist/extensions/export/bootstrap-table-export.js"></script>
+		<script src="/bower_components/bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.js"></script>
+		<script src="/bower_components/moment/moment.js"></script>
+		<script src="/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+		<script src="/bower_components/file-saver.js/FileSaver.js"></script>
+		<script src="/bower_components/html2canvas/build/html2canvas.js"></script>
+		<script src="/bower_components/jspdf/dist/jspdf.min.js"></script>
+		<script src="/bower_components/jspdf-autotable/dist/jspdf.plugin.autotable.js"></script>
+		<script src="/bower_components/tableExport.jquery.plugin/tableExport.min.js"></script>
+		<script src="/bower_components/chartist/dist/chartist.min.js"></script>
+		<script src="/bower_components/chartist-plugin-axistitle/dist/chartist-plugin-axistitle.min.js"></script>
+		<script src="/bower_components/chartist-plugin-pointlabels/dist/chartist-plugin-pointlabels.min.js"></script>
+		<script src="/bower_components/chartist-plugin-tooltip/dist/chartist-plugin-tooltip.min.js"></script>
+		<script src="/bower_components/remarkable-bootstrap-notify/bootstrap-notify.js"></script>
+		<script src="/bower_components/js-cookie/src/js.cookie.js"></script>
+		<script src="/bower_components/blockUI/jquery.blockUI.js"></script>
 		<!-- endbower -->
 		<!-- start js template tags -->
 		<script type="text/javascript" src="js/imgpreview.full.jquery.js"></script>
