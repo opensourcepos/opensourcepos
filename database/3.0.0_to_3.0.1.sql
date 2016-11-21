5231c027af4185678d307c8ed6d8147ffe13ef6a
FrancescoUK <francesco.lodolo.uk@gmail.com>
2016-11-21 22:02:44 +0000

    Revert "Support MySQL5.7 in backward compatible mode (#630)"
    This reverts commit 7ef68fbc76f0bcc10697d1d89e1b42906fa1bfc4.

--- database/3.0.0_to_3.0.1.sql
+++
@@ -1,2 +0,0 @@
--- Disable ONLY_FULL_GROUP_BY required by MySQL 5.7
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
--- database/tables.sql
+++ database/tables.sql
@@ -1,7 +1,4 @@
 
--- Disable ONLY_FULL_GROUP_BY required by MySQL 5.7
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-
 --
 -- Table structure for table `ospos_app_config`
 --
