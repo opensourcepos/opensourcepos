deb235a971003efeaf5675c352dae39c984afd16
Deep Shah <deepshah@live.com>
2016-11-23 15:59:47 +0530

    change ['<key>'] to getenv('<key>')

--- application/config/config.php
+++ application/config/config.php
@@ -358,7 +358,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = isset($_SERVER['ENCRYPTION_KEY']) ? $_SERVER['ENCRYPTION_KEY'] : '';
+$config['encryption_key'] = getenv('ENCRYPTION_KEY') ? getenv('ENCRYPTION_KEY') : '';
 
 /*
 |--------------------------------------------------------------------------
