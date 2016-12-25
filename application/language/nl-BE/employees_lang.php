34fc32a79838c99e02653bc25c2a7bba76278f7c
mitchel <mitchel@nijkamp.tech>
2016-12-25 12:56:36 +0000

    Translated using Weblate (Dutch (Belgium))
    Currently translated at 100.0% (26 of 26 strings)

--- application/language/nl-BE/employees_lang.php
+++ application/language/nl-BE/employees_lang.php
@@ -1,12 +1,13 @@
 <?php 
 
-$lang["employees_basic_information"] = "Information";
-$lang["employees_cannot_be_deleted"] = "Could not deleted selected employees, one or more of the employees has processed sales or you are trying to delete yourself :)";
+$lang["employees_basic_information"] = "Informatie";
+$lang["employees_cannot_be_deleted"] = "Kan geselecteerde medewerker niet verwijderen, de medewerker heeft een of meerdere verkopen afgehandeld of je probeerd je zelf te verwijderen :)
+Could not deleted selected employees, one or more of the employees has processed sales or you are trying to delete yourself :)";
 $lang["employees_confirm_delete"] = "Bent u zeker dat u de geselecteerde werknemers wil verwijderen?";
 $lang["employees_employee"] = "Werknemer";
-$lang["employees_error_adding_updating"] = "Error adding/updating employee";
-$lang["employees_error_deleting_demo_admin"] = "You can not delete the demo admin user";
-$lang["employees_error_updating_demo_admin"] = "You can not change the demo admin user";
+$lang["employees_error_adding_updating"] = "Fout tijden toevoegen/aanpassen medewerker";
+$lang["employees_error_deleting_demo_admin"] = "Je kan de demo admin niet verwijderen";
+$lang["employees_error_updating_demo_admin"] = "Jij kan de demo admin gebruiker niet veranderen";
 $lang["employees_login_info"] = "Login";
 $lang["employees_new"] = "N. Werknemer";
 $lang["employees_none_selected"] = "U hebt geen werknemers geselecteerd";
@@ -15,12 +16,12 @@ $lang["employees_password"] = "Paswoord";
 $lang["employees_password_minlength"] = "Paswoord moet minstens 8 characters lang zijn";
 $lang["employees_password_must_match"] = "Paswoorden komen niet overeen";
 $lang["employees_password_required"] = "Paswoord moet ingevuld worden";
-$lang["employees_permission_desc"] = "Check the boxes below to grant access to modules";
-$lang["employees_permission_info"] = "Permissions";
+$lang["employees_permission_desc"] = "Vink de vierkantjes hieronder aan om toegang te verlenen tot de modules";
+$lang["employees_permission_info"] = "Rechten";
 $lang["employees_repeat_password"] = "Herhaal Paswoord";
-$lang["employees_successful_adding"] = "You have successfully added employee";
+$lang["employees_successful_adding"] = "Je hebt met succes een medewerker toegevoegd";
 $lang["employees_successful_deleted"] = "Er werd(en)";
-$lang["employees_successful_updating"] = "You have successfully updated employee";
+$lang["employees_successful_updating"] = "Je hebt met succes de medewerker gewijzigd";
 $lang["employees_update"] = "Update Werknemer";
 $lang["employees_username"] = "Gebruikersnaam";
 $lang["employees_username_minlength"] = "Gebruikersnaam moet minstens 5 characters lang zijn";
