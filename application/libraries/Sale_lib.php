2719c43e3d41af5337adc1248cfcd52066bbd592
Steve Ireland <stevei@ruledomain.com>
2017-01-30 20:38:24 -0500

    Fix for issue where the warning messages for out of stock or below reorder level were not being sent to the user.

--- application/libraries/Sale_lib.php
+++ application/libraries/Sale_lib.php
@@ -407,7 +407,7 @@ class Sale_lib
 		{
 			$item_info = $this->CI->Item->get_info_by_id_or_number($item_id);
 
-			if ($item_info->stock_type === 0) {
+			if ($item_info->stock_type == '0') {
 				$item_quantity = $this->CI->Item_quantity->get_item_quantity($item_id, $item_location)->quantity;
 				$quantity_added = $this->get_quantity_already_added($item_id, $item_location);
 
