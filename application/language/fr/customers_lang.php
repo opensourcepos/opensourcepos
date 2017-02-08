9bf24fee5feb6e67a765fc520223fb588bc3c09a
Luc Gagnier <gagnier.luc@gmail.com>
2017-02-08 23:01:42 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (178 of 178 strings)
    
    Translated using Weblate (French)
    
    Currently translated at 100.0% (22 of 22 strings)
    
    Translated using Weblate (French)
    
    Currently translated at 98.8% (88 of 89 strings)
    
    Translated using Weblate (French)
    
    Currently translated at 86.0% (43 of 50 strings)
    
    Translated using Weblate (French)
    
    Currently translated at 99.1% (116 of 117 strings)

--- application/language/fr/customers_lang.php
+++ application/language/fr/customers_lang.php
@@ -1,9 +1,9 @@
 <?php 
 
 $lang["customers_account_number"] = "# Compte";
-$lang["customers_account_number_duplicate"] = "This account number is already present in the database";
+$lang["customers_account_number_duplicate"] = "Ce numéro de compte est déjà utilisé dans la base de données";
 $lang["customers_cannot_be_deleted"] = "Impossible de supprimer. Un ou plusiers client(s) sélectionné(s) ont des ventes.";
-$lang["customers_company_name"] = "Company";
+$lang["customers_company_name"] = "Compagnie";
 $lang["customers_confirm_delete"] = "Etes vous sûr(e) de vouloir supprimer ces clients?";
 $lang["customers_customer"] = "Client";
 $lang["customers_discount"] = "Rabais";
@@ -17,8 +17,8 @@ $lang["customers_successful_updating"] = "Édition client réussie";
 $lang["customers_taxable"] = "Imposable";
 $lang["customers_total"] = "Total";
 $lang["customers_update"] = "Éditer Client";
-$lang["customers_import_items_excel"] = "Import customers from Excel sheet";
+$lang["customers_import_items_excel"] = "Importer une liste de client à partir d'une feuille Excel";
 $lang["customers_excel_import_failed"] = "Echec Import d'Excel";
-$lang["customers_excel_import_nodata_wrongformat"] = "Your uploaded file has no data or wrong format";
-$lang["customers_excel_import_success"] = "Import of Customers successful";
-$lang["customers_excel_import_partially_failed"] = "Most Customers imported. But some were not, here is the list";
+$lang["customers_excel_import_nodata_wrongformat"] = "Le fichier envoyé ne contient aucune donnée ou elles sont dans un format erroné";
+$lang["customers_excel_import_success"] = "L'importation des clients est un succès";
+$lang["customers_excel_import_partially_failed"] = "Les clients suivants n'ont pas été importé.";
