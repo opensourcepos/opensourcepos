00f99c524a343f89465d448866e913c8ce3dee70
FrancescoUK <francesco.lodolo.uk@gmail.com>
2017-03-13 14:38:27 +0000

    Updated LICENSE, upgraded js plugin and regen dist files

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -74,7 +74,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/style.css"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=7e9664c539"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=4169189e13"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
