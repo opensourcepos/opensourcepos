e51c79c423409a05d6773b8c9b387d26b27b99fa
Aril Apria Susanto <awriel@gmail.com>
2017-04-02 09:51:57 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (8 of 8 strings)

--- application/language/id/bootstrap_tables_lang.php
+++ application/language/id/bootstrap_tables_lang.php
@@ -1,10 +1,10 @@
 <?php 
 
-$lang["tables_all"] = "All";
-$lang["tables_columns"] = "Columns";
-$lang["tables_hide_show_pagination"] = "Hide/Show pagination";
-$lang["tables_loading"] = "Loading, please wait...";
-$lang["tables_page_from_to"] = "Showing {0} to {1} of {2} rows";
-$lang["tables_refresh"] = "Refresh";
-$lang["tables_rows_per_page"] = "{0} rows per page";
-$lang["tables_toggle"] = "Toggle";
+$lang["tables_all"] = "Semua";
+$lang["tables_columns"] = "Kolom";
+$lang["tables_hide_show_pagination"] = "Sembunyi/Tampilkan paginasi";
+$lang["tables_loading"] = "Memuat, tunggu ...";
+$lang["tables_page_from_to"] = "Menampilkan {0} ke {1} dari {2} baris";
+$lang["tables_refresh"] = "Menyegarkan";
+$lang["tables_rows_per_page"] = "{0} baris per halaman";
+$lang["tables_toggle"] = "Beralih";
