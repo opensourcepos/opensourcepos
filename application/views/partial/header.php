f407702dbe116e0a089442259171077ae2868468
jekkos <jeroen.peelaerts@gmail.com>
2017-06-13 19:17:34 +0200

    Add database.php to docker image (#1297)

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -74,7 +74,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/style.css"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=406c44e716"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=d20e74bbba"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
