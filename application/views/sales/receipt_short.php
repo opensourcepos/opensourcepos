a9afce8e14edf2988cde626f9709241e998f98ee
Steve Ireland <stevei@ruledomain.com>
2017-08-12 22:04:43 -0400

    Drop price from short receipt.

--- application/views/sales/receipt_short.php
+++ application/views/sales/receipt_short.php
@@ -61,10 +61,7 @@
 		{
 		?>
 			<tr>
-				<td><?php echo ucfirst($item['name']); ?></td>
-			</tr>
-			<tr>
-				<td><?php echo to_currency($item['price']); ?></td>
+                <td><?php echo ucfirst($item['name']); ?></td>
 				<td><?php echo to_quantity_decimals($item['quantity']); ?></td>
 				<td class="total-value"><?php echo to_currency($item[($this->config->item('receipt_show_total_discount') ? 'total' : 'discounted_total')]); ?></td>
 			</tr>
