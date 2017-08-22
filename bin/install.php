69b687989db7b645a28e863d4ccaadfc48cfe823
jekkos <jeroen.peelaerts@gmail.com>
2017-08-22 09:00:26 +0200

    Fix travis build (#1458)

--- bin/install.php
+++ bin/install.php
@@ -253,7 +253,7 @@ class Installer
         
         foreach ($iterator as $file) {
             if ($file->isDir()) {
-                @mkdir($dst . '/' . $iterator->getSubPathName()), 0755);
+                @mkdir($dst . '/' . $iterator->getSubPathName(), 0755);
             } else {
 
                 $success = copy($file, $dst . '/' . $iterator->getSubPathName());
