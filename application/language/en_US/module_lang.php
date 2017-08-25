19cc328c6564339440550a5d6723bda4d49b3b5a
Weblate <noreply@weblate.org>
2017-08-25 11:57:51 +0000

    Added files since 3.1 (en_US) (#1485)

---
+++ application/language/en_US/module_lang.php
@@ -0,0 +1,29 @@
+<?php 
+
+$lang["module_config"] = "Store Config";
+$lang["module_config_desc"] = "Change the store's configuration";
+$lang["module_customers"] = "Customers";
+$lang["module_customers_desc"] = "Add, Update, Delete, and Search customers";
+$lang["module_employees"] = "Employees";
+$lang["module_employees_desc"] = "Add, Update, Delete, and Search employees";
+$lang["module_giftcards"] = "Gift Cards";
+$lang["module_giftcards_desc"] = "Add, Update, Delete and Search gift cards";
+$lang["module_home"] = "Home";
+$lang["module_item_kits"] = "Item Kits";
+$lang["module_item_kits_desc"] = "Add, Update, Delete and Search Item Kits";
+$lang["module_items"] = "Items";
+$lang["module_items_desc"] = "Add, Update, Delete, and Search items";
+$lang["module_messages"] = "Messages";
+$lang["module_messages_desc"] = "Send Messages to Customers, Suppliers, Employees et al.";
+$lang["module_migrate"] = "Migrate";
+$lang["module_migrate_desc"] = "Update the OSPOS Database";
+$lang["module_receivings"] = "Receivings";
+$lang["module_receivings_desc"] = "Process Purchase orders";
+$lang["module_reports"] = "Reports";
+$lang["module_reports_desc"] = "View and generate reports";
+$lang["module_sales"] = "Sales";
+$lang["module_sales_desc"] = "Process sales and returns";
+$lang["module_suppliers"] = "Suppliers";
+$lang["module_suppliers_desc"] = "Add, Update, Delete, and Search suppliers";
+$lang["module_taxes"] = "Taxes";
+$lang["module_taxes_desc"] = "Configure sales taxes";
