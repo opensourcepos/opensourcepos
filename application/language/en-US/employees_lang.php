c6a519e4adf95bc14c83133912a8affcd0f8f049
objecttothis <17935339+objecttothis@users.noreply.github.com>
2017-09-07 15:48:56 +0400

    #1546 Updates

--- application/language/en-US/employees_lang.php
+++ application/language/en-US/employees_lang.php
@@ -1,13 +1,13 @@
 <?php 
 
 $lang["employees_basic_information"] = "Information";
-$lang["employees_cannot_be_deleted"] = "Unable to delete selected employees, one or more of the has processed sales or you are trying to delete your account.";
+$lang["employees_cannot_be_deleted"] = "Unable to delete selected employee(s), one or more of the has processed sales or you are trying to delete your account.";
 $lang["employees_change_password"] = "Change Password";
 $lang["employees_confirm_delete"] = "Are you sure you want to delete the selected employee(s)?";
 $lang["employees_current_password"] = "Current Password";
-$lang["employees_current_password_invalid"] = "Invalid Current Password.";
+$lang["employees_current_password_invalid"] = "Current Password is invalid.";
 $lang["employees_employee"] = "Employee";
-$lang["employees_error_adding_updating"] = "Adding/updating Employee failed.";
+$lang["employees_error_adding_updating"] = "Employee add or update failed.";
 $lang["employees_error_deleting_demo_admin"] = "You can not delete the demo admin user.";
 $lang["employees_error_updating_demo_admin"] = "You can not change the demo admin user.";
 $lang["employees_login_info"] = "Login";
@@ -15,9 +15,9 @@ $lang["employees_new"] = "New Employee";
 $lang["employees_none_selected"] = "You have not selected any employee(s) to delete.";
 $lang["employees_one_or_multiple"] = "employee(s)";
 $lang["employees_password"] = "Password";
-$lang["employees_password_minlength"] = "Passwords must be at least 8 characters in length.";
+$lang["employees_password_minlength"] = "Password must be at least 8 characters in length.";
 $lang["employees_password_must_match"] = "Passwords do not match.";
-$lang["employees_password_not_must_match"] = "Current password and new password should not be the same.";
+$lang["employees_password_not_must_match"] = "Current password and new password can not be the same.";
 $lang["employees_password_required"] = "Password is required.";
 $lang["employees_permission_desc"] = "Check the boxes below to grant access to modules.";
 $lang["employees_permission_info"] = "Permissions";
