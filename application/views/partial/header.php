c01663f90151ca28966a39ec3c85aaf53b3ad373
FrancescoUK <francesco.lodolo.uk@gmail.com>
2017-09-09 16:21:31 +0100

    Fix stylesheet for Office view (#1543)

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -71,10 +71,10 @@
 		<![endif]-->
 		<!-- start mincss template tags -->
 		<link rel="stylesheet" type="text/css" href="dist/jquery-ui/jquery-ui.min.css"/>
-		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=87da435917"/>
+		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=7f52cbc5d2"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=9fa876b344"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=f6ce7fb090"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
