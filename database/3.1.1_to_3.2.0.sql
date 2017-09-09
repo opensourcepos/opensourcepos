302fb23c4d0e0e41e32a247a27fe0a25a8f637a6
objecttothis <17935339+objecttothis@users.noreply.github.com>
2017-09-09 18:19:12 +0400

    correcting size of fields

--- database/3.1.1_to_3.2.0.sql
+++ database/3.1.1_to_3.2.0.sql
@@ -1,4 +1,4 @@
 -- Add columns to save per-user language selection
 ALTER TABLE `ospos_employees` 
 	ADD COLUMN `language` VARCHAR(48) DEFAULT NULL,
-	ADD COLUMN `language_code` VARCHAR(48) DEFAULT NULL;
+	ADD COLUMN `language_code` VARCHAR(8) DEFAULT NULL;
