1ee854b0c6dea2dd5163f3ceecfd04e788b0b648
objecttothis <17935339+objecttothis@users.noreply.github.com>
2017-09-09 17:59:44 +0400

    Update get language functions to return correct language
    returns language of logged in employee or the system language if no one is logged in.  Also returns the system language in the event that the employee has no selected language.

--- application/helpers/locale_helper.php
+++ application/helpers/locale_helper.php
@@ -6,12 +6,34 @@
 
 function current_language_code()
 {
-    return get_instance()->config->item('language_code');
+//Returns the language code of the employee if set or system language code if not
+	if(get_instance()->Employee->is_logged_in())
+	{
+		//$employee_language_code = 'ru';
+		$employee_language_code = get_instance()->Employee->get_logged_in_employee_info()->language_code;
+		if($employee_language_code != NULL && $employee_language_code != '')
+			return $employee_language_code;
+		return get_instance()->config->item('language_code');
+	}
+	else
+		return get_instance()->config->item('language_code');
+
 }
 
 function current_language()
 {
-    return get_instance()->config->item('language');
+//Returns the language of the employee if set or system language if not
+	if(get_instance()->Employee->is_logged_in())
+	{
+		//$employee_language = 'russian';
+		$employee_language = get_instance()->Employee->get_logged_in_employee_info()->language;
+		if($employee_language != NULL && $employee_language != '')
+			return $employee_language;
+		return get_instance()->config->item('language');
+	}
+	else
+		return get_instance()->config->item('language');
+
 }
 
 function currency_side()
