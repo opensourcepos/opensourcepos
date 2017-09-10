63620233c0edefb4d29575cb8f9665005e6f5abc
objecttothis <17935339+objecttothis@users.noreply.github.com>
2017-09-10 16:08:46 +0400

    Temp load system lang

--- application/views/sales/receipt_short.php
+++ application/views/sales/receipt_short.php
@@ -1,3 +1,10 @@
+<?php 
+//Temporarily loads the system language for _lang to print invoice in the system language rather than user defined.
+	$this->lang->load('customers',current_language_code(TRUE));
+	$this->lang->load('sales',current_language_code(TRUE));
+	$this->lang->load('employees',current_language_code(TRUE));
+?>
+
 <div id="receipt_wrapper" style="font-size:<?php echo $this->config->item('receipt_font_size');?>px">
 	<div id="receipt_header">
 		<?php
@@ -190,3 +197,9 @@
 		<?php echo $sale_id; ?>
 	</div>
 </div>
+<?php 
+//Reloads the user _lang 
+	$this->lang->load('customers',current_language_code());
+	$this->lang->load('sales',current_language_code());
+	$this->lang->load('employees',current_language_code());
+?>
