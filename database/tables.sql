83bca7852fe6a2b17c2be1546df11c5a98dfa03d
objecttothis <17935339+objecttothis@users.noreply.github.com>
2017-09-12 12:39:28 +0400

    misspelling

--- database/tables.sql
+++ database/tables.sql
@@ -115,9 +115,7 @@ INSERT INTO `ospos_app_config` (`key`, `value`) VALUES
 ('work_order_enable', '0'),
 ('work_order_format', 'W%y{WSEQ:6}'),
 ('last_used_work_order_number', '0'),
-('allow_duplicate_barcode','');
-
-
+('allow_duplicate_barcodes','');
 
 -- --------------------------------------------------------
 
