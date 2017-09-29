f9490ba5717578ddc1cd360ac5131eeb2b47bf11
Chiraq Bookstore <objecttothis@gmail.com>
2017-09-29 07:42:53 +0000

    Translated using Weblate (az_AZ (generated))
    Currently translated at 0.0% (0 of 8 strings)

--- application/language/az-AZ/bootstrap_tables_lang.php
+++ application/language/az-AZ/bootstrap_tables_lang.php
@@ -1,10 +1,10 @@
 <?php 
 
-$lang["tables_all"] = "All";
-$lang["tables_columns"] = "Columns";
-$lang["tables_hide_show_pagination"] = "Hide/Show pagination";
-$lang["tables_loading"] = "Loading, please wait...";
-$lang["tables_page_from_to"] = "Showing {0} to {1} of {2} rows";
-$lang["tables_refresh"] = "Refresh";
-$lang["tables_rows_per_page"] = "{0} rows per page";
-$lang["tables_toggle"] = "Toggle";
+$lang["tables_all"] = "";
+$lang["tables_columns"] = "";
+$lang["tables_hide_show_pagination"] = "";
+$lang["tables_loading"] = "";
+$lang["tables_page_from_to"] = "";
+$lang["tables_refresh"] = "";
+$lang["tables_rows_per_page"] = "";
+$lang["tables_toggle"] = "";
