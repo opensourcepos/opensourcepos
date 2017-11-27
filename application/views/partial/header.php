983830ef70c6dbecc67461976fedef6d61f817d1
FrancescoUK <francesco.lodolo.uk@gmail.com>
2017-11-27 21:14:01 +0000

    Allow receipt item name to be wrapped.

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -71,10 +71,10 @@
 		<![endif]-->
 		<!-- start mincss template tags -->
 		<link rel="stylesheet" type="text/css" href="dist/jquery-ui/jquery-ui.min.css"/>
-		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=94a8b206d6"/>
+		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=1a7dd79d6d"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=47cade4b67"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=63c868fd57"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
