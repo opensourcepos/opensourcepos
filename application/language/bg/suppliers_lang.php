ce321877ff27b7f54b544357b0b7a732b7b041b3
Weblate <jeroen.peelaerts@gmail.com>
2017-12-24 18:00:56 +0000

    Translated using Weblate (Bulgarian)
    Currently translated at 100.0% (20 of 20 strings)

---
+++ application/language/bg/suppliers_lang.php
@@ -0,0 +1,18 @@
+<?php 
+
+$lang["suppliers_account_number"] = "Account Number";
+$lang["suppliers_agency_name"] = "Agency Name";
+$lang["suppliers_cannot_be_deleted"] = "Could not delete selected Supplier(s). One or more have Sales.";
+$lang["suppliers_company_name"] = "Company Name";
+$lang["suppliers_company_name_required"] = "Company Name is a required field.";
+$lang["suppliers_confirm_delete"] = "Are you sure you want to delete the selected Supplier(s)?";
+$lang["suppliers_error_adding_updating"] = "Supplier update or add failed.";
+$lang["suppliers_new"] = "New Supplier";
+$lang["suppliers_none_selected"] = "You have not selected Supplier(s) to delete.";
+$lang["suppliers_one_or_multiple"] = "Supplier(s)";
+$lang["suppliers_successful_adding"] = "You have successfully added Supplier";
+$lang["suppliers_successful_deleted"] = "You have successfully deleted";
+$lang["suppliers_successful_updating"] = "You have successfully updated Supplier";
+$lang["suppliers_supplier"] = "Supplier";
+$lang["suppliers_supplier_id"] = "Id";
+$lang["suppliers_update"] = "Update Supplier";
