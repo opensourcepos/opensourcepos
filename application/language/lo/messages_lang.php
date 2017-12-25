5f3a42592e8bac9161dcf7c30e189927f9e78190
Thin Siphachanh <tspcsystem@gmail.com>
2017-12-25 06:19:18 +0000

    Translated using Weblate (Lao)
    Currently translated at 100.0% (12 of 12 strings)

---
+++ application/language/lo/messages_lang.php
@@ -0,0 +1,14 @@
+<?php 
+
+$lang["messages_first_name"] = "ຊື່";
+$lang["messages_last_name"] = "ນາມສະກຸນ";
+$lang["messages_message"] = "ຂໍ້ຄວາມ";
+$lang["messages_message_placeholder"] = "ໃສ່ຂໍ້ຄວາມຂອງທ່ານບ່ອນນີ້...";
+$lang["messages_message_required"] = "ຂໍ້ຄວາມຈຳເປັນຕ້ອງໃສ່";
+$lang["messages_multiple_phones"] = "(ໃນກໍລະນີມີຜູ້ຮັບຫຼາຍຄົນ, ໃຫ້ໃສ່ເບີໂທຂັ້ນດ້ວຍໝາຍຈຸດ)";
+$lang["messages_phone"] = "ເບີໂທ";
+$lang["messages_phone_number_required"] = "ເບີໂທຈຳເປັນຕ້ອງໃສ່";
+$lang["messages_phone_placeholder"] = "ໃສ່ເບີໂທບ່ອນນີ້...";
+$lang["messages_sms_send"] = "ສົ່ງ SMS";
+$lang["messages_successfully_sent"] = "ສົ່ງຂໍ້ຄວາມສຳເລັດ ໄປຫາ: ";
+$lang["messages_unsuccessfully_sent"] = "ສົ່ງຂໍ້ຄວາມບໍ່ສຳເລັດ ໄປຫາ: ";
