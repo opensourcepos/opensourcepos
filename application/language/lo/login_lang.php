8e27a88d3aae9582a2c2f6a80bba5bc1e4cc8d20
Thin Siphachanh <tspcsystem@gmail.com>
2017-12-25 07:42:44 +0000

    Translated using Weblate (Lao)
    Currently translated at 100.0% (8 of 8 strings)

---
+++ application/language/lo/login_lang.php
@@ -0,0 +1,10 @@
+<?php 
+
+$lang["login_gcaptcha"] = "ຂ້ອຍບໍ່ແມ່ນເຄື່ອງຈັກ.";
+$lang["login_go"] = "Go";
+$lang["login_invalid_gcaptcha"] = "ຍັງບໍ່ໄດ້ຕິກ ຂ້ອຍບໍ່ແມ່ນເຄື່ອງຈັກ.";
+$lang["login_invalid_installation"] = "ການຕິດຕັ້ງມີຂໍ້ຜິດພາດ, ກະລຸນາກວດສອບ php.ini file.";
+$lang["login_invalid_username_and_password"] = "Username ຫຼື Password ບໍ່ຖືກຕ້ອງ.";
+$lang["login_login"] = "ເຂົ້າລະບົບ";
+$lang["login_password"] = "Password";
+$lang["login_username"] = "Username";
