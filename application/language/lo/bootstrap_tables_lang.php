56864e6919db03aeb9553ca0bfb59174ce83a2eb
Thin Siphachanh <tspcsystem@gmail.com>
2017-12-25 05:20:51 +0000

    Translated using Weblate (Lao)
    Currently translated at 100.0% (8 of 8 strings)

---
+++ application/language/lo/bootstrap_tables_lang.php
@@ -0,0 +1,10 @@
+<?php 
+
+$lang["tables_all"] = "ທັງໝົດ";
+$lang["tables_columns"] = "ຖັນ";
+$lang["tables_hide_show_pagination"] = "ເຊື່ອງ/ສະແດງເລກທີໜ້າເຈ້ຍ";
+$lang["tables_loading"] = "ກຳລັງໂຫຼດ, ກະລຸນາລໍຖ້າ...";
+$lang["tables_page_from_to"] = "ສະແດງ {0} ຫາ {1} ຂອງ {2} ແຖວ";
+$lang["tables_refresh"] = "Refresh";
+$lang["tables_rows_per_page"] = "{0} ແຖວຕໍ່ໜ້າ";
+$lang["tables_toggle"] = "Toggle";
