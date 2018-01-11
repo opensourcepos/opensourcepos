713f6106c1f401047bc2a252d349a8c92269b9cb
Weblate <jeroen.peelaerts@gmail.com>
2018-01-11 09:02:17 +0000

    Translated using Weblate (Italian)
    Currently translated at 3.0% (2 of 65 strings)

---
+++ application/language/it/taxes_lang.php
@@ -0,0 +1,31 @@
+<?php 
+
+$lang["taxes_add_exception"] = "Add Exception";
+$lang["taxes_confirm_delete"] = "Are you sure you want to delete this Tax Code? This action cannot be undone.";
+$lang["taxes_default_tax_category"] = "Default Tax Category";
+$lang["taxes_error_adding_updating"] = "Tax Code add or update failed.";
+$lang["taxes_new"] = "New Tax Code";
+$lang["taxes_no_taxes_to_display"] = "No Taxes Code available to display.";
+$lang["taxes_round_half_down"] = "Half Down";
+$lang["taxes_round_half_even"] = "Half Even";
+$lang["taxes_round_half_odd"] = "Half Odd";
+$lang["taxes_round_half_up"] = "Half Up";
+$lang["taxes_rounding_code"] = "Rounding Code";
+$lang["taxes_sales_tax"] = "Sales Tax";
+$lang["taxes_sales_tax_by_invoice"] = "Sales Tax by Invoice";
+$lang["taxes_successful_deleted"] = "You have successfully deleted";
+$lang["taxes_tax_category"] = "Tax Category";
+$lang["taxes_tax_code"] = "Tax Code";
+$lang["taxes_tax_code_cannot_be_deleted"] = "Tax Code delete failed.";
+$lang["taxes_tax_code_name"] = "Tax Code Name";
+$lang["taxes_tax_code_required"] = "Tax Code is a required field.";
+$lang["taxes_tax_code_successful_deleted"] = "You have successfully deleted Tax Code";
+$lang["taxes_tax_code_successful_updated"] = "You have successfully updated";
+$lang["taxes_tax_code_successful_updating"] = "You have successfully updated Tax Code";
+$lang["taxes_tax_code_successfully_added"] = "You have successfully added";
+$lang["taxes_tax_code_type"] = "Tax Code Type";
+$lang["taxes_tax_rate"] = "Tax Rate";
+$lang["taxes_tax_rate_numeric"] = "Tax Rate must be a number.";
+$lang["taxes_tax_rate_required"] = "Tax Rate is a required field.";
+$lang["taxes_update"] = "Update Sales Category Tax";
+$lang["taxes_vat_tax"] = "VAT Tax";
