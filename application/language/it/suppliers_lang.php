364886e39572d68a47b7ce0c67e73c600f06c519
lucailvec <luka190294@gmail.com>
2018-01-11 23:37:13 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (17 of 17 strings)

--- application/language/it/suppliers_lang.php
+++ application/language/it/suppliers_lang.php
@@ -1,18 +1,18 @@
 <?php 
 
 $lang["suppliers_account_number"] = "Account Number";
-$lang["suppliers_agency_name"] = "Agency Name";
-$lang["suppliers_cannot_be_deleted"] = "Could not delete selected Supplier(s). One or more have Sales.";
-$lang["suppliers_company_name"] = "Company Name";
-$lang["suppliers_company_name_required"] = "Company Name is a required field.";
-$lang["suppliers_confirm_delete"] = "Are you sure you want to delete the selected Supplier(s)?";
-$lang["suppliers_error_adding_updating"] = "Supplier update or add failed.";
-$lang["suppliers_new"] = "New Supplier";
-$lang["suppliers_none_selected"] = "You have not selected Supplier(s) to delete.";
-$lang["suppliers_one_or_multiple"] = "Supplier(s)";
-$lang["suppliers_successful_adding"] = "You have successfully added Supplier";
-$lang["suppliers_successful_deleted"] = "You have successfully deleted";
-$lang["suppliers_successful_updating"] = "You have successfully updated Supplier";
-$lang["suppliers_supplier"] = "Supplier";
+$lang["suppliers_agency_name"] = "Nome Agenzia";
+$lang["suppliers_cannot_be_deleted"] = "Eliminazione del fornitore/i non possibile. Uno o più hanno delle vendite.";
+$lang["suppliers_company_name"] = "Nome della ditta";
+$lang["suppliers_company_name_required"] = "Nome della ditta è un campo obbligatorio.";
+$lang["suppliers_confirm_delete"] = "Sei sicuro di voler eliminare il/i Fonritore/i selezionati?";
+$lang["suppliers_error_adding_updating"] = "Fallito aggiornamento o aggiunta Fornitore.";
+$lang["suppliers_new"] = "Nuovo Fornitore";
+$lang["suppliers_none_selected"] = "Non hai selezionato alcun Fornitore da eliminare.";
+$lang["suppliers_one_or_multiple"] = "Fornitore/i";
+$lang["suppliers_successful_adding"] = "Hai aggiunto con successo un Fornitore";
+$lang["suppliers_successful_deleted"] = "Eliminato con successo";
+$lang["suppliers_successful_updating"] = "Aggiornamento Fornitore effettuato con successo";
+$lang["suppliers_supplier"] = "Fornitore";
 $lang["suppliers_supplier_id"] = "Id";
-$lang["suppliers_update"] = "Update Supplier";
+$lang["suppliers_update"] = "Aggiornamento Fornitore";
