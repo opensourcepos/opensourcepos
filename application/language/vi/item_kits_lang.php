4457cf70555d34292fb768abbc59ef0efc1db73d
Weblate <noreply@weblate.org>
2018-01-31 07:57:49 +0000

    Add Vietnamese language

---
+++ application/language/vi/item_kits_lang.php
@@ -0,0 +1,32 @@
+<?php 
+
+$lang["item_kits_add_item"] = "Add Item";
+$lang["item_kits_all"] = "All";
+$lang["item_kits_cannot_be_deleted"] = "Item Kit(s) delete failed.";
+$lang["item_kits_confirm_delete"] = "Are you sure you want to delete the selected Item Kit(s)?";
+$lang["item_kits_confirm_restore"] = "Are you sure you want to restore selected Item Kit(s)?";
+$lang["item_kits_description"] = "Item Kit Description";
+$lang["item_kits_discount_percent"] = "Discount Percent";
+$lang["item_kits_error_adding_updating"] = "Item Kit add or update failed.";
+$lang["item_kits_find_kit_item"] = "Kit Item";
+$lang["item_kits_info"] = "Item Kit Info";
+$lang["item_kits_item"] = "Item";
+$lang["item_kits_items"] = "Items";
+$lang["item_kits_kit"] = "Kit ID";
+$lang["item_kits_kit_and_components"] = "Kit and Components";
+$lang["item_kits_kit_and_stock"] = "Kit and Stock";
+$lang["item_kits_kit_only"] = "Kit Only";
+$lang["item_kits_name"] = "Item Kit Name";
+$lang["item_kits_new"] = "New Item Kit";
+$lang["item_kits_no_item_kits_to_display"] = "No Item Kits to display.";
+$lang["item_kits_none_selected"] = "You have not selected any Item Kits.";
+$lang["item_kits_one_or_multiple"] = "Item Kit(s)";
+$lang["item_kits_price_option"] = "Price Option";
+$lang["item_kits_priced_only"] = "Priced Only";
+$lang["item_kits_print_option"] = "Print Option";
+$lang["item_kits_quantity"] = "Quantity";
+$lang["item_kits_sequence"] = "Sequence";
+$lang["item_kits_successful_adding"] = "You have successfully added Item Kit";
+$lang["item_kits_successful_deleted"] = "You have successfully deleted";
+$lang["item_kits_successful_updating"] = "You have successfully updated Item Kit";
+$lang["item_kits_update"] = "Update Item Kit";
