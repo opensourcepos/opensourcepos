241f05a000834e6855f83080f8744091566200f1
Trần Ngọc Quân <vnwildman@gmail.com>
2018-02-01 07:18:22 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (8 of 8 strings)

--- application/language/vi/login_lang.php
+++ application/language/vi/login_lang.php
@@ -1,10 +1,10 @@
 <?php 
 
-$lang["login_gcaptcha"] = "I'm not a robot.";
-$lang["login_go"] = "Go";
-$lang["login_invalid_gcaptcha"] = "Invalid I'm not a robot.";
-$lang["login_invalid_installation"] = "The installation is not correct, check your php.ini file.";
-$lang["login_invalid_username_and_password"] = "Invalid Username or Password.";
-$lang["login_login"] = "Login";
-$lang["login_password"] = "Password";
-$lang["login_username"] = "Username";
+$lang["login_gcaptcha"] = "Tôi không phải là robot.";
+$lang["login_go"] = "Đăng nhập";
+$lang["login_invalid_gcaptcha"] = "không hợp lệ Tôi không phải là robot.";
+$lang["login_invalid_installation"] = "Phần cài đặt chưa đúng, kiểm tra tập tin php.ini của bạn.";
+$lang["login_invalid_username_and_password"] = "Tài khoản hoặc mật khẩu không hợp lệ.";
+$lang["login_login"] = "Đăng nhập";
+$lang["login_password"] = "Mật khẩu";
+$lang["login_username"] = "Tài khoản";
