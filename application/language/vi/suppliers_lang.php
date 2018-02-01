da64b67e26b0b5f4895d75d37be5a6b740491f39
Trần Ngọc Quân <vnwildman@gmail.com>
2018-02-01 07:03:14 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (17 of 17 strings)

--- application/language/vi/suppliers_lang.php
+++ application/language/vi/suppliers_lang.php
@@ -1,19 +1,19 @@
 <?php 
 
-$lang["suppliers_account_number"] = "Account Number";
-$lang["suppliers_agency_name"] = "Agency Name";
-$lang["suppliers_cannot_be_deleted"] = "Could not delete selected Supplier(s). One or more have Sales.";
-$lang["suppliers_company_name"] = "Company Name";
-$lang["suppliers_company_name_required"] = "Company Name is a required field.";
-$lang["suppliers_confirm_delete"] = "Are you sure you want to delete the selected Supplier(s)?";
-$lang["suppliers_confirm_restore"] = "Are you sure you want to restore selected Supplier(s)?";
-$lang["suppliers_error_adding_updating"] = "Supplier update or add failed.";
-$lang["suppliers_new"] = "New Supplier";
-$lang["suppliers_none_selected"] = "You have not selected Supplier(s) to delete.";
-$lang["suppliers_one_or_multiple"] = "Supplier(s)";
-$lang["suppliers_successful_adding"] = "You have successfully added Supplier";
-$lang["suppliers_successful_deleted"] = "You have successfully deleted";
-$lang["suppliers_successful_updating"] = "You have successfully updated Supplier";
-$lang["suppliers_supplier"] = "Supplier";
-$lang["suppliers_supplier_id"] = "Id";
-$lang["suppliers_update"] = "Update Supplier";
+$lang["suppliers_account_number"] = "Số hiệu tài khoản";
+$lang["suppliers_agency_name"] = "Tên đại lý";
+$lang["suppliers_cannot_be_deleted"] = "Không thể xóa các Nhà cung cấp đã chọn. Một hay nhiều có Bán hàng.";
+$lang["suppliers_company_name"] = "Tên công ty";
+$lang["suppliers_company_name_required"] = "Trường tên công ty là bắt buộc.";
+$lang["suppliers_confirm_delete"] = "Bạn chắc chắn muốn xóa các Nhà cung cấp được chọn không?";
+$lang["suppliers_confirm_restore"] = "Bạn chắc chắn muốn hoàn lại các Nhà cung cấp được chọn không?";
+$lang["suppliers_error_adding_updating"] = "Gặp lỗi khi cập nhật hay thêm Nhà cung cấp";
+$lang["suppliers_new"] = "Nhà cung cấp mới";
+$lang["suppliers_none_selected"] = "Bạn chưa chọn Nhà cung cấp để xóa.";
+$lang["suppliers_one_or_multiple"] = "Nhà cung cấp";
+$lang["suppliers_successful_adding"] = "Bạn đã thêm Nhà cung cấp thành công";
+$lang["suppliers_successful_deleted"] = "Bạn đã xóa thành công";
+$lang["suppliers_successful_updating"] = "Bạn đã cập nhật Nhà cung cấp thành công";
+$lang["suppliers_supplier"] = "Nhà cung cấp";
+$lang["suppliers_supplier_id"] = "Mã số";
+$lang["suppliers_update"] = "Cập nhật Nhà cung cấp";
