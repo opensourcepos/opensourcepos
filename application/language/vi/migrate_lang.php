5f12d8ade38843df5c79c6295234682f22f91fa7
Trần Ngọc Quân <vnwildman@gmail.com>
2018-02-01 07:08:48 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: opensourcepos/migrate
    Translate-URL: http://*/projects/ospos/migrate/vi/

--- application/language/vi/migrate_lang.php
+++ application/language/vi/migrate_lang.php
@@ -1,7 +1,7 @@
 <?php 
 
-$lang["migrate_backup"] = "Be sure to back up your database before applying any database upgrades.";
-$lang["migrate_failed"] = "Migration failed.";
-$lang["migrate_info"] = "Click on Start Migration only if you are ready to apply all database changes and data upgrades to your database.";
-$lang["migrate_start"] = "Start Migration ";
-$lang["migrate_success"] = "Migrate successful.";
+$lang["migrate_backup"] = "Hãy đảm bảo rằng bạn đã sao lưu dự phòng cơ sở dữ liệu trước khi áp dụng các nâng cấp cơ sở dữ liệu.";
+$lang["migrate_failed"] = "Nâng cấp gặp lỗi.";
+$lang["migrate_info"] = "Chỉ bấm vào Bắt đầu nâng cấp nếu bạn đã áp dụng mọi thay đổi và dữ liệu của cơ sở dữ liệu vào cơ sở dữ liệu của bạn.";
+$lang["migrate_start"] = "Bắt đầu Nâng cấp";
+$lang["migrate_success"] = "Nâng cấp thành công.";
