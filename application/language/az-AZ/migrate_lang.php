f0bdb84422068e2bb15851d4623f4a2d603d89f6
Chiraq Bookstore <info@chiraqbookstore.com>
2018-02-08 10:59:36 +0000

    Translated using Weblate (az_AZ (generated))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: opensourcepos/migrate
    Translate-URL: http://*/projects/ospos/migrate/az_AZ/

--- application/language/az-AZ/migrate_lang.php
+++ application/language/az-AZ/migrate_lang.php
@@ -1,7 +1,7 @@
 <?php 
 
-$lang["migrate_backup"] = "Be sure to back up your database before applying any database upgrades.";
-$lang["migrate_failed"] = "The migration did not complete.";
-$lang["migrate_info"] = "Click on Start Migration only if you are ready to apply all database changes and data upgrades to your database";
+$lang["migrate_backup"] = "Hər hansı bir məlumat bazasını yeniləmədən öncə əmin olun ki məlumat bazası yaddaşda saxlanılmışdır.";
+$lang["migrate_failed"] = "Yer dəyişmə uğursuz alındı";
+$lang["migrate_info"] = "Yalnız hazır olduqdan sonra bütun məlumat bazasına dəyişikliklər və məlumat bazasını yeniləmək tətbiqi üçün Köçurməyə Başla düyməsinə basın.";
 $lang["migrate_start"] = "Start Migration ";
-$lang["migrate_success"] = "Migration completed successfully";
+$lang["migrate_success"] = "Köçurülmə Uğurlu Alındı";
