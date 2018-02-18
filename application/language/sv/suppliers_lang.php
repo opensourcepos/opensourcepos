b1bee3196aeb338d667212a62fff8f1256f9d069
Mats Pålsson <caston@caston.mine.nu>
2018-02-18 00:09:04 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (17 of 17 strings)

--- application/language/sv/suppliers_lang.php
+++ application/language/sv/suppliers_lang.php
@@ -1,19 +1,19 @@
 <?php 
 
-$lang["suppliers_account_number"] = "";
-$lang["suppliers_agency_name"] = "";
-$lang["suppliers_cannot_be_deleted"] = "";
-$lang["suppliers_company_name"] = "";
-$lang["suppliers_company_name_required"] = "";
-$lang["suppliers_confirm_delete"] = "";
-$lang["suppliers_confirm_restore"] = "";
-$lang["suppliers_error_adding_updating"] = "";
-$lang["suppliers_new"] = "";
-$lang["suppliers_none_selected"] = "";
-$lang["suppliers_one_or_multiple"] = "";
-$lang["suppliers_successful_adding"] = "";
-$lang["suppliers_successful_deleted"] = "";
-$lang["suppliers_successful_updating"] = "";
-$lang["suppliers_supplier"] = "";
-$lang["suppliers_supplier_id"] = "";
-$lang["suppliers_update"] = "";
+$lang["suppliers_account_number"] = "Kontonummer";
+$lang["suppliers_agency_name"] = "Byråns namn";
+$lang["suppliers_cannot_be_deleted"] = "Kunde inte radera valda leverantör (er). En eller flera har försäljning.";
+$lang["suppliers_company_name"] = "Företagsnamn";
+$lang["suppliers_company_name_required"] = "Företagsnamn är ett obligatoriskt fält.";
+$lang["suppliers_confirm_delete"] = "Är du säker på att du vill radera den valda Leverantören?";
+$lang["suppliers_confirm_restore"] = "Är du säker på att du vill radera den valda Leverantören?";
+$lang["suppliers_error_adding_updating"] = "Leverantörsuppdatering eller tillägg misslyckades.";
+$lang["suppliers_new"] = "Ny Leverantör";
+$lang["suppliers_none_selected"] = "Du har inte valt leverantör (er) att radera.";
+$lang["suppliers_one_or_multiple"] = "Leverantörer";
+$lang["suppliers_successful_adding"] = "Du har lagt till Leverantör";
+$lang["suppliers_successful_deleted"] = "Du har lagt till leverantör";
+$lang["suppliers_successful_updating"] = "Du har lagt till leverantör";
+$lang["suppliers_supplier"] = "Leverantör";
+$lang["suppliers_supplier_id"] = "Id";
+$lang["suppliers_update"] = "Uppdatera leverantör";
