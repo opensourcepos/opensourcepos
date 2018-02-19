ae6e08298384381ec1e20cca68339d5043ccb043
ratana chhun <ratana1987@gmail.com>
2018-02-19 03:17:21 +0000

    Translated using Weblate (Central Khmer)
    Currently translated at 100.0% (8 of 8 strings)

--- application/language/km/login_lang.php
+++ application/language/km/login_lang.php
@@ -1,10 +1,10 @@
 <?php 
 
-$lang["login_gcaptcha"] = "";
-$lang["login_go"] = "";
-$lang["login_invalid_gcaptcha"] = "";
-$lang["login_invalid_installation"] = "";
-$lang["login_invalid_username_and_password"] = "";
-$lang["login_login"] = "";
-$lang["login_password"] = "";
-$lang["login_username"] = "";
+$lang["login_gcaptcha"] = "ខ្ញុំមិនមែនជាម៉ាស៊ីនទេ។";
+$lang["login_go"] = "ចាប់ផ្ដើម";
+$lang["login_invalid_gcaptcha"] = "មិនត្រឹមត្រូវខ្ញុំមិនមែនជាម៉ាស៊ីនទេ។";
+$lang["login_invalid_installation"] = "ការបញ្ចូលមិនត្រឹមត្រូវ, សូមពិនិត្យមើលឯកសារ php.ini របស់អ្នក។";
+$lang["login_invalid_username_and_password"] = "ឈ្មោះឬក៏ពាក្យសំងាត់មិនត្រឹមត្រូវ។";
+$lang["login_login"] = "ចូល";
+$lang["login_password"] = "ពាក្យសំងាត់";
+$lang["login_username"] = "ឈ្មោះ";
