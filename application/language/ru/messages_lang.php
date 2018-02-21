5f487fbf970882b83b17721628946b83e31c1f80
nerv55 <26050096+nerv55@users.noreply.github.com>
2018-02-21 17:19:15 -0300

    Update messages_lang.php

--- application/language/ru/messages_lang.php
+++ application/language/ru/messages_lang.php
@@ -1,14 +1,14 @@
 <?php 
 
-$lang["messages_first_name"] = "";
-$lang["messages_last_name"] = "";
-$lang["messages_message"] = "";
-$lang["messages_message_placeholder"] = "";
-$lang["messages_message_required"] = "";
-$lang["messages_multiple_phones"] = "";
-$lang["messages_phone"] = "";
-$lang["messages_phone_number_required"] = "";
-$lang["messages_phone_placeholder"] = "";
-$lang["messages_sms_send"] = "";
-$lang["messages_successfully_sent"] = "";
-$lang["messages_unsuccessfully_sent"] = "";
+$lang["messages_first_name"] = "имя";
+$lang["messages_last_name"] = "фамилия";
+$lang["messages_message"] = "сообщение";
+$lang["messages_message_placeholder"] = "заполнитель";
+$lang["messages_message_required"] = "требуется сообщение";
+$lang["messages_multiple_phones"] = "несколько телефонов";
+$lang["messages_phone"] = "телефон";
+$lang["messages_phone_number_required"] = "требуется номер телефона";
+$lang["messages_phone_placeholder"] = "заполнитель телефона";
+$lang["messages_sms_send"] = "смс отправить";
+$lang["messages_successfully_sent"] = "успешно отправлено";
+$lang["messages_unsuccessfully_sent"] = "неудачно отправлен";
