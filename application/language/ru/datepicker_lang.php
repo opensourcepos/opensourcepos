d99fee5cb86ec508346157694e788ffef49ad202
nerv55 <26050096+nerv55@users.noreply.github.com>
2018-02-21 10:34:54 -0300

    Update datepicker_lang.php

--- application/language/ru/datepicker_lang.php
+++ application/language/ru/datepicker_lang.php
@@ -1,22 +1,22 @@
 <?php 
 
 $lang["datepicker_all_time"] = "за все время";
-$lang["datepicker_apply"] = "";
-$lang["datepicker_cancel"] = "";
-$lang["datepicker_custom"] = "";
-$lang["datepicker_from"] = "";
+$lang["datepicker_apply"] = "применить";
+$lang["datepicker_cancel"] = "отменa";
+$lang["datepicker_custom"] = "подходит";
+$lang["datepicker_from"] = "от";
 $lang["datepicker_last_30"] = "Последние 30 дней";
 $lang["datepicker_last_7"] = "Последние 7 дней";
-$lang["datepicker_last_financial_year"] = "";
+$lang["datepicker_last_financial_year"] = "прошлый финансовый год";
 $lang["datepicker_last_month"] = "Последний месяц";
 $lang["datepicker_last_year"] = "в прошлом году";
-$lang["datepicker_same_month_last_year"] = "";
-$lang["datepicker_same_month_to_same_day_last_year"] = "";
-$lang["datepicker_this_financial_year"] = "";
+$lang["datepicker_same_month_last_year"] = "в том же месяце прошлого года";
+$lang["datepicker_same_month_to_same_day_last_year"] = "в том же месяце в тот же день в прошлом году";
+$lang["datepicker_this_financial_year"] = "в этом финансовом году";
 $lang["datepicker_this_month"] = "В этом месяце";
 $lang["datepicker_this_year"] = "В этом году";
-$lang["datepicker_to"] = "";
+$lang["datepicker_to"] = "к";
 $lang["datepicker_today"] = "сегодня";
-$lang["datepicker_today_last_year"] = "";
+$lang["datepicker_today_last_year"] = "сегодня в прошлом году";
 $lang["datepicker_weekstart"] = "0";
 $lang["datepicker_yesterday"] = "вчера";
