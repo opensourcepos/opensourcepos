06700b37a0175096355f8d481cd0baf9c25c8732
nerv55 <26050096+nerv55@users.noreply.github.com>
2018-02-21 16:51:23 -0300

    Update form_validation_lang.php

--- application/language/ru/form_validation_lang.php
+++ application/language/ru/form_validation_lang.php
@@ -1,18 +1,18 @@
 <?php 
 
-$lang["alpha_dash"] = "";
-$lang["alpha_numeric"] = "";
-$lang["exact_length"] = "";
-$lang["greater_than"] = "";
-$lang["is_natural"] = "";
-$lang["is_natural_no_zero"] = "";
-$lang["is_numeric"] = "";
-$lang["is_unique"] = "";
-$lang["less_than"] = "";
-$lang["max_length"] = "";
-$lang["min_length"] = "";
-$lang["regex_match"] = "";
-$lang["valid_email"] = "";
-$lang["valid_emails"] = "";
-$lang["valid_ip"] = "";
-$lang["valid_url"] = "";
+$lang["alpha_dash"] = "Альфа-тире";
+$lang["alpha_numeric"] = "буквенно-цифровой";
+$lang["exact_length"] = "точная длина";
+$lang["greater_than"] = "больше";
+$lang["is_natural"] = "натуральное";
+$lang["is_natural_no_zero"] = "натуральные числа без нуля";
+$lang["is_numeric"] = "числовое";
+$lang["is_unique"] = "уникальный";
+$lang["less_than"] = "менее";
+$lang["max_length"] = "максимальная длина";
+$lang["min_length"] = "минимальная длина";
+$lang["regex_match"] = "матч регулярного выражения";
+$lang["valid_email"] = "действительный адрес электронной почты";
+$lang["valid_emails"] = "действительные электронные почты";
+$lang["valid_ip"] = "допустимый ip";
+$lang["valid_url"] = "допустимый url";
