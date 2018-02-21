7a65846e2e246c0e993305e39379f303db5d69a7
nerv55 <26050096+nerv55@users.noreply.github.com>
2018-02-21 17:15:39 -0300

    Update login_lang.php

--- application/language/ru/login_lang.php
+++ application/language/ru/login_lang.php
@@ -1,9 +1,9 @@
 <?php 
 
-$lang["login_gcaptcha"] = "";
+$lang["login_gcaptcha"] = "капчат";
 $lang["login_go"] = "ходите";
-$lang["login_invalid_gcaptcha"] = "";
-$lang["login_invalid_installation"] = "";
+$lang["login_invalid_gcaptcha"] = "Неправелная капчат";
+$lang["login_invalid_installation"] = "Неправелная установка";
 $lang["login_invalid_username_and_password"] = "Неправелная Имя/Пароль";
 $lang["login_login"] = "Вход";
 $lang["login_password"] = "Пароль";
