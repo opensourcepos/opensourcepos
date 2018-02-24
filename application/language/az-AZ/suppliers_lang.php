cf6df1e99c342d0a2228d2b53985f3a36a007fb3
Chiraq Bookstore <info@chiraqbookstore.com>
2018-02-24 13:24:08 +0000

    Translated using Weblate (az_AZ (generated))
    Currently translated at 100.0% (17 of 17 strings)

--- application/language/az-AZ/suppliers_lang.php
+++ application/language/az-AZ/suppliers_lang.php
@@ -1,12 +1,12 @@
 <?php 
 
 $lang["suppliers_account_number"] = "Hesab #";
-$lang["suppliers_agency_name"] = "";
+$lang["suppliers_agency_name"] = "Agentliyin Adı";
 $lang["suppliers_cannot_be_deleted"] = "təchizatçı silmək olmaz onlar bəzisi satılıb.";
 $lang["suppliers_company_name"] = "Kompaniyanın Adı";
 $lang["suppliers_company_name_required"] = "Kompaniyanın adı və mütləq məsafə.";
 $lang["suppliers_confirm_delete"] = "Siz əminsiniz ki seçilmiş təchizatçıları silmək istəyirsiniz?";
-$lang["suppliers_confirm_restore"] = "";
+$lang["suppliers_confirm_restore"] = "Seçilmiş təchizatçıları bərpa etmək istədiyinizə əminsinizmi?";
 $lang["suppliers_error_adding_updating"] = "Təchizatçı əlavə etməkdə və yeniləməkdə səhv var.";
 $lang["suppliers_new"] = "Yeni Təchizatçı";
 $lang["suppliers_none_selected"] = "Siz silmək üçün heç bir təchizatçı seçmədiniz.";
@@ -15,5 +15,5 @@ $lang["suppliers_successful_adding"] = "Siz uğurla təchizatçı əlavə etdini
 $lang["suppliers_successful_deleted"] = "Siz uğurla sildiniz";
 $lang["suppliers_successful_updating"] = "siz ugurla təchizatçı yenilədiniz";
 $lang["suppliers_supplier"] = "Təchizatçı";
-$lang["suppliers_supplier_id"] = "";
+$lang["suppliers_supplier_id"] = "Kimlik";
 $lang["suppliers_update"] = "Təchizatçı Yenilənməsi";
