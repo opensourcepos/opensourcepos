f544b772d8c245b94d5c812b769fd4b6e63517a1
WebShells <Admin@WShells.ws>
2018-02-27 01:30:09 +0200

    General ar Language fixes

--- application/language/ar-EG/migrate_lang.php
+++ application/language/ar-EG/migrate_lang.php
@@ -1,7 +1,7 @@
-<?php 
+﻿<?php 
 
-$lang["migrate_backup"] = "";
-$lang["migrate_failed"] = "";
-$lang["migrate_info"] = "";
-$lang["migrate_start"] = "";
-$lang["migrate_success"] = "";
+$lang["migrate_backup"] = "النسخ الاحتياطي";
+$lang["migrate_failed"] = "عملية النسخ الاحتياطي لم تتم بنجاح";
+$lang["migrate_info"] = "معلومات حول النسخ الاحتياطي";
+$lang["migrate_start"] = "بدء النسخ الاحتياطي";
+$lang["migrate_success"] = "عملية النسخ الاحتياطي تمت بنجاح";
