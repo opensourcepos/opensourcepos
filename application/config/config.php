e07bfbf483105a26dc8877f7b573664cfff4dc3e
ElGranLoky <carvajal.diego@gmail.com>
2018-03-02 13:31:52 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -65,9 +65,15 @@ $config['db_log_enabled'] = FALSE;
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
-$config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+if (isset($_ENV['FORCE_HTTPS'])) {
+  $config['base_url'] = 'https';
+  $config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
+  $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+} else {
+  $config['base_url'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+  $config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
+  $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+}
 
 /*
 |--------------------------------------------------------------------------
