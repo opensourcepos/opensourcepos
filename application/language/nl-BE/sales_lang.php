e89f698dffa35690d20fc92625d28e8e7433a48a
Weblate <jeroen.peelaerts@gmail.com>
2018-03-20 12:04:05 +0000

    Translated using Weblate (Flemish)
    Currently translated at 72.8% (118 of 162 strings)

--- application/language/nl-BE/sales_lang.php
+++ application/language/nl-BE/sales_lang.php
@@ -1,6 +1,6 @@
 <?php 
 
-$lang["customers_available_points"] = "";
+$lang["customers_available_points"] = "Beschikbare Punten";
 $lang["rewards_package"] = "";
 $lang["rewards_remaining_balance"] = "";
 $lang["sales_add_payment"] = "Betaal";
