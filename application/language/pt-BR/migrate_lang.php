7884bab2ed16294ca3365069af8abb15911226fc
Xyko Arteiro <webmaster@xyko.com.br>
2018-03-22 12:54:49 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: opensourcepos/migrate
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/migrate/pt_BR/

--- application/language/pt-BR/migrate_lang.php
+++ application/language/pt-BR/migrate_lang.php
@@ -1,7 +1,7 @@
 <?php 
 
-$lang["migrate_backup"] = "";
-$lang["migrate_failed"] = "";
-$lang["migrate_info"] = "";
-$lang["migrate_start"] = "";
-$lang["migrate_success"] = "";
+$lang["migrate_backup"] = "Faça backup do banco de dados antes de aplicar qualquer atualização de banco de dados.";
+$lang["migrate_failed"] = "A migração falhou.";
+$lang["migrate_info"] = "Clique em Iniciar Migração somente se estiver pronto para aplicar todas as alterações do banco de dados e atualizações de dados ao seu banco de dados.";
+$lang["migrate_start"] = "Iniciar Migração ";
+$lang["migrate_success"] = "Migrar com sucesso.";
