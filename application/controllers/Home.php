4f2b6badd9924917686640d24754df7a400592dd
FrancescoUK <francesco.lodolo.uk@gmail.com>
2018-03-24 21:47:10 +0000

    Allow password change with no restriction (#1894)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,5 +18,20 @@ class Home extends Secure_Controller
 	{
 		$this->Employee->logout();
 	}
+
+	/*
+	Loads the change password form
+	*/
+	public function change_password($employee_id = -1)
+	{
+		$person_info = $this->Employee->get_info($employee_id);
+		foreach(get_object_vars($person_info) as $property => $value)
+		{
+			$person_info->$property = $this->xss_clean($value);
+		}
+		$data['person_info'] = $person_info;
+
+		$this->load->view('employees/form_change_password', $data);
+	}
 }
 ?>
