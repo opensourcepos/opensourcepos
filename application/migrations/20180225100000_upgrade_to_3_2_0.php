fa66646b8d4220062a26dbb1ecc1b90fdc25eeb5
jekkos <jeroen.peelaerts@gmail.com>
2018-04-01 16:24:10 +0200

    Migration refactor: move out common code (#1440)

--- application/migrations/20180225100000_upgrade_to_3_2_0.php
+++ application/migrations/20180225100000_upgrade_to_3_2_0.php
@@ -9,32 +9,7 @@ class Migration_Upgrade_To_3_2_0 extends CI_Migration
 
 	public function up()
 	{
-		error_log('Migrating to 3.2.0');
-
-		$sql = file_get_contents(APPPATH . 'migrations/sqlscripts/3.1.1_to_3.2.0.sql');
-
-		/*
-		CI migration only allows you to run one statement at a time.
-		This small script splits the statements allowing you to run them all in one go.
-		*/
-
-		$sqls = explode(';', $sql);
-		array_pop($sqls);
-
-		foreach($sqls as $statement)
-		{
-			$statement = $statement . ';';
-
-			if(!$this->db->simple_query($statement))
-			{
-				foreach($this->db->error() as $error)
-				{
-					error_log('error: ' . $error);
-				}
-			}
-		}
-
-		error_log('Migrated to 3.2.0');
+		execute_script(APPPATH . 'migrations/sqlscripts/3.1.1_to_3.2.0.sql');
 	}
 
 	public function down()
