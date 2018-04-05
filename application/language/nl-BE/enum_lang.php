3113945f0f3fd30a11902aaf7e547a558c75fca8
Weblate <jeroen.peelaerts@gmail.com>
2018-04-05 20:24:39 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/nl_BE/

--- application/language/nl-BE/enum_lang.php
+++ application/language/nl-BE/enum_lang.php
@@ -1,9 +1,9 @@
 <?php 
 
-$lang["enum_half_down"] = "";
-$lang["enum_half_even"] = "";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+$lang["enum_half_down"] = "Half Omlaag";
+$lang["enum_half_even"] = "Half Even";
+$lang["enum_half_five"] = "Half Vijf";
+$lang["enum_half_odd"] = "Half Oneven";
+$lang["enum_half_up"] = "Half Boven";
+$lang["enum_round_down"] = "Naar Beneden";
+$lang["enum_round_up"] = "Naar Boven";
