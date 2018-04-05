8a9e0f352a17d2ba65118ff4eabafc3688a7004c
FrancescoUK <francesco.lodolo.uk@gmail.com>
2018-04-05 18:46:44 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2 of 2 strings)

--- application/language/it/error_lang.php
+++ application/language/it/error_lang.php
@@ -1,4 +1,4 @@
 <?php 
 
 $lang["error_no_permission_module"] = "Non disponi dei permessi per accedere al seguente modulo";
-$lang["error_unknown"] = "sconosciuto";
+$lang["error_unknown"] = "Errore sconosciuto";
