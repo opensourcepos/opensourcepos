3852bd5c3eecda6aa859f92caa1459abdac21b5f
WebShells <Admin@wshells.ws>
2018-04-11 20:02:42 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/ar_EG/

--- application/language/ar-EG/enum_lang.php
+++ application/language/ar-EG/enum_lang.php
@@ -1,9 +1,9 @@
 <?php 
 
-$lang["enum_half_down"] = "";
-$lang["enum_half_even"] = "";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+$lang["enum_half_down"] = "تقريب نصفي";
+$lang["enum_half_even"] = "نصف مزدوج";
+$lang["enum_half_five"] = "نصف الخمس";
+$lang["enum_half_odd"] = "نصف فردي";
+$lang["enum_half_up"] = "تقريب نصفي";
+$lang["enum_round_down"] = "التقريب";
+$lang["enum_round_up"] = "التقريب";
