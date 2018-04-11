0415cb53e9621a4e9d2ef052a3d45e414f559bad
Leonardo Salazar <leonardo.a.salazar@gmail.com>
2018-04-11 16:53:55 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (30 of 30 strings)

--- application/language/ru/item_kits_lang.php
+++ application/language/ru/item_kits_lang.php
@@ -2,12 +2,12 @@
 
 $lang["item_kits_add_item"] = "Добавить товар";
 $lang["item_kits_all"] = "Все";
-$lang["item_kits_cannot_be_deleted"] = "Не могу удалить Товара комплект (ы)";
-$lang["item_kits_confirm_delete"] = "Вы уверены, что хотите удалить выбранные товар? комплекты";
+$lang["item_kits_cannot_be_deleted"] = "Не могу удалить Товара комплект (ы).";
+$lang["item_kits_confirm_delete"] = "Вы уверены, что хотите удалить выбранные товар комплекты?";
 $lang["item_kits_confirm_restore"] = "Вы уверены, что хотите восстановить выбранные комплекты?";
 $lang["item_kits_description"] = "Описание товара Комплект";
 $lang["item_kits_discount_percent"] = "Процент Скидки";
-$lang["item_kits_error_adding_updating"] = "Ошибка при добавлении/обновлении товара комплекта";
+$lang["item_kits_error_adding_updating"] = "Ошибка при добавлении/обновлении товара комплекта.";
 $lang["item_kits_find_kit_item"] = "Деталь Набора";
 $lang["item_kits_info"] = "Информация о товаре комплекта";
 $lang["item_kits_item"] = "товар";
@@ -18,8 +18,8 @@ $lang["item_kits_kit_and_stock"] = "Набор и запас";
 $lang["item_kits_kit_only"] = "Только Комплект";
 $lang["item_kits_name"] = "Наименование товара комплект";
 $lang["item_kits_new"] = "Новый товар комплект";
-$lang["item_kits_no_item_kits_to_display"] = "Нет товара комплекты для отображения";
-$lang["item_kits_none_selected"] = "Вы не выбрали ни одной товар комплекты";
+$lang["item_kits_no_item_kits_to_display"] = "Нет товара комплекты для отображения.";
+$lang["item_kits_none_selected"] = "Вы не выбрали ни одной товар комплекты.";
 $lang["item_kits_one_or_multiple"] = "Товара комплект (ы)";
 $lang["item_kits_price_option"] = "Цена Опциона";
 $lang["item_kits_priced_only"] = "Только По Цене";
