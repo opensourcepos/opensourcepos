9370d3434adda8ea286a71613222ee293ab792b9
Leonardo Salazar <leonardo.a.salazar@gmail.com>
2018-04-11 15:12:35 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/ru/

--- application/language/ru/expenses_categories_lang.php
+++ application/language/ru/expenses_categories_lang.php
@@ -1,20 +1,20 @@
 <?php 
 
-$lang["category_name_required"] = "";
-$lang["expenses_categories_add_item"] = "";
-$lang["expenses_categories_cannot_be_deleted"] = "";
+$lang["category_name_required"] = "Требуется Название категории расходов";
+$lang["expenses_categories_add_item"] = "Добавить Категорию";
+$lang["expenses_categories_cannot_be_deleted"] = "Не удалось удалить категорию расходов";
 $lang["expenses_categories_category_id"] = "ИД";
-$lang["expenses_categories_confirm_delete"] = "";
-$lang["expenses_categories_description"] = "";
-$lang["expenses_categories_error_adding_updating"] = "";
-$lang["expenses_categories_info"] = "";
-$lang["expenses_categories_name"] = "";
-$lang["expenses_categories_new"] = "";
-$lang["expenses_categories_no_expenses_categories_to_display"] = "";
-$lang["expenses_categories_none_selected"] = "";
-$lang["expenses_categories_one_or_multiple"] = "";
-$lang["expenses_categories_quantity"] = "";
-$lang["expenses_categories_successful_adding"] = "";
-$lang["expenses_categories_successful_deleted"] = "";
-$lang["expenses_categories_successful_updating"] = "";
-$lang["expenses_categories_update"] = "";
+$lang["expenses_categories_confirm_delete"] = "Вы уверены, что хотите удалить выбранную категорию расходов?";
+$lang["expenses_categories_description"] = "описание категории";
+$lang["expenses_categories_error_adding_updating"] = "Ошибка добавления / обновления категории расходов";
+$lang["expenses_categories_info"] = "Категория Информация О Расходах";
+$lang["expenses_categories_name"] = "название категории";
+$lang["expenses_categories_new"] = "новая категория";
+$lang["expenses_categories_no_expenses_categories_to_display"] = "Нет категории для отображения";
+$lang["expenses_categories_none_selected"] = "Вы не выбрали ни одной категории расходов";
+$lang["expenses_categories_one_or_multiple"] = "Категория Расходов";
+$lang["expenses_categories_quantity"] = "Количество";
+$lang["expenses_categories_successful_adding"] = "Категория расходов  добавлена успешно";
+$lang["expenses_categories_successful_deleted"] = "Категория расходов успешно удалена";
+$lang["expenses_categories_successful_updating"] = "Категория расходов успешно обновлена";
+$lang["expenses_categories_update"] = "Обновить Категорию";
