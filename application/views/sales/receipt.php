9d149ad4f393c0b08a884479e1ece3f97aa6332f
FrancescoUK <francesco.lodolo.uk@gmail.com>
2018-05-01 20:46:18 +0100

    Autoreturn to Sale page after receipt printing (#1966)

--- application/views/sales/receipt.php
+++ application/views/sales/receipt.php
@@ -9,34 +9,33 @@ if (isset($error_message))
 ?>
 
 <?php if(!empty($customer_email)): ?>
-<script type="text/javascript">
-$(document).ready(function()
-{
-	var send_email = function()
+	<script type="text/javascript">
+	$(document).ready(function()
 	{
-		$.get('<?php echo site_url() . "/sales/send_receipt/" . $sale_id_num; ?>',
-			function(response)
-			{
-				$.notify(response.message, { type: response.success ? 'success' : 'danger'} );
-			}, 'json'
-		);
-	};
+		var send_email = function()
+		{
+			$.get('<?php echo site_url() . "/sales/send_receipt/" . $sale_id_num; ?>',
+				function(response)
+				{
+					$.notify(response.message, { type: response.success ? 'success' : 'danger'} );
+				}, 'json'
+			);
+		};
 
-	$("#show_email_button").click(send_email);
+		$("#show_email_button").click(send_email);
 
-	<?php if(!empty($email_receipt)): ?>
-		send_email();
-	<?php endif; ?>
-});
-</script>
+		<?php if(!empty($email_receipt)): ?>
+			send_email();
+		<?php endif; ?>
+	});
+	</script>
 <?php endif; ?>
 
 <?php $this->load->view('partial/print_receipt', array('print_after_sale'=>$print_after_sale, 'selected_printer'=>'receipt_printer')); ?>
 
 <div class="print_hide" id="control_buttons" style="text-align:right">
 	<a href="javascript:printdoc();"><div class="btn btn-info btn-sm", id="show_print_button"><?php echo '<span class="glyphicon glyphicon-print">&nbsp</span>' . $this->lang->line('common_print'); ?></div></a>
-	<?php /* this line will allow to print and go back to sales automatically.... echo anchor("sales", '<span class="glyphicon glyphicon-print">&nbsp</span>' . $this->lang->line('common_print'), array('class'=>'btn btn-info btn-sm', 'id'=>'show_print_button', 'onclick'=>'window.print();')); */ ?>
-	<?php if(isset($customer_email) && !empty($customer_email)): ?>
+	<?php if(!empty($customer_email)): ?>
 		<a href="javascript:void(0);"><div class="btn btn-info btn-sm", id="show_email_button"><?php echo '<span class="glyphicon glyphicon-envelope">&nbsp</span>' . $this->lang->line('sales_send_receipt'); ?></div></a>
 	<?php endif; ?>
 	<?php echo anchor("sales", '<span class="glyphicon glyphicon-shopping-cart">&nbsp</span>' . $this->lang->line('sales_register'), array('class'=>'btn btn-info btn-sm', 'id'=>'show_sales_button')); ?>
