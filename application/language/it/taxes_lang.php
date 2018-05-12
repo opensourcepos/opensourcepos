c803f017744e3e71346230cc909e0ce8d922b230
FrancescoUK <francesco.lodolo.uk@gmail.com>
2018-05-12 12:59:18 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: opensourcepos/taxes
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/taxes/it/

--- application/language/it/taxes_lang.php
+++ application/language/it/taxes_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["taxes_add_exception"] = "Aggiungi Eccezione";
 $lang["taxes_confirm_delete"] = "Sei sicuro che vuoi eliminare questo Codice Imposta? Questa operazione non può essere annullata.";
 $lang["taxes_confirm_restore"] = "Sei sicuro di voler ripristinare i Codice Tassa selezionati?";
@@ -8,8 +7,8 @@ $lang["taxes_error_adding_updating"] = "Codice Imposta aggiunto o aggiornamento 
 $lang["taxes_new"] = "Nuovo Codice Imposta";
 $lang["taxes_no_taxes_to_display"] = "Nessun Codice Imposta disponibile da mostrare.";
 $lang["taxes_round_half_down"] = "Arrotonda in difetto";
-$lang["taxes_round_half_even"] = "Arrotonda \"Half Even\"";
-$lang["taxes_round_half_odd"] = "Arrotonda \"Half Odd\"";
+$lang["taxes_round_half_even"] = "Arrotonda Half Even";
+$lang["taxes_round_half_odd"] = "Arrotonda Half Odd";
 $lang["taxes_round_half_up"] = "Arrotonda in eccesso";
 $lang["taxes_rounding_code"] = "Codice arrotondamento";
 $lang["taxes_sales_tax"] = "Imposte di vendita";
