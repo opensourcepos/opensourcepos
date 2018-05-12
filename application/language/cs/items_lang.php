15c161b6d1d7351a090a61905ac6dd97f0d5927f
Rostislav Hučka <hucka@midgard.cz>
2018-05-12 19:59:35 +0000

    Translated using Weblate (Czech)
    Currently translated at 15.5% (16 of 103 strings)

--- application/language/cs/items_lang.php
+++ application/language/cs/items_lang.php
@@ -1,1 +1,17 @@
 <?php
+$lang["items_add_minus"] = "Přidat nebo odebrat položku.";
+$lang["items_allow_alt_description"] = "Povolit alternativní popis";
+$lang["items_amount_entry"] = "Množství";
+$lang["items_bulk_edit"] = "Hromadná editace";
+$lang["items_buy_price_required"] = "Je nutno zadat nákupní cenu.";
+$lang["items_cannot_be_deleted"] = "Nelze smazat vybrané pokožky protože již mají záznamy o prodeji.";
+$lang["items_cannot_find_item"] = "Položka nenalezena.";
+$lang["items_category"] = "Kategorie";
+$lang["items_category_required"] = "Je nutno zadat kategorii.";
+$lang["items_change_all_to_allow_alt_desc"] = "Povolit pro všechny alternativní popis.";
+$lang["items_change_all_to_not_allow_allow_desc"] = "Nepovolit alternativní popis pro všechny.";
+$lang["items_change_all_to_serialized"] = "Upravit vše na položky se sériovým číslem";
+$lang["items_change_all_to_unserialized"] = "Upravit vše na položky bez sériového čísla";
+$lang["items_change_image"] = "Změnit obrázek";
+$lang["items_confirm_bulk_edit"] = "Opravdu chcete upravit vybrané položky?";
+$lang["items_confirm_bulk_edit_wipe_taxes"] = "Všechny záznamy o daních u položky budou nahrazeny.";
