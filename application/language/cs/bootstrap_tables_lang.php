8b006f538628c4ed3d506bee4c94b115dba51190
Rostislav Hučka <hucka@midgard.cz>
2018-05-13 08:02:10 +0000

    Translated using Weblate (Czech)
    Currently translated at 87.5% (7 of 8 strings)

--- application/language/cs/bootstrap_tables_lang.php
+++ application/language/cs/bootstrap_tables_lang.php
@@ -6,4 +6,4 @@ $lang["tables_loading"] = "Nahrávám, prosím počkejte...";
 $lang["tables_page_from_to"] = "Zobrazeno {0} až {1} z {2} řádků";
 $lang["tables_refresh"] = "Obnovit";
 $lang["tables_rows_per_page"] = "{0} řádků na stránku";
-$lang["tables_toggle"] = "Toggle";
+$lang["tables_toggle"] = "Přepnout";
