e3e4c8b3a4710cbff9174fef8de8057853fcff49
WebShells <Admin@wshells.ws>
2018-05-31 12:36:15 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/fr/

--- application/language/fr/expenses_categories_lang.php
+++ application/language/fr/expenses_categories_lang.php
@@ -1,20 +1,19 @@
-<?php 
-
-$lang["category_name_required"] = "";
-$lang["expenses_categories_add_item"] = "";
-$lang["expenses_categories_cannot_be_deleted"] = "";
-$lang["expenses_categories_category_id"] = "";
-$lang["expenses_categories_confirm_delete"] = "";
-$lang["expenses_categories_description"] = "";
-$lang["expenses_categories_error_adding_updating"] = "";
-$lang["expenses_categories_info"] = "";
-$lang["expenses_categories_name"] = "";
-$lang["expenses_categories_new"] = "";
-$lang["expenses_categories_no_expenses_categories_to_display"] = "";
-$lang["expenses_categories_none_selected"] = "";
-$lang["expenses_categories_one_or_multiple"] = "";
-$lang["expenses_categories_quantity"] = "";
-$lang["expenses_categories_successful_adding"] = "";
-$lang["expenses_categories_successful_deleted"] = "";
-$lang["expenses_categories_successful_updating"] = "";
-$lang["expenses_categories_update"] = "";
+<?php
+$lang["category_name_required"] = "Nom de la catégorie de dépenses requis";
+$lang["expenses_categories_add_item"] = "Ajouter une catégorie";
+$lang["expenses_categories_cannot_be_deleted"] = "Impossible de supprimer la ou les dépenses de catégorie";
+$lang["expenses_categories_category_id"] = "ID";
+$lang["expenses_categories_confirm_delete"] = "Êtes-vous sûr de vouloir supprimer la catégorie de dépenses sélectionnée ?";
+$lang["expenses_categories_description"] = "Description de la catégorie";
+$lang["expenses_categories_error_adding_updating"] = "Erreur lors de l'ajout / mise à jour de la catégorie de dépenses";
+$lang["expenses_categories_info"] = "Catégorie Info sur les dépenses";
+$lang["expenses_categories_name"] = "Nom de catégorie";
+$lang["expenses_categories_new"] = "Nouvelle catégorie";
+$lang["expenses_categories_no_expenses_categories_to_display"] = "Aucune catégorie à afficher";
+$lang["expenses_categories_none_selected"] = "Vous n'avez sélectionné aucun frais de catégorie";
+$lang["expenses_categories_one_or_multiple"] = "Dépense Catégorie";
+$lang["expenses_categories_quantity"] = "Quantité";
+$lang["expenses_categories_successful_adding"] = "Catégorie de dépenses ajoutée réussie";
+$lang["expenses_categories_successful_deleted"] = "La catégorie de dépenses supprimer a réussi";
+$lang["expenses_categories_successful_updating"] = "Actualiser de la catégorie de dépenses réussie";
+$lang["expenses_categories_update"] = "Actualiser Catégorie";
