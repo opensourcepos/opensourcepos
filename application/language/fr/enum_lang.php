3c87bdec574dc5d8df5ef6c81dad7ef966509c53
WebShells <Admin@wshells.ws>
2018-05-31 12:25:03 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/fr/

--- application/language/fr/enum_lang.php
+++ application/language/fr/enum_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
-$lang["enum_half_down"] = "";
-$lang["enum_half_even"] = "";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+<?php
+$lang["enum_half_down"] = "Demi-bas";
+$lang["enum_half_even"] = "La moitié";
+$lang["enum_half_five"] = "Moitié de cinq";
+$lang["enum_half_odd"] = "Moitié impair";
+$lang["enum_half_up"] = "À moitié";
+$lang["enum_round_down"] = "Arrondir vers le bas";
+$lang["enum_round_up"] = "Rassembler";
