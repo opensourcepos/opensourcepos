72245c8631f10ff4ed6dcce51a5a6bbd2943bfa8
Steve Ireland <stevei@ruledomain.com>
2018-06-28 20:40:41 -0400

    Fix issue with item kits not being listed.

--- application/controllers/Item_kits.php
+++ application/controllers/Item_kits.php
@@ -14,10 +14,10 @@ class Item_kits extends Secure_Controller
 	*/
 	private function _add_totals_to_item_kit($item_kit)
 	{
-		$kit_item_info = $this->Item->get_info($item_kit->item_id);
+		$kit_item_info = $this->Item->get_info(isset($item_kit->kit_item_id) ? $item_kit->kit_item_id : $item_kit->item_id);
 
 		$item_kit->total_cost_price = 0;
-		$item_kit->total_unit_price = $kit_item_info->unit_price;
+		$item_kit->total_unit_price = (float)$kit_item_info->unit_price;
 
 		foreach($this->Item_kit_items->get_info($item_kit->item_kit_id) as $item_kit_item)
 		{
