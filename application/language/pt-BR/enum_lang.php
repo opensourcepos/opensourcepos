c3063b32fe5c65ee39b1dfdf9e827dcac8c6614f
Xyko Arteiro <webmaster@xyko.com.br>
2018-08-09 20:59:37 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 85.7% (6 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/pt_BR/

--- application/language/pt-BR/enum_lang.php
+++ application/language/pt-BR/enum_lang.php
@@ -2,7 +2,7 @@
 $lang["enum_half_down"] = "Metade para baixo";
 $lang["enum_half_even"] = "Metade";
 $lang["enum_half_five"] = "Metade cinco";
-$lang["enum_half_odd"] = "Meio estranho";
+$lang["enum_half_odd"] = "Meio";
 $lang["enum_half_up"] = "Para cima";
 $lang["enum_round_down"] = "Arredondar para baixo";
 $lang["enum_round_up"] = "Arredondar para cima";
