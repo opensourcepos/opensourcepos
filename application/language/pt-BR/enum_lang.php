b46e69efe9ec235f6c61bfab00f05a97cd63137d
Aril Apria Susanto <awriel@gmail.com>
2018-08-11 00:19:12 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (48 of 48 strings)

--- application/language/pt-BR/enum_lang.php
+++ application/language/pt-BR/enum_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
-$lang["enum_half_down"] = "";
-$lang["enum_half_even"] = "";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+<?php
+$lang["enum_half_down"] = "Metade para baixo";
+$lang["enum_half_even"] = "Metade";
+$lang["enum_half_five"] = "Metade cinco";
+$lang["enum_half_odd"] = "Meio estranho";
+$lang["enum_half_up"] = "Para cima";
+$lang["enum_round_down"] = "Arredondar para baixo";
+$lang["enum_round_up"] = "Arredondar para cima";
