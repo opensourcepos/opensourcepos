dfb33b09eecb996a10b7d39cb9cb7aefd45c5ddb
Aril Apria Susanto <awriel@gmail.com>
2018-08-11 00:33:25 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 57.1% (4 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/id/

--- application/language/id/enum_lang.php
+++ application/language/id/enum_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
-$lang["enum_half_down"] = "";
+<?php
+$lang["enum_half_down"] = "Turun Setengah";
 $lang["enum_half_even"] = "";
 $lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
+$lang["enum_half_odd"] = "Setengah ganjil";
 $lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+$lang["enum_round_down"] = "Bulatkan ke bawah";
+$lang["enum_round_up"] = "Bulatkan ke atas";
