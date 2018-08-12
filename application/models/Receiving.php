909cd52d56fcf9ad4d18664332ce511c25526909
Emilio Silva Schlenker <emilioss@gmail.com>
2018-08-12 14:05:47 -0500

    added 'due' as a payment option for receivings

--- application/models/Receiving.php
+++ application/models/Receiving.php
@@ -221,7 +221,8 @@ class Receiving extends CI_Model
 			$this->lang->line('sales_cash') => $this->lang->line('sales_cash'),
 			$this->lang->line('sales_check') => $this->lang->line('sales_check'),
 			$this->lang->line('sales_debit') => $this->lang->line('sales_debit'),
-			$this->lang->line('sales_credit') => $this->lang->line('sales_credit')
+			$this->lang->line('sales_credit') => $this->lang->line('sales_credit'),
+			$this->lang->line('sales_due') => $this->lang->line('sales_due')
 		);
 	}
 
