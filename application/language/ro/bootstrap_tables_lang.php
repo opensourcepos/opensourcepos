e7b89e1d9a8c457224444c276911e7dc461d04fd
Emanuel Ioan Cretu <cretu.ioan@hotmail.com>
2018-09-03 11:53:53 +0000

    Translated using Weblate (Romanian)
    Currently translated at 75.0% (6 of 8 strings)

--- application/language/ro/bootstrap_tables_lang.php
+++ application/language/ro/bootstrap_tables_lang.php
@@ -1,1 +1,7 @@
 <?php
+$lang["tables_all"] = "Toate";
+$lang["tables_columns"] = "Coloane";
+$lang["tables_hide_show_pagination"] = "Ascunde/Arată paginare";
+$lang["tables_loading"] = "Se încarcă, vă rugăm așteptați...";
+$lang["tables_page_from_to"] = "Arată {0} până la {1} din {2} rânduri";
+$lang["tables_refresh"] = "Refresh";
