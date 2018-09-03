d40bc6cc4d345d0d0336771a73dcd96051805934
Emanuel Ioan Cretu <cretu.ioan@hotmail.com>
2018-09-03 11:58:41 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Romanian people know what means Refresh,

--- application/language/ro/bootstrap_tables_lang.php
+++ application/language/ro/bootstrap_tables_lang.php
@@ -4,4 +4,6 @@ $lang["tables_columns"] = "Coloane";
 $lang["tables_hide_show_pagination"] = "Ascunde/Arată paginare";
 $lang["tables_loading"] = "Se încarcă, vă rugăm așteptați...";
 $lang["tables_page_from_to"] = "Arată {0} până la {1} din {2} rânduri";
-$lang["tables_refresh"] = "Refresh";
+$lang["tables_refresh"] = "Reîmprospătare";
+$lang["tables_rows_per_page"] = "{0} rânduri per pagină";
+$lang["tables_toggle"] = "Comută";
