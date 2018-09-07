c9411a1430fd8899c99330419e6e18855b43c891
Weblate <jeroen.peelaerts@gmail.com>
2018-09-07 17:07:50 +0000

    Translated using Weblate (Flemish)
    Currently translated at 94.4% (17 of 18 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/nl_BE/

--- application/language/nl-BE/expenses_categories_lang.php
+++ application/language/nl-BE/expenses_categories_lang.php
@@ -1,20 +1,19 @@
-<?php 
-
+<?php
 $lang["category_name_required"] = "De naam van dit veld is verplicht";
 $lang["expenses_categories_add_item"] = "Voeg categorie toe";
 $lang["expenses_categories_cannot_be_deleted"] = "Kan de categorie niet verwijderen";
 $lang["expenses_categories_category_id"] = "Id";
 $lang["expenses_categories_confirm_delete"] = "Bent u zeker dat u de geselecteerde uitgave categorie wil verwijderen?";
-$lang["expenses_categories_description"] = "";
-$lang["expenses_categories_error_adding_updating"] = "";
-$lang["expenses_categories_info"] = "";
-$lang["expenses_categories_name"] = "";
-$lang["expenses_categories_new"] = "";
-$lang["expenses_categories_no_expenses_categories_to_display"] = "";
-$lang["expenses_categories_none_selected"] = "";
-$lang["expenses_categories_one_or_multiple"] = "";
-$lang["expenses_categories_quantity"] = "";
-$lang["expenses_categories_successful_adding"] = "";
-$lang["expenses_categories_successful_deleted"] = "";
-$lang["expenses_categories_successful_updating"] = "";
-$lang["expenses_categories_update"] = "";
+$lang["expenses_categories_description"] = "Categorie Omschrijving";
+$lang["expenses_categories_error_adding_updating"] = "Fout bij het toevoegen of updaten van een Categorie Onkosten";
+$lang["expenses_categories_info"] = "Categorie Onkostennota";
+$lang["expenses_categories_name"] = "Categorie Naam";
+$lang["expenses_categories_new"] = "Nieuwe Categorie";
+$lang["expenses_categories_no_expenses_categories_to_display"] = "Geen Categorie om weer te geven";
+$lang["expenses_categories_none_selected"] = "U hebt geen Onkosten Categorie geselecteerd";
+$lang["expenses_categories_one_or_multiple"] = "Categorie Onkost";
+$lang["expenses_categories_quantity"] = "Hoeveelheid";
+$lang["expenses_categories_successful_adding"] = "Categorie Onkosten toevoegen geslaagd";
+$lang["expenses_categories_successful_deleted"] = "Categorie Onkosten verwijderen is geslaagd";
+$lang["expenses_categories_successful_updating"] = "Categorie Onkosten bijwerken voltooid";
+$lang["expenses_categories_update"] = "Categorie Bijwerken";
