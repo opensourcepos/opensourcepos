1ffbbefca446e1f4261adcd330de646d2929e642
Xyko Arteiro <webmaster@xyko.com.br>
2018-09-23 18:41:37 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/pt_BR/

--- application/language/pt-BR/expenses_categories_lang.php
+++ application/language/pt-BR/expenses_categories_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["category_name_required"] = "Nome da Categoria de Despesa Requerido";
 $lang["expenses_categories_add_item"] = "Adicionar categoria";
 $lang["expenses_categories_cannot_be_deleted"] = "Não foi possível excluir a (s) despesa (s) de categoria";
@@ -18,3 +17,4 @@ $lang["expenses_categories_successful_adding"] = "Categoria de despesa adiciona 
 $lang["expenses_categories_successful_deleted"] = "Categoria de Despesas excluída com sucesso";
 $lang["expenses_categories_successful_updating"] = "Atualização de categoria de despesa bem-sucedida";
 $lang["expenses_categories_update"] = "Atualizar categoria";
+$lang["expenses_categories_confirm_restore"] = "Tem certeza de que deseja restaurar a categoria de despesa selecionada?";
