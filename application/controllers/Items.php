abc067a10b94070a8083873089447fa46b4b9952
jekkos <jeroen.peelaerts@gmail.com>
2018-09-29 17:57:12 +0200

    Fix development mode warning (#68)

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -408,7 +408,8 @@ class Items extends Secure_Controller
 			if ($definition_value['definition_type'] == DROPDOWN)
 			{
 				$values['values'] = $this->Attribute->get_definition_values($definition_id);
-				$values['selected_value'] = $this->Attribute->get_link_value($item_id, $definition_id)->attribute_id;
+				$link_value = $this->Attribute->get_link_value($item_id, $definition_id);
+				$values['selected_value'] = (empty($link_value)) ? '' : $link_value->attribute_id;
 			}
 
 			if (!empty($definition_ids[$definition_id]))
