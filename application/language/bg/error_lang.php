a268c0a48d6cf3a02208310d61061c083dfd86d6
Zhivko Vanev <jiwkowanew@gmail.com>
2018-09-30 07:53:42 +0000

    Translated using Weblate (Bulgarian)
    Currently translated at 100.0% (2 of 2 strings)

--- application/language/bg/error_lang.php
+++ application/language/bg/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
+<?php
 $lang["error_no_permission_module"] = "Нямате разрешение за достъп до модула с име";
-$lang["error_unknown"] = "неизвестен";
+$lang["error_unknown"] = "Неизвестна грешка";
