6342f5e4b81c2849c13759f0a0a7c281873b146b
FrancescoUK <francesco.lodolo.uk@gmail.com>
2018-10-07 16:43:41 +0100

    Fix chartist broken dependency, upgrade composer dependencies

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -77,7 +77,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/opensourcepos.min.css?rel=88039333a5"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=11c54984ca"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=05b66e812a"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
