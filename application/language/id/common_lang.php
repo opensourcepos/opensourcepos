d02cd24ebf73ed20a423111b7976b448e75cb764
WIbi <wibinem@yahoo.com>
2018-10-22 07:29:12 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (65 of 65 strings)

--- application/language/id/common_lang.php
+++ application/language/id/common_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["common_address_1"] = "Alamat 1";
 $lang["common_address_2"] = "Alamat 2";
 $lang["common_city"] = "Kota";
@@ -14,7 +13,7 @@ $lang["common_det"] = "Detail";
 $lang["common_download_import_template"] = "Download atau Import format Excel (CSV)";
 $lang["common_edit"] = "Ubah";
 $lang["common_email"] = "Email";
-$lang["common_email_invalid_format"] = "Format alamat email tidak benar";
+$lang["common_email_invalid_format"] = "Format alamat email tidak benar.";
 $lang["common_export_excel"] = "Expor ke Excel";
 $lang["common_export_excel_no"] = "Tidak";
 $lang["common_export_excel_yes"] = "Ya";
@@ -30,18 +29,18 @@ $lang["common_import"] = "Impor";
 $lang["common_import_change_file"] = "Ubah";
 $lang["common_import_excel"] = "Impor Excel";
 $lang["common_import_full_path"] = "Jalur lengkap ke file excel diperlukan";
-$lang["common_import_remove_file"] = "Remove";
-$lang["common_import_select_file"] = "Select file";
+$lang["common_import_remove_file"] = "Hapus";
+$lang["common_import_select_file"] = "Pilih file";
 $lang["common_inv"] = "Persediaan";
 $lang["common_last_name"] = "Nama Belakang";
 $lang["common_last_name_required"] = "Nama belakang wajib diisi.";
 $lang["common_last_page"] = "Akhir";
-$lang["common_learn_about_project"] = "Untuk belajar informasi terbaru tentang proyek ini";
+$lang["common_learn_about_project"] = "Untuk belajar informasi terbaru tentang proyek ini.";
 $lang["common_list_of"] = "Daftar";
 $lang["common_logout"] = "Keluar";
 $lang["common_new"] = "Baru";
-$lang["common_no_persons_to_display"] = "Tidak ada orang untuk ditampilkan";
-$lang["common_none_selected_text"] = "Tidak ada yang dipilih";
+$lang["common_no_persons_to_display"] = "Tidak ada orang untuk ditampilkan.";
+$lang["common_none_selected_text"] = "Tidak ada yang dipilih.";
 $lang["common_or"] = "ATAU";
 $lang["common_phone_number"] = "Telepon";
 $lang["common_phone_number_required"] = "Nomer Telepon Wajib Diisi";
@@ -63,6 +62,6 @@ $lang["common_unknown"] = "Tidak diketahui";
 $lang["common_view_recent_sales"] = "Lihat Penjualan Terkini";
 $lang["common_website"] = "Situs";
 $lang["common_welcome"] = "Selamat Datang";
-$lang["common_welcome_message"] = "Selamat Datang, klik modul di bawah ini untuk memulai";
+$lang["common_welcome_message"] = "Selamat Datang, klik modul di bawah ini untuk memulai.";
 $lang["common_you_are_using_ospos"] = "Anda menggunakan Open Source Point Of Sale Versi";
 $lang["common_zip"] = "Kode POS";
