2f3c75db2573a3a031562daa766ef238f65f2dcb
jekkos <jeroen.peelaerts@gmail.com>
2018-12-03 23:09:11 +0100

    Show attribute values in search results (#2248)

--- application/models/Item.php
+++ application/models/Item.php
@@ -192,7 +192,8 @@ class Item extends CI_Model
 				$this->db->join('attribute_values', 'attribute_links.attribute_id = attribute_values.attribute_id');
 			}
 		}
-		else if (count($filters['definition_ids']) > 0)
+
+		if (count($filters['definition_ids']) > 0)
 		{
 			$this->db->select('GROUP_CONCAT(DISTINCT CONCAT_WS(\':\', definition_id, attribute_value) ORDER BY definition_id SEPARATOR \'|\') AS attribute_values');
 			$this->db->join('attribute_links', 'attribute_links.item_id = items.item_id AND attribute_links.receiving_id IS NULL AND attribute_links.sale_id IS NULL AND definition_id IN (' . implode(',', $filters['definition_ids']) . ')', 'left');
