e9d00db9125892df6454ea7c2ea032c3851e326b
WebShells <Admin@WShells.ws>
2018-12-13 21:20:24 +0000

    Translated using Weblate (Arabic (Egypt))
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: ospos/datepicker
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/datepicker/ar_EG/

--- application/language/ar-EG/datepicker_lang.php
+++ application/language/ar-EG/datepicker_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["datepicker_all_time"] = "جميع الأوقات";
 $lang["datepicker_apply"] = "تطبيق";
 $lang["datepicker_cancel"] = "إلغاء";
@@ -7,12 +6,12 @@ $lang["datepicker_custom"] = "مخصص";
 $lang["datepicker_from"] = "من";
 $lang["datepicker_last_30"] = "أخر 30 يوم";
 $lang["datepicker_last_7"] = "أخر سبعة أيام";
-$lang["datepicker_last_financial_year"] = "";
+$lang["datepicker_last_financial_year"] = "السنة المالية الماضية";
 $lang["datepicker_last_month"] = "أخر شهر";
 $lang["datepicker_last_year"] = "السنة الماضية";
 $lang["datepicker_same_month_last_year"] = "نفس الشهر من العام الماضي";
 $lang["datepicker_same_month_to_same_day_last_year"] = "نفس الشهر حتى اليوم من السنة الماضية";
-$lang["datepicker_this_financial_year"] = "";
+$lang["datepicker_this_financial_year"] = "السنة المالية الحالية";
 $lang["datepicker_this_month"] = "هذا الشهر";
 $lang["datepicker_this_month_last_year"] = "";
 $lang["datepicker_this_year"] = "السنة الحالية";
