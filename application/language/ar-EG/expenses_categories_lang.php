38bc7cacae5f823ba37ce60b6293585c2a7a9908
WebShells <Admin@WShells.ws>
2018-12-13 21:22:31 +0000

    Translated using Weblate (Arabic (Egypt))
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: ospos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/ar_EG/

--- application/language/ar-EG/expenses_categories_lang.php
+++ application/language/ar-EG/expenses_categories_lang.php
@@ -1,11 +1,10 @@
-<?php 
-
+<?php
 $lang["category_name_required"] = "اسم نوع المصاريف مطلوب";
 $lang["expenses_categories_add_item"] = "إضافة نوع";
 $lang["expenses_categories_cannot_be_deleted"] = "فشلت عملية مسح نوع المصاريف";
 $lang["expenses_categories_category_id"] = "الكود";
 $lang["expenses_categories_confirm_delete"] = "هل أنت متأكد من مسح نوع المصاريف الذي أخترته؟";
-$lang["expenses_categories_confirm_restore"] = "";
+$lang["expenses_categories_confirm_restore"] = "هل أنت متأكد من أنك تريد استعادة فئة المصاريف المحددة؟";
 $lang["expenses_categories_description"] = "وصف النوع";
 $lang["expenses_categories_error_adding_updating"] = "مشكلة أثناء أضافة أو تعديل نوع المصاريف";
 $lang["expenses_categories_info"] = "معلومات عن نوع المصاريف";
