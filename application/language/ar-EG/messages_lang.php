de62b9441c01bcd19cec79f6d044a5ea94deea00
WebShells <Admin@WShells.ws>
2018-12-13 21:15:44 +0000

    Translated using Weblate (Arabic (Egypt))
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: ospos/messages
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/messages/ar_EG/

--- application/language/ar-EG/messages_lang.php
+++ application/language/ar-EG/messages_lang.php
@@ -1,11 +1,10 @@
-<?php 
-
+<?php
 $lang["messages_first_name"] = "الإسم الأول";
 $lang["messages_last_name"] = "الأسم الأخير";
 $lang["messages_message"] = "الرسالة";
 $lang["messages_message_placeholder"] = "رسالتك هنا...";
 $lang["messages_message_required"] = "الرسالة مطلوبة";
-$lang["messages_multiple_phones"] = "  فى حالة إرسال الرسالة لأكثر من شخص قم بفصل الأرقام بعلامة الفاصلة";
+$lang["messages_multiple_phones"] = "فى حالة إرسال الرسالة لأكثر من شخص قم بفصل الأرقام بعلامة الفاصلة";
 $lang["messages_phone"] = "رقم المحمول";
 $lang["messages_phone_number_required"] = "رقم المحمول مطلوب";
 $lang["messages_phone_placeholder"] = "رقم/أرقام المحمول هنا...";
