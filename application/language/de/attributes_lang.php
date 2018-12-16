aa309028eac7f0157e57b350762c5f7ee2f1b7c2
Ethan Horseshoe <ethan.horseshoe@outlook.com>
2018-12-16 12:41:49 +0000

    Translated using Weblate (German)
    Currently translated at 42.3% (11 of 26 strings)
    
    Translation: ospos/attributes
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/attributes/de/

--- application/language/de/attributes_lang.php
+++ application/language/de/attributes_lang.php
@@ -1,19 +1,19 @@
 <?php
-$lang["attributes_attribute_value_invalid_chars"] = "";
+$lang["attributes_attribute_value_invalid_chars"] = "Attributwert darf nicht \":\" oder \"|\" enthalten";
 $lang["attributes_confirm_delete"] = "";
 $lang["attributes_definition_cannot_be_deleted"] = "";
 $lang["attributes_definition_flags"] = "";
 $lang["attributes_definition_group"] = "Gruppe";
-$lang["attributes_definition_id"] = "";
+$lang["attributes_definition_id"] = "ID";
 $lang["attributes_definition_name"] = "Attributname";
 $lang["attributes_definition_name_required"] = "Attributname ist ein Pflichtfeld";
 $lang["attributes_definition_one_or_multiple"] = "Attribut(e)";
-$lang["attributes_definition_successful_adding"] = "";
-$lang["attributes_definition_successful_deleted"] = "";
-$lang["attributes_definition_successful_updating"] = "";
-$lang["attributes_definition_type"] = "";
-$lang["attributes_definition_type_required"] = "";
-$lang["attributes_definition_values"] = "";
+$lang["attributes_definition_successful_adding"] = "Artikel erfolgreich hinzugefügt";
+$lang["attributes_definition_successful_deleted"] = "Löschung erfolgreich";
+$lang["attributes_definition_successful_updating"] = "Sie haben das Attributerfolgreich aktualisiert";
+$lang["attributes_definition_type"] = "Attribut Typ";
+$lang["attributes_definition_type_required"] = "Attribut ist ein Pflichtfeld";
+$lang["attributes_definition_values"] = "Attribut Werte";
 $lang["attributes_new"] = "";
 $lang["attributes_no_attributes_to_display"] = "";
 $lang["attributes_receipt_visibility"] = "";
