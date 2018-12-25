7bdaafce08ce345afc2e0c32188c04e23282efb0
Jenaro Centeno <jcenteno@inteligi.com>
2018-12-25 23:52:41 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ospos/bootstrap_tables
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/bootstrap_tables/es_MX/

--- application/language/es_MX/bootstrap_tables_lang.php
+++ application/language/es_MX/bootstrap_tables_lang.php
@@ -1,9 +1,9 @@
 <?php
-$lang["tables_all"] = "All";
-$lang["tables_columns"] = "Columns";
-$lang["tables_hide_show_pagination"] = "Hide/Show pagination";
-$lang["tables_loading"] = "Loading, please wait...";
-$lang["tables_page_from_to"] = "Showing {0} to {1} of {2} rows";
-$lang["tables_refresh"] = "Refresh";
-$lang["tables_rows_per_page"] = "{0} rows per page";
-$lang["tables_toggle"] = "Toggle";
+$lang["tables_all"] = "Todos";
+$lang["tables_columns"] = "Columnas";
+$lang["tables_hide_show_pagination"] = "Ocultar/Mostrar paginación";
+$lang["tables_loading"] = "Cargando, por favor espere...";
+$lang["tables_page_from_to"] = "Mostrando de {0} a {1} de {2} registros";
+$lang["tables_refresh"] = "Actualizar";
+$lang["tables_rows_per_page"] = "{0} registros por página";
+$lang["tables_toggle"] = "Establecer";
