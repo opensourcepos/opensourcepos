cbcbc70b91fdb15a4cd9e6e005ad146b802d8995
Johannes Roesch <johannes.roesch@googlemail.com>
2018-12-26 11:41:38 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ospos/login
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/login/de/

--- application/language/de/login_lang.php
+++ application/language/de/login_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
-$lang["login_gcaptcha"] = "";
-$lang["login_go"] = "Start";
-$lang["login_invalid_gcaptcha"] = "";
-$lang["login_invalid_installation"] = "";
-$lang["login_invalid_username_and_password"] = "Ungültiger Benutzername/Passwort";
+<?php
+$lang["login_gcaptcha"] = "Ich bin kein Roboter.";
+$lang["login_go"] = "Los";
+$lang["login_invalid_gcaptcha"] = "Ich bin kein Roboter ist ungültig.";
+$lang["login_invalid_installation"] = "Die Installation ist nicht korrekt, überprüfen Sie Ihre php.ini-Datei.";
+$lang["login_invalid_username_and_password"] = "Ungültiger Benutzername oder Passwort.";
 $lang["login_login"] = "Login";
 $lang["login_password"] = "Passwort";
 $lang["login_username"] = "Benutzername";
