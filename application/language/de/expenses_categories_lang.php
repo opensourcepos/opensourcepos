f82e69defc40d81297f0e34e182feae4b6c03936
Johannes Roesch <johannes.roesch@googlemail.com>
2018-12-26 13:45:56 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: ospos/expenses_categories
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/expenses_categories/de/

--- application/language/de/expenses_categories_lang.php
+++ application/language/de/expenses_categories_lang.php
@@ -1,21 +1,20 @@
-<?php 
-
-$lang["category_name_required"] = "";
-$lang["expenses_categories_add_item"] = "";
-$lang["expenses_categories_cannot_be_deleted"] = "";
-$lang["expenses_categories_category_id"] = "";
-$lang["expenses_categories_confirm_delete"] = "";
-$lang["expenses_categories_confirm_restore"] = "";
-$lang["expenses_categories_description"] = "";
-$lang["expenses_categories_error_adding_updating"] = "";
-$lang["expenses_categories_info"] = "";
-$lang["expenses_categories_name"] = "";
-$lang["expenses_categories_new"] = "";
-$lang["expenses_categories_no_expenses_categories_to_display"] = "";
-$lang["expenses_categories_none_selected"] = "";
-$lang["expenses_categories_one_or_multiple"] = "";
-$lang["expenses_categories_quantity"] = "";
-$lang["expenses_categories_successful_adding"] = "";
-$lang["expenses_categories_successful_deleted"] = "";
-$lang["expenses_categories_successful_updating"] = "";
-$lang["expenses_categories_update"] = "";
+<?php
+$lang["category_name_required"] = "Kategorie Name erforderlich";
+$lang["expenses_categories_add_item"] = "Kategorie hinzufügen";
+$lang["expenses_categories_cannot_be_deleted"] = "Kategorie konnte nicht gelöscht werden";
+$lang["expenses_categories_category_id"] = "ID";
+$lang["expenses_categories_confirm_delete"] = "Sind Sie sicher, dass Sie die ausgewählte Kategorie löschen möchten?";
+$lang["expenses_categories_confirm_restore"] = "Sind Sie sicher, dass Sie die ausgewählte Kategorie wiederherstellen möchten?";
+$lang["expenses_categories_description"] = "Beschreibung";
+$lang["expenses_categories_error_adding_updating"] = "Fehler beim Hinzufügen/Ändern der Kategorie";
+$lang["expenses_categories_info"] = "Kategorie-Info";
+$lang["expenses_categories_name"] = "Name";
+$lang["expenses_categories_new"] = "Neue Kategorie";
+$lang["expenses_categories_no_expenses_categories_to_display"] = "Keine Kategorie zum Anzeigen";
+$lang["expenses_categories_none_selected"] = "Du hast keine Kategorie ausgewählt";
+$lang["expenses_categories_one_or_multiple"] = "Ausgabenkategorie";
+$lang["expenses_categories_quantity"] = "Menge";
+$lang["expenses_categories_successful_adding"] = "Kategorie erfolgreich hinzugefügt";
+$lang["expenses_categories_successful_deleted"] = "Kategorie erfolgreich gelöscht";
+$lang["expenses_categories_successful_updating"] = "Kategorie erfolgreich geändert";
+$lang["expenses_categories_update"] = "Kategorie ändern";
