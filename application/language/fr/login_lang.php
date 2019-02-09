7e2a82ee19900e61dd0fa5ebce427d1efab96d3a
wd-code <wdcode@outlook.com>
2019-02-09 09:56:49 +0000

    Translated using Weblate (French)
    Currently translated at 62.5% (5 of 8 strings)
    
    Translation: ospos/login
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/login/fr/

--- application/language/fr/login_lang.php
+++ application/language/fr/login_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "";
 $lang["login_go"] = "Lancer";
 $lang["login_invalid_gcaptcha"] = "";
 $lang["login_invalid_installation"] = "";
-$lang["login_invalid_username_and_password"] = "Entrée invalide";
+$lang["login_invalid_username_and_password"] = "Entrée invalide.";
 $lang["login_login"] = "Login";
 $lang["login_password"] = "Mot de passe";
 $lang["login_username"] = "Nom d'utilisateur";
