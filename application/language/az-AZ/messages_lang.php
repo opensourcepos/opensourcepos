c276318790e4d3d7d401796968d39e4dc5965256
Natig Asad <natiq@zaka.biz>
2019-02-12 18:24:01 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: ospos/messages
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/messages/az/

--- application/language/az-AZ/messages_lang.php
+++ application/language/az-AZ/messages_lang.php
@@ -1,14 +1,13 @@
-<?php 
-
-$lang["messages_first_name"] = "";
-$lang["messages_last_name"] = "";
-$lang["messages_message"] = "";
-$lang["messages_message_placeholder"] = "";
-$lang["messages_message_required"] = "";
-$lang["messages_multiple_phones"] = "";
-$lang["messages_phone"] = "";
-$lang["messages_phone_number_required"] = "";
-$lang["messages_phone_placeholder"] = "";
-$lang["messages_sms_send"] = "";
-$lang["messages_successfully_sent"] = "";
-$lang["messages_unsuccessfully_sent"] = "";
+<?php
+$lang["messages_first_name"] = "Adı";
+$lang["messages_last_name"] = "Soyadı";
+$lang["messages_message"] = "Mesaj";
+$lang["messages_message_placeholder"] = "Mesajınız burada...";
+$lang["messages_message_required"] = "Mesaj tələb olunur";
+$lang["messages_multiple_phones"] = "(Birdən çox alıcı halında, vergüllə ayrılan mobil nömrələri daxil edin)";
+$lang["messages_phone"] = "Telefon";
+$lang["messages_phone_number_required"] = "Telefon nömrəsi tələb olunur";
+$lang["messages_phone_placeholder"] = "Mobil Nömrə(lər) burada...";
+$lang["messages_sms_send"] = "SMS Göndər";
+$lang["messages_successfully_sent"] = "Mesaj uğurla göndərildi: ";
+$lang["messages_unsuccessfully_sent"] = "Mesaj uğursuz olaraq göndərildi: ";
