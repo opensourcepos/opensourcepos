0dcb032b1da90d6cf041b26ddf20800e06b49d37
Natig Asad <natiq@zaka.biz>
2019-02-14 23:25:56 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: ospos/datepicker
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/datepicker/az/

--- application/language/az-AZ/datepicker_lang.php
+++ application/language/az-AZ/datepicker_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["datepicker_all_time"] = "Bütün vaxta";
 $lang["datepicker_apply"] = "Uygula";
 $lang["datepicker_cancel"] = "İmtina";
@@ -7,15 +6,15 @@ $lang["datepicker_custom"] = "Fərqli";
 $lang["datepicker_from"] = "Bundan";
 $lang["datepicker_last_30"] = "Son 30 gün";
 $lang["datepicker_last_7"] = "Son 7 Gün";
-$lang["datepicker_last_financial_year"] = "Last Financial Year";
-$lang["datepicker_last_month"] = "ast Month";
-$lang["datepicker_last_year"] = "Keçən il";
+$lang["datepicker_last_financial_year"] = "Son Maliyyə ili";
+$lang["datepicker_last_month"] = "Ötən ay";
+$lang["datepicker_last_year"] = "Ötən il";
 $lang["datepicker_same_month_last_year"] = "Ötən il eyni ay";
 $lang["datepicker_same_month_to_same_day_last_year"] = "Ötən ilin eyni ayindan";
-$lang["datepicker_this_financial_year"] = "This Financial Year";
-$lang["datepicker_this_month"] = "This Month";
+$lang["datepicker_this_financial_year"] = "Cari maliyyə ili";
+$lang["datepicker_this_month"] = "Bu ay";
 $lang["datepicker_this_month_last_year"] = "";
-$lang["datepicker_this_year"] = "This Year";
+$lang["datepicker_this_year"] = "Bu il";
 $lang["datepicker_to"] = "Buna";
 $lang["datepicker_today"] = "Bugün";
 $lang["datepicker_today_last_year"] = "Keçən il bu gün";
