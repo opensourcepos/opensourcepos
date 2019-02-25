a4e644fe146ca2754cb5b21ed948cfdc70222b38
tomer <petbuy.rehovot@gmail.com>
2019-02-25 13:15:24 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ospos/bootstrap_tables
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/bootstrap_tables/he/

--- application/language/he/bootstrap_tables_lang.php
+++ application/language/he/bootstrap_tables_lang.php
@@ -1,9 +1,9 @@
 <?php
-$lang["tables_all"] = "All";
-$lang["tables_columns"] = "Columns";
-$lang["tables_hide_show_pagination"] = "Hide/Show pagination";
-$lang["tables_loading"] = "Loading, please wait...";
-$lang["tables_page_from_to"] = "Showing {0} to {1} of {2} rows";
-$lang["tables_refresh"] = "Refresh";
-$lang["tables_rows_per_page"] = "{0} rows per page";
-$lang["tables_toggle"] = "Toggle";
+$lang["tables_all"] = "הכול";
+$lang["tables_columns"] = "עמודות";
+$lang["tables_hide_show_pagination"] = "הסתר / הצג מספור דפים";
+$lang["tables_loading"] = "טוען, אנא המתן ...";
+$lang["tables_page_from_to"] = "מציג {0} ל {1} מתוך {2} שורות";
+$lang["tables_refresh"] = "רענן";
+$lang["tables_rows_per_page"] = "{0} שורות לדף";
+$lang["tables_toggle"] = "החלף";
