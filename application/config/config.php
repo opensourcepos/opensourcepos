fe4b6dc640805ddf52b8d37e17b37d9061da0537
objecttothis <17935339+objecttothis@users.noreply.github.com>
2019-02-27 11:27:05 +0400

    Update config.php
    Security Restriction per #2377

--- application/config/config.php
+++ application/config/config.php
@@ -294,7 +294,7 @@ $config['log_file_extension'] = '';
 | IMPORTANT: This MUST be an integer (no quotes) and you MUST use octal
 |            integer notation (i.e. 0700, 0644, etc.)
 */
-$config['log_file_permissions'] = 0644;
+$config['log_file_permissions'] = 0640;
 
 /*
 |--------------------------------------------------------------------------
