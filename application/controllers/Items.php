bc81da9909905ac20c71d2613902879e1bf34bd4
dtomlins <48651887+dtomlins@users.noreply.github.com>
2019-03-23 12:25:01 -0400

    Add files via upload
    Add two items to the dropdown to allow 'inventory' inclusion to be optional.
    Temporarily turn off dbprefix so CI doesn't corrupt the select OPTION.

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -13,8 +13,6 @@ class Items extends Secure_Controller
 	
 	public function index()
 	{
-		$this->session->set_userdata('allow_temp_items', 0);
-
 		$data['table_headers'] = $this->xss_clean(get_items_manage_table_headers());
 		
 		$data['stock_location'] = $this->xss_clean($this->item_lib->get_item_location());
@@ -25,9 +23,12 @@ class Items extends Secure_Controller
 			'low_inventory' => $this->lang->line('items_low_inventory_items'),
 			'is_serialized' => $this->lang->line('items_serialized_items'),
 			'no_description' => $this->lang->line('items_no_description_items'),
-			'search_custom' => $this->lang->line('items_search_attributes'),
-			'is_deleted' => $this->lang->line('items_is_deleted'),
-			'temporary' => $this->lang->line('items_temp'));
+			'search_custom' => $this->lang->line('items_search_custom_items'),
+//NEED XLATION			'has_transactions' => $this->lang->line('items_search_custom_items'),  // dmt
+//NEED XLATION			'no_transactions' => $this->lang->line('items_search_custom_items'),   // dmt
+			'has_transactions' => "Has Transactions during Range",				//dmt
+			'no_transactions' => "No Transactions during Range",				//dmt
+			'is_deleted' => $this->lang->line('items_is_deleted'));
 
 		$this->load->view('items/manage', $data);
 	}
@@ -45,8 +46,6 @@ class Items extends Secure_Controller
 
 		$this->item_lib->set_item_location($this->input->get('stock_location'));
 
-		$definition_names = $this->Attribute->get_definitions_by_flags(Attribute::SHOW_IN_ITEMS);
-
 		$filters = array('start_date' => $this->input->get('start_date'),
 						'end_date' => $this->input->get('end_date'),
 						'stock_location_id' => $this->item_lib->get_item_location(),
@@ -55,17 +54,20 @@ class Items extends Secure_Controller
 						'is_serialized' => FALSE,
 						'no_description' => FALSE,
 						'search_custom' => FALSE,
-						'is_deleted' => FALSE,
-						'temporary' => FALSE,
-						'definition_ids' => array_keys($definition_names));
+						'is_deleted' => FALSE);
 		
 		// check if any filter is set in the multiselect dropdown
 		$filledup = array_fill_keys($this->input->get('filters'), TRUE);
 		$filters = array_merge($filters, $filledup);
 
+
+// CI attempts to prefix the SQL directive when prefix is enabled  //dmt
+$dmthold_prefix = $this->db->dbprefix;	//dmt
+$this->db->set_dbprefix(''); //dmt
 		$items = $this->Item->search($search, $filters, $limit, $offset, $sort, $order);
 
 		$total_rows = $this->Item->get_found_rows($search, $filters);
+$this->db->set_dbprefix($dmthold_prefix); //dmt
 
 		$data_rows = array();
 		foreach($items->result() as $item)
@@ -131,15 +133,6 @@ class Items extends Secure_Controller
 		echo json_encode($suggestions);
 	}
 
-
-	public function suggest_low_sell()
-	{
-		$suggestions = $this->xss_clean($this->Item->get_low_sell_suggestions($this->input->post_get('name')));
-
-		echo json_encode($suggestions);
-	}
-
-
 	public function suggest_kits()
 	{
 		$suggestions = $this->xss_clean($this->Item->get_kit_search_suggestions($this->input->post_get('term'),
@@ -167,6 +160,16 @@ class Items extends Secure_Controller
 
 		echo json_encode($suggestions);
 	}
+	
+	/*
+	 Gives search suggestions based on what is being searched for
+	*/
+	public function suggest_custom()
+	{
+		$suggestions = $this->xss_clean($this->Item->get_custom_suggestions($this->input->post('term'), $this->input->post('field_no')));
+
+		echo json_encode($suggestions);
+	}
 
 	public function get_row($item_ids)
 	{
@@ -183,52 +186,16 @@ class Items extends Secure_Controller
 
 	public function view($item_id = -1)
 	{
-		if($item_id == -1)
-		{
-			$data = array();
-		}
-
-		// allow_temp_items is set in the index function of items.php or sales.php
-		$data['allow_temp_item'] = $this->session->userdata('allow_temp_items');
 		$data['item_tax_info'] = $this->xss_clean($this->Item_taxes->get_info($item_id));
 		$data['default_tax_1_rate'] = '';
 		$data['default_tax_2_rate'] = '';
-		$data['item_type_disabled'] = !$this->Employee->has_grant('item_kits', $this->Employee->get_logged_in_employee_info()->person_id);
-		$data['definition_values'] = $this->Attribute->get_attributes_by_item($item_id);
-		$data['definition_names'] = $this->Attribute->get_definition_names();
-
-		foreach($data['definition_values'] as $definition_id => $definition)
-		{
-			unset($data['definition_names'][$definition_id]);
-		}
+		$data['item_kits_enabled'] = $this->Employee->has_grant('item_kits', $this->Employee->get_logged_in_employee_info()->person_id);
 
 		$item_info = $this->Item->get_info($item_id);
-
 		foreach(get_object_vars($item_info) as $property => $value)
 		{
 			$item_info->$property = $this->xss_clean($value);
 		}
-
-		if($data['allow_temp_item'] == 1)
-		{
-			if($item_id != -1)
-			{
-				if($item_info->item_type != ITEM_TEMP)
-				{
-					$data['allow_temp_item'] = 0;
-				}
-			}
-		}
-		else
-		{
-			if($item_info->item_type == ITEM_TEMP)
-			{
-				$data['allow_temp_item'] = 1;
-			}
-		}
-
-		$use_destination_based_tax = (boolean)$this->config->item('use_destination_based_tax');
-		$data['include_hsn'] = $this->config->item('include_hsn') == '1';
 
 		if($item_id == -1)
 		{
@@ -238,16 +205,8 @@ class Items extends Secure_Controller
 			$item_info->receiving_quantity = 1;
 			$item_info->reorder_level = 1;
 			$item_info->item_type = ITEM; // standard
-			$item_info->item_id = $item_id;
 			$item_info->stock_type = HAS_STOCK;
-			$item_info->tax_category_id = NULL;
-			$item_info->qty_per_pack = 1;
-			$item_info->pack_name = $this->lang->line('items_default_pack_name');
-			$data['hsn_code'] = '';
-			if($use_destination_based_tax)
-			{
-				$item_info->tax_category_id = $this->config->item('default_tax_category');
-			}
+			$item_info->tax_category_id = 1;  // Standard
 		}
 
 		$data['item_info'] = $item_info;
@@ -260,38 +219,23 @@ class Items extends Secure_Controller
 		$data['suppliers'] = $suppliers;
 		$data['selected_supplier'] = $item_info->supplier_id;
 
-		if($data['include_hsn'])
+		$customer_sales_tax_support = $this->config->item('customer_sales_tax_support');
+		if($customer_sales_tax_support == '1')
 		{
-			$data['hsn_code'] = $item_info->hsn_code;
-		}
-		else
-		{
-			$data['hsn_code'] = '';
-		}
-
-		if($use_destination_based_tax)
-		{
-			$data['use_destination_based_tax'] = TRUE;
+			$data['customer_sales_tax_enabled'] = TRUE;
 			$tax_categories = array();
-			foreach($this->Tax_category->get_all()->result_array() as $row)
+			foreach($this->Tax->get_all_tax_categories()->result_array() as $row)
 			{
 				$tax_categories[$this->xss_clean($row['tax_category_id'])] = $this->xss_clean($row['tax_category']);
 			}
-			$tax_category = "";
-			if ($item_info->tax_category_id != NULL)
-			{
-				$tax_category_info=$this->Tax_category->get_info($item_info->tax_category_id);
-				$tax_category= $tax_category_info->tax_category;
-			}
 			$data['tax_categories'] = $tax_categories;
-			$data['tax_category'] = $tax_category;
-			$data['tax_category_id'] = $item_info->tax_category_id;
+			$data['selected_tax_category'] = $item_info->tax_category_id;
 		}
 		else
 		{
-			$data['use_destination_based_tax'] = FALSE;
+			$data['customer_sales_tax_enabled'] = FALSE;
 			$data['tax_categories'] = array();
-			$data['tax_category'] = '';
+			$data['selected_tax_category'] = '';
 		}
 
 		$data['logo_exists'] = $item_info->pic_filename != '';
@@ -316,18 +260,6 @@ class Items extends Secure_Controller
 			$quantity = ($item_id == -1) ? 0 : $quantity;
 			$location_array[$location['location_id']] = array('location_name' => $location['location_name'], 'quantity' => $quantity);
 			$data['stock_locations'] = $location_array;
-		}
-
-		$data['selected_low_sell_item_id'] = $item_info->low_sell_item_id;
-
-		if($item_id != -1 && $item_info->item_id != $item_info->low_sell_item_id)
-		{
-			$low_sell_item_info = $this->Item->get_info($item_info->low_sell_item_id);
-			$data['selected_low_sell_item'] = implode(NAME_SEPARATOR, array($low_sell_item_info->name, $low_sell_item_info->pack_name));
-		}
-		else
-		{
-			$data['selected_low_sell_item'] = '';
 		}
 
 		$this->load->view('items/form', $data);
@@ -413,40 +345,6 @@ class Items extends Secure_Controller
 		$this->load->view('barcodes/barcode_sheet', $data);
 	}
 
-	public function attributes($item_id)
-	{
-		$data['item_id'] = $item_id;
-		$definition_ids = json_decode($this->input->post('definition_ids'), TRUE);
-		$data['definition_values'] = $this->Attribute->get_attributes_by_item($item_id) + $this->Attribute->get_values_by_definitions($definition_ids);
-		$data['definition_names'] = $this->Attribute->get_definition_names();
-
-		foreach($data['definition_values'] as $definition_id => $definition_value)
-		{
-			$attribute_value = $this->Attribute->get_attribute_value($item_id, $definition_id);
-			$attribute_id = (empty($attribute_value) || empty($attribute_value->attribute_id)) ? NULL : $attribute_value->attribute_id;
-			$values = &$data['definition_values'][$definition_id];
-			$values['attribute_id'] = $attribute_id;
-			$values['attribute_value'] = $attribute_value;
-			$values['selected_value'] = '';
-			
-			if ($definition_value['definition_type'] == DROPDOWN)
-			{
-				$values['values'] = $this->Attribute->get_definition_values($definition_id);
-				$link_value = $this->Attribute->get_link_value($item_id, $definition_id);
-				$values['selected_value'] = (empty($link_value)) ? '' : $link_value->attribute_id;
-			}
-
-			if (!empty($definition_ids[$definition_id]))
-			{
-				$values['selected_value'] = $definition_ids[$definition_id];
-			}
-
-			unset($data['definition_names'][$definition_id]);
-		}
-
- 		$this->load->view('attributes/item', $data);
-	}
-
 	public function bulk_edit()
 	{
 		$suppliers = array('' => $this->lang->line('items_none'));
@@ -475,56 +373,33 @@ class Items extends Secure_Controller
 		$upload_success = $this->_handle_image_upload();
 		$upload_data = $this->upload->data();
 
-		$receiving_quantity = parse_decimals($this->input->post('receiving_quantity'));
-		$item_type = $this->input->post('item_type') == NULL ? ITEM : $this->input->post('item_type');
-
-		if($receiving_quantity == '0' && $item_type!= ITEM_TEMP)
-		{
-			$receiving_quantity = '1';
-		}
-		$default_pack_name = $this->lang->line('items_default_pack_name');
-
-
-		if ($item_id == -1)
-		{
-			// Set low_sell_item_id to -1 for a new item so that when it is saved the item will point to itself for the
-			// low sell item.
-			$low_sell_item_id = $this->input->post('low_sell_item_id') == NULL ? -1 : $this->input->post('low_sell_item_id');
-		}
-		else
-		{
-			// On an existing item where the low sell item is not prompted for always force the item_id as the low sell item Id
-			$low_sell_item_id = $this->input->post('low_sell_item_id') == NULL ? $item_id : $this->input->post('low_sell_item_id');
-		}
-
 		//Save item data
 		$item_data = array(
 			'name' => $this->input->post('name'),
 			'description' => $this->input->post('description'),
 			'category' => $this->input->post('category'),
-			'item_type' => $item_type,
+			'item_type' => $this->input->post('item_type') == NULL ? ITEM : $this->input->post('item_type'),
 			'stock_type' => $this->input->post('stock_type') == NULL ? HAS_STOCK : $this->input->post('stock_type'),
 			'supplier_id' => $this->input->post('supplier_id') == '' ? NULL : $this->input->post('supplier_id'),
 			'item_number' => $this->input->post('item_number') == '' ? NULL : $this->input->post('item_number'),
 			'cost_price' => parse_decimals($this->input->post('cost_price')),
 			'unit_price' => parse_decimals($this->input->post('unit_price')),
 			'reorder_level' => parse_decimals($this->input->post('reorder_level')),
-			'receiving_quantity' => $receiving_quantity,
+			'receiving_quantity' => parse_decimals($this->input->post('receiving_quantity')),
 			'allow_alt_description' => $this->input->post('allow_alt_description') != NULL,
 			'is_serialized' => $this->input->post('is_serialized') != NULL,
-			'qty_per_pack' => $this->input->post('qty_per_pack') == NULL ? 1 : $this->input->post('qty_per_pack'),
-			'pack_name' => $this->input->post('pack_name') == NULL ? $default_pack_name : $this->input->post('pack_name'),
-			'low_sell_item_id' => $low_sell_item_id,
 			'deleted' => $this->input->post('is_deleted') != NULL,
-			'hsn_code' => $this->input->post('hsn_code') == NULL ? '' : $this->input->post('hsn_code')
+			'custom1' => $this->input->post('custom1') == NULL ? '' : $this->input->post('custom1'),
+			'custom2' => $this->input->post('custom2') == NULL ? '' : $this->input->post('custom2'),
+			'custom3' => $this->input->post('custom3') == NULL ? '' : $this->input->post('custom3'),
+			'custom4' => $this->input->post('custom4') == NULL ? '' : $this->input->post('custom4'),
+			'custom5' => $this->input->post('custom5') == NULL ? '' : $this->input->post('custom5'),
+			'custom6' => $this->input->post('custom6') == NULL ? '' : $this->input->post('custom6'),
+			'custom7' => $this->input->post('custom7') == NULL ? '' : $this->input->post('custom7'),
+			'custom8' => $this->input->post('custom8') == NULL ? '' : $this->input->post('custom8'),
+			'custom9' => $this->input->post('custom9') == NULL ? '' : $this->input->post('custom9'),
+			'custom10' => $this->input->post('custom10') == NULL ? '' : $this->input->post('custom10')
 		);
-
-		if($item_data['item_type'] == ITEM_TEMP)
-		{
-			$item_data['stock_type'] = HAS_NO_STOCK;
-			$item_data['receiving_quantity'] = 0;
-			$item_data['reorder_level'] = 0;
-		}
 
 		$x = $this->input->post('tax_category_id');
 		if(!isset($x))
@@ -533,7 +408,7 @@ class Items extends Secure_Controller
 		}
 		else
 		{
-			$item_data['tax_category_id'] = $this->input->post('tax_category_id') == '' ? NULL : $this->input->post('tax_category_id');
+			$item_data['tax_category_id'] = $this->input->post('tax_category_id');
 		}
 		
 		if(!empty($upload_data['orig_name']))
@@ -546,7 +421,8 @@ class Items extends Secure_Controller
 		}
 		
 		$employee_id = $this->Employee->get_logged_in_employee_info()->person_id;
-
+		$cur_item_info = $this->Item->get_info($item_id);
+		
 		if($this->Item->save($item_data, $item_id))
 		{
 			$success = TRUE;
@@ -557,40 +433,29 @@ class Items extends Secure_Controller
 				$item_id = $item_data['item_id'];
 				$new_item = TRUE;
 			}
-
-			$use_destination_based_tax = (boolean)$this->config->item('use_destination_based_tax');
-
-			if(!$use_destination_based_tax)
+			
+			$items_taxes_data = array();
+			$tax_names = $this->input->post('tax_names');
+			$tax_percents = $this->input->post('tax_percents');
+			$count = count($tax_percents);
+			for ($k = 0; $k < $count; ++$k)
 			{
-				$items_taxes_data = array();
-				$tax_names = $this->input->post('tax_names');
-				$tax_percents = $this->input->post('tax_percents');
-				$count = count($tax_percents);
-				for ($k = 0; $k < $count; ++$k)
+				$tax_percentage = parse_decimals($tax_percents[$k]);
+				if(is_numeric($tax_percentage))
 				{
-					$tax_percentage = parse_decimals($tax_percents[$k]);
-					if(is_numeric($tax_percentage))
-					{
-						$items_taxes_data[] = array('name' => $tax_names[$k], 'percent' => $tax_percentage);
-					}
+					$items_taxes_data[] = array('name' => $tax_names[$k], 'percent' => $tax_percentage);
 				}
-				$success &= $this->Item_taxes->save($items_taxes_data, $item_id);
 			}
+			$success &= $this->Item_taxes->save($items_taxes_data, $item_id);
 
 			//Save item quantity
 			$stock_locations = $this->Stock_location->get_undeleted_all()->result_array();
 			foreach($stock_locations as $location)
 			{
 				$updated_quantity = parse_decimals($this->input->post('quantity_' . $location['location_id']));
-				if($item_data['item_type'] == ITEM_TEMP)
-				{
-					$updated_quantity = 0;
-				}
 				$location_detail = array('item_id' => $item_id,
 										'location_id' => $location['location_id'],
 										'quantity' => $updated_quantity);
-
-
 				$item_quantity = $this->Item_quantity->get_item_quantity($item_id, $location['location_id']);
 				if($item_quantity->quantity != $updated_quantity || $new_item)
 				{
@@ -607,20 +472,6 @@ class Items extends Secure_Controller
 
 					$success &= $this->Inventory->insert($inv_data);
 				}
-			}
-
-			// Save item attributes
-			$attribute_links = $this->input->post('attribute_links') != NULL ? $this->input->post('attribute_links') : array();
-			$attribute_ids = $this->input->post('attribute_ids');
-			$this->Attribute->delete_link($item_id);
-			foreach($attribute_links as $definition_id => $attribute_id)
-			{
-				$definition_type = $this->Attribute->get_info($definition_id)->definition_type;
-				if($definition_type != DROPDOWN)
-				{
-					$attribute_id = $this->Attribute->save_value($attribute_id, $definition_id, $item_id, $attribute_ids[$definition_id], $definition_type);
-				}
-				$this->Attribute->save_link($item_id, $definition_id, $attribute_id);
 			}
 
 			if($success && $upload_success)
@@ -755,7 +606,7 @@ class Items extends Secure_Controller
 			$tax_percents = $this->input->post('tax_percents');
 			$tax_updated = FALSE;
 			$count = count($tax_percents);
-			for($k = 0; $k < $count; ++$k)
+			for ($k = 0; $k < $count; ++$k)
 			{		
 				if(!empty($tax_names[$k]) && is_numeric($tax_percents[$k]))
 				{
@@ -810,8 +661,6 @@ class Items extends Secure_Controller
 
 	public function do_excel_import()
 	{
-		$non_repeating_element_count = 18;
-
 		if($_FILES['file_path']['error'] != UPLOAD_ERR_OK)
 		{
 			echo json_encode(array('success' => FALSE, 'message' => $this->lang->line('items_excel_import_failed')));
@@ -825,13 +674,14 @@ class Items extends Secure_Controller
 				$i = 1;
 				
 				$failCodes = array();
-
+		
 				while(($data = fgetcsv($handle)) !== FALSE)
 				{
 					// XSS file data sanity check
 					$data = $this->xss_clean($data);
 					
-					if(sizeof($data) >= $non_repeating_element_count)
+					/* haven't touched this so old templates will work, or so I guess... */
+					if(sizeof($data) >= 23)
 					{
 						$item_data = array(
 							'name'					=> $data[1],
@@ -843,7 +693,16 @@ class Items extends Secure_Controller
 							'supplier_id'			=> $this->Supplier->exists($data[3]) ? $data[3] : NULL,
 							'allow_alt_description'	=> $data[12] != '' ? '1' : '0',
 							'is_serialized'			=> $data[13] != '' ? '1' : '0',
-							'hsn_code'					=> $data[15]
+							'custom1'				=> $data[14],
+							'custom2'				=> $data[15],
+							'custom3'				=> $data[16],
+							'custom4'				=> $data[17],
+							'custom5'				=> $data[18],
+							'custom6'				=> $data[19],
+							'custom7'				=> $data[20],
+							'custom8'				=> $data[21],
+							'custom9'				=> $data[22],
+							'custom10'				=> $data[23]
 						);
 
 						/* we could do something like this, however, the effectiveness of
@@ -851,7 +710,7 @@ class Items extends Secure_Controller
 						  into that directory, so you really can do whatever you want, this probably
 						  needs further discussion  */
 
-						$pic_file = $data[14];
+						$pic_file = $data[24];
 						/*if(strcmp('.htaccess', $pic_file)==0)
 						{
 							$pic_file='';
@@ -901,7 +760,7 @@ class Items extends Secure_Controller
 
 						// array to store information if location got a quantity
 						$allowed_locations = $this->Stock_location->get_allowed_locations();
-						for($col = 16; $col < $cols; $col = $col + 2)
+						for($col = 25; $col < $cols; $col = $col + 2)
 						{
 							$location_id = $data[$col];
 							if(array_key_exists($location_id, $allowed_locations))
