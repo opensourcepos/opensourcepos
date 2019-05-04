79a563cf664e888c57d55de5830bcb1a65112818
Gonzalo Fleming <gonzafg2@gmail.com>
2019-05-04 08:03:32 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (67 of 67 strings)
    
    Translation: ospos/common
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/common/es/

--- application/language/es/common_lang.php
+++ application/language/es/common_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["common_address_1"] = "Dirección 1";
 $lang["common_address_2"] = "Dirección 2";
 $lang["common_city"] = "Ciudad";
@@ -7,7 +6,7 @@ $lang["common_close"] = "Cerrar";
 $lang["common_comments"] = "Comentarios";
 $lang["common_common"] = "común";
 $lang["common_confirm_search"] = "Has seleccionado una o más filas. Éstas no estarán seleccionadas después de tu búsqueda. ¿Seguro(a) que quieres hacer esta búsqueda?";
-$lang["common_correct_errors"] = "";
+$lang["common_correct_errors"] = "Por favor, corrija los errores identificados antes de guardar";
 $lang["common_country"] = "País";
 $lang["common_date"] = "Fecha";
 $lang["common_delete"] = "Borrar";
