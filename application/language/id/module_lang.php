b0c3ff84c9f3deebe0e6fcde36c8dd8159a8593f
Robby Triadi Susanto <robbytriadi@gmail.com>
2019-05-06 17:41:01 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (39 of 39 strings)
    
    Translation: ospos/module
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/module/id/

--- application/language/id/module_lang.php
+++ application/language/id/module_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
-$lang["module_attributes"] = "";
-$lang["module_attributes_desc"] = "";
+<?php
+$lang["module_attributes"] = "Atribut";
+$lang["module_attributes_desc"] = "Tambah, Perbaharui, Hapus dan Cari atribut.";
 $lang["module_both"] = "Keduanya";
-$lang["module_cashups"] = "";
-$lang["module_cashups_desc"] = "";
+$lang["module_cashups"] = "Uang Tunai";
+$lang["module_cashups_desc"] = "Tambah, Perbaharui, Hapus dan Cari Uang Tunai.";
 $lang["module_config"] = "Konfigurasi";
 $lang["module_config_desc"] = "Ubah Konfigurasi Toko.";
 $lang["module_customers"] = "Pelanggan";
