0a2894c582826f3c691f43b49a6802a69782dbd5
jekkos <jeroen.peelaerts@gmail.com>
2019-08-04 22:29:44 +0200

    Left join on attribute_values to show items with no value (#2485)

--- application/models/Item.php
+++ application/models/Item.php
@@ -199,7 +199,7 @@ class Item extends CI_Model
 			$this->db->select("GROUP_CONCAT(DISTINCT CONCAT_WS('_', definition_id, DATE_FORMAT(attribute_date, $format)) SEPARATOR '|') AS attribute_dtvalues");
 			$this->db->select('GROUP_CONCAT(DISTINCT CONCAT_WS(\'_\', definition_id, attribute_decimal) SEPARATOR \'|\') AS attribute_dvalues');
 			$this->db->join('attribute_links', 'attribute_links.item_id = items.item_id AND attribute_links.receiving_id IS NULL AND attribute_links.sale_id IS NULL AND definition_id IN (' . implode(',', $filters['definition_ids']) . ')', 'left');
-			$this->db->join('attribute_values', 'attribute_values.attribute_id = attribute_links.attribute_id');
+			$this->db->join('attribute_values', 'attribute_values.attribute_id = attribute_links.attribute_id', 'left');
 		}
 
 		$this->db->where('items.deleted', $filters['is_deleted']);
