4f089aa68dc75eef9528a45354c41cbffa64b46a
WebShells <26513147+WebShells@users.noreply.github.com>
2019-09-10 20:08:03 +0300

    Re-added EG
    Re-added EG, need to be double checked to make sure it fits Egyptian Language. ( most probably 70% no differences in language ).

---
+++ application/language/ar-EG/attributes_lang.php
@@ -0,0 +1,31 @@
+<?php 
+
+$lang["attributes_attribute_value_invalid_chars"] = "الميزات لا يمكن أن تحتوي على ':' أو'|'";
+$lang["attributes_confirm_delete"] = "هل أنت متأكد من أنك تريد حذف الميزات المحددة ؟";
+$lang["attributes_confirm_restore"] = "هل أنت متأكد من أنك تريد استعادة السمة (السمات) المحددة؟";
+$lang["attributes_definition_cannot_be_deleted"] = "لا يمكن حذف السمات المحددة";
+$lang["attributes_definition_error_adding_updating"] = "لا يمكن إضافة السمة ٪1 أو تحديثها. يرجى التحقق من سجل الخطأ.";
+$lang["attributes_definition_flags"] = "رؤية الميزات";
+$lang["attributes_definition_group"] = "المجموعة";
+$lang["attributes_definition_id"] = "كود";
+$lang["attributes_definition_name"] = "إضافة ميزة";
+$lang["attributes_definition_name_required"] = "اسم الميزة هي خانة اجبارية";
+$lang["attributes_definition_one_or_multiple"] = "ميزة/ميزات";
+$lang["attributes_definition_successful_adding"] = "لقد تم إضافة صنف بنجاح";
+$lang["attributes_definition_successful_deleted"] = "لقد تم حذف ميزة بنجاح";
+$lang["attributes_definition_successful_updating"] = "تم تعديل الميزة بنجاح";
+$lang["attributes_definition_type"] = "نوع الميزة";
+$lang["attributes_definition_type_required"] = "نوع الميزة هي خانة إجبارية";
+$lang["attributes_definition_unit"] = "وحدة قياس";
+$lang["attributes_definition_values"] = "قيمة الميزة";
+$lang["attributes_new"] = "اضافة ميزة جديده";
+$lang["attributes_no_attributes_to_display"] = "لا يوجد اصناف للعرض";
+$lang["attributes_receipt_visibility"] = "وصل";
+$lang["attributes_show_in_items"] = "اظهار في الصنف";
+$lang["attributes_show_in_items_visibility"] = "الصنف";
+$lang["attributes_show_in_receipt"] = "اظهار على الوصل";
+$lang["attributes_show_in_receivings"] = "اظهار في استلام البضائع";
+$lang["attributes_show_in_receivings_visibility"] = "استلام البضائع";
+$lang["attributes_show_in_sales"] = "اظهار خلال البيع";
+$lang["attributes_show_in_sales_visibility"] = "البيع";
+$lang["attributes_update"] = "تحديث الميزات";
