a59236f4aef7627a8d115262e13882c14a81c746
Jens <Jens.v.haren@hotmail.nl>
2019-11-14 23:50:31 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: ospos/datepicker
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/datepicker/nl/

--- application/language/nl/datepicker_lang.php
+++ application/language/nl/datepicker_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["datepicker_all_time"] = "Alle";
 $lang["datepicker_apply"] = "Toepassen";
 $lang["datepicker_cancel"] = "Annuleren";
@@ -7,12 +6,12 @@ $lang["datepicker_custom"] = "Aangepaste";
 $lang["datepicker_from"] = "Van";
 $lang["datepicker_last_30"] = "Vorige 30 Dagen";
 $lang["datepicker_last_7"] = "Vorige 7 Dagen";
-$lang["datepicker_last_financial_year"] = "";
+$lang["datepicker_last_financial_year"] = "Laatste fiscaal jaar";
 $lang["datepicker_last_month"] = "Vorige Maand";
 $lang["datepicker_last_year"] = "Vorig Jaar";
 $lang["datepicker_same_month_last_year"] = "Dezelfde maand vorig jaar";
 $lang["datepicker_same_month_to_same_day_last_year"] = "Dezelfde maand op dezelfde dag vorig jaar";
-$lang["datepicker_this_financial_year"] = "";
+$lang["datepicker_this_financial_year"] = "Huidige fiscaal jaar";
 $lang["datepicker_this_month"] = "Deze Maand";
 $lang["datepicker_this_year"] = "Dit Jaar";
 $lang["datepicker_to"] = "To";
