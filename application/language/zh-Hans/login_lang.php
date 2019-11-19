b7443be75616b40f8197e166b133608ca56921a3
Jintao-Yao <yjt_0221@126.com>
2019-11-19 09:16:59 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ospos/login
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/login/zh_Hans/

--- application/language/zh-Hans/login_lang.php
+++ application/language/zh-Hans/login_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
-$lang["login_gcaptcha"] = "";
+<?php
+$lang["login_gcaptcha"] = "我不是机器人。";
 $lang["login_go"] = "登入";
-$lang["login_invalid_gcaptcha"] = "";
-$lang["login_invalid_installation"] = "";
+$lang["login_invalid_gcaptcha"] = "无效，我不是机器人。";
+$lang["login_invalid_installation"] = "安装不正确，请检查您的php.ini文件。";
 $lang["login_invalid_username_and_password"] = "錯誤的帳號或密碼";
 $lang["login_login"] = "登入";
 $lang["login_password"] = "密碼";
