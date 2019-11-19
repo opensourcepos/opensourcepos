420957ecb784d381483dc0d587af1548e44757bb
Jintao-Yao <yjt_0221@126.com>
2019-11-19 08:59:21 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: ospos/attributes
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/attributes/zh_Hans/

--- application/language/zh-Hans/attributes_lang.php
+++ application/language/zh-Hans/attributes_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $lang["attributes_attribute_value_invalid_chars"] = "属性特征不能包含':' or '|'";
 $lang["attributes_confirm_delete"] = "确定要删除所选属性吗";
-$lang["attributes_confirm_restore"] = "";
+$lang["attributes_confirm_restore"] = "您确定要还原所选属性吗？";
 $lang["attributes_definition_cannot_be_deleted"] = "不能删除该特征属性";
-$lang["attributes_definition_error_adding_updating"] = "";
+$lang["attributes_definition_error_adding_updating"] = "无法添加或更新属性%1。 请检查错误日志。";
 $lang["attributes_definition_flags"] = "属性可见性";
 $lang["attributes_definition_group"] = "组";
 $lang["attributes_definition_id"] = "ID卡";
@@ -16,16 +15,16 @@ $lang["attributes_definition_successful_deleted"] = "已经成功删除";
 $lang["attributes_definition_successful_updating"] = "更新属性成功";
 $lang["attributes_definition_type"] = "属性类别";
 $lang["attributes_definition_type_required"] = "属性必填";
-$lang["attributes_definition_unit"] = "";
+$lang["attributes_definition_unit"] = "计量单位";
 $lang["attributes_definition_values"] = "属性特征值";
 $lang["attributes_new"] = "新属性";
 $lang["attributes_no_attributes_to_display"] = "没有物品需要显示";
 $lang["attributes_receipt_visibility"] = "小票";
-$lang["attributes_show_in_items"] = "";
-$lang["attributes_show_in_items_visibility"] = "";
-$lang["attributes_show_in_receipt"] = "";
-$lang["attributes_show_in_receivings"] = "";
+$lang["attributes_show_in_items"] = "在项目中显示";
+$lang["attributes_show_in_items_visibility"] = "物品";
+$lang["attributes_show_in_receipt"] = "在收据中显示";
+$lang["attributes_show_in_receivings"] = "在收据中显示";
 $lang["attributes_show_in_receivings_visibility"] = "";
-$lang["attributes_show_in_sales"] = "";
-$lang["attributes_show_in_sales_visibility"] = "";
-$lang["attributes_update"] = "";
+$lang["attributes_show_in_sales"] = "在销售中显示";
+$lang["attributes_show_in_sales_visibility"] = "销售";
+$lang["attributes_update"] = "更新属性";
