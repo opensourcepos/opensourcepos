bd52f0b00463589c4c1dfbefbb846ee697214a05
Robby Triadi Susanto <robbytriadi@gmail.com>
2020-01-05 16:50:08 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (175 of 175 strings)
    
    Translation: ospos/sales
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/sales/id/

--- application/language/id/sales_lang.php
+++ application/language/id/sales_lang.php
@@ -173,3 +173,5 @@ $lang["sales_work_order_number"] = "Nomor Perintah Kerja";
 $lang["sales_work_order_number_duplicate"] = "Nomor Perintah Kerja tidak boleh sama.";
 $lang["sales_work_order_sent"] = "Perintah Kerja dikirim ke";
 $lang["sales_work_order_unsent"] = "Perintah Kerja gagal dikirim ke";
+$lang["sales_nontaxed_ind"] = " . ";
+$lang["sales_taxed_ind"] = "T";
