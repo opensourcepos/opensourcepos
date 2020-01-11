35a6095d3022f1f3f8dbb91f1b284a82425b4e04
Emin Tufan Çetin <etcetin@gmail.com>
2020-01-11 13:01:11 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: ospos/login
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/login/tr/

--- application/language/tr/login_lang.php
+++ application/language/tr/login_lang.php
@@ -1,9 +1,9 @@
 <?php
-$lang["login_gcaptcha"] = "Ben Robot Değilim.";
+$lang["login_gcaptcha"] = "Ben robot değilim.";
 $lang["login_go"] = "Giriş";
-$lang["login_invalid_gcaptcha"] = "Robot Olmadığınızı Kanıtlayamadınız.";
-$lang["login_invalid_installation"] = "Yükleme doğru değil, php.ini dosyanızı kontrol edin.";
-$lang["login_invalid_username_and_password"] = "Kullanıcı adı/Şifre yanlış.";
+$lang["login_invalid_gcaptcha"] = "Robot olmadığınızı kanıtlayamadınız.";
+$lang["login_invalid_installation"] = "Yükleme doğru değil, php.ini dosyanızı gözden geçirin.";
+$lang["login_invalid_username_and_password"] = "Geçersiz Kullanıcı Adı veya Parola.";
 $lang["login_login"] = "Giriş";
-$lang["login_password"] = "Şifre";
+$lang["login_password"] = "Parola";
 $lang["login_username"] = "Kullanıcı Adı";
