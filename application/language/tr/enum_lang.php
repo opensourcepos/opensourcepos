d6147b4292c8983972873a51be1dbffb2db54539
Emin Tufan Çetin <etcetin@gmail.com>
2020-01-12 11:24:17 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: ospos/enum
    Translate-URL: http://weblate.jpeelaer.net/projects/ospos/enum/tr/

--- application/language/tr/enum_lang.php
+++ application/language/tr/enum_lang.php
@@ -1,8 +1,8 @@
 <?php
-$lang["enum_half_down"] = "Yarıya";
-$lang["enum_half_even"] = "Yarım Kadar";
+$lang["enum_half_down"] = "Yarım Aşağı";
+$lang["enum_half_even"] = "Yarım Çift";
 $lang["enum_half_five"] = "Beş Buçuk";
-$lang["enum_half_odd"] = "Sadece Yarım";
-$lang["enum_half_up"] = "Yarım Kadar";
-$lang["enum_round_down"] = "Aşağı yuvarlama";
-$lang["enum_round_up"] = "Hesabı yuvarlamak";
+$lang["enum_half_odd"] = "Yarım Tek";
+$lang["enum_half_up"] = "Yarım Yukarı";
+$lang["enum_round_down"] = "Aşağı Yuvarla";
+$lang["enum_round_up"] = "Yukarı Yuvarla";
