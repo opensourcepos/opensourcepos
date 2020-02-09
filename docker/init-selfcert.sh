a9077b344573dca4ef694e1ac94f78aef3be3e70
andriux1990 <neo@neo>
2020-02-09 07:20:16 -0600

    master

---
+++ docker/init-selfcert.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. ./.env
+
+domains=${OSPOS_DOMAIN_NAME}
+rsa_key_size=4096
+data_path="./data/certbot"
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### Downloading recommended TLS parameters ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
+
+
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker-compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
+
+
+echo "### Starting nginx ..."
+docker-compose -f ../docker-compose.yml up --force-recreate -d nginx
+echo
+
+
+#echo "### Reloading nginx ..."
+#docker-compose exec nginx nginx -s reload
---
+++ docker/install-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd docker
+
+# load local environment variables
+if [ ! -e ".env" ]; then
+  echo "The .env (environment variables) file is missing"
+  exit 1
+fi
+
+. ./.env
+
+docker-compose -f ../docker-compose.yml build
+
+/bin/bash ./init-letsencrypt.sh
