562443b8b5085144e7f6884e35090dd8cf5b21e3
jekkos-t520 <jeroen.peelaerts@gmail.com>
2020-02-28 23:49:24 +0100

    Overwrite backupGlobals + base_url generation in testing mode

--- application/config/config.php
+++ application/config/config.php
@@ -65,7 +65,9 @@ $config['db_log_enabled'] = FALSE;
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_ENV['FORCE_HTTPS']) && $_ENV['FORCE_HTTPS'] == 'true')) ? 'https' : 'http';
+$config['base_url'] .= '://' . (ENVIRONMENT == "testing") ? 'localhost' : $_SERVER['HTTP_HOST'] ;
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
