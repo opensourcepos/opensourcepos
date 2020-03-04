e82479e80071dcf14515c16317e6d6a17ea8797b
jekkos-t520 <jeroen.peelaerts@gmail.com>
2020-03-05 00:02:43 +0100

    Fix unit test

--- application/tests/libraries/Barcode_lib_test.php
+++ application/tests/libraries/Barcode_lib_test.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @backupGlobals disabled
  */
@@ -8,42 +7,33 @@ class Barcode_lib_test extends UnitTestCase
 	public function setUp()
 	{
 		$this->resetInstance();
-
+		
 		$this->obj = $this->newLibrary('Barcode_lib');
+
 	}
 
 	public function test_barcode_weight_first()
 	{
-		$config = $this->getMockBuilder('CI_Config')
-			->disableOriginalConstructor()
-			->setMethods(['item'])
-			->getMock();
-		$config->method('item')
-			->willReturn("02(\d{5})(\w{6})");
+        $this->CI->config->set_item('barcode_formats', json_encode(array("02(\d{5})(\w{6})")));
 
-		$item_number = "025000abcdef";
+		$item_number = "0250000123456";
 		$quantity = 0;
 		$this->obj->parse_barcode_fields($quantity, $item_number);
 
 		echo $quantity;
-		$this->assertEquals($quantity, 1);
-		$this->assertEquals($item_number, "025000123456");
+		$this->assertEquals($quantity, 123.456);
+		$this->assertEquals($item_number, "50000");
 	}
 
 	public function test_barcode_weight_last()
 	{
-		$config = $this->getMockBuilder('CI_Config')
-			->disableOriginalConstructor()
-			->setMethods(['item'])
-			->getMock();
-		$config->method('item')
-			->willReturn("02(\w{6})(\d{5})");
+        $this->CI->config->set_item('barcode_formats', json_encode(array("02(\w{6})(\d{5})")));
 
 		$item_number = "0212345650001";
 		$quantity = 0;
 		$this->obj->parse_barcode_fields($quantity, $item_number);
 
-		$this->assertEquals($quantity, 5.001);
+		$this->assertEquals($quantity, 50.001);
 		$this->assertEquals($item_number, 123456);
 	}
 }
