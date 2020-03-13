ecb21d2d5ec9c567315152b178e371f62d825673
objecttothis <17935339+objecttothis@users.noreply.github.com>
2020-03-13 16:38:18 +0400

    Same correction for removing attribute value

--- application/views/attributes/form.php
+++ application/views/attributes/form.php
@@ -128,7 +128,7 @@ $(document).ready(function()
 		}
 		else
 		{
-			$.post('<?php echo site_url($controller_name . "/delete_attribute_value/");?>' + value, {definition_id: definition_id});
+			$.post('<?php echo site_url($controller_name . "/delete_attribute_value/");?>' + escape(value), {definition_id: definition_id});
 		}
 		$(this).parents("li").remove();
 	};
