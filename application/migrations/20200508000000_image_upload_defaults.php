9b18072f1ef1c070d313f029f5477da24db168fe
objecttothis <objecttothis@gmail.com>
2020-05-08 14:23:12 +0400

    migration corrections

--- application/migrations/20200508000000_image_upload_defaults.php
+++ application/migrations/20200508000000_image_upload_defaults.php
@@ -10,17 +10,17 @@ class Migration_image_upload_defaults extends CI_Migration
 	public function up()
 	{
 		$image_values = array(
-			array('key' => 'allowed_types', 'value' => 'gif|jpg|png'),
-			array('key' => 'max_height', 'value' => '480'),
-			array('key' => 'max_size', 'value' => '100'),
-			array('key' => 'max_width', 'value' => '640'));
+			array('key' => 'image_allowed_types', 'value' => 'gif|jpg|png'),
+			array('key' => 'image_max_height', 'value' => '480'),
+			array('key' => 'image_max_size', 'value' => '100'),
+			array('key' => 'image_max_width', 'value' => '640'));
 
 		$this->db->insert_batch('app_config', $image_values);
 	}
 
 	public function down()
 	{
-		$this->db->where_in('key', array('allowed_types','max_height','max_size','max_width'));
+		$this->db->where_in('key', array('image_allowed_types','image_max_height','image_max_size','image_max_width'));
 		$this->db->delete('app_config');
 	}
 }
