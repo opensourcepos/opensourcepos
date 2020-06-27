687ae74a62ec414000f2104cc55f558fe00c2fc5
objecttothis <17935339+objecttothis@users.noreply.github.com>
2020-06-27 14:54:39 +0400

    Update footer.php
    replaced " with ' to prevent the string from being parsed for variables unnecessarily.

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -3,7 +3,7 @@
 
 	<div id="footer">
 		<div class="jumbotron push-spaces">
-			<strong><?php echo $this->lang->line('common_copryrights', date("Y")); ?> · 
+			<strong><?php echo $this->lang->line('common_copryrights', date('Y')); ?> · 
 			<a href="https://opensourcepos.org" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>  · 
   			<?php echo $this->config->item('application_version'); ?> - <?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></strong>.
 		</div>
