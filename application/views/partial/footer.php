af0eb218a35ceeeb9dce81c2891f1bdb5f390061
objecttothis <objecttothis@gmail.com>
2020-07-02 15:31:04 +0400

    Formatting Fixes

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -1,10 +1,10 @@
 		</div>
 	</div>
 
-	<div id="footer">
-		<div class="jumbotron push-spaces">
-			<strong><?php echo $this->lang->line('common_copryrights', date('Y')); ?> · 
-			<a href="https://opensourcepos.org" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>  · 
+	<div id='footer'>
+		<div class='jumbotron push-spaces'>
+			<strong><?php echo $this->lang->line('common_copryrights', date('Y')); ?> ·
+			<a href="https://opensourcepos.org" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>  ·
   			<?php echo $this->config->item('application_version'); ?> - <?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></strong>.
 		</div>
 	</div>
