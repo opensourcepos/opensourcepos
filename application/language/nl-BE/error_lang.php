5ec7e11c376c8e8127ebc1a830885ef60ec3b27a
jekkos <jeroen_peelaerts@hotmail.com>
2020-10-19 19:45:38 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/nl_BE/

--- application/language/nl-BE/error_lang.php
+++ application/language/nl-BE/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
-$lang["error_no_permission_module"] = "U hebt geen toegang tot de module genaamd ";
+<?php
+$lang["error_no_permission_module"] = "U hebt geen toegang tot de module genaamd";
 $lang["error_unknown"] = "onbekend";
