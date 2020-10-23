625fb584ede0b2b05ad6d7ce6bd71e51ec72bc02
Jeroen Peelaerts <jeroen.peelaerts@gmail.com>
2020-10-23 21:25:10 +0200

    Fix migration errors on clean install

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20171126100000;
+$config['migration_version'] = 20180225100000;
 
 /*
 |--------------------------------------------------------------------------
