d571734f982468edc896c59f0d35189a5ef66abe
Carl Hunter <cdhunter2@yahoo.com>
2020-12-02 06:22:29 -0500

    Fixed sorting of Quantity column in Customer Summary Report

--- application/models/reports/Summary_customers.php
+++ application/models/reports/Summary_customers.php
@@ -9,7 +9,7 @@ class Summary_customers extends Summary_report
 		return array(
 			array('customer_name' => $this->lang->line('reports_customer')),
 			array('sales' => $this->lang->line('reports_sales'), 'sorter' => 'number_sorter'),
-			array('quantity' => $this->lang->line('reports_quantity')),
+			array('quantity' => $this->lang->line('reports_quantity'), 'sorter' => 'number_sorter'),
 			array('subtotal' => $this->lang->line('reports_subtotal'), 'sorter' => 'number_sorter'),
 			array('tax' => $this->lang->line('reports_tax'), 'sorter' => 'number_sorter'),
 			array('total' => $this->lang->line('reports_total'), 'sorter' => 'number_sorter'),
