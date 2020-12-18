29031cbc76ab5359c881d40979fc32d29d1465de
Jeroen Peelaerts <jeroen.peelaerts@gmail.com>
2020-12-18 11:45:49 +0100

    Enable httpOnly for session cookie only (#1903)

--- application/config/config.php
+++ application/config/config.php
@@ -438,7 +438,7 @@ $config['cookie_prefix'] = '';
 $config['cookie_domain'] = '';
 $config['cookie_path'] = '/';
 $config['cookie_secure'] = FALSE;
-$config['cookie_httponly'] = FALSE;
+$config['cookie_httponly'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
