49ef2a2105c99c0c483707025b23d01fdce0581b
Jeroen Peelaerts <jeroen.peelaerts@gmail.com>
2020-12-18 11:45:49 +0100

    Enable httpOnly for session cookie only (#1903)

--- application/core/MY_Security.php
+++ application/core/MY_Security.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class MY_Security extends CI_Security
 {
-
     /**
      * CSRF Set Cookie with samesite
      *
@@ -15,18 +14,19 @@ class MY_Security extends CI_Security
         $expire = time() + $this->_csrf_expire;
         $secure_cookie = (bool)config_item('cookie_secure');
 
-        if ($secure_cookie && !is_https()) {
+        if ($secure_cookie && !is_https())
+        {
             return FALSE;
         }
 
         setcookie($this->_csrf_cookie_name,
             $this->_csrf_hash,
             ['samesite' => 'Strict',
-                'secure' => config_item('cookie_httponly'),
+                'secure' => $secure_cookie,
                 'expires' => $expire,
                 'path' => config_item('cookie_path'),
                 'domain' => config_item('cookie_domain'),
-                'httponly' => config_item('cookie_httponly')]);
+                'httponly' => FALSE]);
 
         log_message('info', 'CSRF cookie sent');
 
