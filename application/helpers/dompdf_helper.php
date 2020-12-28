3cc7b721ae652d8d8249bbc97c5bf3804607410e
Jeroen Peelaerts <jeroen.peelaerts@gmail.com>
2020-12-28 22:19:08 +0100

    Enable PHP rendering in DOMPdf (#3019)

--- application/helpers/dompdf_helper.php
+++ application/helpers/dompdf_helper.php
@@ -7,7 +7,7 @@
 function create_pdf($html, $filename = '')
 {
     // need to enable magic quotes for the
-    $dompdf = new Dompdf\Dompdf();
+    $dompdf = new Dompdf\Dompdf(array("isRemoteEnabled" => TRUE, "isPhpEnabled" => TRUE));
     $dompdf->loadHtml(str_replace(array("\n", "\r"), '', $html));
     $dompdf->render();
     
