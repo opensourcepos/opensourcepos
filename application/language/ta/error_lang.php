34e0c2888636712884591c65f5f65273d13a8860
Kumaran <kumaranar@gmail.com>
2021-04-21 19:09:30 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/ta/

--- application/language/ta/error_lang.php
+++ application/language/ta/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
-$lang["error_no_permission_module"] = "You do not have permission to access the module named";
-$lang["error_unknown"] = "Unexpected error";
+<?php
+$lang["error_no_permission_module"] = "பெயரிடப்பட்ட தொகுதியை பயன்படுத்த உங்களுக்கு அனுமதி இல்லை";
+$lang["error_unknown"] = "எதிர்பாராத பிழை";
