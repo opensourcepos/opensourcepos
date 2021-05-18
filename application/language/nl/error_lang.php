ac6e4219066de1aa319f3da3afd05a9e0919921e
BudsieBuds <bas_hubers@hotmail.com>
2021-05-18 17:43:36 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/nl/

--- application/language/nl/error_lang.php
+++ application/language/nl/error_lang.php
@@ -1,3 +1,3 @@
 <?php
-$lang["error_no_permission_module"] = "U heeft geen toegang tot de module genaamd";
+$lang["error_no_permission_module"] = "U bent niet gemachtigd voor toegang tot de module genaamd";
 $lang["error_unknown"] = "Onverwachte fout";
