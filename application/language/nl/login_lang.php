b0190e416faadbffe6551001c464fa9bd5d242b3
BudsieBuds <bas_hubers@hotmail.com>
2021-05-18 17:41:45 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/nl/

--- application/language/nl/login_lang.php
+++ application/language/nl/login_lang.php
@@ -1,9 +1,9 @@
 <?php
 $lang["login_gcaptcha"] = "Ik ben geen robot.";
-$lang["login_go"] = "Versturen";
+$lang["login_go"] = "Aanmelden";
 $lang["login_invalid_gcaptcha"] = "Ik ben geen robot is ongeldig.";
 $lang["login_invalid_installation"] = "De installatie is niet juist, controleer uw php.ini bestand.";
 $lang["login_invalid_username_and_password"] = "Ongeldige gebruikersnaam of wachtwoord.";
-$lang["login_login"] = "Inloggen";
+$lang["login_login"] = "Aanmelden";
 $lang["login_password"] = "Wachtwoord";
 $lang["login_username"] = "Gebruikersnaam";
