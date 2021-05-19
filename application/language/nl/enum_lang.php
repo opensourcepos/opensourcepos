8a904ba7492c2e3b7c28c481361e33335978fd6d
BudsieBuds <bas_hubers@hotmail.com>
2021-05-19 19:01:24 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/enum/nl/

--- application/language/nl/enum_lang.php
+++ application/language/nl/enum_lang.php
@@ -1,8 +1,8 @@
 <?php
-$lang["enum_half_down"] = "Half naar beneden";
-$lang["enum_half_even"] = "Half even";
-$lang["enum_half_five"] = "Half vijf";
-$lang["enum_half_odd"] = "Half oneven";
-$lang["enum_half_up"] = "Half naar boven";
+$lang["enum_half_down"] = "Halveren naar beneden";
+$lang["enum_half_even"] = "Halveren even";
+$lang["enum_half_five"] = "Halveren vijf";
+$lang["enum_half_odd"] = "Halveren oneven";
+$lang["enum_half_up"] = "Halveren naar boven";
 $lang["enum_round_down"] = "Naar beneden afronden";
 $lang["enum_round_up"] = "Naar boven afronden";
