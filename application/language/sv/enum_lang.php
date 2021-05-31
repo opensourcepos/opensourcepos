6ecd9a91c49531dd3e361ffe726e901af6977ac5
Mats Pålsson <caston@caston.mine.nu>
2021-05-31 12:18:02 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/enum/sv/

--- application/language/sv/enum_lang.php
+++ application/language/sv/enum_lang.php
@@ -1,8 +1,8 @@
 <?php
 $lang["enum_half_down"] = "Halv ner";
 $lang["enum_half_even"] = "Halv ojämn";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+$lang["enum_half_five"] = "Halv fem";
+$lang["enum_half_odd"] = "Halv ojämn";
+$lang["enum_half_up"] = "Halv upp";
+$lang["enum_round_down"] = "Avrunda ner";
+$lang["enum_round_up"] = "Avrunda upp";
