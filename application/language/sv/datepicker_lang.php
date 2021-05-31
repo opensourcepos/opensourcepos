274ad1afde54bd74d942bf24671d6ec3665eeb69
Mats Pålsson <caston@caston.mine.nu>
2021-05-31 11:37:24 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: opensourcepos/datepicker
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/datepicker/sv/

--- application/language/sv/datepicker_lang.php
+++ application/language/sv/datepicker_lang.php
@@ -1,22 +1,21 @@
-<?php 
-
-$lang["datepicker_all_time"] = "";
-$lang["datepicker_apply"] = "";
-$lang["datepicker_cancel"] = "";
-$lang["datepicker_custom"] = "";
-$lang["datepicker_from"] = "";
-$lang["datepicker_last_30"] = "";
-$lang["datepicker_last_7"] = "";
-$lang["datepicker_last_financial_year"] = "";
-$lang["datepicker_last_month"] = "";
-$lang["datepicker_last_year"] = "";
-$lang["datepicker_same_month_last_year"] = "";
-$lang["datepicker_same_month_to_same_day_last_year"] = "";
-$lang["datepicker_this_financial_year"] = "";
-$lang["datepicker_this_month"] = "";
-$lang["datepicker_this_year"] = "";
-$lang["datepicker_to"] = "";
-$lang["datepicker_today"] = "";
-$lang["datepicker_today_last_year"] = "";
-$lang["datepicker_weekstart"] = "";
-$lang["datepicker_yesterday"] = "";
+<?php
+$lang["datepicker_all_time"] = "All Tid";
+$lang["datepicker_apply"] = "Skicka";
+$lang["datepicker_cancel"] = "Avbryt";
+$lang["datepicker_custom"] = "Anpassa";
+$lang["datepicker_from"] = "Från";
+$lang["datepicker_last_30"] = "Senaste 30 dagar";
+$lang["datepicker_last_7"] = "Senaste 7 dagar";
+$lang["datepicker_last_financial_year"] = "Senaste bokföringsår";
+$lang["datepicker_last_month"] = "Senaste månaden";
+$lang["datepicker_last_year"] = "Senaste året";
+$lang["datepicker_same_month_last_year"] = "Samma månad föregående år";
+$lang["datepicker_same_month_to_same_day_last_year"] = "Samma månad till dag förra året";
+$lang["datepicker_this_financial_year"] = "Nuvarande bokföringsår";
+$lang["datepicker_this_month"] = "Nuvarande månad";
+$lang["datepicker_this_year"] = "Nuvarande år";
+$lang["datepicker_to"] = "Till";
+$lang["datepicker_today"] = "Idag";
+$lang["datepicker_today_last_year"] = "Idag föregående år";
+$lang["datepicker_weekstart"] = "0";
+$lang["datepicker_yesterday"] = "I går";
