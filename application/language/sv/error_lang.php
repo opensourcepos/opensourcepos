25b3de4f2f607ed6e514ced5ca01738e1aba68ea
Mats Pålsson <caston@caston.mine.nu>
2021-05-31 08:59:39 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/sv/

--- application/language/sv/error_lang.php
+++ application/language/sv/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
-$lang["error_no_permission_module"] = "";
-$lang["error_unknown"] = "";
+<?php
+$lang["error_no_permission_module"] = "Du har inte rättigheter till modulen";
+$lang["error_unknown"] = "Oväntat fel";
