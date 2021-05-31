dfb47060fa2fda64685befc85012bae6d648155b
Mats Pålsson <caston@caston.mine.nu>
2021-05-31 08:57:09 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/sv/

--- application/language/sv/bootstrap_tables_lang.php
+++ application/language/sv/bootstrap_tables_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
-$lang["tables_all"] = "";
-$lang["tables_columns"] = "";
-$lang["tables_hide_show_pagination"] = "";
-$lang["tables_loading"] = "";
-$lang["tables_page_from_to"] = "";
-$lang["tables_refresh"] = "";
-$lang["tables_rows_per_page"] = "";
-$lang["tables_toggle"] = "";
+<?php
+$lang["tables_all"] = "alla";
+$lang["tables_columns"] = "Kolumner";
+$lang["tables_hide_show_pagination"] = "Dölj/visa sida";
+$lang["tables_loading"] = "Laddar, ha tålamod...";
+$lang["tables_page_from_to"] = "Visar {0} till {1} av {2} rade";
+$lang["tables_refresh"] = "Ladda om";
+$lang["tables_rows_per_page"] = "{0} rader per sida";
+$lang["tables_toggle"] = "Växla";
