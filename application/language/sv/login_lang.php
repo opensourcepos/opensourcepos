3fb4b398eaa964e5c98246523b0855a12b8c23e1
Mats Pålsson <caston@caston.mine.nu>
2021-06-01 19:27:41 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/sv/

--- application/language/sv/login_lang.php
+++ application/language/sv/login_lang.php
@@ -1,6 +1,6 @@
 <?php
 $lang["login_gcaptcha"] = "Jag är inte en robot.";
-$lang["login_go"] = "Gå";
+$lang["login_go"] = "Logga in";
 $lang["login_invalid_gcaptcha"] = "Ogiltig jag är ingen robot.";
 $lang["login_invalid_installation"] = "Installationen är inte korrekt, kontrollera din php.ini fil.";
 $lang["login_invalid_username_and_password"] = "Ogiltigt användarnamn eller lösenord.";
