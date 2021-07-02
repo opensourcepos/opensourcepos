2e09cd31c2487a865b478399ffc3f26d795ff8bd
khao_lek <khao_lek@yahoo.com>
2021-07-02 08:07:58 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/th/

--- application/language/th/login_lang.php
+++ application/language/th/login_lang.php
@@ -1,8 +1,7 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "ฉันไม่ใช่หุ่นยนต์";
 $lang["login_go"] = "ไป";
-$lang["login_invalid_gcaptcha"] = "ไม่ถูกต้องฉันไม่ใช่หุ่นยนต์";
+$lang["login_invalid_gcaptcha"] = "กรุณาแสดงตัวตนว่าคุณไม่ใช่หุ่นยนต์";
 $lang["login_invalid_installation"] = "การติดตั้งไม่ถูกต้องตรวจสอบการตั้งค่าที่ไฟล์ php.ini ของคุณ";
 $lang["login_invalid_username_and_password"] = "ชื่อผู้ใช้/รหัส ไม่ถูกต้อง";
 $lang["login_login"] = "ลงชื่อเข้าใช้";
