e33c10e8c444ad5e2203b1ece7727277f9ceeef7
Ir. Anggun Nugroho, M.Kom <anggun.bali@gmail.com>
2021-07-07 20:11:46 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 87.5% (7 of 8 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/id/

--- application/language/id/login_lang.php
+++ application/language/id/login_lang.php
@@ -1,8 +1,7 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Saya bukan robot.";
 $lang["login_go"] = "Lanjutkan";
-$lang["login_invalid_gcaptcha"] = "Tidak valid saya bukan robot.";
+$lang["login_invalid_gcaptcha"] = "Silahkan verifikasi bahwa anda bukan robot.";
 $lang["login_invalid_installation"] = "Instalasi tidak benar, periksa file php.ini Anda.";
 $lang["login_invalid_username_and_password"] = "Nama Pengguna/Kata Kunci Salah.";
 $lang["login_login"] = "Masuk";
