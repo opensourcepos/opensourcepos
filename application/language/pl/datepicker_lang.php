393cee62aae7b65e68271432eb199591f05df79c
jacekz123 <jacek.z123@outlook.com>
2021-07-13 15:05:40 +0000

    Translated using Weblate (Polish)
    Currently translated at 55.0% (11 of 20 strings)
    
    Translation: opensourcepos/datepicker
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/datepicker/pl/

--- application/language/pl/datepicker_lang.php
+++ application/language/pl/datepicker_lang.php
@@ -1,16 +1,15 @@
-<?php 
-
-$lang["datepicker_all_time"] = "";
-$lang["datepicker_apply"] = "";
-$lang["datepicker_cancel"] = "";
-$lang["datepicker_custom"] = "";
-$lang["datepicker_from"] = "";
-$lang["datepicker_last_30"] = "";
-$lang["datepicker_last_7"] = "";
-$lang["datepicker_last_financial_year"] = "";
-$lang["datepicker_last_month"] = "";
-$lang["datepicker_last_year"] = "";
-$lang["datepicker_same_month_last_year"] = "";
+<?php
+$lang["datepicker_all_time"] = "Cały czas";
+$lang["datepicker_apply"] = "Zastosuj";
+$lang["datepicker_cancel"] = "Anuluj";
+$lang["datepicker_custom"] = "Niestandardowy";
+$lang["datepicker_from"] = "Od";
+$lang["datepicker_last_30"] = "Ostatnie 30 dni";
+$lang["datepicker_last_7"] = "Ostatnie 7 dni";
+$lang["datepicker_last_financial_year"] = "Ostatni rok fiskalny";
+$lang["datepicker_last_month"] = "Ostatni miesiąc";
+$lang["datepicker_last_year"] = "Ostatni rok";
+$lang["datepicker_same_month_last_year"] = "Ten sam miesiąc w zeszłym roku";
 $lang["datepicker_same_month_to_same_day_last_year"] = "";
 $lang["datepicker_this_financial_year"] = "";
 $lang["datepicker_this_month"] = "";
