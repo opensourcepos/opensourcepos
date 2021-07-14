6ee8757b123e37e9cb745a5538571af26470007b
gurulenin <guruleninn@gmail.com>
2021-07-14 02:27:19 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/ta/

--- application/language/ta/login_lang.php
+++ application/language/ta/login_lang.php
@@ -2,7 +2,7 @@
 
 $lang["login_gcaptcha"] = "ரோபோ ஸ்கிரிப்ட்கள் அல்ல.";
 $lang["login_go"] = "செல்";
-$lang["login_invalid_gcaptcha"] = "ரோபோ ஸ்கிரிப்ட்கள் அல்ல.";
+$lang["login_invalid_gcaptcha"] = "நீங்கள் தானியங்கி அல்ல என்பதை உறுதி செய்யவும்";
 $lang["login_invalid_installation"] = "நிறுவியத்தில் தவறு , php.ini கோப்பை சரி பார்க்கவும் .";
 $lang["login_invalid_username_and_password"] = "பயனர்பெயர் அல்லது கடவுச்சொல் தவறு.";
 $lang["login_login"] = "உள்நுழைய";
