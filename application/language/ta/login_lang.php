bfc1c2e55e5fdfdc729b29db4a4bdbd3e7d4c94d
gurulenin <guruleninn@gmail.com>
2021-07-15 02:39:00 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/ta/

--- application/language/ta/login_lang.php
+++ application/language/ta/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "ரோபோ ஸ்கிரிப்ட்கள் அல்ல.";
 $lang["login_go"] = "செல்";
-$lang["login_invalid_gcaptcha"] = "நீங்கள் தானியங்கி அல்ல என்பதை உறுதி செய்யவும்";
+$lang["login_invalid_gcaptcha"] = "நீங்கள் தானியங்கி அல்ல என்பதை உறுதி செய்யவும்.";
 $lang["login_invalid_installation"] = "நிறுவியத்தில் தவறு , php.ini கோப்பை சரி பார்க்கவும் .";
 $lang["login_invalid_username_and_password"] = "பயனர்பெயர் அல்லது கடவுச்சொல் தவறு.";
 $lang["login_login"] = "உள்நுழைய";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "விடுபதிகை";
+$lang["login_migration_needed"] = "% 1 க்கு தரவுத்தள இடம்பெயர்வு உள்நுழைந்த பிறகு தொடங்கும்.";
 $lang["login_password"] = "கடவுச்சொல்";
 $lang["login_username"] = "பயனர்பெயர்";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "% 1 க்கு வருக!";
