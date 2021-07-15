8d0c5c6ee99e4f96adf21fdd84c639e49e465a02
teddy tang <chtang28c@gmail.com>
2021-07-15 01:14:52 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/zh_Hant/

--- application/language/zh-Hant/login_lang.php
+++ application/language/zh-Hant/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "我不是機器人。";
 $lang["login_go"] = "登入";
 $lang["login_invalid_gcaptcha"] = "請確認您不是機器人。";
 $lang["login_invalid_installation"] = "安裝不正確，請檢查您的php.ini文件。";
 $lang["login_invalid_username_and_password"] = "帳號或密碼錯誤。";
 $lang["login_login"] = "登入";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "登出";
+$lang["login_migration_needed"] = "登錄後將開始向 %1 的數據庫遷移。";
 $lang["login_password"] = "密碼";
 $lang["login_username"] = "帳號";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "歡迎來到 %1！";
