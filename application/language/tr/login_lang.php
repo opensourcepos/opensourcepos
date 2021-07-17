c38fc60f6a29ac5c0364ca8a4b864149c9d74c4e
Emin Tufan Çetin <etcetin@gmail.com>
2021-07-17 17:56:51 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/tr/

--- application/language/tr/login_lang.php
+++ application/language/tr/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Ben robot değilim.";
 $lang["login_go"] = "Giriş";
-$lang["login_invalid_gcaptcha"] = "Robot olmadığınızı kanıtlayamadınız.";
+$lang["login_invalid_gcaptcha"] = "Robot olmadığınızı kanıtlayınız.";
 $lang["login_invalid_installation"] = "Yükleme doğru değil, php.ini dosyanızı gözden geçirin.";
-$lang["login_invalid_username_and_password"] = "Geçersiz Kullanıcı Adı veya Parola.";
+$lang["login_invalid_username_and_password"] = "Geçersiz kullanıcı adı ve/veya parola.";
 $lang["login_login"] = "Giriş";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Çıkış";
+$lang["login_migration_needed"] = "Girişten sonra %1 veri tabanına göç başlayacak.";
 $lang["login_password"] = "Parola";
 $lang["login_username"] = "Kullanıcı Adı";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "%1'e Hoş Geldiniz!";
