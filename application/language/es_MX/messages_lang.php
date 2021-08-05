9d320772f5fbc3a7ab62b082fdd159cd31025851
sonnysk76 <shurtado976@gmail.com>
2021-08-05 02:00:34 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/es_MX/

--- application/language/es_MX/messages_lang.php
+++ application/language/es_MX/messages_lang.php
@@ -1,14 +1,13 @@
-<?php 
-
-$lang["messages_first_name"] = "";
-$lang["messages_last_name"] = "";
-$lang["messages_message"] = "";
-$lang["messages_message_placeholder"] = "";
-$lang["messages_message_required"] = "";
-$lang["messages_multiple_phones"] = "";
-$lang["messages_phone"] = "";
-$lang["messages_phone_number_required"] = "";
-$lang["messages_phone_placeholder"] = "";
-$lang["messages_sms_send"] = "";
-$lang["messages_successfully_sent"] = "";
-$lang["messages_unsuccessfully_sent"] = "";
+<?php
+$lang["messages_first_name"] = "Nombre(s)";
+$lang["messages_last_name"] = "Apellido(s)";
+$lang["messages_message"] = "Mensaje";
+$lang["messages_message_placeholder"] = "Tu mensaje aquí...";
+$lang["messages_message_required"] = "Requiere mensaje";
+$lang["messages_multiple_phones"] = "(En caso de varios destinatarios, ingrese los números de teléfono separado por comas)";
+$lang["messages_phone"] = "Número de teléfono";
+$lang["messages_phone_number_required"] = "Requiere de número telefónico";
+$lang["messages_phone_placeholder"] = "Números de teléfonos celulares aquí...";
+$lang["messages_sms_send"] = "Enviar SMS";
+$lang["messages_successfully_sent"] = "Mensaje enviado exitosamente a: ";
+$lang["messages_unsuccessfully_sent"] = "Mensaje no enviado a: ";
