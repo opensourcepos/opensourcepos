29bdb7b75e08fc463d00a8c5ed9dc51b499f355e
Johntini <ekardian@gmail.com>
2021-08-20 01:56:05 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/es/

--- application/language/es/login_lang.php
+++ application/language/es/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "No soy un robot.";
 $lang["login_go"] = "Ir";
-$lang["login_invalid_gcaptcha"] = "Inválido, no soy un robot.";
+$lang["login_invalid_gcaptcha"] = "Por favor verifique si usted no es un robot.";
 $lang["login_invalid_installation"] = "La instalación no es correcta, comprueba el fichero php.ini.";
-$lang["login_invalid_username_and_password"] = "Usuario o Contraseña no válidos.";
+$lang["login_invalid_username_and_password"] = "Usuario y/o Contraseña no validos.";
 $lang["login_login"] = "Iniciar Sesión";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Cerrar sesión";
+$lang["login_migration_needed"] = "La migración de la base de datos a %1 se iniciará después del inicio de sesión.";
 $lang["login_password"] = "Contraseña";
 $lang["login_username"] = "Usuario";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Bienvenido a %1!";
