2267bf6896c01bd31a353bc264c186cb4f144f66
FrancescoUK <francesco.lodolo.uk@gmail.com>
2021-10-07 11:41:08 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 96.2% (51 of 53 strings)
    
    Translation: opensourcepos/customers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/customers/en_GB/

--- application/language/en-GB/customers_lang.php
+++ application/language/en-GB/customers_lang.php
@@ -1,8 +1,7 @@
-<?php 
-
+<?php
 $lang["customers_account_number"] = "Account #";
 $lang["customers_account_number_duplicate"] = "Account Number is already present in the database";
-$lang["customers_available_points"] = "Available points";
+$lang["customers_available_points"] = "Points Value";
 $lang["customers_average"] = "Average spent";
 $lang["customers_avg_discount"] = "Average discount";
 $lang["customers_basic_information"] = "Information";
