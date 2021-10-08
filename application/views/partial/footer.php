986ab7e86c98596bbb872f5f7bc87d37c886c3ce
jekkos <jeroen.peelaerts@gmail.com>
2021-10-08 23:05:27 +0200

    Make footer revision clickable (#3306)

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -5,7 +5,7 @@
 		<div class="jumbotron push-spaces">
 			<strong><?php echo $this->lang->line('common_copyrights', date('Y')); ?> · 
 			<a href="https://opensourcepos.org" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>  · 
-  			<?php echo $this->config->item('application_version'); ?> - <?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></strong>.
+			<?php echo $this->config->item('application_version'); ?> - <a target="_blank" href="https://github.com/opensourcepos/opensourcepos/commit/<?php echo $this->config->item('commit_sha1'); ?>"><?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></a></strong>.
 		</div>
 	</div>
 </body>
