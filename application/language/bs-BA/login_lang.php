c9716a890afb28c33dd3be2c8c2a64bdf6c1b96b
Cedo <vokic@gradiska.com>
2021-10-30 23:28:19 +0000

    Translated using Weblate (Bosnian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/bs/

--- application/language/bs-BA/login_lang.php
+++ application/language/bs-BA/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Ja nisam robot.";
 $lang["login_go"] = "Idi";
-$lang["login_invalid_gcaptcha"] = "Pogrešno ja nisam robot.";
+$lang["login_invalid_gcaptcha"] = "Molimo potvrdite da niste robot.";
 $lang["login_invalid_installation"] = "Instalacija nije ispravna, provjerite vašu php.ini datoteku.";
-$lang["login_invalid_username_and_password"] = "Pogrešno korisničko ime ili lozinka";
+$lang["login_invalid_username_and_password"] = "Pogrešno korisničko ime i/ili lozinka.";
 $lang["login_login"] = "Prijava";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Odjava";
+$lang["login_migration_needed"] = "Migracija baze podataka na %1 će početi nakon prijavljivanja.";
 $lang["login_password"] = "Lozinka";
 $lang["login_username"] = "Korisničko ime";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Dobrodošli u %1!";
