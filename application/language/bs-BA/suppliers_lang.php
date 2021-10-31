4d506fec09a4834acdc67292b3b61f35f39f7d17
Cedo <vokic@gradiska.com>
2021-10-31 09:53:41 +0000

    Translated using Weblate (Bosnian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: opensourcepos/suppliers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/suppliers/bs/

--- application/language/bs-BA/suppliers_lang.php
+++ application/language/bs-BA/suppliers_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["suppliers_account_number"] = "Broj računa";
 $lang["suppliers_agency_name"] = "Naziv agencije";
 $lang["suppliers_cannot_be_deleted"] = "Brisanje izabranih dobavljača nije uspjelo. Jedan ili više njih imaju prodaju.";
@@ -9,10 +8,10 @@ $lang["suppliers_company_name_required"] = "Dobavljač je obavezan.";
 $lang["suppliers_confirm_delete"] = "Da li ste sigurni da želite da izbrišete izabranog dobavljača?";
 $lang["suppliers_confirm_restore"] = "Da li ste sigurni da želite vratiti izabranog dobavljača?";
 $lang["suppliers_cost"] = "Trošak dobavljača";
-$lang["suppliers_error_adding_updating"] = "Ažuriranje ili dodavanje dobavljača nije uspjelo";
+$lang["suppliers_error_adding_updating"] = "Ažuriranje ili dodavanje dobavljača nije uspjelo.";
 $lang["suppliers_goods"] = "Roba dobavljača";
 $lang["suppliers_new"] = "Novi dobavljač";
-$lang["suppliers_none_selected"] = "Niste izabrali nijednog dobavljača za brisanje";
+$lang["suppliers_none_selected"] = "Niste izabrali nijednog dobavljača za brisanje.";
 $lang["suppliers_one_or_multiple"] = "Dobavljač(i)";
 $lang["suppliers_successful_adding"] = "Uspješno ste dodali dobavljača";
 $lang["suppliers_successful_deleted"] = "Uspješno ste Izbrisali dobavljača";
