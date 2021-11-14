59a17255013d525e96e81fc2ffa782b048576387
Cedo <vokic@gradiska.com>
2021-11-14 16:29:06 +0000

    Translated using Weblate (Bosnian)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/enum/bs/

--- application/language/bs-BA/enum_lang.php
+++ application/language/bs-BA/enum_lang.php
@@ -1,8 +1,8 @@
 <?php
 $lang["enum_half_down"] = "Pola dolje";
-$lang["enum_half_even"] = "Napola izjednačeno";
-$lang["enum_half_five"] = "Polovina pet";
+$lang["enum_half_even"] = "Pola ravnomjerno";
+$lang["enum_half_five"] = "Pola na pet";
 $lang["enum_half_odd"] = "Pola neparno";
-$lang["enum_half_up"] = "Polovinu gore";
+$lang["enum_half_up"] = "Pola gore";
 $lang["enum_round_down"] = "Zaokruži naniže";
 $lang["enum_round_up"] = "Zaokruži naviše";
