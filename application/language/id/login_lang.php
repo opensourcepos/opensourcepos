01a9810a0c48bbce627543a92901661304e6fcb7
MushlihTechFoundation <mohammaddrifanmushlih17@gmail.com>
2021-11-19 23:19:27 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/id/

--- application/language/id/login_lang.php
+++ application/language/id/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Saya bukan robot.";
 $lang["login_go"] = "Lanjutkan";
-$lang["login_invalid_gcaptcha"] = "Silahkan verifikasi bahwa anda bukan robot.";
+$lang["login_invalid_gcaptcha"] = "Tolong buktikan bahwa anda bukan robot.";
 $lang["login_invalid_installation"] = "Instalasi tidak benar, periksa file php.ini Anda.";
-$lang["login_invalid_username_and_password"] = "Nama Pengguna/Kata Kunci Salah.";
+$lang["login_invalid_username_and_password"] = "Nama Pengguna Atau Sandi Salah.";
 $lang["login_login"] = "Masuk";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Keluar";
+$lang["login_migration_needed"] = "Migrasi basis data untuk %1 akan di mulai setelah masuk.";
 $lang["login_password"] = "Kata kunci";
 $lang["login_username"] = "Nama Anda";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Selamat Datang di %1!";
