df932b887056a23edc7fc6d3f97cf7a223f90f26
programmer111213 <anon-50@mail.ru>
2021-11-22 06:06:55 +0000

    Translated using Weblate (Russian)
    Currently translated at 25.0% (3 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/ru/

--- application/language/ru/messages_lang.php
+++ application/language/ru/messages_lang.php
@@ -1,14 +1,13 @@
-<?php 
-
+<?php
 $lang["messages_first_name"] = "";
 $lang["messages_last_name"] = "";
-$lang["messages_message"] = "";
+$lang["messages_message"] = "Сообщение";
 $lang["messages_message_placeholder"] = "";
 $lang["messages_message_required"] = "";
 $lang["messages_multiple_phones"] = "";
-$lang["messages_phone"] = "";
+$lang["messages_phone"] = "Номер телефона";
 $lang["messages_phone_number_required"] = "";
 $lang["messages_phone_placeholder"] = "";
-$lang["messages_sms_send"] = "";
+$lang["messages_sms_send"] = "Отправить SMS";
 $lang["messages_successfully_sent"] = "";
 $lang["messages_unsuccessfully_sent"] = "";
