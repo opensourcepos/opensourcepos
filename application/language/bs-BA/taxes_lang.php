24cb0247d57e5d38a36913b52074db81217d5cf7
Cedo <vokic@gradiska.com>
2021-12-04 17:57:49 +0000

    Translated using Weblate (Bosnian)
    Currently translated at 100.0% (78 of 78 strings)
    
    Translation: opensourcepos/taxes
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/taxes/bs/

--- application/language/bs-BA/taxes_lang.php
+++ application/language/bs-BA/taxes_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang["taxes_add_exception"] = "Dodaj izuzetak";
 $lang["taxes_cascade"] = "Kaskada";
-$lang["taxes_cascade_sequence"] = "Kaskadni slijed";
+$lang["taxes_cascade_sequence"] = "Kaskadni redoslijed";
 $lang["taxes_city"] = "Mjesto";
 $lang["taxes_code"] = "Šifra poreza";
 $lang["taxes_confirm_delete"] = "Da li ste sigurni da želite da izbrišete ovu šifru poreza? Ova radnja se ne može opozvati";
@@ -16,11 +16,11 @@ $lang["taxes_new"] = "Nova šifra poreza";
 $lang["taxes_no_taxes"] = "";
 $lang["taxes_no_taxes_to_display"] = "Nije dostupna nijedan šifra poreza";
 $lang["taxes_reporting_authority"] = "Organ za izvještavanje";
-$lang["taxes_round_half_down"] = "Pola naniže";
-$lang["taxes_round_half_even"] = "Napola izjednačeno";
+$lang["taxes_round_half_down"] = "Pola dolje";
+$lang["taxes_round_half_even"] = "Pola ravnomjerno";
 $lang["taxes_round_half_odd"] = "Pola neparno";
-$lang["taxes_round_half_up"] = "Polovinu gore";
-$lang["taxes_rounding_code"] = "Zaokruživanje";
+$lang["taxes_round_half_up"] = "Pola gore";
+$lang["taxes_rounding_code"] = "Kod zaokruživanja";
 $lang["taxes_sales_tax"] = "Porez na promet";
 $lang["taxes_sales_tax_by_invoice"] = "Porez na promet po fakturi";
 $lang["taxes_sequence"] = "Sekv.";
