67881c172f9ee0793127c803e5c16ccf7c3773f5
jacekz123 <jacek.z123@outlook.com>
2022-01-06 20:09:56 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: opensourcepos/datepicker
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/datepicker/pl/

--- application/language/pl/datepicker_lang.php
+++ application/language/pl/datepicker_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["datepicker_all_time"] = "Cały czas";
 $lang["datepicker_apply"] = "Zastosuj";
 $lang["datepicker_cancel"] = "Anuluj";
@@ -11,12 +10,12 @@ $lang["datepicker_last_financial_year"] = "Ostatni rok fiskalny";
 $lang["datepicker_last_month"] = "Ostatni miesiąc";
 $lang["datepicker_last_year"] = "Ostatni rok";
 $lang["datepicker_same_month_last_year"] = "Ten sam miesiąc w zeszłym roku";
-$lang["datepicker_same_month_to_same_day_last_year"] = "";
-$lang["datepicker_this_financial_year"] = "";
-$lang["datepicker_this_month"] = "";
-$lang["datepicker_this_year"] = "";
-$lang["datepicker_to"] = "";
-$lang["datepicker_today"] = "";
-$lang["datepicker_today_last_year"] = "";
-$lang["datepicker_weekstart"] = "";
-$lang["datepicker_yesterday"] = "";
+$lang["datepicker_same_month_to_same_day_last_year"] = "Ten sam dzień w tym samym miesiącu rok temu";
+$lang["datepicker_this_financial_year"] = "Obecny rok fiskalny";
+$lang["datepicker_this_month"] = "Obecny miesiąc";
+$lang["datepicker_this_year"] = "Obecny rok";
+$lang["datepicker_to"] = "Do";
+$lang["datepicker_today"] = "Dzisiaj";
+$lang["datepicker_today_last_year"] = "Dzisiaj rok temu";
+$lang["datepicker_weekstart"] = "0";
+$lang["datepicker_yesterday"] = "Wczoraj";
