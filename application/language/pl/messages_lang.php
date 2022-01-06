2c331b6244a8473eb923e1feff1633ac2fe592f9
jacekz123 <jacek.z123@outlook.com>
2022-01-06 20:16:06 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/pl/

--- application/language/pl/messages_lang.php
+++ application/language/pl/messages_lang.php
@@ -1,14 +1,13 @@
-<?php 
-
+<?php
 $lang["messages_first_name"] = "Pierwsze imię";
-$lang["messages_last_name"] = "";
-$lang["messages_message"] = "";
-$lang["messages_message_placeholder"] = "";
-$lang["messages_message_required"] = "";
-$lang["messages_multiple_phones"] = "";
-$lang["messages_phone"] = "";
-$lang["messages_phone_number_required"] = "";
-$lang["messages_phone_placeholder"] = "";
-$lang["messages_sms_send"] = "";
-$lang["messages_successfully_sent"] = "";
-$lang["messages_unsuccessfully_sent"] = "";
+$lang["messages_last_name"] = "Nazwisko";
+$lang["messages_message"] = "Wiadomość";
+$lang["messages_message_placeholder"] = "Twoja wiadomość tutaj...";
+$lang["messages_message_required"] = "Wiadomość wymagana";
+$lang["messages_multiple_phones"] = "(W przypadku wielu odbiorców, wpisz numery telefonów oddzielone przecinkami)";
+$lang["messages_phone"] = "Numer telefonu";
+$lang["messages_phone_number_required"] = "Numer telefonu wymagany";
+$lang["messages_phone_placeholder"] = "Numer(y) telefonu tutaj...";
+$lang["messages_sms_send"] = "Wyślij SMS";
+$lang["messages_successfully_sent"] = "Wiadomość pomyślnie wysłana do: ";
+$lang["messages_unsuccessfully_sent"] = "Wiadomość nie została wysłana z powodzeniem do: ";
