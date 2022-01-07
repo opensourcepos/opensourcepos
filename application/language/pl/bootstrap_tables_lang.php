f3e41a4535b35b1cd38884f7ca75203318185b9f
jacekz123 <jacek.z123@outlook.com>
2022-01-07 12:15:07 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/pl/

--- application/language/pl/bootstrap_tables_lang.php
+++ application/language/pl/bootstrap_tables_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $lang["tables_all"] = "wszystko";
-$lang["tables_columns"] = "";
-$lang["tables_hide_show_pagination"] = "";
-$lang["tables_loading"] = "";
-$lang["tables_page_from_to"] = "";
-$lang["tables_refresh"] = "";
-$lang["tables_rows_per_page"] = "";
-$lang["tables_toggle"] = "";
+$lang["tables_columns"] = "Kolumny";
+$lang["tables_hide_show_pagination"] = "Ukryj/pokaż paginację";
+$lang["tables_loading"] = "Ładowanie, proszę czekać...";
+$lang["tables_page_from_to"] = "Pokazuję od {0} do {1} z {2} wierszy";
+$lang["tables_refresh"] = "Odśwież";
+$lang["tables_rows_per_page"] = "{0} wierszy na stronę";
+$lang["tables_toggle"] = "Włącz";
