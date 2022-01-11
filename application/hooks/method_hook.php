d40a2d37869c9896ee8683f48b7aab874f5c025a
jekkos <jekkos@users.noreply.github.com>
2022-01-11 22:17:34 +0100

    Use anchor in supplier regex (#3402)
    Adding a unit test to check the url patterns allowed/disallowed would be a good idea. I should practice what I preach.

--- application/hooks/method_hook.php
+++ application/hooks/method_hook.php
@@ -5,7 +5,7 @@ function validate_method()
 
     $url = $_SERVER['REQUEST_URI'];
 
-    $post_required = preg_match('/(save|delete*|remove*)\/?\d*?/', $url);
+    $post_required = preg_match('/\/(save|delete*|remove*)\/?\d*?/', $url);
 
     if($post_required && $_SERVER["REQUEST_METHOD"] != "POST" && empty($_POST))
     {
