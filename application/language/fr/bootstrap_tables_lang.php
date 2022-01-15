f7bbc7c634edfe806e5e70754d114b054a9c3562
Nicolas Hurtubise <316k@legtux.org>
2022-01-15 21:58:40 +0000

    Translated using Weblate (French)
    Currently translated at 87.5% (7 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/fr/

--- application/language/fr/bootstrap_tables_lang.php
+++ application/language/fr/bootstrap_tables_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $lang["tables_all"] = "Tous";
 $lang["tables_columns"] = "Colonnes";
 $lang["tables_hide_show_pagination"] = "Masquer/Afficher la pagination";
-$lang["tables_loading"] = "Chargement en cours, patientez, s?il vous pla?t ...";
+$lang["tables_loading"] = "Chargement en cours, patientez, s'il vous plaît ...";
 $lang["tables_page_from_to"] = "Affichage des lignes {0} à {1} sur {2} lignes au total";
-$lang["tables_refresh"] = "Rafra?chir";
+$lang["tables_refresh"] = "Rafraîchir";
 $lang["tables_rows_per_page"] = "{0} lignes par page";
 $lang["tables_toggle"] = "Alterner";
