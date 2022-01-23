d3906cbbc4b93a998446902d67032a6f331ad1d1
Elio Enzo Papais <support@infoportal.it>
2022-01-23 21:10:06 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: opensourcepos/item_kits
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/item_kits/it/

--- application/language/it/item_kits_lang.php
+++ application/language/it/item_kits_lang.php
@@ -1,21 +1,20 @@
-<?php 
-
+<?php
 $lang["item_kits_add_item"] = "Aggiungi Articolo";
 $lang["item_kits_all"] = "Tutti";
 $lang["item_kits_cannot_be_deleted"] = "Eliminazione Kit Articolo fallita.";
 $lang["item_kits_confirm_delete"] = "Sei sicuro di voler eliminare i seguenti Kit di Articoli?";
 $lang["item_kits_confirm_restore"] = "Sei sicuro di voler ripristinare i Kit Articolo selezionati?";
 $lang["item_kits_description"] = "Descrizione Kit Articoli";
-$lang["item_kits_discount"] = "";
-$lang["item_kits_discount_fixed"] = "";
+$lang["item_kits_discount"] = "Sconto";
+$lang["item_kits_discount_fixed"] = "Sconto fisso";
 $lang["item_kits_discount_percent"] = "Percentuale Sconto";
-$lang["item_kits_discount_type"] = "";
+$lang["item_kits_discount_type"] = "Tipo sconto";
 $lang["item_kits_error_adding_updating"] = "Aggiunta o modifica Kit Articoli fallita.";
 $lang["item_kits_find_kit_item"] = "Kit Articoli";
 $lang["item_kits_info"] = "Informazioni Kit";
 $lang["item_kits_item"] = "Articolo";
-$lang["item_kits_item_kit_number"] = "";
-$lang["item_kits_item_kit_number_duplicate"] = "";
+$lang["item_kits_item_kit_number"] = "Codice a Barre";
+$lang["item_kits_item_kit_number_duplicate"] = "Il numero del kit dell'articolo è già presente nel database.";
 $lang["item_kits_item_number"] = "";
 $lang["item_kits_item_number_duplicate"] = "";
 $lang["item_kits_items"] = "Articoli";
