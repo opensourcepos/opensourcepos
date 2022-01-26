e6072ee9fa2cfe79883fa6e6af9a684782847918
Elio Enzo Papais <support@infoportal.it>
2022-01-26 15:24:41 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/it/

--- application/language/it/login_lang.php
+++ application/language/it/login_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang["login_gcaptcha"] = "Non sono un robot.";
 $lang["login_go"] = "Go";
-$lang["login_invalid_gcaptcha"] = "Non valido Non sono un robot.";
+$lang["login_invalid_gcaptcha"] = "Verifica di non essere un robot.";
 $lang["login_invalid_installation"] = "L'installazione non è corretta, controlla il tuo file php.ini.";
 $lang["login_invalid_username_and_password"] = "Username o Password non validi.";
 $lang["login_login"] = "Login";
