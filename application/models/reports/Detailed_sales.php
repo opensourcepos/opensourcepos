18684adeda132bce6cdfe2302c323115a5257e57
jekkos <jekkos@users.noreply.github.com>
2022-03-31 00:05:07 +0200

    Fix transaction summary for serialized items (#3445)

--- application/models/reports/Detailed_sales.php
+++ application/models/reports/Detailed_sales.php
@@ -169,7 +169,7 @@ class Detailed_sales extends Report
 				$this->db->join('attribute_links', 'attribute_links.item_id = sales_items_temp.item_id AND attribute_links.sale_id = sales_items_temp.sale_id AND definition_id IN (' . implode(',', $inputs['definition_ids']) . ')', 'left');
 				$this->db->join('attribute_values', 'attribute_values.attribute_id = attribute_links.attribute_id', 'left');
 			}
-			$this->db->group_by('sales_items_temp.sale_id, sales_items_temp.item_id');
+			$this->db->group_by('sales_items_temp.sale_id, sales_items_temp.item_id, sales_items_temp.line');
 			$this->db->where('sales_items_temp.sale_id', $value['sale_id']);
 			$data['details'][$key] = $this->db->get()->result_array();
 
