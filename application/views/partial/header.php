7b3eb48a5ac3f2acf896356c068ff6d1e52a9d09
Vishal Thakur <64231702+vishal262@users.noreply.github.com>
2022-03-31 02:47:39 +0530

    Added meta tag for mobile responsive in header.php
    Mobile responsive is not working properly so I have added meta tag

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<base href="<?php echo base_url();?>" />
 	<title><?php echo $this->config->item('company') . ' | ' . $this->lang->line('common_powered_by') . ' OSPOS ' . $this->config->item('application_version') ?></title>
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
