4afe734bb883a4d09750fdfdf6d1edac5a4b9cd3
objecttothis <objecttothis@gmail.com>
2022-03-31 21:26:46 +0400

    CI 4 Changes - Modified migration function

--- app/Libraries/MY_Migration.php
+++ app/Libraries/MY_Migration.php
@@ -15,16 +15,17 @@ class MY_Migration extends MigrationRunner {
 	public function get_current_version(): string
 	{
 		$builder = $this->db->table('migrations');
-		$builder->select('')
+		$builder->select('version');
+		return $builder->get()->getRow()->version;
 	}
 
 	public function is_latest(): bool
 	{
 		$last_migration = $this->get_last_migration();
 		$last_version = $this->getMigrationNumber($last_migration);
-		$current_version = $this->get_current_version();	//TODO: Need to figure out how to get current version in CI4.  I think CI4 just skips all this and uses latest() to both check and then migrate.
+		$current_version = $this->get_current_version();
 
-		return $last_version == $current_version;	//TODO: ===
+		return $last_version == $current_version;
 	}
 
 	public function up(): void
