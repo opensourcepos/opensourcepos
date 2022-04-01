17973151e495bc1653135c0b9b536148db759a8a
Aril Apria Susanto <awriel@gmail.com>
2022-04-01 04:14:17 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (45 of 45 strings)
    
    Translation: opensourcepos/module
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/module/id/

--- application/language/id/module_lang.php
+++ application/language/id/module_lang.php
@@ -1,11 +1,10 @@
-<?php 
-
+<?php
 $lang["module_admin_cashups"] = "";
 $lang["module_admin_cashups_desc"] = "";
 $lang["module_attributes"] = "Atribut";
 $lang["module_attributes_desc"] = "Tambah, Perbaharui, Hapus dan Cari atribut.";
 $lang["module_both"] = "Keduanya";
-$lang["module_cashups"] = "Uang Tunai";
+$lang["module_cashups"] = "Kasir";
 $lang["module_cashups_desc"] = "Tambah, Perbaharui, Hapus dan Cari Uang Tunai.";
 $lang["module_config"] = "Konfigurasi";
 $lang["module_config_desc"] = "Ubah Konfigurasi Toko.";
