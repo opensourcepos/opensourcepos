cb3d84f1bfc40faa22ea54a9063ec91a3ce80c0c
Aril Apria Susanto <awriel@gmail.com>
2022-04-01 04:08:29 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (53 of 53 strings)
    
    Translation: opensourcepos/customers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/customers/id/

--- application/language/id/customers_lang.php
+++ application/language/id/customers_lang.php
@@ -1,8 +1,7 @@
-<?php 
-
+<?php
 $lang["customers_account_number"] = "No Pelanggan";
 $lang["customers_account_number_duplicate"] = "Nomor akun ini telah tersimpan di basis data.";
-$lang["customers_available_points"] = "Poin yg tersedia";
+$lang["customers_available_points"] = "Poin yang tersedia";
 $lang["customers_available_points_value"] = "";
 $lang["customers_average"] = "Rata-rata yang dihabiskan";
 $lang["customers_avg_discount"] = "Rata-rata diskon";
@@ -33,7 +32,7 @@ $lang["customers_mailchimp_activity_open"] = "Buka email";
 $lang["customers_mailchimp_activity_total"] = "Email terkirim";
 $lang["customers_mailchimp_activity_unopen"] = "Email belum dibuka";
 $lang["customers_mailchimp_email_client"] = "Klien email";
-$lang["customers_mailchimp_info"] = "Mailchimp";
+$lang["customers_mailchimp_info"] = "MailChimp";
 $lang["customers_mailchimp_member_rating"] = "Peringkat";
 $lang["customers_mailchimp_status"] = "Status";
 $lang["customers_mailchimp_vip"] = "VIP";
