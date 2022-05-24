9f8cf4846753cd02357ee76d3857e17a36cb649f
FastAct <alex.rijckaert@gmail.com>
2022-05-24 14:33:44 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/nl_BE/

--- application/language/nl-BE/login_lang.php
+++ application/language/nl-BE/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Ik ben geen robot.";
 $lang["login_go"] = "Verstuur";
-$lang["login_invalid_gcaptcha"] = "Ongeldige 'Ik ben geen robot'.";
+$lang["login_invalid_gcaptcha"] = "Controleer of u geen robot bent.";
 $lang["login_invalid_installation"] = "De installatie is onvolledig. Kijk uw php.ini file na.";
-$lang["login_invalid_username_and_password"] = "Ongeldige gebruiker of paswoord.";
+$lang["login_invalid_username_and_password"] = "Ongeldige gebruiker en/of paswoord.";
 $lang["login_login"] = "Login";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Log-uit";
+$lang["login_migration_needed"] = "Een database migratie naar %1 zal starten na het inloggen.";
 $lang["login_password"] = "Paswoord";
 $lang["login_username"] = "Gebruiker";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Welkom op %1!";
