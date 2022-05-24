070d9895487f132eaa0428f12fc8de07fe273782
FastAct <alex.rijckaert@gmail.com>
2022-05-24 14:35:36 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: opensourcepos/suppliers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/suppliers/nl_BE/

--- application/language/nl-BE/suppliers_lang.php
+++ application/language/nl-BE/suppliers_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["suppliers_account_number"] = "Btw nummer";
 $lang["suppliers_agency_name"] = "Agencynaam";
 $lang["suppliers_cannot_be_deleted"] = "De geselecteeerde leveranciers konden niet worden verwijderd. Eén of meerdere leveranciers hebben ordergegevens in de database zitten.";
@@ -8,9 +7,9 @@ $lang["suppliers_company_name"] = "Leverancier";
 $lang["suppliers_company_name_required"] = "Bedrijfsnaam moet ingevuld worden.";
 $lang["suppliers_confirm_delete"] = "Bent u zeker dat u de geselecteerde leveranciers wil verwijderen?";
 $lang["suppliers_confirm_restore"] = "Bent u zeker dat u de geselecteerde leverancier(s) wil herstellen?";
-$lang["suppliers_cost"] = "";
+$lang["suppliers_cost"] = "Kostprijs Leverancier";
 $lang["suppliers_error_adding_updating"] = "Fout bij het toevoegen/aanpassen van een leverancier.";
-$lang["suppliers_goods"] = "";
+$lang["suppliers_goods"] = "Goederen Leverancier";
 $lang["suppliers_new"] = "N. Leverancier";
 $lang["suppliers_none_selected"] = "U hebt geen leveranciers geselecteerd om te verwijderen.";
 $lang["suppliers_one_or_multiple"] = "leverancier(s) verwijderd";
@@ -19,5 +18,5 @@ $lang["suppliers_successful_deleted"] = "Er werd(en)";
 $lang["suppliers_successful_updating"] = "Wijzigingen leveranciersgegevens bewaard";
 $lang["suppliers_supplier"] = "Leverancier";
 $lang["suppliers_supplier_id"] = "Id";
-$lang["suppliers_tax_id"] = "";
+$lang["suppliers_tax_id"] = "BTW Id";
 $lang["suppliers_update"] = "Bewerk Leverancier";
