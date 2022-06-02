b366641fbc4a8ea3e29eb648848d81aa28d200a2
Dzung Do <dungdo@osd.vn>
2022-06-02 18:06:41 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 90.9% (10 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/vi/

--- application/language/vi/login_lang.php
+++ application/language/vi/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Tôi không phải là robot.";
 $lang["login_go"] = "Đăng nhập";
-$lang["login_invalid_gcaptcha"] = "không hợp lệ Tôi không phải là robot.";
+$lang["login_invalid_gcaptcha"] = "Xin vui lòng xác nhận bạn không phải là robot.";
 $lang["login_invalid_installation"] = "Phần cài đặt chưa đúng, kiểm tra tập tin php.ini của bạn.";
 $lang["login_invalid_username_and_password"] = "Tài khoản hoặc mật khẩu không hợp lệ.";
 $lang["login_login"] = "Đăng nhập";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Đăng xuất";
+$lang["login_migration_needed"] = "Di chuyển cơ sở dữ liệu sang %1 sẽ được bắt đầu sau khi đăng nhập.";
 $lang["login_password"] = "Mật khẩu";
 $lang["login_username"] = "Tài khoản";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Chào mừng đến với %1!";
