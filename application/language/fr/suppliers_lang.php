8ea4869f4dfbc39fd724c52211ff1e85eb143b54
Val Thi <valerie.b.thibeault@gmail.com>
2022-07-14 04:13:20 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: opensourcepos/suppliers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/suppliers/fr/

--- application/language/fr/suppliers_lang.php
+++ application/language/fr/suppliers_lang.php
@@ -1,23 +1,22 @@
-<?php 
-
-$lang["suppliers_account_number"] = "Numéro de Compte";
-$lang["suppliers_agency_name"] = "Nom de l'Agence";
-$lang["suppliers_cannot_be_deleted"] = "Impossible de supprimer le(s) Fournisseur(s) sélectionné(s). Un ou plusieurs ont des Ventes.";
+<?php
+$lang["suppliers_account_number"] = "Numéro de compte";
+$lang["suppliers_agency_name"] = "Nom de l'agence";
+$lang["suppliers_cannot_be_deleted"] = "Impossible de supprimer le(s) fournisseur(s) sélectionné(s). Un ou plusieurs ont des ventes.";
 $lang["suppliers_category"] = "Catégorie";
-$lang["suppliers_company_name"] = "Nom de l'Entreprise";
-$lang["suppliers_company_name_required"] = "Le Nom de l'Entreprise est requis.";
-$lang["suppliers_confirm_delete"] = "Êtes vous sûr(e) de vouloir supprimer ce(s) Fournisseur(s) ?";
-$lang["suppliers_confirm_restore"] = "Êtes-vous sûr de vouloir restaurer le(s) Fournisseur(s) sélectionné(s) ?";
-$lang["suppliers_cost"] = "";
-$lang["suppliers_error_adding_updating"] = "Erreur lors de l'ajout/suppression du Fournisseur.";
-$lang["suppliers_goods"] = "";
-$lang["suppliers_new"] = "Nouveau Fournisseur";
-$lang["suppliers_none_selected"] = "Vous n'avez pas sélectionné de Fournisseur(s) à supprimer.";
+$lang["suppliers_company_name"] = "Nom de l'entreprise";
+$lang["suppliers_company_name_required"] = "Le nom de l'entreprise est requis.";
+$lang["suppliers_confirm_delete"] = "Êtes-vous certain vouloir supprimer ce(s) fournisseur(s) ?";
+$lang["suppliers_confirm_restore"] = "Êtes-vous certain de vouloir restaurer le(s) fournisseur(s) sélectionné(s) ?";
+$lang["suppliers_cost"] = "Fournisseur de coût";
+$lang["suppliers_error_adding_updating"] = "Erreur lors de l'ajout/modification du fournisseur.";
+$lang["suppliers_goods"] = "Fournisseur de biens";
+$lang["suppliers_new"] = "Nouveau fournisseur";
+$lang["suppliers_none_selected"] = "Vous n'avez sélectionné aucun fournisseur à supprimer.";
 $lang["suppliers_one_or_multiple"] = "Fournisseur(s)";
 $lang["suppliers_successful_adding"] = "Fournisseur ajouté avec succès";
 $lang["suppliers_successful_deleted"] = "Suppression réussie";
 $lang["suppliers_successful_updating"] = "Fournisseur édité avec succès";
 $lang["suppliers_supplier"] = "Fournisseur";
 $lang["suppliers_supplier_id"] = "Id";
-$lang["suppliers_tax_id"] = "Id. d'Imposition";
-$lang["suppliers_update"] = "Modifier le Fournisseur";
+$lang["suppliers_tax_id"] = "Id. de taxe";
+$lang["suppliers_update"] = "Modifier le fournisseur";
