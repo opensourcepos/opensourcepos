f11f4eeb2d950ed4f38afe0e5f8b1a989e308be5
Val Thi <valerie.b.thibeault@gmail.com>
2022-07-14 00:36:07 +0000

    Translated using Weblate (French)
    Currently translated at 95.0% (19 of 20 strings)
    
    Translation: opensourcepos/datepicker
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/datepicker/fr/

--- application/language/fr/datepicker_lang.php
+++ application/language/fr/datepicker_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["datepicker_all_time"] = "Depuis le début";
 $lang["datepicker_apply"] = "Appliquer";
 $lang["datepicker_cancel"] = "Annuler la Vente";
@@ -7,7 +6,7 @@ $lang["datepicker_custom"] = "Personnaliser";
 $lang["datepicker_from"] = "De";
 $lang["datepicker_last_30"] = "Ces 30 derniers jours";
 $lang["datepicker_last_7"] = "Ces 7 derniers jours";
-$lang["datepicker_last_financial_year"] = "";
+$lang["datepicker_last_financial_year"] = "Dernière année fiscale";
 $lang["datepicker_last_month"] = "Le mois dernier";
 $lang["datepicker_last_year"] = "L'année passée";
 $lang["datepicker_same_month_last_year"] = "Même mois de l'Année Dernière";
