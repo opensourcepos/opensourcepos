2da289506221713876aa58cd766511ec23dc9e88
Val Thi <valerie.b.thibeault@gmail.com>
2022-07-14 00:36:50 +0000

    Translated using Weblate (French)
    Currently translated at 90.9% (10 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/fr/

--- application/language/fr/login_lang.php
+++ application/language/fr/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Je ne suis pas un robot.";
 $lang["login_go"] = "Lancer";
 $lang["login_invalid_gcaptcha"] = "Veuillez vérifier que vous n'êtes pas un robot.";
 $lang["login_invalid_installation"] = "Cette installation est incorrecte, veuillez vérifier votre fichier php.ini.";
-$lang["login_invalid_username_and_password"] = "Entrée invalide.";
+$lang["login_invalid_username_and_password"] = "Nom d'utilisateur et/ou mot de passe invalide.";
 $lang["login_login"] = "Login";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "Déconnexion";
+$lang["login_migration_needed"] = "Une migration de base de données vers %1 débutera après l'ouverture de session.";
 $lang["login_password"] = "Mot de passe";
 $lang["login_username"] = "Nom d'utilisateur";
 $lang["login_welcome"] = "";
