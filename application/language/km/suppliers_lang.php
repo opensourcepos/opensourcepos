2bf9effe1d913302a633f17c549ca1cc161c1c6d
Cheabhak Ezecom <cheabhak.ezecom@gmail.com>
2022-09-06 07:52:45 +0000

    Translated using Weblate (Central Khmer)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: opensourcepos/suppliers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/suppliers/km/

--- application/language/km/suppliers_lang.php
+++ application/language/km/suppliers_lang.php
@@ -1,23 +1,22 @@
-<?php 
-
-$lang["suppliers_account_number"] = "";
-$lang["suppliers_agency_name"] = "";
-$lang["suppliers_cannot_be_deleted"] = "";
-$lang["suppliers_category"] = "";
-$lang["suppliers_company_name"] = "";
-$lang["suppliers_company_name_required"] = "";
-$lang["suppliers_confirm_delete"] = "";
-$lang["suppliers_confirm_restore"] = "";
-$lang["suppliers_cost"] = "";
-$lang["suppliers_error_adding_updating"] = "";
-$lang["suppliers_goods"] = "";
-$lang["suppliers_new"] = "";
-$lang["suppliers_none_selected"] = "";
-$lang["suppliers_one_or_multiple"] = "";
-$lang["suppliers_successful_adding"] = "";
-$lang["suppliers_successful_deleted"] = "";
-$lang["suppliers_successful_updating"] = "";
-$lang["suppliers_supplier"] = "";
-$lang["suppliers_supplier_id"] = "";
-$lang["suppliers_tax_id"] = "";
-$lang["suppliers_update"] = "";
+<?php
+$lang["suppliers_account_number"] = "លេខគណនី";
+$lang["suppliers_agency_name"] = "លេខភ្នាក់ងារ";
+$lang["suppliers_cannot_be_deleted"] = "មិនអាចលុបអ្នកផ្គត់ផ្គងដែលបានជ្រើសរើស​ ។ មានទំនិញកំពុងដាក់លក់";
+$lang["suppliers_category"] = "ប្រភេទទំនិញ";
+$lang["suppliers_company_name"] = "ឈ្មោះក្រុមហ៊ុន";
+$lang["suppliers_company_name_required"] = "ត្រូវការបំពេញឈ្មោះក្រុមហ៊ុន​។";
+$lang["suppliers_confirm_delete"] = "តើអ្នកពិតជាចង់លុបអ្នកផ្គត់ផ្គងដែរបានជ្រើសរើសមែនទេ?";
+$lang["suppliers_confirm_restore"] = "តើអ្នកពិតជាចង់ដាក់អ្នកផ្គត់ផ្គង់ដែរបានជ្រើសរើសទៅកន្លែងដើម?";
+$lang["suppliers_cost"] = "តម្លៃពីអ្នកផ្គត់ផ្គង់";
+$lang["suppliers_error_adding_updating"] = "ការកែប្រែ រឺបន្ថែមអ្នកផ្គត់ផ្គង់មិនបានសំរេច។";
+$lang["suppliers_goods"] = "ទំនិញរបស់អ្នកផ្គត់ផ្គង់";
+$lang["suppliers_new"] = "អ្នកផ្គត់ផ្គង់ថ្មី";
+$lang["suppliers_none_selected"] = "អ្នកមិនបានជ្រើសរើសអ្នកផ្គត់ផ្គង់ណាមួយដើម្បីលុបនោះទេ។";
+$lang["suppliers_one_or_multiple"] = "អ្នកផ្គត់ផ្គង់";
+$lang["suppliers_successful_adding"] = "អ្នកបានបន្ថែមអ្នកផ្គត់ផ្គង់បានសំរេច";
+$lang["suppliers_successful_deleted"] = "អ្នកបានលុបអ្នកផ្គត់ផ្គង់បានសំរេច";
+$lang["suppliers_successful_updating"] = "អ្នកបានកែប្រែអ្នកផ្គត់ផ្គង់បានសំរេច";
+$lang["suppliers_supplier"] = "អ្នកផ្គត់ផ្គង់";
+$lang["suppliers_supplier_id"] = "លេខរៀង";
+$lang["suppliers_tax_id"] = "លេខរៀងពន្ធ";
+$lang["suppliers_update"] = "កែប្រែអ្នកផ្គត់ផ្គង់";
