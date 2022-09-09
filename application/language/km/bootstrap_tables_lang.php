1da81e95b35d6bfe196d23841a5f14eb741f6ec6
Cheabhak Ezecom <cheabhak.ezecom@gmail.com>
2022-09-09 04:01:25 +0000

    Translated using Weblate (Central Khmer)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/km/

--- application/language/km/bootstrap_tables_lang.php
+++ application/language/km/bootstrap_tables_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
-$lang["tables_all"] = "";
-$lang["tables_columns"] = "";
-$lang["tables_hide_show_pagination"] = "";
-$lang["tables_loading"] = "";
-$lang["tables_page_from_to"] = "";
-$lang["tables_refresh"] = "";
-$lang["tables_rows_per_page"] = "";
-$lang["tables_toggle"] = "";
+<?php
+$lang["tables_all"] = "ទាំងអស់";
+$lang["tables_columns"] = "ជួរឈរ";
+$lang["tables_hide_show_pagination"] = "លាក់/បង្ហាញ ទំព័រ";
+$lang["tables_loading"] = "កំពុងរៀបចំ, សូមរងចាំ...";
+$lang["tables_page_from_to"] = "បង្ហាញពី {0} ដល់​ {1} នៃ​ {2} ជួរដេក";
+$lang["tables_refresh"] = "ធ្វើអោយថ្មី";
+$lang["tables_rows_per_page"] = "{0} ជួរដេក ក្នុងមួយទំព័រ";
+$lang["tables_toggle"] = "បិទបើក";
