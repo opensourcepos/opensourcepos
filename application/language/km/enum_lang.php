5f5fe5eb476bce46bb43b3318199959c2f587d78
Cheabhak Ezecom <cheabhak.ezecom@gmail.com>
2022-09-09 04:04:28 +0000

    Translated using Weblate (Central Khmer)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/enum/km/

--- application/language/km/enum_lang.php
+++ application/language/km/enum_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
-$lang["enum_half_down"] = "";
-$lang["enum_half_even"] = "";
-$lang["enum_half_five"] = "";
-$lang["enum_half_odd"] = "";
-$lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+<?php
+$lang["enum_half_down"] = "ត្រឹមពាក់កណ្ដាលចុះក្រោម";
+$lang["enum_half_even"] = "ពាក់កណ្តាលគូ";
+$lang["enum_half_five"] = "ពាក់កណ្តាលប្រាំ";
+$lang["enum_half_odd"] = "ពាក់កណ្តាលសេស";
+$lang["enum_half_up"] = "ពាក់កណ្តាលឡើងលើ";
+$lang["enum_round_down"] = "បង្គត់ចុះ";
+$lang["enum_round_up"] = "បង្គត់ឡើង";
