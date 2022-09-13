6c60a6aa7800a0a43d2833d3757a17260cab5768
Cheabhak Ezecom <cheabhak.ezecom@gmail.com>
2022-09-13 07:54:42 +0000

    Translated using Weblate (Central Khmer)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/km/

--- application/language/km/error_lang.php
+++ application/language/km/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
-$lang["error_no_permission_module"] = "";
-$lang["error_unknown"] = "";
+<?php
+$lang["error_no_permission_module"] = "អ្នកមិនមានសិទ្ធដើម្បីចូលទៅ ផ្នែកនោះទេ";
+$lang["error_unknown"] = "កំហុសមិនស្គាល់";
