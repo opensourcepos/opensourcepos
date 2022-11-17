e29bcb72d9801469e0ccf10f1afab8ea7eb963e4
objecttothis <objecttothis@gmail.com>
2022-11-17 17:24:30 +0400

    Porting over 986ab7e - Also deleted files that shouldn't be there.

--- app/Views/partial/footer.php
+++ app/Views/partial/footer.php
@@ -3,9 +3,9 @@
 
 		<div id="footer">
 			<div class="jumbotron push-spaces">
-				<strong><?php echo lang('Common.copyrights', ['current_year' => date('Y')]) ?> ·
-				<a href="https://opensourcepos.org" target="_blank"><?php echo lang('Common.website') ?></a>  ·
-				<?php echo esc(config('OSPOS')->application_version) ?> - <?php echo esc(substr(config('OSPOS')->commit_sha1, 0, 6)) ?></strong>.
+				<strong><?php echo lang('common_copyrights', date('Y')); ?> ·
+					<a href="https://opensourcepos.org" target="_blank"><?php echo lang('common_website'); ?></a>  ·
+					<?php echo esc(config('OSPOS')->application_version) ?> - <a target="_blank" href="https://github.com/opensourcepos/opensourcepos/commit/<?php echo esc(config('OSPOS')->commit_sha1) ?>"><?php echo esc(substr(config('OSPOS')->commit_sha1, 0, 6)); ?></a></strong>.
 			</div>
 		</div>
 	</body>
