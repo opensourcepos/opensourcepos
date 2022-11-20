d0b5905634ee24e39b33527a19aa3a311673cc9a
jekkos <jeroen.peelaerts@gmail.com>
2022-11-20 22:51:19 +0100

    Use double quotes in language files (#3468)

--- app/Language/en-US/Login.php
+++ app/Language/en-US/Login.php
@@ -1,13 +1,14 @@
-<?php 
-
-$lang["login_gcaptcha"] = "I'm not a robot.";
-$lang["login_go"] = "Go";
-$lang["login_invalid_gcaptcha"] = "Please verify that you are not a robot.";
-$lang["login_invalid_installation"] = "The installation is not correct, check your php.ini file.";
-$lang["login_invalid_username_and_password"] = "Invalid username and/or password.";
-$lang["login_login"] = "Login";
-$lang["login_logout"] = "Logout";
-$lang["login_migration_needed"] = "A database migration to %1 will start after login.";
-$lang["login_password"] = "Password";
-$lang["login_username"] = "Username";
-$lang["login_welcome"] = "Welcome to %1!";
+<?php
+return [
+	"login_gcaptcha" => "I'm not a robot.",
+	"login_go" => "Go",
+	"login_invalid_gcaptcha" => "Please verify that you are not a robot.",
+	"login_invalid_installation" => "The installation is not correct, check your php.ini file.",
+	"login_invalid_username_and_password" => "Invalid username and/or password.",
+	"login_login" => "Login",
+	"login_logout" => "Logout",
+	"login_migration_needed" => "A database migration to %1 will start after login.",
+	"login_password" => "Password",
+	"login_username" => "Username",
+	"login_welcome" => "Welcome to %1!",
+];
---
+++ convert.php
@@ -0,0 +1,12 @@
+<?php
+
+include($argv[1]);
+
+echo "<?php\n";
+echo "return [\n";
+foreach ($lang as $key=>$value) {
+    echo "\t\«"$ke\"' => \"$value\",\n";
+}
+echo "];";
+?>
+
