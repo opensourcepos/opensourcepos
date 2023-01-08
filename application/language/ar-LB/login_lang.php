1547663439182675227a892e945195e70c3bb7a3
Boors96 <mohamdabd858@gmail.com>
2023-01-08 09:16:14 +0000

    Translated using Weblate (Arabic (ar_LB))
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/ar_LB/

--- application/language/ar-LB/login_lang.php
+++ application/language/ar-LB/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
-$lang["login_gcaptcha"] = "أنا لست بوت.";
+<?php
+$lang["login_gcaptcha"] = "أنا لست روبوت.";
 $lang["login_go"] = "البدء";
-$lang["login_invalid_gcaptcha"] = "رمز التحقق غير صحيح.";
+$lang["login_invalid_gcaptcha"] = "يرجى التحقق من أنك لست روبوتًا.";
 $lang["login_invalid_installation"] = "يوجد مشكلة بالتنصيب, الرجاء التحقق من ملف php.ini.";
-$lang["login_invalid_username_and_password"] = "اسم مستخدم/كلمة سر غير صحيح.";
+$lang["login_invalid_username_and_password"] = "اسم المستخدم/كلمة المرور غير صحيحة.";
 $lang["login_login"] = "دخول";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "تسجيل خروج";
+$lang["login_migration_needed"] = "سيبدأ ترحيل قاعدة البيانات إلى٪ 1 بعد تسجيل الدخول.";
 $lang["login_password"] = "كلمة السر";
 $lang["login_username"] = "اسم المستخدم";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "مرحباً بك في٪ 1!";
