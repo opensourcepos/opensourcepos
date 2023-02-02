95c8c79293eadcde1de6b0c6d6581874a08cd893
objecttothis <objecttothis@gmail.com>
2023-02-02 17:54:29 +0400

    Fixed Login Validation - Ported validation rules to CI4 formatting. - Added Custom Validation Ruleset. - Formatting and refactoring

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -4,8 +4,6 @@ namespace App\Controllers;
 
 use App\Libraries\MY_Migration;
 use App\Models\Employee;
-use CodeIgniter\HTTP\RedirectResponse;
-use Config\Migrations;
 use Config\Services;
 
 /**
@@ -18,115 +16,33 @@ class Login extends BaseController
 	public function index()
 	{
 		$this->employee = model('Employee');
-
 		if(!$this->employee->is_logged_in())
 		{
 			$migration = new MY_Migration(config('Migrations'));
 			$data = [
-				'validation' => Services::validation(),
+				'has_errors' => false,
 				'is_latest' => $migration->is_latest(),
 				'latest_version' => $migration->get_last_migration()
 			];
 
-			if($this->request->getMethod() != 'post')
+			if(strtolower($this->request->getMethod()) !== 'post')
 			{
-				echo view('login', $data);
+				return view('login', $data);
 			}
 
-//TODO: Validation isn't working. #3595
-//			if(!$this->validate(['username' => 'required|login_check']))
-//			{
-//				echo view('login', ['validation' => $this->validator->getErrors()]);
-//			}
-		}
+			$rules = ['username' => 'required|login_check[data]'];
+			$messages = ['username' => lang('Login.invalid_username_and_password')];
 
-		//return redirect()->to('home');
-	}
-
-	public function login_check(string $username): bool
-	{
-		if(!$this->installation_check())
-		{
-			$this->validator->setMessage('login_check', lang('login_invalid_installation'));	//TODO: This is going to need some work https://codeigniter.com/user_guide/libraries/validation.html?highlight=validation#setting-custom-error-messages
-
-			return FALSE;
-		}
-
-		$password = $this->request->getPost('password');
-
-		if(!$this->employee->login($username, $password))
-		{
-			$this->validator->setMessage('login_check', $this->lang->line('login_invalid_username_and_password'));
-
-			return FALSE;
-		}
-
-		if(config('OSPOS')->settings['gcaptcha_enable'])
-		{
-			$g_recaptcha_response = $this->request->getPost('g-recaptcha-response');
-
-			if(!$this->gcaptcha_check($g_recaptcha_response))
+			if(!$this->validate($rules, $messages))
 			{
-				$this->validator->setMessage('login_check', lang('login_invalid_gcaptcha'));
+				$validation = Services::validation();
+				$data['has_errors'] = !empty($validation->getErrors());
 
-				return FALSE;
+				return view('login', $data);
 			}
+
 		}
-		return TRUE;
-	}
-
-	private function gcaptcha_check($response): bool
-	{
-		if(!empty($response))
-		{
-			$check = array(
-				'secret'   => config('OSPOS')->settings['gcaptcha_secret_key'],
-				'response' => $response,
-				'remoteip' => $this->request->getIPAddress()
-			);
-
-			$ch = curl_init();
-
-			curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-			curl_setopt($ch, CURLOPT_POST, TRUE);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($check));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-			$result = curl_exec($ch);
-
-			curl_close($ch);
-
-			$status = json_decode($result, TRUE);
-
-			if(!empty($status['success']))
-			{
-				return TRUE;
-			}
-		}
-
-		return FALSE;
-	}
-
-	private function installation_check()
-	{
-		// get PHP extensions and check that the required ones are installed
-		$extensions = implode(', ', get_loaded_extensions());
-		$keys = array('bcmath', 'intl', 'gd', 'openssl', 'mbstring', 'curl');
-		$pattern = '/';
-		foreach($keys as $key)
-		{
-			$pattern .= '(?=.*\b' . preg_quote($key, '/') . '\b)';
-		}
-		$pattern .= '/i';
-		$result = preg_match($pattern, $extensions);
-
-		if(!$result)
-		{
-			error_log('Check your php.ini');
-			error_log('PHP installed extensions: ' . $extensions);
-			error_log('PHP required extensions: ' . implode(', ', $keys));
-		}
-
-		return $result;
+		echo "validated";
+		return redirect()->to('home');
 	}
 }
