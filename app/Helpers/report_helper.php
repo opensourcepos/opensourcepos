a217dfd84a81effdf58daba3cedbf649bf7fadf1
Edwin Smith <thesojourn@gmail.com>
2023-03-08 20:49:45 +0400

    update code style

--- app/Helpers/report_helper.php
+++ app/Helpers/report_helper.php
@@ -1,51 +1,45 @@
 <?php
 
-/**
- * @param string $report_name
- * @param string $report_prefix
- * @param string $lang_key
- * @return array
- */
 function get_report_link(string $report_name, string $report_prefix = '', string $lang_key = ''): array
 {
-	$path = 'reports/';
-	if ($report_prefix !== '') {
-		$path .= $report_prefix . '_';
-	}
+    $path = 'reports/';
+    if ($report_prefix !== '') {
+        $path .= $report_prefix . '_';
+    }
 
-	/**
-	 * Sanitize the report name in case it has come from the permissions table.
-	 */
-	$report_name = str_replace('reports_','',$report_name);
-	$path .= $report_name;
+    /**
+     * Sanitize the report name in case it has come from the permissions table.
+     */
+    $report_name = str_replace('reports_', '', $report_name);
+    $path .= $report_name;
 
-	if ($lang_key === '') {
-		$lang_key = 'Reports.' . $report_name;
-	}
+    if ($lang_key === '') {
+        $lang_key = 'Reports.' . $report_name;
+    }
 
-	return [
-		'path' => site_url($path),
-		'label' => lang($lang_key),
-	];
+    return [
+        'path'  => site_url($path),
+        'label' => lang($lang_key),
+    ];
 }
 
 /**
- * @param string $permission_id
+ * @param string   $permission_id
  * @param string[] $restrict_views
+ *
  * @return bool
  */
-function can_show_report($permission_id, array $restrict_views = [])
+function can_show_report($permission_id, array $restrict_views = []): bool
 {
-	if (strpos($permission_id, 'reports_') === false) {
-		return false;
-	}
+    if (strpos($permission_id, 'reports_') === false) {
+        return false;
+    }
 
-	foreach ($restrict_views as $restrict_view) {
-		if (strpos($permission_id, $restrict_view) !== false) {
-			return false;
-		}
-	}
+    foreach ($restrict_views as $restrict_view) {
+        if (strpos($permission_id, $restrict_view) !== false) {
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
-
--- app/Views/reports/listing.php
+++ app/Views/reports/listing.php
@@ -1,23 +1,27 @@
 <?php
 /**
- * @var int $person_id
+ * @var int   $person_id
  * @var array $permission_ids
  * @var array $grants
  */
-
-use App\Models\Employee;
-
+$detailed_reports = [
+    'reports_sales'      => 'detailed',
+    'reports_receivings' => 'detailed',
+    'reports_customers'  => 'specific',
+    'reports_discounts'  => 'specific',
+    'reports_employees'  => 'specific',
+    'reports_suppliers'  => 'specific',
+];
 ?>
-<?php echo view('partial/header') ?>
+<?= view('partial/header') ?>
 
 <script type="text/javascript">
 	dialog_support.init("a.modal-dlg");
 </script>
 
 <?php
-if(isset($error))
-{
-	echo '<div class=\'alert alert-dismissible alert-danger\'>' . esc($error) . '</div>';
+if (isset($error)) {
+    echo '<div class=\'alert alert-dismissible alert-danger\'>' . esc($error) . '</div>';
 }
 ?>
 
@@ -25,18 +29,18 @@ if(isset($error))
 	<div class="col-md-4">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title"><span class="glyphicon glyphicon-stats">&nbsp</span><?php echo lang('Reports.graphical_reports') ?></h3>
+				<h3 class="panel-title"><span class="glyphicon glyphicon-stats">&nbsp</span><?= lang('Reports.graphical_reports') ?></h3>
 			</div>
 			<div class="list-group">
-				<?php foreach ($permission_ids as $permission_id) :
-					if (can_show_report($permission_id, ['inventory', 'receiving'])) :
-						$link = get_report_link($permission_id, 'graphical_summary');
-						?>
-						<a class="list-group-item" href="<?php echo $link['path']; ?>"><?php echo $link['label']; ?></a>
+				<?php foreach ($permission_ids as $permission_id) {
+				    if (can_show_report($permission_id, ['inventory', 'receiving'])) {
+				        $link = get_report_link($permission_id, 'graphical_summary');
+				        ?>
+						<a class="list-group-item" href="<?= $link['path']; ?>"><?= $link['label']; ?></a>
 						<?php
-					endif;
-				endforeach;
-				?>
+				    }
+				}
+?>
 			 </div>
 		</div>
 	</div>
@@ -44,18 +48,18 @@ if(isset($error))
 	<div class="col-md-4">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title"><span class="glyphicon glyphicon-list">&nbsp</span><?php echo lang('Reports.summary_reports') ?></h3>
+				<h3 class="panel-title"><span class="glyphicon glyphicon-list">&nbsp</span><?= lang('Reports.summary_reports') ?></h3>
 			</div>
 			<div class="list-group">
-				<?php foreach ($permission_ids as $permission_id) :
-					if (can_show_report($permission_id, ['inventory', 'receiving'])) :
-						$link = get_report_link($permission_id, 'summary');
-						?>
-						<a class="list-group-item" href="<?php echo $link['path']; ?>"><?php echo $link['label']; ?></a>
+				<?php foreach ($permission_ids as $permission_id) {
+				    if (can_show_report($permission_id, ['inventory', 'receiving'])) {
+				        $link = get_report_link($permission_id, 'summary');
+				        ?>
+						<a class="list-group-item" href="<?= $link['path']; ?>"><?= $link['label']; ?></a>
 						<?php
-					endif;
-				endforeach;
-				?>
+				    }
+				}
+?>
 			 </div>
 		</div>
 	</div>
@@ -63,51 +67,41 @@ if(isset($error))
 	<div class="col-md-4">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title"><span class="glyphicon glyphicon-list-alt">&nbsp</span><?php echo lang('Reports.detailed_reports') ?></h3>
+				<h3 class="panel-title"><span class="glyphicon glyphicon-list-alt">&nbsp</span><?= lang('Reports.detailed_reports') ?></h3>
 			</div>
 			<div class="list-group">
-				<?php
-				$detailed_reports = [
-					'reports_sales' => 'detailed',
-					'reports_receivings' => 'detailed',
-					'reports_customers' => 'specific',
-					'reports_discounts' => 'specific',
-					'reports_employees' => 'specific',
-					'reports_suppliers' => 'specific',
-				];
-				foreach ($detailed_reports as $report_name => $prefix) :
-					if (in_array($report_name, $permission_ids)) :
-						$link = get_report_link($report_name, $prefix);
-						?>
-						<a class="list-group-item" href="<?php echo $link['path']; ?>"><?php echo $link['label']; ?></a>
-					<?php
-					endif;
-				endforeach;
-				?>
+				<?php foreach ($detailed_reports as $report_name => $prefix) {
+				    if (in_array($report_name, $permission_ids, true)) {
+				        $link = get_report_link($report_name, $prefix);
+				        ?>
+						<a class="list-group-item" href="<?= $link['path']; ?>"><?= $link['label']; ?></a>
+						<?php
+				    }
+				}
+?>
 			 </div>
 		</div>
 
 		<?php
-		if (in_array('reports_inventory', $permission_ids))
-		 {
-		?>
+        if (in_array('reports_inventory', $permission_ids, true)) {
+            ?>
 			<div class="panel panel-primary">
 				<div class="panel-heading">
-					<h3 class="panel-title"><span class="glyphicon glyphicon-book">&nbsp</span><?php echo lang('Reports.inventory_reports') ?></h3>
+					<h3 class="panel-title"><span class="glyphicon glyphicon-book">&nbsp</span><?= lang('Reports.inventory_reports') ?></h3>
 				</div>
 				<div class="list-group">
 				<?php
-				$inventory_low_report = get_report_link('reports_inventory_low');
-				$inventory_summary_report = get_report_link('reports_inventory_summary');
-				?>
-					<a class="list-group-item" href="<?php echo $inventory_low_report['path']; ?>"><?php echo $inventory_low_report['label']; ?></a>
-					<a class="list-group-item" href="<?php echo $inventory_summary_report['path']; ?>"><?php echo $inventory_summary_report['label']; ?></a>
+                    $inventory_low_report = get_report_link('reports_inventory_low');
+            $inventory_summary_report     = get_report_link('reports_inventory_summary');
+            ?>
+					<a class="list-group-item" href="<?= $inventory_low_report['path']; ?>"><?= $inventory_low_report['label']; ?></a>
+					<a class="list-group-item" href="<?= $inventory_summary_report['path']; ?>"><?= $inventory_summary_report['label']; ?></a>
 				</div>
 			</div>
-		<?php 
-		}
-		?>
+		<?php
+        }
+?>
 	</div>
 </div>
 
-<?php echo view('partial/footer') ?>
+<?= view('partial/footer') ?>
