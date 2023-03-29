d5b4a2745ec3b75f521ce49cc095e8ca7740a655
WebShells <Shady.Sh@WShells.ws>
2023-03-29 18:26:29 +0000

    Translated using Weblate (Arabic (Egypt))
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/ar_EG/

--- application/language/ar-EG/login_lang.php
+++ application/language/ar-EG/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "أنا لست بوت.";
 $lang["login_go"] = "البدء";
-$lang["login_invalid_gcaptcha"] = "رمز التحقق غير صحيح.";
+$lang["login_invalid_gcaptcha"] = "يرجى التأكيد على أنك لست روبوتا.";
 $lang["login_invalid_installation"] = "يوجد مشكلة بالتنصيب, الرجاء التحقق من ملف php.ini.";
 $lang["login_invalid_username_and_password"] = "اسم مستخدم/كلمة سر غير صحيح.";
 $lang["login_login"] = "دخول";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_logout"] = "تسجيل خروج";
+$lang["login_migration_needed"] = "سيبدأ ترحيل قاعدة البيانات إلى٪ 1 بعد تسجيل الدخول.";
 $lang["login_password"] = "كلمة السر";
 $lang["login_username"] = "اسم المستخدم";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "مرحباً بك في٪ 1!";
