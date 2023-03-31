f73740547b5f442d85dd0ceb4e6c839e830fe9e8
WebShells <Shady.Sh@WShells.ws>
2023-03-31 22:06:58 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: opensourcepos/item_kits
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/item_kits/en_GB/

--- application/language/en-GB/item_kits_lang.php
+++ application/language/en-GB/item_kits_lang.php
@@ -1,8 +1,7 @@
-<?php 
-
+<?php
 $lang["item_kits_add_item"] = "Add Item";
 $lang["item_kits_all"] = "All";
-$lang["item_kits_cannot_be_deleted"] = "Item Kit(s) delete failed";
+$lang["item_kits_cannot_be_deleted"] = "Item Kit(s) delete failed.";
 $lang["item_kits_confirm_delete"] = "Are you sure you want to delete the selected Item Kit(s)?";
 $lang["item_kits_confirm_restore"] = "Are you sure you want to restore the selected Item Kit(s)?";
 $lang["item_kits_description"] = "Item Kit Description";
@@ -10,7 +9,7 @@ $lang["item_kits_discount"] = "Discount";
 $lang["item_kits_discount_fixed"] = "Fixed Discount";
 $lang["item_kits_discount_percent"] = "Percentage Discount";
 $lang["item_kits_discount_type"] = "Discount Type";
-$lang["item_kits_error_adding_updating"] = "Item Kit add or update failed";
+$lang["item_kits_error_adding_updating"] = "Item Kit add or update failed.";
 $lang["item_kits_find_kit_item"] = "Kit Item";
 $lang["item_kits_info"] = "Item Kit Info";
 $lang["item_kits_item"] = "Item";
@@ -25,8 +24,8 @@ $lang["item_kits_kit_and_stock"] = "Kit and Stock";
 $lang["item_kits_kit_only"] = "Kit Only";
 $lang["item_kits_name"] = "Item Kit Name";
 $lang["item_kits_new"] = "New Item Kit";
-$lang["item_kits_no_item_kits_to_display"] = "No Item Kits to display";
-$lang["item_kits_none_selected"] = "You have not selected any Item Kit(s)";
+$lang["item_kits_no_item_kits_to_display"] = "No Item Kits to display.";
+$lang["item_kits_none_selected"] = "You have not selected any Item Kit(s).";
 $lang["item_kits_one_or_multiple"] = "Item Kit(s)";
 $lang["item_kits_price_option"] = "Price Option";
 $lang["item_kits_priced_only"] = "Priced Only";
