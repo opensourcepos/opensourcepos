2fe82484d7ee18c3faf4db10abc96c709d78d33d
Aril Apria Susanto <awriel@gmail.com>
2023-04-12 09:01:56 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/expenses_categories/id/

--- application/language/id/expenses_categories_lang.php
+++ application/language/id/expenses_categories_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
+<?php
 $lang["category_name_required"] = "Nama Kategori Biaya harus diisi";
 $lang["expenses_categories_add_item"] = "Tambah Kategori";
 $lang["expenses_categories_cannot_be_deleted"] = "Tidak dapat menghapus Kategori Biaya";
-$lang["expenses_categories_category_id"] = "Id";
-$lang["expenses_categories_confirm_delete"] = "Anda yakin ingin mengpus Kategori Biaya terpilih?";
+$lang["expenses_categories_category_id"] = "ID";
+$lang["expenses_categories_confirm_delete"] = "Anda yakin ingin menghapus Kategori Biaya terpilih?";
 $lang["expenses_categories_confirm_restore"] = "Apakah Anda yakin ingin mengembalikan Kategori Biaya yang dipilih?";
 $lang["expenses_categories_description"] = "Deskripsi Kategori";
 $lang["expenses_categories_error_adding_updating"] = "Galat menambah / memperbaharui Kategori Biaya";
@@ -14,7 +13,7 @@ $lang["expenses_categories_new"] = "Kategori Baru";
 $lang["expenses_categories_no_expenses_categories_to_display"] = "Belum ada Kategori yang dapat ditampilkan";
 $lang["expenses_categories_none_selected"] = "Anda belum memilih Kategori Biaya";
 $lang["expenses_categories_one_or_multiple"] = "Kategori Biaya";
-$lang["expenses_categories_quantity"] = "Kuaititas";
+$lang["expenses_categories_quantity"] = "Kuantitas";
 $lang["expenses_categories_successful_adding"] = "Berhasil menambahkan Kategori Biaya";
 $lang["expenses_categories_successful_deleted"] = "Berhasil menghapus Kategori Biaya";
 $lang["expenses_categories_successful_updating"] = "Berhasil memperbaharui Kategori Biaya";
