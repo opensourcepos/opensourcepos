38563d822eec266f4597716138a4ee62c4951b39
Steve Ireland <stevei@ruledomain.com>
2023-04-26 21:57:08 -0400

    For #3741. Manual migration of commits from master to branch through April 3, 2023.

--- app/Language/ar-EG/Login.php
+++ app/Language/ar-EG/Login.php
@@ -2,13 +2,13 @@
 return [
 	"login_gcaptcha" => "أنا لست بوت.",
 	"login_go" => "البدء",
-	"login_invalid_gcaptcha" => "رمز التحقق غير صحيح.",
+	"login_invalid_gcaptcha" => "يرجى التأكيد على أنك لست روبوتا.",
 	"login_invalid_installation" => "يوجد مشكلة بالتنصيب, الرجاء التحقق من ملف php.ini.",
 	"login_invalid_username_and_password" => "اسم مستخدم/كلمة سر غير صحيح.",
 	"login_login" => "دخول",
-	"login_logout" => "",
-	"login_migration_needed" => "",
+	"login_logout" => "تسجيل خروج",
+	"login_migration_needed" => "سيبدأ ترحيل قاعدة البيانات إلى٪ 1 بعد تسجيل الدخول.",
 	"login_password" => "كلمة السر",
 	"login_username" => "اسم المستخدم",
-	"login_welcome" => "",
+	"login_welcome" => "مرحباً بك في٪ 1!",
 ];
--- app/Language/en-GB/Login.php
+++ app/Language/en-GB/Login.php
@@ -3,7 +3,7 @@ return [
 	"login_gcaptcha" => "I'm not a robot",
 	"login_go" => "Go",
 	"login_invalid_gcaptcha" => "Please verify that you are not a robot.",
-	"login_invalid_installation" => "The installation is not correct, check your php.ini file",
+	"login_invalid_installation" => "The installation is not correct, check your php.ini file.",
 	"login_invalid_username_and_password" => "Invalid username and/or password.",
 	"login_login" => "Login",
 	"login_logout" => "Logout",
