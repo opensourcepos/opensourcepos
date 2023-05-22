35d44ffccc070c38f0df6d6e3032b801c5a1c5f0
Oleg <oleg.pischuk@bk.ru>
2023-05-22 15:02:45 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: opensourcepos/datepicker
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/datepicker/ru/

--- application/language/ru/datepicker_lang.php
+++ application/language/ru/datepicker_lang.php
@@ -1,22 +1,21 @@
-<?php 
-
-$lang["datepicker_all_time"] = "за все время";
-$lang["datepicker_apply"] = "";
-$lang["datepicker_cancel"] = "";
-$lang["datepicker_custom"] = "";
-$lang["datepicker_from"] = "";
+<?php
+$lang["datepicker_all_time"] = "За все время";
+$lang["datepicker_apply"] = "Применить";
+$lang["datepicker_cancel"] = "Отмена";
+$lang["datepicker_custom"] = "Пользовательское";
+$lang["datepicker_from"] = "От";
 $lang["datepicker_last_30"] = "Последние 30 дней";
 $lang["datepicker_last_7"] = "Последние 7 дней";
-$lang["datepicker_last_financial_year"] = "";
+$lang["datepicker_last_financial_year"] = "Последний финансовый год";
 $lang["datepicker_last_month"] = "Последний месяц";
-$lang["datepicker_last_year"] = "в прошлом году";
-$lang["datepicker_same_month_last_year"] = "";
-$lang["datepicker_same_month_to_same_day_last_year"] = "";
-$lang["datepicker_this_financial_year"] = "";
+$lang["datepicker_last_year"] = "За последний год";
+$lang["datepicker_same_month_last_year"] = "Тот же месяц прошлого года";
+$lang["datepicker_same_month_to_same_day_last_year"] = "В том же месяце в тот же день в прошлом году";
+$lang["datepicker_this_financial_year"] = "Текущий финансовый год";
 $lang["datepicker_this_month"] = "В этом месяце";
 $lang["datepicker_this_year"] = "В этом году";
-$lang["datepicker_to"] = "";
-$lang["datepicker_today"] = "сегодня";
-$lang["datepicker_today_last_year"] = "";
+$lang["datepicker_to"] = "Кому";
+$lang["datepicker_today"] = "Сегодня";
+$lang["datepicker_today_last_year"] = "Сегодняшний день в прошлом году";
 $lang["datepicker_weekstart"] = "0";
-$lang["datepicker_yesterday"] = "вчера";
+$lang["datepicker_yesterday"] = "Вчера";
