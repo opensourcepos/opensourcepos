3004f1e9eaef7e97951838cc87dd5dc07638d94c
Oleg <oleg.pischuk@bk.ru>
2023-05-22 15:24:31 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/expenses_categories/ru/

--- application/language/ru/expenses_categories_lang.php
+++ application/language/ru/expenses_categories_lang.php
@@ -1,21 +1,20 @@
-<?php 
-
-$lang["category_name_required"] = "Требуется Название категории расходов";
-$lang["expenses_categories_add_item"] = "Добавить Категорию";
+<?php
+$lang["category_name_required"] = "Название категории расходов - обязательно для заполнения";
+$lang["expenses_categories_add_item"] = "Добавить категорию";
 $lang["expenses_categories_cannot_be_deleted"] = "Не удалось удалить категорию расходов";
-$lang["expenses_categories_category_id"] = "ИД";
+$lang["expenses_categories_category_id"] = "№";
 $lang["expenses_categories_confirm_delete"] = "Вы уверены, что хотите удалить выбранную категорию расходов?";
-$lang["expenses_categories_confirm_restore"] = "";
-$lang["expenses_categories_description"] = "описание категории";
+$lang["expenses_categories_confirm_restore"] = "Вы уверены, что хотите восстановить выбранную категорию расходов?";
+$lang["expenses_categories_description"] = "Описание категории";
 $lang["expenses_categories_error_adding_updating"] = "Ошибка добавления / обновления категории расходов";
-$lang["expenses_categories_info"] = "Категория Информация О Расходах";
-$lang["expenses_categories_name"] = "название категории";
-$lang["expenses_categories_new"] = "новая категория";
+$lang["expenses_categories_info"] = "Информация о категории расходов";
+$lang["expenses_categories_name"] = "Название категории";
+$lang["expenses_categories_new"] = "Новая категория";
 $lang["expenses_categories_no_expenses_categories_to_display"] = "Нет категории для отображения";
 $lang["expenses_categories_none_selected"] = "Вы не выбрали ни одной категории расходов";
-$lang["expenses_categories_one_or_multiple"] = "Категория Расходов";
+$lang["expenses_categories_one_or_multiple"] = "Категория расходов";
 $lang["expenses_categories_quantity"] = "Количество";
-$lang["expenses_categories_successful_adding"] = "Категория расходов  добавлена успешно";
+$lang["expenses_categories_successful_adding"] = "Категория расходов добавлена успешно";
 $lang["expenses_categories_successful_deleted"] = "Категория расходов успешно удалена";
 $lang["expenses_categories_successful_updating"] = "Категория расходов успешно обновлена";
-$lang["expenses_categories_update"] = "Обновить Категорию";
+$lang["expenses_categories_update"] = "Обновить категорию";
