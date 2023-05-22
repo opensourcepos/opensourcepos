75c61b3e49dc49173142cd2f1c623cb89216b31a
Oleg <oleg.pischuk@bk.ru>
2023-05-22 15:14:35 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: opensourcepos/error
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/error/ru/

--- application/language/ru/error_lang.php
+++ application/language/ru/error_lang.php
@@ -1,4 +1,3 @@
-<?php 
-
-$lang["error_no_permission_module"] = "У вас нет разрешения на доступ к модуля называется";
-$lang["error_unknown"] = "неизвестный";
+<?php
+$lang["error_no_permission_module"] = "У вас нет разрешения на доступ к модулю";
+$lang["error_unknown"] = "Неизвестная ошибка";
