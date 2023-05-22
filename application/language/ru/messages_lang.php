80cdd2f2d1aaeac80a29a8f92ba6790cc6860796
Oleg <oleg.pischuk@bk.ru>
2023-05-22 17:48:40 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/ru/

--- application/language/ru/messages_lang.php
+++ application/language/ru/messages_lang.php
@@ -1,13 +1,13 @@
 <?php
-$lang["messages_first_name"] = "";
-$lang["messages_last_name"] = "";
+$lang["messages_first_name"] = "Имя";
+$lang["messages_last_name"] = "Фамилия";
 $lang["messages_message"] = "Сообщение";
-$lang["messages_message_placeholder"] = "";
-$lang["messages_message_required"] = "";
-$lang["messages_multiple_phones"] = "";
+$lang["messages_message_placeholder"] = "Введите сообщение...";
+$lang["messages_message_required"] = "Сообщенние - обязательно для заполнения";
+$lang["messages_multiple_phones"] = "(В случае нескольких получателей укажите номера мобильных телефонов через запятую)";
 $lang["messages_phone"] = "Номер телефона";
-$lang["messages_phone_number_required"] = "";
-$lang["messages_phone_placeholder"] = "";
+$lang["messages_phone_number_required"] = "Номер телефона обязателен для заполнения";
+$lang["messages_phone_placeholder"] = "Введите номер мобильного телефона...";
 $lang["messages_sms_send"] = "Отправить SMS";
-$lang["messages_successfully_sent"] = "";
-$lang["messages_unsuccessfully_sent"] = "";
+$lang["messages_successfully_sent"] = "Сообщение успешно отправлено: ";
+$lang["messages_unsuccessfully_sent"] = "Сообщение не отправлено: ";
