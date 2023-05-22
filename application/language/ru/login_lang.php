ad0476d99eadf65e2a37d6b89416e72d6819d09f
Oleg <oleg.pischuk@bk.ru>
2023-05-22 17:46:19 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: opensourcepos/login
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/login/ru/

--- application/language/ru/login_lang.php
+++ application/language/ru/login_lang.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $lang["login_gcaptcha"] = "Я не робот.";
-$lang["login_go"] = "ходите";
-$lang["login_invalid_gcaptcha"] = "Неверный Логин";
-$lang["login_invalid_installation"] = "Настройки неверные, проверьте php.ini";
-$lang["login_invalid_username_and_password"] = "Неправильное Имя или Пароль";
-$lang["login_login"] = "Логин";
-$lang["login_logout"] = "";
-$lang["login_migration_needed"] = "";
+$lang["login_go"] = "Войти";
+$lang["login_invalid_gcaptcha"] = "Пожалуйста, подтвердите, что вы не робот.";
+$lang["login_invalid_installation"] = "Ошибка установки, проверьте файл php.ini.";
+$lang["login_invalid_username_and_password"] = "Неверное имя пользователя или пароль.";
+$lang["login_login"] = "Вход";
+$lang["login_logout"] = "Выход";
+$lang["login_migration_needed"] = "Миграция базы данных в %1 начнется после входа в систему.";
 $lang["login_password"] = "Пароль";
 $lang["login_username"] = "Имя пользователя";
-$lang["login_welcome"] = "";
+$lang["login_welcome"] = "Панель управления";
