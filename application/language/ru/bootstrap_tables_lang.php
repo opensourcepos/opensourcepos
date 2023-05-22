cf5e6dee3956fbb7e87a9a9a784b722662dc315d
Oleg <oleg.pischuk@bk.ru>
2023-05-22 10:28:20 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/ru/

--- application/language/ru/bootstrap_tables_lang.php
+++ application/language/ru/bootstrap_tables_lang.php
@@ -1,10 +1,9 @@
-<?php 
-
-$lang["tables_all"] = "Все";
+<?php
+$lang["tables_all"] = "все";
 $lang["tables_columns"] = "Колонки";
-$lang["tables_hide_show_pagination"] = "Скривай/Покажи разбивку на страницы";
+$lang["tables_hide_show_pagination"] = "Скрыть/Показать нумерацию страниц";
 $lang["tables_loading"] = "Пожалуйста, подождите, идёт загрузка...";
-$lang["tables_page_from_to"] = "Записи с {0} по {1} из {2}";
+$lang["tables_page_from_to"] = "Показать с {0} по {1} из {2} записей";
 $lang["tables_refresh"] = "Обновить";
 $lang["tables_rows_per_page"] = "{0} записей на страницу";
 $lang["tables_toggle"] = "Переключить";
