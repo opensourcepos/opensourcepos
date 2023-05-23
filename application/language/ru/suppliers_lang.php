fd8da944877f231e23995b519a59116af1e742fa
Oleg <oleg.pischuk@bk.ru>
2023-05-23 12:03:23 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: opensourcepos/suppliers
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/suppliers/ru/

--- application/language/ru/suppliers_lang.php
+++ application/language/ru/suppliers_lang.php
@@ -1,23 +1,22 @@
-<?php 
-
-$lang["suppliers_account_number"] = "Номер Счета";
-$lang["suppliers_agency_name"] = " ";
-$lang["suppliers_cannot_be_deleted"] = "Не удалось удалить выбранные поставщиков, один или более из выбранных поставщиков имеет продаж.";
-$lang["suppliers_category"] = "";
+<?php
+$lang["suppliers_account_number"] = "Номер счета";
+$lang["suppliers_agency_name"] = "Название агентства";
+$lang["suppliers_cannot_be_deleted"] = "Не удалось удалить выбранных поставщиков. У одного или нескольких из них есть продажи.";
+$lang["suppliers_category"] = "Категория";
 $lang["suppliers_company_name"] = "Название компании";
-$lang["suppliers_company_name_required"] = "Название Компании - обязательное поле для заполнения.";
+$lang["suppliers_company_name_required"] = "Название компании - обязательное поле для заполнения.";
 $lang["suppliers_confirm_delete"] = "Вы уверены, что хотите удалить выбранных поставщиков?";
 $lang["suppliers_confirm_restore"] = "Вы уверены, что хотите восстановить выбранных поставщиков?";
-$lang["suppliers_cost"] = "";
-$lang["suppliers_error_adding_updating"] = "Ошибка при добавлении/обновлении поставщиком.";
-$lang["suppliers_goods"] = "";
+$lang["suppliers_cost"] = "Расходы поставщика";
+$lang["suppliers_error_adding_updating"] = "Ошибка при добавлении/обновлении поставщика.";
+$lang["suppliers_goods"] = "Поставщик товаров";
 $lang["suppliers_new"] = "Новый поставщик";
-$lang["suppliers_none_selected"] = "Вы не выбрали ни поставщики удалить.";
-$lang["suppliers_one_or_multiple"] = "поставщик (а)";
-$lang["suppliers_successful_adding"] = "Вы успешно добавили поставщиком";
-$lang["suppliers_successful_deleted"] = "Вы успешно удален";
-$lang["suppliers_successful_updating"] = "Вы успешно обновляли поставщиком";
-$lang["suppliers_supplier"] = "поставщик";
-$lang["suppliers_supplier_id"] = "Ид";
-$lang["suppliers_tax_id"] = "";
+$lang["suppliers_none_selected"] = "Вы не выбрали поставщиков для удаления.";
+$lang["suppliers_one_or_multiple"] = "Поставщик(и)";
+$lang["suppliers_successful_adding"] = "Поставщик был успешно добавлен";
+$lang["suppliers_successful_deleted"] = "Успешно удалено";
+$lang["suppliers_successful_updating"] = "Поставщик успешно обновлен";
+$lang["suppliers_supplier"] = "Поставщик";
+$lang["suppliers_supplier_id"] = "№";
+$lang["suppliers_tax_id"] = "№ налога";
 $lang["suppliers_update"] = "Обновить поставщика";
