82d7f48ee028b245b4e0020b79eb31d1621b9bac
jekkos <jeroen.peelaerts@gmail.com>
2023-08-29 00:49:20 +0200

    Add semicolon (#3792)

--- application/config/config.php
+++ application/config/config.php
@@ -436,7 +436,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix'] = '';
 $config['cookie_domain'] = '';
 $config['cookie_path'] = '/';
-$config['cookie_secure'] = TRUE
+$config['cookie_secure'] = TRUE;
 $config['cookie_httponly'] = TRUE;
 
 /*
