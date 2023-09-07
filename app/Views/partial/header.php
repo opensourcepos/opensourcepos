06b9ab4d286b2de89b197aa80c6271f9731015ca
Doug Hutcheson <owlbrudder@gmail.com>
2023-09-08 07:45:41 +0800

    Revert "Initial setup in a new environment"
    This reverts commit 23e06dea7fd0a09b2cbc80e974fdf9c1951e3ca6.

--- app/Views/partial/header.php
+++ app/Views/partial/header.php
@@ -21,65 +21,11 @@ $request = Services::request();
 
 	<?php if (ENVIRONMENT == 'development' || get_cookie('debug') == 'true' || $request->getGet('debug') == 'true') : ?>
 		<!-- inject:debug:css -->
-		<link rel="stylesheet" href="resources/css/jquery-ui-6fd5a6e819.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-dialog-1716ef6e7c.css">
-		<link rel="stylesheet" href="resources/css/jasny-bootstrap-40bf85f3ed.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-datetimepicker-6be929e975.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-select-66d5473b84.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-table-4b619bcd8f.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-table-sticky-header-07d65e7533.css">
-		<link rel="stylesheet" href="resources/css/daterangepicker-85523b7dfe.css">
-		<link rel="stylesheet" href="resources/css/chartist-c19aedb81a.css">
-		<link rel="stylesheet" href="resources/css/chartist-plugin-tooltip-2e0ec92e60.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-tagsinput-01a1887ade.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-toggle-e12db6c1f3.css">
-		<link rel="stylesheet" href="resources/css/bootstrap-019ef57791.autocomplete.css">
-		<link rel="stylesheet" href="resources/css/invoice-6a526688bd.css">
-		<link rel="stylesheet" href="resources/css/ospos_print-ad4fa36376.css">
-		<link rel="stylesheet" href="resources/css/ospos-5ccf48a62d.css">
-		<link rel="stylesheet" href="resources/css/popupbox-df1682d394.css">
-		<link rel="stylesheet" href="resources/css/receipt-c2c74c776e.css">
-		<link rel="stylesheet" href="resources/css/register-84b1267d84.css">
-		<link rel="stylesheet" href="resources/css/reports-4b8616a379.css">
 		<!-- endinject -->
 		<!-- inject:debug:js -->
-		<script src="resources/js/jquery-107fbe9555.js"></script>
-		<script src="resources/js/jquery-4fa896f615.form.js"></script>
-		<script src="resources/js/jquery-272ed07e41.validate.js"></script>
-		<script src="resources/js/jquery-ui-ab5284de5e.js"></script>
-		<script src="resources/js/bootstrap-894d79839f.js"></script>
-		<script src="resources/js/bootstrap-dialog-27123abb65.js"></script>
-		<script src="resources/js/jasny-bootstrap-7c6d7b8adf.js"></script>
-		<script src="resources/js/bootstrap-datetimepicker-9ae21e5aa6.js"></script>
-		<script src="resources/js/bootstrap-select-b01896a67b.js"></script>
-		<script src="resources/js/bootstrap-table-63b5d3c3e6.js"></script>
-		<script src="resources/js/bootstrap-table-export-e43b035e30.js"></script>
-		<script src="resources/js/bootstrap-table-mobile-a8fb9324f4.js"></script>
-		<script src="resources/js/bootstrap-table-sticky-header-7b4bb951c4.js"></script>
-		<script src="resources/js/moment-6c0a2330b0.min.js"></script>
-		<script src="resources/js/daterangepicker-2bb3f09fd8.js"></script>
-		<script src="resources/js/es6-promise-855125e6f5.js"></script>
-		<script src="resources/js/FileSaver-e73b1946e8.js"></script>
-		<script src="resources/js/html2canvas-e1d3a8d7cd.js"></script>
-		<script src="resources/js/jspdf-a03c76b858.debug.js"></script>
-		<script src="resources/js/jspdf.plugin-b7d17cf8db.autotable.src.js"></script>
-		<script src="resources/js/tableExport-8fad1e1d0f.min.js"></script>
-		<script src="resources/js/chartist-8a7ecb4445.js"></script>
-		<script src="resources/js/chartist-plugin-pointlabels-bc4349d572.js"></script>
-		<script src="resources/js/chartist-plugin-tooltip-9c9958544c.js"></script>
-		<script src="resources/js/chartist-plugin-barlabels-4165273742.js"></script>
-		<script src="resources/js/bootstrap-notify-376bc6eb87.js"></script>
-		<script src="resources/js/js-fa93e8894e.cookie.js"></script>
-		<script src="resources/js/bootstrap-tagsinput-9d43ec6292.js"></script>
-		<script src="resources/js/bootstrap-toggle-1c7a19a049.js"></script>
-		<script src="resources/js/clipboard-908af414ab.js"></script>
-		<script src="resources/js/imgpreview-4836346e15.full.jquery.js"></script>
-		<script src="resources/js/manage_tables-e48b5201f8.js"></script>
-		<script src="resources/js/nominatim-0f6e7d2e69.autocomplete.js"></script>
 		<!-- endinject -->
 	<?php else : ?>
 		<!--inject:prod:css -->
-		<link rel="stylesheet" href="resources/opensourcepos-215ec0f7db.min.css">
 		<!-- endinject -->
 
 		<!-- Tweaks to the UI for a particular theme should drop here  -->
@@ -87,8 +33,6 @@ $request = Services::request();
 		<link rel="stylesheet" type="text/css" href="<?php echo 'css/' . esc($config['theme']) . '.css' ?>"/>
 	<?php } ?>
 		<!-- inject:prod:js -->
-		<script src="resources/jquery-4a356126b9.min.js"></script>
-		<script src="resources/opensourcepos-086e360103.min.js"></script>
 		<!-- endinject -->
 	<?php endif; ?>
 
