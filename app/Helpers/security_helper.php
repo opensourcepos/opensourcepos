1ad70461949c88e8de9af63daacec3fcfe1f0b45
Doug Hutcheson <owlbrudder@gmail.com>
2023-09-15 07:21:19 +0800

    CI4 Enhancement: Upgrade security_helper.php
    This version removes surplus comments, removes surplus empty lines and improves responses to fwrite() errors

--- app/Helpers/security_helper.php
+++ app/Helpers/security_helper.php
@@ -2,7 +2,6 @@
 
 use CodeIgniter\Encryption\Encryption;
 
-
 /**
  * @return bool
  */
@@ -18,15 +17,6 @@ function check_encryption(): bool
 		config('Encryption')->key = $key;
 
 		//Write to .env
-		//For security reasons. at some sites the web server may not have write
-		//access to the base url, so we must not try to create files there.
-		//This code attempts to make just .env writable so it can be updated.
-		//A backup copy is written to the writable/backup directory.
-		//To make this code work, create the backup directory as below and
-		//ensure it is writeable by the webserver, including setting
-		//any SELinux permissions needed under eg Fedora
-		//We log a message to the user telling them what has happened.
-
 		$config_path = ROOTPATH . '.env';
 		$new_config_path = WRITEPATH . '/backup/.env' ;
 		$backup_path = WRITEPATH . '/backup/.env.bak';
@@ -35,13 +25,8 @@ function check_encryption(): bool
 		if(!copy($config_path, $backup_path))
 		{
 			log_message('error', "Unable to copy $config_path to $backup_path");
-			//we could return false at this point,
-			//but better for business to try the update first
 		}
 
-		//Attempt to make these files writable
-		//by the webserver user and group
-		//even if the directory is not writable
 		@chmod($config_path, 0770);
 		@chmod($backup_path, 0770);
 		
@@ -50,29 +35,29 @@ function check_encryption(): bool
 
 		if(!empty($old_key))
 		{
-			
 			$old_line = "# encryption.key = '$old_key' REMOVE IF UNNEEDED\r\n";
 			$insertion_point = stripos($config_file, 'encryption.key');
 			$config_file = substr_replace($config_file, $old_line, $insertion_point,0);
 		}
 
-		//Update the master copy
 		$handle = @fopen($config_path, 'w+');
 		
-		//Cope with failure to open the handle
 		if(empty($handle))
 		{
 			log_message('error', "Unable to open $config_path for updating");
 			return false;
 		}
-		else 
-		{	
-			@chmod($config_path, 0770);
-			fwrite($handle, $config_file) === FALSE;
-			fclose($handle);
-
-			log_message('info', "File $config_path has been updated."); 
+		
+		@chmod($config_path, 0770);
+		$retval = fwrite($handle, $config_file);
+		fclose($handle);
+		
+		if($retval === false)
+		{
+			log_message('error', "Unable to write to $config_path for updating.");
+			return false; 
 		}
+		log_message('info', "File $config_path has been updated."); 
 	}
 
 	return true;
@@ -85,20 +70,22 @@ function abort_encryption_conversion()
 
 	$config_file = file_get_contents($backup_path);
 
-	//Update the master copy
 	$handle = @fopen($config_path, 'w+');
 	
-	//Cope with failure to open the handle
 	if(empty($handle))
 	{
-		log_message('error', "Unable to open $config_path for updating");
+		log_message('error', "Unable to open $config_path to undo encryption conversion");
 	}
 	else 
 	{	
 		@chmod($config_path, 0770);
-		fwrite($handle, $config_file) === FALSE;
+		$retval = fwrite($handle, $config_file);
 		fclose($handle);
 
-		log_message('info', "File $config_path has been updated."); 
+		if($retval === false)
+		{
+			log_message('error', "Unable to write to $config_path to undo encryption conversion.");
+		}
+		log_message('info', "File $config_path has been updated to undo encryption conversion"); 
 	}
 }
