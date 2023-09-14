7caaca6c0a5629a305cec21dc8f9cbd8fc515cd1
Doug Hutcheson <owlbrudder@gmail.com>
2023-09-14 15:37:39 +0800

    Updade security_helper.php CI4: enhancement
    This code significantly rewritten to use CI4's WRITEPATH variable and to wrap file management in defensive code, as well as other changes.

--- app/Helpers/security_helper.php
+++ app/Helpers/security_helper.php
@@ -10,7 +10,7 @@ function check_encryption(): bool
 {
 	$old_key = config('Encryption')->key;
 
-	if(strlen($old_key) < 64)
+	if((empty($old_key)) or (strlen($old_key) < 64))
 	{
 		//Create Key
 		$encryption = new Encryption();
@@ -18,43 +18,60 @@ function check_encryption(): bool
 		config('Encryption')->key = $key;
 
 		//Write to .env
-		//For security reasons. the web server must not have write
-		//access to the base url, so we must not try to write or update files there.
+		//For security reasons. at some sites the web server may not have write
+		//access to the base url, so we must not try to create files there.
+		//This code attempts to make just .env writable so it can be updated.
+		//A backup copy is written to the writable/backup directory.
 		//To make this code work, create the backup directory as below and
 		//ensure it is writeable by the webserver, including setting
 		//any SELinux permissions needed under eg Fedora
-		//We present a message to the user telling them to
-		//copy the .env file into place manually.
+		//We log a message to the user telling them what has happened.
+
 		$config_path = ROOTPATH . '.env';
-		$backup_path = ROOTPATH . '/writable/backup/.env.bak';
+		$new_config_path = WRITEPATH . '/backup/.env' ;
+		$backup_path = WRITEPATH . '/backup/.env.bak';
 
-		copy($config_path, $backup_path);
+		//Copy to backup
+		if(!copy($config_path, $backup_path))
+		{
+			log_message('error', "Unable to copy $config_path to $backup_path");
+			//we could return false at this point,
+			//but better for business to try the update first
+		}
+
+		//Attempt to make these files writable
+		//by the webserver user and group
+		//even if the directory is not writable
+		@chmod($config_path, 0770);
 		@chmod($backup_path, 0770);
+		
 		$config_file = file_get_contents($config_path);
-
 		$config_file = preg_replace("/(encryption\.key.*=.*)('.*')/", "$1'$key'", $config_file);
 
 		if(!empty($old_key))
 		{
+			
 			$old_line = "# encryption.key = '$old_key' REMOVE IF UNNEEDED\r\n";
 			$insertion_point = stripos($config_file, 'encryption.key');
 			$config_file = substr_replace($config_file, $old_line, $insertion_point,0);
 		}
 
-		$new_config_path = ROOTPATH . '/writable/backup/.env' ;
-
-		//Changes to .env must only be invoked by Login controller
-		$router = service('router');
-		$controller = $router->controllerName();
-		if(strtolower(class_basename($controller)) == 'login')
+		//Update the master copy
+		$handle = @fopen($config_path, 'w+');
+		
+		//Cope with failure to open the handle
+		if(empty($handle))
 		{
-			$handle = @fopen($new_config_path, 'w+') or die("Unable to open " . $new_config_path . " for writing");
-			@chmod($new_config_path, 0770);
+			log_message('error', "Unable to open $config_path for updating");
+			return false;
+		}
+		else 
+		{	
+			@chmod($config_path, 0770);
 			fwrite($handle, $config_file) === FALSE;
 			fclose($handle);
-			echo '<script type="text/javascript">';
-			echo ' alert("File ' . $new_config_path . ' has been created. You must move this to ' . ROOTPATH . '")'; 
-			echo '</script>';
+
+			log_message('info', "File $config_path has been updated."); 
 		}
 	}
 
@@ -63,9 +80,25 @@ function check_encryption(): bool
 
 function abort_encryption_conversion()
 {
-	$new_config_path = ROOTPATH . '/writable/backup/.env' ;
-	$backup_path = ROOTPATH . '/writable/backup/.env.bak';
+	$config_path = ROOTPATH . '.env';
+	$backup_path = WRITEPATH . '/backup/.env.bak';
 
-	unlink($new_config_path);
-	rename($backup_path, $new_config_path);
+	$config_file = file_get_contents($backup_path);
+
+	//Update the master copy
+	$handle = @fopen($config_path, 'w+');
+	
+	//Cope with failure to open the handle
+	if(empty($handle))
+	{
+		log_message('error', "Unable to open $config_path for updating");
+	}
+	else 
+	{	
+		@chmod($config_path, 0770);
+		fwrite($handle, $config_file) === FALSE;
+		fclose($handle);
+
+		log_message('info', "File $config_path has been updated."); 
+	}
 }
