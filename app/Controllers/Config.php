3bdc2c184616f9115113a75017e8cc63f3031af0
Doug Hutcheson <owlbrudder@gmail.com>
2023-10-19 08:12:52 +0800

    CI4: Bugfix test sanity of config item
    Add a !empty test when dereferencing mailchimp_api_key and mailchimp_list_id from the config array.

--- app/Controllers/Config.php
+++ app/Controllers/Config.php
@@ -305,11 +305,11 @@ class Config extends Secure_Controller
 				$this->encrypter = Services::encrypter();
 			}
 			
-			$mailchimp_api_key = isset($this->config['mailchimp_api_key'])
+			$mailchimp_api_key = (isset($this->config['mailchimp_api_key']) && !empty($this->config['mailchimp_api_key']))
 				? $this->encrypter->decrypt($this->config['mailchimp_api_key'])
 				: '';
 
-			$mailchimp_list_id = isset($this->config['mailchimp_list_id'])
+			$mailchimp_list_id = (isset($this->config['mailchimp_list_id']) && !empty($this->config['mailchimp_list_id']))
 				? $this->encrypter->decrypt($this->config['mailchimp_list_id'])
 				: '';
 
