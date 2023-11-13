d21b3f7ba9bd25fb3b6110f96fb88d9fa5ffc6aa
donjade <zhouppei@gmail.com>
2023-11-13 20:31:34 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 28.5% (2 of 7 strings)
    
    Translation: opensourcepos/enum
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/enum/zh_Hans/

--- application/language/zh-Hans/enum_lang.php
+++ application/language/zh-Hans/enum_lang.php
@@ -1,9 +1,8 @@
-<?php 
-
+<?php
 $lang["enum_half_down"] = "";
 $lang["enum_half_even"] = "";
 $lang["enum_half_five"] = "";
 $lang["enum_half_odd"] = "";
 $lang["enum_half_up"] = "";
-$lang["enum_round_down"] = "";
-$lang["enum_round_up"] = "";
+$lang["enum_round_down"] = "去尾法";
+$lang["enum_round_up"] = "进一法";
