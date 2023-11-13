ed371a056828d8e36aaadc12f29ef8dde1080fbb
donjade <zhouppei@gmail.com>
2023-11-13 21:22:32 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/zh_Hans/

--- application/language/zh-Hans/messages_lang.php
+++ application/language/zh-Hans/messages_lang.php
@@ -1,14 +1,13 @@
-<?php 
-
-$lang["messages_first_name"] = "First name";
-$lang["messages_last_name"] = "Last name";
+<?php
+$lang["messages_first_name"] = "名";
+$lang["messages_last_name"] = "姓";
 $lang["messages_message"] = "Message";
 $lang["messages_message_placeholder"] = "Your Message here...";
 $lang["messages_message_required"] = "Message required";
 $lang["messages_multiple_phones"] = "(In case of multiple recipients, enter mobile numbers separated by commas)";
-$lang["messages_phone"] = "Phone number";
-$lang["messages_phone_number_required"] = "Phone number required";
+$lang["messages_phone"] = "电话号码";
+$lang["messages_phone_number_required"] = "电话号码是比填的";
 $lang["messages_phone_placeholder"] = "Mobile Number(s) here...";
 $lang["messages_sms_send"] = "Send SMS";
-$lang["messages_successfully_sent"] = "Message successfully sent to: ";
+$lang["messages_successfully_sent"] = "信息成功发送到 ";
 $lang["messages_unsuccessfully_sent"] = "Message unsuccessfully sent to: ";
