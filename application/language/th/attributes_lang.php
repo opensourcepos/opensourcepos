ead1603213f9fbfbb406ffbbb6f11a79953d547b
khao_lek <khao_lek@yahoo.com>
2023-12-19 04:03:22 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: opensourcepos/attributes
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/attributes/th/

--- application/language/th/attributes_lang.php
+++ application/language/th/attributes_lang.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 $lang["attributes_attribute_value_invalid_chars"] = "คุณลักษณะไม่สามารถมีเครื่องหมาย ':' หรือเครื่องหมาย '|' ได้";
 $lang["attributes_confirm_delete"] = "ต้องการลบคุณลักษณะที่เลือกหรือไม่ ?";
 $lang["attributes_confirm_restore"] = "ต้องการคืนค่าคุณลักษณะที่เลือกหรือไม่ ?";
