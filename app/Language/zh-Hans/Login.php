7cdd478636fbad311c1a2e7649aa2531031b90d6
jekkos <jeroen.peelaerts@gmail.com>
2023-12-24 14:05:42 +0100

    Sync language files (#3468)

--- app/Language/zh-Hans/Login.php
+++ app/Language/zh-Hans/Login.php
@@ -6,9 +6,9 @@ return [
 	"invalid_installation" => "安装不正确，请检查您的php.ini文件。",
 	"invalid_username_and_password" => "无效的用户名或密码。",
 	"login" => "登入",
-	"logout" => "",
+	"logout" => "退出",
 	"migration_needed" => "",
 	"password" => "密码",
 	"username" => "用户名",
-	"welcome" => "",
+	"welcome" => "欢迎来到 {0}!",
 ];
