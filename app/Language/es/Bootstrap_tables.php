b34380d6aaa6f9bfe6aa97780614081d306ffbe1
jekkos <jeroen.peelaerts@gmail.com>
2023-12-29 11:58:18 +0100

    Use loading animation only (#3891)

--- app/Language/es/Bootstrap_tables.php
+++ app/Language/es/Bootstrap_tables.php
@@ -3,7 +3,7 @@ return [
 	"all" => "Todas",
 	"columns" => "Columnas",
 	"hide_show_pagination" => "Ocultar/Mostrar paginación",
-	"loading" => "Por favor espere...",
+	"loading" => "",
 	"page_from_to" => "Mostrando desde {0} hasta {1} - En total {2} resultados",
 	"refresh" => "Refrescar",
 	"rows_per_page" => "{0} resultados por página",
--- app/Language/hr-HR/Bootstrap_tables.php
+++ app/Language/hr-HR/Bootstrap_tables.php
@@ -3,7 +3,7 @@ return [
 	"all" => "Sve",
 	"columns" => "Kolone",
 	"hide_show_pagination" => "Prikaži/sakrij stranice",
-	"loading" => "Molimo pričekajte ...",
+	"loading" => "",
 	"page_from_to" => "Prikazujem {0}. - {1} od ukupnog broja zapisa {2}",
 	"refresh" => "Osvježi",
 	"rows_per_page" => "{0} broj zapisa po stranici",
