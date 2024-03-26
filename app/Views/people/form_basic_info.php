6d374144447e529a5965422fc171c4c5d27e2ca1
objecttothis <objecttothis@gmail.com>
2024-03-26 06:45:23 +0400

    Removed escaping

--- app/Views/people/form_basic_info.php
+++ app/Views/people/form_basic_info.php
@@ -11,7 +11,7 @@
 			'name' => 'first_name',
 			'id' => 'first_name',
 			'class' => 'form-control input-sm',
-			'value' => html_limited_decode(esc_safe($person_info->first_name), ['\''])
+			'value' => $person_info->first_name
 		]) ?>
 	</div>
 </div>
@@ -23,7 +23,7 @@
 			'name' => 'last_name',
 			'id' => 'last_name',
 			'class' => 'form-control input-sm',
-			'value' => html_limited_decode(esc_safe($person_info->last_name), ['\''])
+			'value' => $person_info->last_name
 		]) ?>
 	</div>
 </div>
@@ -62,7 +62,7 @@
 				'name' => 'email',
 				'id' => 'email',
 				'class' => 'form-control input-sm',
-				'value' => esc($person_info->email)
+				'value' => $person_info->email
 			]) ?>
 		</div>
 	</div>
@@ -77,7 +77,7 @@
 				'name' => 'phone_number',
 				'id' => 'phone_number',
 				'class' => 'form-control input-sm',
-				'value' => esc($person_info->phone_number)
+				'value' => $person_info->phone_number
 			]) ?>
 		</div>
 	</div>
@@ -90,7 +90,7 @@
 			'name' => 'address_1',
 			'id' => 'address_1',
 			'class' => 'form-control input-sm',
-			'value' => esc($person_info->address_1)
+			'value' => $person_info->address_1
 		]) ?>
 	</div>
 </div>
@@ -102,7 +102,7 @@
 			'name' => 'address_2',
 			'id' => 'address_2',
 			'class' => 'form-control input-sm',
-			'value' => esc($person_info->address_2)
+			'value' => $person_info->address_2
 		]) ?>
 	</div>
 </div>
@@ -114,7 +114,7 @@
 			'name' => 'city',
 			'id' => 'city',
 			'class' => 'form-control input-sm',
-			'value' => html_limited_decode(esc($person_info->city), ['\''])
+			'value' => $person_info->city
 		]) ?>
 	</div>
 </div>
@@ -126,7 +126,7 @@
 			'name' => 'state',
 			'id' => 'state',
 			'class' => 'form-control input-sm',
-			'value' => esc($person_info->state)
+			'value' => $person_info->state
 		]) ?>
 	</div>
 </div>
@@ -138,7 +138,7 @@
 			'name' => 'zip',
 			'id' => 'postcode',
 			'class' => 'form-control input-sm',
-			'value' => esc($person_info->zip)
+			'value' => $person_info->zip
 		]) ?>
 	</div>
 </div>
@@ -150,7 +150,7 @@
 			'name' => 'country',
 			'id' => 'country',
 			'class' => 'form-control input-sm',
-			'value' => html_limited_decode(esc($person_info->country), ['\''])
+			'value' => $person_info->country
 		]) ?>
 	</div>
 </div>
@@ -162,7 +162,7 @@
 			'name' => 'comments',
 			'id' => 'comments',
 			'class' => 'form-control input-sm',
-			'value' => html_limited_decode(esc($person_info->comments), ['\'', '&', '"'])
+			'value' => $person_info->comments
 		]) ?>
 	</div>
 </div>
