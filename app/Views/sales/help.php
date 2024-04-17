50d5d61071dd95e634c94b22971ff8a2cff919de
WShells <26513147+WebShells@users.noreply.github.com>
2024-04-17 15:24:26 +0300

    Fix for Shortcuts
    Keyboard Shortcuts Help modl returning 404 / not displaying

--- app/Views/sales/help.php
+++ app/Views/sales/help.php
@@ -1,57 +1,57 @@
 <div class="container-fluid">
 	<ul class="nav nav-tabs" id="SCTabs" data-toggle="tab">
-		<li class="active"><a href="#system_shortcuts" data-toggle="tab" title="<?= $this->lang->line('sales_key_system'); ?>"><?= $this->lang->line('sales_key_system'); ?></a></li>
-		<li><a href="#browser_shortcuts" data-toggle="tab" title="<?= $this->lang->line('sales_key_browser'); ?>"><?= $this->lang->line('sales_key_browser'); ?></a></li>
+		<li class="active"><a href="#system_shortcuts" data-toggle="tab" title="<?= lang('Sales.key_system'); ?>"><?= lang('Sales.key_system'); ?></a></li>
+		<li><a href="#browser_shortcuts" data-toggle="tab" title="<?= lang('Sales.key_browser'); ?>"><?= lang('Sales.key_browser'); ?></a></li>
 	</ul>  
 	<div class="tab-content">
 		<div class="tab-pane active" id="system_shortcuts">
 		<table class="table table-striped table-hover">
 		<thead>
 			<tr>
-				<th><?= $this->lang->line('sales_key_help'); ?></th>
-				<th><?= $this->lang->line('sales_key_function'); ?></th>
+				<th><?= lang('Sales.key_help'); ?></th>
+				<th><?= lang('Sales.key_function'); ?></th>
 			</tr>
 		</thead>
 			<tbody>
 				<tr>
 					<td><code>ESC</code></td>
-					<td><?= $this->lang->line('sales_key_cancel'); ?></td>
+					<td><?= lang('Sales.key_cancel'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 1</code></td>
-					<td><?= $this->lang->line('sales_key_item_search'); ?></td>
+					<td><?= lang('Sales.key_item_search'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 2</code></td>
-					<td><?= $this->lang->line('sales_key_customer_search'); ?></td>
+					<td><?= lang('Sales.key_customer_search'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 3</code></td>
-					<td><?= $this->lang->line('sales_key_suspend'); ?></td>
+					<td><?= lang('Sales.key_suspend'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 4</code></td>
-					<td><?= $this->lang->line('sales_key_suspended'); ?></td>
+					<td><?= lang('Sales.key_suspended'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 5</code></td>
-					<td><?= $this->lang->line('sales_key_tendered'); ?></td>
+					<td><?= lang('Sales.key_tendered'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 6</code></td>
-					<td><?= $this->lang->line('sales_key_payment'); ?></td>
+					<td><?= lang('Sales.key_payment'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 7</code></td>
-					<td><?= $this->lang->line('sales_key_finish_sale'); ?></td>
+					<td><?= lang('Sales.key_finish_sale'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 8</code></td>
-					<td><?= $this->lang->line('sales_key_finish_quote'); ?></td>
+					<td><?= lang('Sales.key_finish_quote'); ?></td>
 				</tr>
 				<tr>
 					<td><code>ALT + 9</code></td>
-					<td><?= $this->lang->line('sales_key_help_modal'); ?></td>
+					<td><?= lang('Sales.key_help_modal'); ?></td>
 				</tr>
 			</tbody>
 		</table>
@@ -61,34 +61,34 @@
 		<table class="table table-striped table-hover">
 		<thead>
 			<tr>
-				<th><?= $this->lang->line('sales_key_help'); ?></th>
-				<th><?= $this->lang->line('sales_key_function'); ?></th>
+				<th><?= lang('Sales.key_help'); ?></th>
+				<th><?= lang('Sales.key_function'); ?></th>
 			</tr>
 		</thead>
 			<tbody>
 				<tr>
 					<td><code>F11</code></td>
-					<td><?= $this->lang->line('sales_key_full'); ?></td>
+					<td><?= lang('Sales.key_full'); ?></td>
 				</tr>
 				<tr>
 					<td><code>CTRL + </code></td>
-					<td><?= $this->lang->line('sales_key_in'); ?></td>
+					<td><?= lang('Sales.key_in'); ?></td>
 				</tr>
 				<tr>
 					<td><code>CTRL -</code></td>
-					<td><?= $this->lang->line('sales_key_out'); ?></td>
+					<td><?= lang('Sales.key_out'); ?></td>
 				</tr>
 				<tr>
 					<td><code>CTRL + 0</code></td>
-					<td><?= $this->lang->line('sales_key_restore'); ?></td>
+					<td><?= lang('Sales.key_restore'); ?></td>
 				</tr>
 				<tr>
 					<td><code>CTRL + P</code></td>
-					<td><?= $this->lang->line('sales_key_print'); ?></td>
+					<td><?= lang('Sales.key_print'); ?></td>
 				</tr>
 				<tr>
 					<td><code>CTRL + F</code></td>
-					<td><?= $this->lang->line('sales_key_search'); ?></td>
+					<td><?= lang('Sales.key_search'); ?></td>
 				</tr>
 			</tbody>
 		</table>
