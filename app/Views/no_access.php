f22235eb85e1f4ae10499d7e6273dc6f70938549
jekkos <jeroen.peelaerts@gmail.com>
2024-05-04 10:16:46 +0200

    Fix redirect + no permission error message

--- app/Views/no_access.php
+++ app/Views/no_access.php
@@ -2,4 +2,4 @@
 /**
  * @var string $module_name
  */
-echo lang('Errors.no_permission_module') . " $module_name" . (!empty($permission_id) ? " ($permission_id)" : '');
+echo lang('Error.no_permission_module') . " $module_name" . (!empty($permission_id) ? " ($permission_id)" : '');
