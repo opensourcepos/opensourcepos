e1f8b7300542286bbe41e8dcf605cbdbec88a585
objecttothis <17935339+objecttothis@users.noreply.github.com>
2024-07-27 00:08:49 +0400

    Add check to migration to prevent errors (#4032)
    * Add check to migration
    
    - Only drop the constraint if it exists.
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>
    
    * Automatic bump of package-lock.json
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>
    
    ---------
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>

--- app/Database/Migrations/20240630000001_fix_keys_for_db_upgrade.php
+++ app/Database/Migrations/20240630000001_fix_keys_for_db_upgrade.php
@@ -11,7 +11,14 @@ class Migration_fix_keys_for_db_upgrade extends Migration
 	 */
 	public function up(): void
 	{
-		$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes') . ' DROP FOREIGN KEY ospos_sales_items_taxes_ibfk_1');
+		$checkSql = "SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = DATABASE() AND TABLE_NAME = '" . $this->db->prefixTable('sales_items_taxes') . "' AND CONSTRAINT_NAME = 'ospos_sales_items_taxes_ibfk_1'";
+		$foreignKeyExists = $this->db->query($checkSql)->getRow();
+
+		if($foreignKeyExists)
+		{
+			$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes') . ' DROP FOREIGN KEY ospos_sales_items_taxes_ibfk_1');
+		}
+
 		$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes')
 			. ' ADD CONSTRAINT ospos_sales_items_taxes_ibfk_1 FOREIGN KEY (sale_id, item_id, line) '
 			. ' REFERENCES ' . $this->db->prefixTable('sales_items') . ' (sale_id, item_id, line)');
@@ -22,7 +29,14 @@ class Migration_fix_keys_for_db_upgrade extends Migration
 	 */
 	public function down(): void
 	{
-		$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes') . ' DROP CONSTRAINT ospos_sales_items_taxes_ibfk_1');
+		$checkSql = "SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = DATABASE() AND TABLE_NAME = '" . $this->db->prefixTable('sales_items_taxes') . "' AND CONSTRAINT_NAME = 'ospos_sales_items_taxes_ibfk_1'";
+		$foreignKeyExists = $this->db->query($checkSql)->getRow();
+
+		if($foreignKeyExists)
+		{
+			$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes') . ' DROP CONSTRAINT ospos_sales_items_taxes_ibfk_1');
+		}
+
 		$this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes')
 			. ' ADD CONSTRAINT ospos_sales_items_taxes_ibfk_1 FOREIGN KEY (sale_id) '
 			. ' REFERENCES ' . $this->db->prefixTable('sales_items') . ' (sale_id)');
