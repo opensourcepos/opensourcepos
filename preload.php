c412d15673e9e5dace31c4f88b5453cab735bb0d
objecttothis <objecttothis@gmail.com>
2024-11-04 14:37:50 +0400

    Bump CodeIgniter4 to 4.5.5
    - Updated composer.json and composer.lock.
    - Ran through steps in https://codeigniter.com/user_guide/installation/upgrade_452.html
    - Ran through steps in https://codeigniter.com/user_guide/installation/upgrade_453.html (this bumps several packages)
    - Ran through steps in https://codeigniter.com/user_guide/installation/upgrade_454.html
    - Ran through steps in https://codeigniter.com/user_guide/installation/upgrade_455.html
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>

--- preload.php
+++ preload.php
@@ -29,19 +29,6 @@ require __DIR__ . '/app/Config/Paths.php';
 // Path to the front controller
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
 
-/**
- * See https://www.php.net/manual/en/function.str-contains.php#126277
- */
-if (! function_exists('str_contains')) {
-    /**
-     * Polyfill of str_contains()
-     */
-    function str_contains(string $haystack, string $needle): bool
-    {
-        return empty($needle) || strpos($haystack, $needle) !== false;
-    }
-}
-
 class preload
 {
     /**
@@ -56,17 +43,20 @@ class preload
                 '/system/Database/Postgre/',
                 '/system/Database/SQLite3/',
                 '/system/Database/SQLSRV/',
-                // Not needed.
+                // Not needed for web apps.
                 '/system/Database/Seeder.php',
                 '/system/Test/',
-                '/system/Language/',
                 '/system/CLI/',
                 '/system/Commands/',
                 '/system/Publisher/',
                 '/system/ComposerScripts.php',
+                // Not Class/Function files.
+                '/system/Config/Routes.php',
+                '/system/Language/',
+                '/system/bootstrap.php',
+                '/system/rewrite.php',
                 '/Views/',
                 // Errors occur.
-                '/system/Config/Routes.php',
                 '/system/ThirdParty/',
             ],
         ],
@@ -77,16 +67,18 @@ class preload
         $this->loadAutoloader();
     }
 
-    private function loadAutoloader()
+    private function loadAutoloader(): void
     {
         $paths = new Config\Paths();
-        require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+        require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'Boot.php';
+
+        CodeIgniter\Boot::preload($paths);
     }
 
     /**
      * Load PHP files.
      */
-    public function load()
+    public function load(): void
     {
         foreach ($this->paths as $path) {
             $directory = new RecursiveDirectoryIterator($path['include']);
