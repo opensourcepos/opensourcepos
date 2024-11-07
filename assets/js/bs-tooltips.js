c77c9f7c191ed227a32134adbde91522b268c986
BudsieBuds <BudsieBuds@users.noreply.github.com>
2024-11-07 20:09:19 +0100

    Modernize Jekyll site (#4110)
    * Modernize Jekyll site
    
    The site now boasts the new README, Bootstrap 5 goodness, light/dark mode, and a footer so sleek it could moonwalk.
    
    * Fixed broken links

---
+++ assets/js/bs-tooltips.js
@@ -0,0 +1,3 @@
+// Script from https://getbootstrap.com/docs/5.3/components/tooltips/#enable-tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
