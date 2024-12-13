4fd1c64c6167498c1d0c331eb611eadb40361626
Munibullah Shah <munibs47@gmail.com>
2024-12-13 00:08:13 +0000

    Translated using Weblate (Urdu)
    Currently translated at 12.5% (1 of 8 strings)
    
    Translation: opensourcepos/bootstrap_tables
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/bootstrap_tables/ur/

--- app/Language/ur/Bootstrap_tables.php
+++ app/Language/ur/Bootstrap_tables.php
@@ -1,11 +1,12 @@
 <?php
+
 return [
-	"all" => "",
-	"columns" => "",
-	"hide_show_pagination" => "",
-	"loading" => "",
-	"page_from_to" => "",
-	"refresh" => "",
-	"rows_per_page" => "",
-	"toggle" => "",
+    'all' => "سب",
+    'columns' => "",
+    'hide_show_pagination' => "",
+    'loading' => "",
+    'page_from_to' => "",
+    'refresh' => "",
+    'rows_per_page' => "",
+    'toggle' => "",
 ];
