a5b2b5f77161985069e05839faa57949db974d9f
El_Coloso <diegoramosp@gmail.com>
2025-01-17 02:26:39 -0500

    Fixes for receipt + invoice (#2682)
    * Email invoice bar code
    * Send invoice by email
    * Remove default comment on invoice if comment was set

--- app/Views/sales/receipt.php
+++ app/Views/sales/receipt.php
@@ -24,7 +24,7 @@ if (isset($error_message))
 	{
 		var send_email = function()
 		{
-			$.get('<?= site_url() . esc("/sales/sendReceipt/$sale_id_num", 'url') ?>',
+			$.get('<?= site_url() . "sales/sendReceipt/$sale_id_num" ?>',
 				function(response)
 				{
 					$.notify( { message: response.message }, { type: response.success ? 'success' : 'danger'} )
