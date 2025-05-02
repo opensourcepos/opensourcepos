cedcbf459e4694b7698033997d9dfcaebb568d87
Mohamed-Qadir <mohamedqadir011@gmail.com>
2025-05-02 09:19:41 +0000

    Translated using Weblate (Kurdish (Central))
    Currently translated at 41.6% (5 of 12 strings)
    
    Translation: opensourcepos/messages
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/messages/ckb/

--- app/Language/ckb/Messages.php
+++ app/Language/ckb/Messages.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'first_name' => "",
-    'last_name' => "",
+    'first_name' => "ناوی یەکەم",
+    'last_name' => "ناوی خێزانی",
     'message' => "نامە",
-    'message_placeholder' => "",
-    'message_required' => "",
+    'message_placeholder' => "نامەکەت لێرە...",
+    'message_required' => "نامە پێویستە",
     'multiple_phones' => "",
     'phone' => "",
     'phone_number_required' => "",
