13ac2948c84e93fdb45c7b9685b8c20249a7a78c
objecttothis <objecttothis@gmail.com>
2025-05-02 17:24:18 +0400

    Refactoring
    - PSR-12 Compliance formatting changes
    - Refactored several generic functions into the migration_helper.php
    - First check if primary key exists before attempting to create it.
    - Grouped functions together in migration_helper.php
    - phpdoc commenting functions
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>

--- app/Database/Migrations/20240630000001_fix_keys_for_db_upgrade.php
+++ app/Database/Migrations/20240630000001_fix_keys_for_db_upgrade.php
@@ -12,16 +12,28 @@ class Migration_fix_keys_for_db_upgrade extends Migration
      */
     public function up(): void
     {
-        $this->db->query("ALTER TABLE `ospos_tax_codes` MODIFY `deleted` tinyint(1) DEFAULT 0 NOT NULL;");
+        helper('migration');
 
-        if (!$this->indexExists('ospos_customers', 'company_name')) {
-            $this->db->query("ALTER TABLE `ospos_customers` ADD INDEX(`company_name`)");
+        $forge = Database::forge();
+        $fields = [
+            'deleted' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'null' => false,
+            ],
+        ];
+        $forge->modifyColumn('tax_codes', $fields);
+
+        if (!indexExists('customers', 'company_name')) {
+            $forge->addKey('company_name', false, false, 'company_name');
+            $forge->processIndexes('customers');
         }
 
         $checkSql = "SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = DATABASE() AND TABLE_NAME = '" . $this->db->prefixTable('sales_items_taxes') . "' AND CONSTRAINT_NAME = 'ospos_sales_items_taxes_ibfk_1'";
         $foreignKeyExists = $this->db->query($checkSql)->getRow();
 
-        if ($foreignKeyExists) {
+        if($foreignKeyExists) {
             $this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes') . ' DROP FOREIGN KEY ospos_sales_items_taxes_ibfk_1');
         }
 
@@ -29,10 +41,9 @@ class Migration_fix_keys_for_db_upgrade extends Migration
             . ' ADD CONSTRAINT ospos_sales_items_taxes_ibfk_1 FOREIGN KEY (sale_id, item_id, line) '
             . ' REFERENCES ' . $this->db->prefixTable('sales_items') . ' (sale_id, item_id, line)');
 
-
-        $this->createPrimaryKey('customers', 'person_id');
-        $this->createPrimaryKey('employees', 'person_id');
-        $this->createPrimaryKey('suppliers', 'person_id');
+        createPrimaryKey('customers', 'person_id');
+        createPrimaryKey('employees', 'person_id');
+        createPrimaryKey('suppliers', 'person_id');
     }
 
     /**
@@ -50,93 +61,5 @@ class Migration_fix_keys_for_db_upgrade extends Migration
         $this->db->query('ALTER TABLE ' . $this->db->prefixTable('sales_items_taxes')
             . ' ADD CONSTRAINT ospos_sales_items_taxes_ibfk_1 FOREIGN KEY (sale_id) '
             . ' REFERENCES ' . $this->db->prefixTable('sales_items') . ' (sale_id)');
-    }
-
-    private function createPrimaryKey(string $table, string $index): void
-    {
-        $result = $this->db->query('SELECT 1 FROM information_schema.columns WHERE table_schema = DATABASE() AND table_name= \'' . $this->db->getPrefix() . "$table' AND column_key = '$index'");
-
-        if (! $result->getRowArray()) {
-            $constraints = $this->deleteForeignKeyConstraints($table, $index);
-            $this->deleteIndex($table, $index);
-            $forge = Database::forge();
-            $forge->addPrimaryKey($index,'PRIMARY');
-            $forge->processIndexes($table);
-            $this->recreateForeignKeyConstraints($constraints);
-        }
-    }
-
-    private function deleteForeignKeyConstraints(string $table, string $column): array
-    {
-        $result = $this->db->query("
-            SELECT DISTINCT
-                kcu.CONSTRAINT_NAME,
-                kcu.TABLE_NAME,
-                kcu.COLUMN_NAME,
-                kcu.REFERENCED_TABLE_NAME,
-                kcu.REFERENCED_COLUMN_NAME,
-                rc.DELETE_RULE,
-                rc.UPDATE_RULE
-            FROM information_schema.KEY_COLUMN_USAGE kcu
-            LEFT JOIN information_schema.REFERENTIAL_CONSTRAINTS rc
-                ON kcu.CONSTRAINT_NAME = rc.CONSTRAINT_NAME
-                AND kcu.TABLE_NAME = rc.TABLE_NAME
-            WHERE kcu.TABLE_SCHEMA = DATABASE()
-                AND ((kcu.REFERENCED_TABLE_NAME = '" . $this->db->getPrefix() . "$table' AND kcu.REFERENCED_COLUMN_NAME = '$column')
-                OR (kcu.TABLE_NAME = '" . $this->db->getPrefix() . "$table' AND kcu.COLUMN_NAME = '$column'))
-        ");
-
-        $deletedConstraints = [];
-
-        foreach ($result->getResultArray() as $constraint) {
-            $deletedConstraints[] = [
-                'constraintName' => $constraint['CONSTRAINT_NAME'],
-                'tableName' => str_starts_with($constraint['TABLE_NAME'], $this->db->DBPrefix)
-                    ? substr($constraint['TABLE_NAME'], strlen($this->db->DBPrefix))
-                    : $constraint['TABLE_NAME'],
-                'columnName' => $constraint['COLUMN_NAME'],
-                'referencedTable' => str_starts_with($constraint['REFERENCED_TABLE_NAME'], $this->db->DBPrefix)
-                    ? substr($constraint['REFERENCED_TABLE_NAME'], strlen($this->db->DBPrefix))
-                    : $constraint['REFERENCED_TABLE_NAME'],
-                'referencedColumn' => $constraint['REFERENCED_COLUMN_NAME'],
-                'onDelete' => $constraint['DELETE_RULE'],
-                'onUpdate' => $constraint['UPDATE_RULE'],
-            ];
-        }
-
-        if ($deletedConstraints) {
-            $forge = Database::forge();
-            foreach ($deletedConstraints as $foreignKey) {
-                $forge->dropForeignKey($foreignKey['tableName'], $foreignKey['constraintName']);
-            }
-        }
-
-        return $deletedConstraints;
-    }
-
-    private function deleteIndex(string $table, string $index): void
-    {
-        if ($this->indexExists($table, $index)) {
-            $forge = Database::forge();
-            $forge->dropKey($table, $index, FALSE);
-        }
-    }
-
-    private function indexExists(string $table, string $index): bool
-    {
-        $result = $this->db->query('SELECT COUNT(*) FROM information_schema.statistics WHERE table_schema = DATABASE() AND table_name = \'' . $this->db->getPrefix() . "$table' AND index_name = '$index'");
-        $row_array = $result->getRowArray();
-        return $row_array && $row_array['COUNT(*)'] > 0;
-    }
-
-    private function recreateForeignKeyConstraints(array $constraints): void
-    {
-        if ($constraints) {
-            $forge = Database::forge();
-            foreach ($constraints as $constraint) {
-                $forge->addForeignKey($constraint['columnName'], $constraint['referencedTable'], $constraint['referencedColumn'], $constraint['onUpdate'], $constraint['onDelete'], $constraint['constraintName']);
-                $forge->processIndexes($constraint['tableName']);
-            }
-        }
     }
 }
