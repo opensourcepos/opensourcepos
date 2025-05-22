b24dee4326cadfac3249b693ac10335a4f9afa65
objecttothis <objecttothis@gmail.com>
2025-05-22 14:21:09 +0400

    Add Unique constraint back into attribute_links
    - This migration reverts ospos_attribute_links_ibfk_1 and 2 to ON DELETE RESTRICT. Cascade delete is done programmatically. This is needed to have a unique column on the attribute_links table which prevents duplicate attributes from begin created with the same item_id-attribute_id-definition_id combination
    
    Signed-off-by: objecttothis <objecttothis@gmail.com>

---
+++ app/Database/Migrations/20250522000000_AttributeLinksUniqueConstraint.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use Config\Database;
+
+class AttributeLinksUniqueConstraint extends Migration
+{
+    /**
+     * Perform a migration step.
+     */
+    public function up(): void
+    {
+        error_log('Migrating Attribute Links Unique Constraint');
+
+        execute_script(APPPATH . 'Database/Migrations/sqlscripts/3.4.1_attribute_links_unique_constraint.sql');
+
+        error_log('Migrating Attribute Links Unique Constraint');
+    }
+
+    /**
+     * Revert a migration step.
+     */
+    public function down(): void
+    {
+    }
+}
