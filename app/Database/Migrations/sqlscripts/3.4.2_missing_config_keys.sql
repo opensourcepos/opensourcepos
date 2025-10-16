7350ad93abec7ba088941d5dd6963ec6c5d27965
Joe Williams <hey-there-joe@outlook.com>
2025-10-15 22:38:12 -0700

    Updated SQL
    Replaced INSERT WHERE NOT EXISTS in missing config keys sql script to use a single INSERT IGNORE.

--- app/Database/Migrations/sqlscripts/3.4.2_missing_config_keys.sql
+++ app/Database/Migrations/sqlscripts/3.4.2_missing_config_keys.sql
@@ -1,35 +1,11 @@
 INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'msg_msg', ''
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'msg_msg');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'msg_pwd', ''
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'msg_pwd');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'msg_uid', ''
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'msg_uid');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'msg_src', ''
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'msg_src');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'smtp_timeout', 5000
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'smtp_timeout');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'smtp_crypto', 'tls'
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'smtp_crypto');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'smtp_port', 587
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'smtp_port');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'mailpath', '/usr/bin/sendmail'
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'mailpath');
-
-INSERT IGNORE INTO ospos_app_config (`key`, `value`)
-SELECT 'protocol', 'sendmail'
-    WHERE NOT EXISTS (SELECT 1 FROM ospos_app_config WHERE `key` = 'protocol');
+VALUES
+    ('msg_msg', ''),
+    ('msg_pwd', ''),
+    ('msg_uid', ''),
+    ('msg_src', ''),
+    ('smtp_timeout', 5000),
+    ('smtp_crypto', 'tls'),
+    ('smtp_port', 587),
+    ('mailpath', '/usr/bin/sendmail'),
+    ('protocol', 'sendmail');
