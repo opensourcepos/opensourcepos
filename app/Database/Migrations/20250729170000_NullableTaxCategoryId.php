2acdec431f9ab8e258144834e8c36f82f034d85c
jekkos <jeroen.peelaerts@gmail.com>
2025-12-08 23:06:33 +0100

    Fix wrong migration script location (#4285)

--- app/Database/Migrations/20250729170000_NullableTaxCategoryId.php
+++ app/Database/Migrations/20250729170000_NullableTaxCategoryId.php
@@ -12,7 +12,7 @@ class Migration_NullableTaxCategoryId extends Migration
     public function up(): void
     {
         helper('migration');
-        execute_script(APPPATH . 'Database/Migrations/sqlscripts/3.4.2_missing_config_keys.sql');
+        execute_script(APPPATH . 'Database/Migrations/sqlscripts/3.4.2_nullable_tax_category_id.sql');
     }
 
     /**
