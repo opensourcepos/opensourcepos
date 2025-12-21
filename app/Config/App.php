25680f05dbff2b10754f7ecaf61302ed5f97b650
Chathura Dilushanka <90453201+dilushanka@users.noreply.github.com>
2025-12-22 03:11:36 +0530

    Add equals as permitted URI character (#4329)
    This should resolve the 400 error when deleting payments with base64 encoded IDs containing `=`.

--- app/Config/App.php
+++ app/Config/App.php
@@ -117,7 +117,7 @@ class App extends BaseConfig
     | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
     |
     */
-    public string $permittedURIChars = 'a-z 0-9~%.:_\-';
+    public string $permittedURIChars = 'a-z 0-9~%.:_\-=';
 
     /**
      * --------------------------------------------------------------------------
