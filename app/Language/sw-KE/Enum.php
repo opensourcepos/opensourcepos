be17b2471a39f26d3f36bafafbe6bcdd4d6a0b29
Lucas Lyimo <81020211+lyimolucasl@users.noreply.github.com>
2026-01-31 13:41:22 +0300

    feat(i18n): add Swahili-KE language support
    This commit introduces Swahili-KE (Kenya) language translations to the repository.
    It includes updates to locale helper functions and the addition of relevant language files.

---
+++ app/Language/sw-KE/Enum.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "half_down"  => "Nusu Chini",
+    "half_even"  => "Nusu Shufwa",
+    "half_five"  => "Nusu Tano",
+    "half_odd"   => "Nusu Witiri",
+    "half_up"    => "Nusu Juu",
+    "round_down" => "Punguza",
+    "round_up"   => "Ongeza",
+];
