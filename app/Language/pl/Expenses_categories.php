985c1c55ce2820267a4fe99ed038807f5c2d1ab0
yakub3k <yakub3k+git@gmail.com>
2026-03-27 11:47:20 +0000

    Translated using Weblate (Polish)
    Currently translated at 21.0% (4 of 19 strings)
    
    Translation: opensourcepos/expenses_categories
    Translate-URL: https://translate.opensourcepos.org/projects/opensourcepos/expenses_categories/pl/

--- app/Language/pl/Expenses_categories.php
+++ app/Language/pl/Expenses_categories.php
@@ -4,7 +4,7 @@ return [
     'category_name_required' => "Nazwa Kategorii Wydatków jest wymagana",
     'add_item' => "Dodaj Kategorię",
     'cannot_be_deleted' => "Nie można usunąć Kategorię Wydatki",
-    'category_id' => "",
+    'category_id' => "Id",
     'confirm_delete' => "",
     'confirm_restore' => "",
     'description' => "",
