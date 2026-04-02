8b14ed81e0230ad13c4d77366387099cbdbe1532
objec <objecttothis@gmail.com>
2026-04-02 15:38:20 +0400

    Restore CI4 PHPdocs to prevent complicated upgrade diffs
    Signed-off-by: objec <objecttothis@gmail.com>

--- app/Config/Events.php
+++ app/Config/Events.php
@@ -10,6 +10,22 @@ use App\Events\Load_config;
 use App\Events\Method;
 use App\Libraries\Plugins\PluginManager;
 
+/*
+ * --------------------------------------------------------------------
+ * Application Events
+ * --------------------------------------------------------------------
+ * Events allow you to tap into the execution of the program without
+ * modifying or extending core files. This file provides a central
+ * location to define your events, though they can always be added
+ * at run-time, also, if needed.
+ *
+ * You create code that can execute by subscribing to events with
+ * the 'on()' method. This accepts any form of callable, including
+ * Closures, that will be executed when the event is triggered.
+ *
+ * Example:
+ *      Events::on('create', [$myInstance, 'myMethod']);
+ */
 Events::on('pre_system', static function (): void {
     if (ENVIRONMENT !== 'testing') {
         if (ini_get('zlib.output_compression')) {
@@ -23,6 +39,12 @@ Events::on('pre_system', static function (): void {
         ob_start(static fn ($buffer) => $buffer);
     }
 
+    /*
+     * --------------------------------------------------------------------
+     * Debug Toolbar Listeners.
+     * --------------------------------------------------------------------
+     * If you delete, they will no longer be collected.
+     */
     if (CI_DEBUG && ! is_cli()) {
         Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
         service('toolbar')->respond();
@@ -32,7 +54,7 @@ Events::on('pre_system', static function (): void {
             });
         }
     }
-    
+
     $pluginManager = new PluginManager();
     $pluginManager->discoverPlugins();
     $pluginManager->registerPluginEvents();
