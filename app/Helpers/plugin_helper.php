15227523d9e0d534163e0d035f0457ef312c99a0
objec <objecttothis@gmail.com>
2026-04-08 11:37:32 +0400

    Correct function naming to use PSR compliant naming conventions
    Signed-off-by: objec <objecttothis@gmail.com>

--- app/Helpers/plugin_helper.php
+++ app/Helpers/plugin_helper.php
@@ -2,21 +2,21 @@
 
 use CodeIgniter\Events\Events;
 
-if (!function_exists('plugin_content')) {
-    function plugin_content(string $section, array $data = []): string
+if (!function_exists('pluginContent')) {
+    function pluginContent(string $section, array $data = []): string
     {
         $results = Events::trigger("view:{$section}", $data);
-        
+
         if (is_array($results)) {
-            return implode('', array_filter($results, fn($r) => is_string($r)));
+            return implode('', array_filter($results, fn ($r) => is_string($r)));
         }
-        
+
         return is_string($results) ? $results : '';
     }
 }
 
-if (!function_exists('plugin_content_exists')) {
-    function plugin_content_exists(string $section): bool
+if (!function_exists('pluginContentExists')) {
+    function pluginContentExists(string $section): bool
     {
         $observers = Events::listRegistered("view:{$section}");
         return !empty($observers);
