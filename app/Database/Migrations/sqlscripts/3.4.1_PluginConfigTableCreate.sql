f863be68f94df503766685400c8fd1d9ea8e4352
objec <objecttothis@gmail.com>
2026-04-23 11:25:21 +0400

    Plugins migration
    - Remove blank line
    - Add plugin SVG icon to gulpfile.js
    - Add plugin details to SQL migration script
    
    Signed-off-by: objec <objecttothis@gmail.com>

--- app/Database/Migrations/sqlscripts/3.4.1_PluginConfigTableCreate.sql
+++ app/Database/Migrations/sqlscripts/3.4.1_PluginConfigTableCreate.sql
@@ -5,3 +5,12 @@ CREATE TABLE IF NOT EXISTS `ospos_plugin_config` (
     `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
     PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT IGNORE INTO `ospos_modules` (`name_lang_key`, `desc_lang_key`, `sort`, `module_id`) VALUES
+    ('module_plugins', 'module_plugins_desc', 111, 'plugins');
+
+INSERT IGNORE INTO `ospos_permissions` (`permission_id`, `module_id`) VALUES
+   ('plugins', 'plugins');
+
+INSERT IGNORE INTO `ospos_grants` (`permission_id`, `person_id`, `menu_group`) VALUES
+    ('plugins', 1, 'office');
--- app/Plugins/MailchimpPlugin/Views/customer_tab.php
+++ app/Plugins/MailchimpPlugin/Views/customer_tab.php
@@ -5,7 +5,6 @@
  * @var array $subscriptionStatusOptions
  */
 
-
 ?>
 
 
