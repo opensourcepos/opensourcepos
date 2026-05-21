opensourcepos/opensourcepos

commit 42ba39d29089086d2ac81eee80931b00b9b63db8

chore: miscellaneous updates and improvements (#4530)
- reinstated 'update-licenses' task in gulp (accidentally removed in 3e844f2f891497e65523e859d626b368ec2f77d3)
- updated bootstrap, bootswatch, and various dev dependencies
- refinded text across UI
- applied consistency fixes
- added 'number' and 'tel' input types to relevant settings
- improved system info layout (still room for improvement, but better)
- updated and fixed changelog

app/Views/configs/tax_config.php
@@ -51,6 +51,10 @@
                 </div>
                 <div class="col-xs-1 input-group">
                     <?= form_input([
+                        'type'  => 'number',
+                        'step'  => 'any',
+                        'min'   => '0',
+                        'max'   => '100',
                         'name'  => 'default_tax_1_rate',
                         'id'    => 'default_tax_1_rate',
                         'class' => 'form-control input-sm',
@@ -72,6 +76,10 @@
                 </div>
                 <div class="col-xs-1 input-group">
                     <?= form_input([
+                        'type'  => 'number',
+                        'step'  => 'any',
+                        'min'   => '0',
+                        'max'   => '100',
                         'name'  => 'default_tax_2_rate',
                         'id'    => 'default_tax_2_rate',
                         'class' => 'form-control input-sm',

app/Controllers/Config.php
@@ -82,7 +82,7 @@ class Config extends Secure_Controller
         $npmDev = false;
         $license = [];
 
-        $license[$i]['title'] = 'Open Source Point Of Sale ' . config('App')->application_version;
+        $license[$i]['title'] = 'Open Source Point of Sale ' . config('App')->application_version;
 
         if (file_exists('license/LICENSE')) {
             $license[$i]['text'] = file_get_contents('license/LICENSE', false, null, 0, 3000);

app/Views/barcodes/barcode_sheet.php
@@ -11,31 +11,34 @@ $barcode_lib = new Barcode_lib();
 
 <!doctype html>
 <html lang="<?= current_language_code() ?>">
-    <head>
-        <meta charset="utf-8">
-        <title><?= esc(lang('Items.generate_barcodes')) ?></title>
-        <link rel="stylesheet" href="<?= esc(base_url('css/barcode_font.css'), 'url') ?>">
-        <style>
-            .barcode svg {
-                height: <?= (int) $barcode_config['barcode_height'] ?>px;
-                width: <?= (int) $barcode_config['barcode_width'] ?>px;
-            }
-        </style>
-    </head>
-    <body class="<?= esc('font_' . $barcode_lib->get_font_name($barcode_config['barcode_font']), 'attr') ?>" style="font-size: <?= (int) $barcode_config['barcode_font_size'] ?>px;">
-        <table style="border-spacing: <?= (int) $barcode_config['barcode_page_cellspacing'] ?>px; width: <?= (int) $barcode_config['barcode_page_width'] ?>%;">
-            <tr>
-                <?php
-                    $count = 0;
-                    foreach ($items as $item) {
-                        if ($count % $barcode_config['barcode_num_in_row'] == 0 && $count != 0) {
-                            echo '</tr><tr>';
-                        }
-                        echo '<td>' . $barcode_lib->display_barcode($item, $barcode_config) . '</td>';
-                        $count++;
+
+<head>
+    <meta charset="utf-8">
+    <title><?= esc(lang('Items.generate_barcodes')) ?></title>
+    <link rel="stylesheet" href="<?= esc(base_url('css/barcode_font.css'), 'url') ?>">
+    <style>
+        .barcode svg {
+            height: <?= (int) $barcode_config['barcode_height'] ?>px;
+            width: <?= (int) $barcode_config['barcode_width'] ?>px;
+        }
+    </style>
+</head>
+
+<body class="<?= esc('font_' . $barcode_lib->get_font_name($barcode_config['barcode_font']), 'attr') ?>" style="font-size: <?= (int) $barcode_config['barcode_font_size'] ?>px;">
+    <table style="border-spacing: <?= (int) $barcode_config['barcode_page_cellspacing'] ?>px; width: <?= (int) $barcode_config['barcode_page_width'] ?>%;">
+        <tr>
+            <?php
+                $count = 0;
+                foreach ($items as $item) {
+                    if ($count % $barcode_config['barcode_num_in_row'] == 0 && $count != 0) {
+                        echo '</tr><tr>';
                     }
-                ?>
-            </tr>
-        </table>
-    </body>
+                    echo '<td>' . $barcode_lib->display_barcode($item, $barcode_config) . '</td>';
+                    $count++;
+                }
+            ?>
+        </tr>
+    </table>
+</body>
+
 </html>

app/Views/configs/barcode_config.php
@@ -204,6 +204,7 @@
                 <?= form_label(lang('Config.barcode_number_in_row'), 'barcode_num_in_row', ['class' => 'control-label col-xs-2 required']) ?>
                 <div class="col-xs-2">
                     <?= form_input([
+                        'type'  => 'number',
                         'name'  => 'barcode_num_in_row',
                         'id'    => 'barcode_num_in_row',
                         'class' => 'form-control input-sm required',
@@ -217,6 +218,9 @@
                 <div class="col-sm-2">
                     <div class="input-group">
                         <?= form_input([
+                            'type'  => 'number',
+                            'min'   => '0',
+                            'max'   => '100',
                             'name'  => 'barcode_page_width',
                             'id'    => 'barcode_page_width',
                             'class' => 'form-control input-sm required',
@@ -232,6 +236,7 @@
                 <div class="col-sm-2">
                     <div class="input-group">
                         <?= form_input([
+                            'type'  => 'number',
                             'name'  => 'barcode_page_cellspacing',
                             'id'    => 'barcode_page_cellspacing',
                             'class' => 'form-control input-sm required',

app/Views/configs/email_config.php
@@ -17,9 +17,9 @@
                     <?= form_dropdown(
                         'protocol',
                         [
-                            'mail'     => 'mail',
-                            'sendmail' => 'sendmail',
-                            'smtp'     => 'smtp'
+                            'mail'     => 'Mail',
+                            'sendmail' => 'Sendmail',
+                            'smtp'     => 'SMTP'
                         ],
                         $config['protocol'],
                         'class="form-control input-sm" id="protocol"'
@@ -55,6 +55,7 @@
                 <?= form_label(lang('Config.email_smtp_port'), 'smtp_port', ['class' => 'control-label col-xs-2']) ?>
                 <div class="col-xs-2">
                     <?= form_input([
+                        'type'  => 'number',
                         'name'  => 'smtp_port',
                         'id'    => 'smtp_port',
                         'class' => 'form-control input-sm',
@@ -83,6 +84,7 @@
                 <?= form_label(lang('Config.email_smtp_timeout'), 'smtp_timeout', ['class' => 'control-label col-xs-2']) ?>
                 <div class="col-xs-2">
                     <?= form_input([
+                        'type'  => 'number',
                         'name'  => 'smtp_timeout',
                         'id'    => 'smtp_timeout',
                         'class' => 'form-control input-sm',

app/Views/configs/info_config.php
@@ -105,6 +105,7 @@
                             <span class="glyphicon glyphicon-phone-alt"></span>
                         </span>
                         <?= form_input([
+                            'type'  => 'tel',
                             'name'  => 'phone',
                             'id'    => 'phone',
                             'class' => 'form-control input-sm required',
@@ -122,6 +123,7 @@
                             <span class="glyphicon glyphicon-phone-alt"></span>
                         </span>
                         <?= form_input([
+                            'type'  => 'tel',
                             'name'  => 'fax',
                             'id'    => 'fax',
                             'class' => 'form-control input-sm',

app/Views/configs/system_info.php
@@ -25,8 +25,8 @@ use Config\OSPOS;
     <div class="container">
         <div class="row">
             <div class="col-sm-2" style="text-align: left;"><br>
-                <p style="min-height: 14.7em; font-weight: bold;">General Info</p>
-                <p style="min-height: 10.5em; font-weight: bold;">User Setup</p><br>
+                <p style="min-height: 17.7em; font-weight: bold;">General Info</p>
+                <p style="min-height: 12.2em; font-weight: bold;">User Setup</p><br>
                 <p style="font-weight: bold;">Permissions</p>
             </div>
             <div class="col-sm-8" id="issuetemplate" style="text-align: left;"><br>
@@ -42,7 +42,7 @@ use Config\OSPOS;
                     echo "&#187; OpenSSL: ", extension_loaded('openssl') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br>';
                     echo "&#187; MBString: ", extension_loaded('mbstring') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br>';
                     echo "&#187; Curl: ", extension_loaded('curl') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br>';
-                    echo "&#187; Json: ", extension_loaded('json') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br><br>';
+                    echo "&#187; Json: ", extension_loaded('json') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br>';
                     echo "&#187; Xml: ", extension_loaded('xml') ? '<span style="color: green;">Enabled &#x2713</span>' : '<span style="color: red;">Disabled &#x2717</span>', '<br><br>';
                 ?>
                 User Configuration:<br>

app/Views/login.php
@@ -5,9 +5,14 @@
  * @var bool $is_new_install
  * @var string $latest_version
  * @var bool $gcaptcha_enabled
+ * @var CodeIgniter\HTTP\IncomingRequest $request
  * @var array $config
  * @var $validation
  */
+
+use Config\Services;
+
+$request = Services::request();
 ?>
 
 <!doctype html>
@@ -153,11 +158,6 @@
             <span><?= lang('Common.software_title') ?></span>
         </div>
     </footer>
-
-    <?php
-    use Config\Services;
-    $request = Services::request();
-    ?>
 
     <?php if (ENVIRONMENT == 'development' || get_cookie('debug') == 'true' || $request->getGet('debug') == 'true') : ?>
         <!-- inject:login:debug:js -->

commit 30d5ac44962e310bb42b2a00d2f68d9d3d494e87

fix: tax rate inputs blank with comma-decimal locales
The to_tax_decimals() function returns locale-formatted values
(e.g. "18,00" for comma-decimal locales like fr_FR, de_DE).
Browsers reject comma-decimal values in <input type="number">
and render the field blank.

Use raw float value instead - PHP serializes floats with period
decimal regardless of locale. The parse_tax() on the save side
already handles locale-aware parsing, so round-tripping works
correctly.

Fixes #4553
Regression from commit 42ba39d29

app/Views/configs/tax_config.php
@@ -58,7 +58,7 @@
                         'name'  => 'default_tax_1_rate',
                         'id'    => 'default_tax_1_rate',
                         'class' => 'form-control input-sm',
-                        'value' => to_tax_decimals($config['default_tax_1_rate'])
+                        'value' => $config['default_tax_1_rate'] !== '' ? (float) $config['default_tax_1_rate'] : ''
                     ]) ?>
                     <span class="input-group-addon input-sm">%</span>
                 </div>
@@ -83,7 +83,7 @@
                         'name'  => 'default_tax_2_rate',
                         'id'    => 'default_tax_2_rate',
                         'class' => 'form-control input-sm',
-                        'value' => to_tax_decimals($config['default_tax_2_rate'])
+                        'value' => $config['default_tax_2_rate'] !== '' ? (float) $config['default_tax_2_rate'] : ''
                     ]) ?>
                     <span class="input-group-addon input-sm">%</span>
                 </div>