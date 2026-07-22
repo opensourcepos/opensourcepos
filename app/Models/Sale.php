17d67e9b4e25b08f297bdb7828a45c3996f899ba
Travis Garrison <travis@chiraqbookstore.com>
2026-07-22 17:41:38 +0400

    fix(sales): exclude empty strings from payment reference code aggregation
    Signed-off-by: Travis Garrison <travis@chiraqbookstore.com>

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -1095,7 +1095,7 @@ class Sale extends Model
                     SUM(CASE WHEN payments.cash_adjustment = 1 THEN payments.payment_amount ELSE 0 END) AS sale_cash_adjustment,
                     SUM(payments.cash_refund) AS sale_cash_refund,
                     GROUP_CONCAT(CONCAT(payments.payment_type, " ", (payments.payment_amount - payments.cash_refund)) SEPARATOR ", ") AS payment_type,
-                    GROUP_CONCAT(payments.reference_code SEPARATOR ", ") AS reference_code
+                    GROUP_CONCAT(NULLIF(payments.reference_code, '') SEPARATOR ", ") AS reference_code
                 FROM ' . $this->db->prefixTable('sales_payments') . ' AS payments
                 INNER JOIN ' . $this->db->prefixTable('sales') . ' AS sales
                     ON sales.sale_id = payments.sale_id
