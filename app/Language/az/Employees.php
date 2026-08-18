cfbf08d03af7d11849088a787a54135181f8d387
Travis Garrison <travis@chiraqbookstore.com>
2026-08-18 16:21:15 +0400

    fix(language): correct typo in Azerbaijani "cannot be deleted" message
    - Fix "heasabınızı" to "hesabınızı" for improved accuracy and clarity.
    
    Signed-off-by: Travis Garrison <travis@chiraqbookstore.com>

--- app/Language/az/Employees.php
+++ app/Language/az/Employees.php
@@ -3,7 +3,7 @@
 return [
     'administrator'                    => '',
     'basic_information'                => 'Məlumat',
-    'cannot_be_deleted'                => 'Seçilmiş əməkdaş (lar) silinə bilməz, bir və ya birdən çox əməkdaş satışlar edib, əks halda siz öz heasabınızı silməyə çalışırsınız.',
+    'cannot_be_deleted'                => 'Seçilmiş əməkdaş (lar) silinə bilməz, bir və ya birdən çox əməkdaş satışlar edib, əks halda siz öz hesabınızı silməyə çalışırsınız.',
     'change_employee'                  => '',
     'change_password'                  => 'Şifrəni Dəyiş',
     'clerk'                            => '',
