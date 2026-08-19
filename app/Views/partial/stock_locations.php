593a2f0d7d7df90abdf07512450c50c52edbcb48
objecttothis <17935339+objecttothis@users.noreply.github.com>
2026-08-19 17:10:08 +0400

    fix(view): add aria-label for default stock location and update element IDs
    - Improved accessibility by adding an `aria-label` to default stock location radio inputs.
    - Updated ID formatting for stock location input elements.
    
    Signed-off-by: objecttothis <17935339+objecttothis@users.noreply.github.com>

--- app/Views/partial/stock_locations.php
+++ app/Views/partial/stock_locations.php
@@ -17,17 +17,18 @@ foreach ($stock_locations as $location => $locationData) {
         <span class="drag_handle glyphicon glyphicon-resize-vertical col-xs-1" style="cursor: move; padding-top: 0.5em;"></span>
         <div class="col-xs-1">
             <?= form_radio([
-                'name'    => 'stock_location_default',
-                'value'   => $locationId,
-                'checked' => (bool) $locationData['is_default'],
-                'class'   => 'stock_location_default'
+                'name'      => 'stock_location_default',
+                'value'     => $locationId,
+                'checked'   => (bool) $locationData['is_default'],
+                'class'     => 'stock_location_default',
+                'aria-label' => lang('Config.stock_location_default')
             ]) ?>
         </div>
         <?= form_label(lang('Config.stock_location') . " $i", "stock_location_$i", ['class' => 'required control-label col-xs-2']) ?>
         <div class="col-xs-2">
             <?php $formData = [
                 'name'  => "stock_location[$locationId]",
-                'id'    => "stock_location[$locationId]",
+                'id'    => "stock_location_$i",
                 'class' => 'stock_location valid_chars form-control input-sm required',
                 'value' => $locationName
             ];
